b133538a7dd2bd81e630d7858588558a8946e508
Alexander Kaufmann <ak@vivid-planet.com>
2019-02-25 15:04:47 +0100

    style menu item mit eigener styled component ohne ts-ignore, juhu

--- react-admin-mui/menu/Item.tsx
+++ react-admin-mui/menu/Item.tsx
@@ -1,9 +1,9 @@
-import { Icon, ListItem, ListItemIcon, ListItemSecondaryAction, ListItemText, Theme } from "@material-ui/core";
+import { Icon, ListItemIcon, ListItemSecondaryAction, ListItemText } from "@material-ui/core";
 import { ListItemProps } from "@material-ui/core/ListItem";
-import { makeStyles } from "@material-ui/styles";
-import { IMenuContext, MenuContext } from "@vivid-planet/react-admin-mui";
+import { MenuContext } from "@vivid-planet/react-admin-mui";
 import { IMenuLevel } from "@vivid-planet/react-admin-mui/menu/CollapsibleItem";
 import * as React from "react";
+import * as sc from "./Item.sc";
 
 export interface IMenuItemProps extends IMenuLevel {
     text: string;
@@ -11,34 +11,20 @@ export interface IMenuItemProps extends IMenuLevel {
     secondaryAction?: React.ReactNode;
 }
 
-interface IMenuItemStyleProps {
-    level: number;
-    menuOpen: boolean;
-}
+type MuiListItemProps = Pick<ListItemProps, Exclude<keyof ListItemProps, "innerRef">>;
 
-// @ts-ignore
-const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        paddingLeft: ({ level, menuOpen }: IMenuItemStyleProps) => theme.spacing.unit * 2 * (menuOpen ? level : 1),
-    },
-}));
-
-const Item: React.FunctionComponent<IMenuItemProps & ListItemProps> = ({ text, icon, level, secondaryAction, ...otherProps }) => {
+const Item: React.FunctionComponent<IMenuItemProps & MuiListItemProps> = ({ text, icon, level, secondaryAction, ...otherProps }) => {
     const context = React.useContext(MenuContext);
     if (!context) throw new Error("Could not find context for menu");
 
-    const classes = useStyles({
-        level: level !== undefined ? level : 1,
-        menuOpen: context.open,
-    });
     if (!icon && !context.open) icon = <Icon>{text.substr(0, 1).toUpperCase()}</Icon>;
 
     return (
-        <ListItem className={classes.root} button {...otherProps}>
+        <sc.ListItem level={level !== undefined ? level : 1} menuOpen={context.open} button {...otherProps}>
             {!!icon && <ListItemIcon>{icon}</ListItemIcon>}
             <ListItemText primary={text} primaryTypographyProps={{ noWrap: true }} inset={!icon} />
             {!!secondaryAction && context.open && <ListItemSecondaryAction>{secondaryAction}</ListItemSecondaryAction>}
-        </ListItem>
+        </sc.ListItem>
     );
 };
 
