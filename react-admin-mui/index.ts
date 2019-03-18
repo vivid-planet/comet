3b20d267104a34434201056337d959752c22bc2d
Niko Sams <ns@vivid-planet.com>
2019-03-18 17:23:33 +0100

    Add index.ts files with exports so you don't have to import invidiual files in app code

--- react-admin-mui/index.ts
+++ react-admin-mui/index.ts
@@ -1,2 +1,4 @@
 export * from "./breadcrumbs";
 export * from "./menu";
+export { default as ThemeProvider } from "./ThemeProvider";
+export { default as styledComponents } from "./styled-components";
