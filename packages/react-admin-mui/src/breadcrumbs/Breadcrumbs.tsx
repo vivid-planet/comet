d2d48cb0788b6cd38cbe4bfb4e85fccb33435ba4
Ricky Smith <rs@vivid-planet.com>
2019-04-10 16:41:35 +0200

    Style breadcrumbs

--- packages/react-admin-mui/src/breadcrumbs/Breadcrumbs.tsx
+++ packages/react-admin-mui/src/breadcrumbs/Breadcrumbs.tsx
@@ -1,8 +1,9 @@
-import { Link, Typography } from "@material-ui/core";
+import { Link } from "@material-ui/core";
 import { LinkProps } from "@material-ui/core/Link";
 import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 import * as React from "react";
 import { Link as RouterLink } from "react-router-dom";
+import { Root, Title } from "./Breadcrumbs.sc";
 
 export interface IBreadcrumbItem {
     id: string;
@@ -22,22 +23,20 @@ const BreadcrumbLink = (props: LinkProps) => (
 
 const Breadcrumbs = ({ pages }: IBreadcrumbProps) => {
     return (
-        <>
+        <Root>
             {pages.map(({ id, url, title }, index) => {
                 const isLast = index + 1 >= pages.length;
 
                 return (
                     <React.Fragment key={id}>
                         <Link href={url} component={BreadcrumbLink} color={isLast ? "primary" : "inherit"}>
-                            <Typography variant="subtitle1" color={isLast ? "primary" : "inherit"}>
-                                {title}
-                            </Typography>
+                            <Title color={isLast ? "primary" : "inherit"}>{title}</Title>
                         </Link>
                         {!isLast && <KeyboardArrowRight />}
                     </React.Fragment>
                 );
             })}
-        </>
+        </Root>
     );
 };
 
