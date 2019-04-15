aab2d6801493b830a78bb0f1ff311a4660029eca
Ricky Smith <rs@vivid-planet.com>
2019-04-15 10:24:45 +0200

    Style form-label

--- packages/react-admin-form/src/FieldContainerLabelAbove.tsx
+++ packages/react-admin-form/src/FieldContainerLabelAbove.tsx
@@ -1,24 +1,11 @@
-import { FormControl, FormLabel, Grid } from "@material-ui/core";
-import { styledComponents as styled } from "@vivid-planet/react-admin-mui";
 import * as React from "react";
-
-const StyledFormControl = styled(FormControl)`
-    && {
-        padding-bottom: 16px;
-    }
-`;
-
-const StyledFormLabel = styled(FormLabel)`
-    && {
-        margin: 8px 0;
-        display: block;
-    }
-`;
+import { StyledFormControl, StyledFormLabel } from "./FieldContainerLabelAbove.sc";
 
 interface IProps {
     label?: string | React.ReactNode;
     required?: boolean;
 }
+
 const FieldContainerLabelAbove: React.FunctionComponent<IProps> = props => (
     <StyledFormControl fullWidth={true}>
         <div>
