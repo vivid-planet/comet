a6a43c9a6b3e8837164efc6a5c7df4a1336272ca
Ricky Smith <rs@vivid-planet.com>
2019-04-23 15:16:46 +0200

    Display breadcrumb-links next to each other

--- packages/react-admin-mui/src/breadcrumbs/Breadcrumbs.sc.ts
+++ packages/react-admin-mui/src/breadcrumbs/Breadcrumbs.sc.ts
@@ -1,4 +1,5 @@
 import { Typography } from "@material-ui/core";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 import { styled } from "../styled-components";
 
 export const Root = styled.div`
@@ -9,5 +10,18 @@ export const Root = styled.div`
 export const Title = styled(Typography)`
     && {
         font-size: 13px;
+        line-height: 24px;
     }
+`;
+
+export const LinkWrapper = styled.div`
+    display: inline-block;
+    vertical-align: middle;
+`;
+
+export const ArrowIcon = styled(KeyboardArrowRight)`
+    display: inline-block;
+    vertical-align: middle;
+    margin-left: 8px;
+    margin-right: 8px;
 `;
--- packages/react-admin-mui/src/breadcrumbs/Breadcrumbs.tsx
+++ packages/react-admin-mui/src/breadcrumbs/Breadcrumbs.tsx
@@ -3,7 +3,7 @@ import { LinkProps } from "@material-ui/core/Link";
 import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 import * as React from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { Root, Title } from "./Breadcrumbs.sc";
+import * as sc from "./Breadcrumbs.sc";
 
 export interface IBreadcrumbItem {
     id: string;
@@ -23,19 +23,21 @@ const BreadcrumbLink = (props: LinkProps) => (
 
 export const Breadcrumbs = ({ pages }: IBreadcrumbProps) => {
     return (
-        <Root>
+        <sc.Root>
             {pages.map(({ id, url, title }, index) => {
                 const isLast = index + 1 >= pages.length;
 
                 return (
                     <React.Fragment key={id}>
-                        <Link href={url} component={BreadcrumbLink} color={isLast ? "primary" : "inherit"}>
-                            <Title color={isLast ? "primary" : "inherit"}>{title}</Title>
-                        </Link>
-                        {!isLast && <KeyboardArrowRight />}
+                        <sc.LinkWrapper>
+                            <Link href={url} component={BreadcrumbLink} color={isLast ? "primary" : "inherit"}>
+                                <sc.Title color={isLast ? "primary" : "inherit"}>{title}</sc.Title>
+                            </Link>
+                        </sc.LinkWrapper>
+                        {!isLast && <sc.ArrowIcon />}
                     </React.Fragment>
                 );
             })}
-        </Root>
+        </sc.Root>
     );
 };
