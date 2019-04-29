19713708b0dc78e962e7b4a6516f8c94be95efe7
Ricky Smith <rs@vivid-planet.com>
2019-04-29 17:32:52 +0200

    Style buttons in FinalForm

---
+++ packages/react-admin-core/src/FinalForm.sc.ts
@@ -0,0 +1,11 @@
+import { styled } from "@vivid-planet/react-admin-mui";
+
+export const InnerForm = styled.div`
+    margin-bottom: 20px;
+`;
+
+export const ButtonIconWrapper = styled.div`
+    margin-right: 5px;
+    font-size: 20px;
+    line-height: 28px;
+`;
