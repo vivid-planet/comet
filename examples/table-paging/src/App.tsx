2d851ca78aba3926cfa4cbca5a7bf549ef983ced
Niko Sams <ns@vivid-planet.com>
2019-05-31 11:04:30 +0200

    Add example showing table-paging with a real rest api, also update dependencies

---
+++ examples/table-paging/src/App.tsx
@@ -0,0 +1,72 @@
+import { createRestPagingActions, ITableQueryApi, Table, TableQuery, useTableQuery } from "@vivid-planet/react-admin-core";
+import gql from "graphql-tag";
+import * as React from "react";
+
+const gqlRest = gql;
+
+const query = gqlRest`
+query people(
+    $page: Int
+) {
+    people(
+        page: $page
+    ) @rest(type: "PeoplePayload", path: "people?page={args.page}") {
+        count
+        next
+        previous
+        results @type(name: "People") {
+            name
+            url
+        }
+    }
+}
+`;
+
+interface IQueryData {
+    people: {
+        count: number;
+        next: string;
+        previous: string;
+        results: Array<{
+            name: string;
+            url: string;
+        }>;
+    };
+}
+
+interface IVariables {
+    page: number;
+}
+
+export default function App() {
+    const { tableData, api, loading, error } = useTableQuery<IQueryData, IVariables>()(query, {
+        variables: {
+            page: 1,
+        },
+        resolveTableData: data => ({
+            data: data.people.results.map(i => ({ ...i, id: i.url.match(/.*\/(\d+)\//)![1] })),
+            totalCount: data.people.count,
+            pagingActions: createRestPagingActions({
+                nextPage: data.people.next,
+                previousPage: data.people.previous,
+            }),
+        }),
+        notifyOnNetworkStatusChange: true,
+    });
+
+    return (
+        <TableQuery api={api} loading={loading} error={error}>
+            {tableData && (
+                <Table
+                    {...tableData}
+                    columns={[
+                        {
+                            name: "name",
+                            header: "Name",
+                        },
+                    ]}
+                />
+            )}
+        </TableQuery>
+    );
+}
