0134a2c9e14761b553145bbc3a52afa8ef8b001b
Lukas Kern <lukas.kern@vivid-planet.com>
2019-11-05 16:26:22 +0100

    add React Select examples

--- packages/react-admin-stories/src/react-admin-form/ReactSelect.tsx
+++ packages/react-admin-stories/src/react-admin-form/ReactSelect.tsx
@@ -1,3 +1,4 @@
+import { Button, ListItem } from "@material-ui/core";
 import { storiesOf } from "@storybook/react";
 import { Field, FieldContainerLabelAbove, Input, ReactSelectStaticOptions } from "@vivid-planet/react-admin-form";
 import * as React from "react";
@@ -6,18 +7,24 @@ import { Form } from "react-final-form";
 function Story() {
     const options = [
         { value: "chocolate", label: "Chocolate" },
-        { value: "strawberry", label: "Strawberry", isDisabled: true },
+        { value: "strawberry", label: "Strawberry" },
         { value: "vanilla", label: "Vanilla" },
-        { value: "cherry", label: "Cherry" },
     ];
     return (
         <div style={{ width: "300px" }}>
+            <ListItem>
+                <Button component={"button"} disableTouchRipple>
+                    blah
+                </Button>
+            </ListItem>
+
             <Form
                 onSubmit={values => {
                     //
                 }}
                 render={({ handleSubmit, pristine, invalid }) => (
                     <form onSubmit={handleSubmit}>
+                        <Field name="name" label="Name" type="text" fieldContainerComponent={FieldContainerLabelAbove} component={Input} fullWidth />
                         <Field
                             name="flavor"
                             label="Flavor"
--- packages/react-admin-stories/src/react-admin-form/ReactSelectDisabledOption.tsx
+++ packages/react-admin-stories/src/react-admin-form/ReactSelectDisabledOption.tsx
@@ -1,30 +1,23 @@
-import { Button, ListItem } from "@material-ui/core";
 import { storiesOf } from "@storybook/react";
-import { Field, FieldContainerLabelAbove, Input, ReactSelectStaticOptions } from "@vivid-planet/react-admin-form";
+import { Field, FieldContainerLabelAbove, ReactSelectStaticOptions } from "@vivid-planet/react-admin-form";
 import * as React from "react";
 import { Form } from "react-final-form";
 
 function Story() {
     const options = [
         { value: "chocolate", label: "Chocolate" },
-        { value: "strawberry", label: "Strawberry" },
+        { value: "strawberry", label: "Strawberry", isDisabled: true },
         { value: "vanilla", label: "Vanilla" },
+        { value: "cherry", label: "Cherry" },
     ];
     return (
         <div style={{ width: "300px" }}>
-            <ListItem>
-                <Button component={"button"} disableTouchRipple>
-                    blah
-                </Button>
-            </ListItem>
-
             <Form
                 onSubmit={values => {
                     //
                 }}
                 render={({ handleSubmit, pristine, invalid }) => (
                     <form onSubmit={handleSubmit}>
-                        <Field name="name" label="Name" type="text" fieldContainerComponent={FieldContainerLabelAbove} component={Input} fullWidth />
                         <Field
                             name="flavor"
                             label="Flavor"
@@ -42,4 +35,4 @@ function Story() {
     );
 }
 
-storiesOf("material-ui-react-select", module).add("React Select", () => <Story />);
+storiesOf("react-admin-form", module).add("React Select Disabled Option", () => <Story />);
