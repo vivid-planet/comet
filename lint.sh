d118535a7ff9359aba8ac9c6ecba5ab834c8318e
Niko Sams <ns@vivid-planet.com>
2020-02-06 09:21:55 +0100

    use master branch (as yarn is now also in master)

--- lint.sh
+++ lint.sh
@@ -5,11 +5,11 @@ set -e
 if [ -n "$INSTALL_DEPS" ]; then 
     docker run --rm -v `pwd`:/opt/app-root/src \
         -u ${RUN_AS_UID}:0 \
-        eu.gcr.io/vivid-planet-public/dev/node-10-dev:node-yarn \
+        eu.gcr.io/vivid-planet-public/dev/node-10-dev:master \
         bash -c "yarn install"
 fi
 
 docker run --rm -v `pwd`:/opt/app-root/src \
     -u ${RUN_AS_UID}:0 \
-    eu.gcr.io/vivid-planet-public/dev/node-10-dev:node-yarn \
+    eu.gcr.io/vivid-planet-public/dev/node-10-dev:master \
     bash -c "yarn run lint"
