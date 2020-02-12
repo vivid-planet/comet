350ef433364cd9532671b0ae7e0fc70ff4bf5cff
Niko Sams <ns@vivid-planet.com>
2020-02-12 14:10:23 +0100

    use docker-compose

--- lint.sh
+++ lint.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 set -e
 
-#docker-compose run --rm client bash -c "yarn install && yarn run lint"
 if [ -n "$INSTALL_DEPS" ]; then 
-    docker run --rm -v `pwd`:/opt/app-root/src \
-        -u ${RUN_AS_UID}:0 \
-        eu.gcr.io/vivid-planet-public/dev/node-10-dev:master \
-        bash -c "yarn install"
+    docker-compose run --rm react-admin bash -c "yarn install"
 fi
 
-docker run --rm -v `pwd`:/opt/app-root/src \
-    -u ${RUN_AS_UID}:0 \
-    eu.gcr.io/vivid-planet-public/dev/node-10-dev:master \
-    bash -c "yarn run lint"
+docker-compose run --rm react-admin bash -c "yarn run lint"
