f8d7f7530818ede4d9ce4d84d47b227c19e54785
Ricky Smith <jamesricky@me.com>
2020-03-23 15:59:39 +0100

    Prevent inconsistent height of menu-items
    Make sure the width of the menu-items is always the width of the
    opened menu. This prevents the text inside the menu-item from breaking
    into the next line and therefore increasing it's height.

--- packages/react-admin-mui/src/menu/Menu.sc.tsx
+++ packages/react-admin-mui/src/menu/Menu.sc.tsx
@@ -1,5 +1,11 @@
+import * as React from "react";
 import { styled } from "../styled-components";
 
-export const MenuItemsWrapper = styled.div`
+interface IMenuItemsWrapperProps {
+    width: React.CSSProperties["width"];
+}
+
+export const MenuItemsWrapper = styled.div<IMenuItemsWrapperProps>`
+    width: ${({ width }) => width};
     margin-top: 20px;
 `;
