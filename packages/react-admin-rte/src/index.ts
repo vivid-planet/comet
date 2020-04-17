1fe2c0da238b102981e842eb6b684ef17a502365
Simon Radner <sr@vivid-planet.com>
2020-04-17 10:56:39 +0000

    Export package-api

--- packages/react-admin-rte/src/index.ts
+++ packages/react-admin-rte/src/index.ts
@@ -1,3 +1,7 @@
-export { default as Rte, IRteRef, IOptions as IRteOptions } from "./core/Rte";
+export { default as Rte, IRteRef, IOptions as IRteOptions, IProps as IRteProps } from "./core/Rte";
 export { default as makeRteApi, IMakeRteApiProps, OnDebouncedContentChangeFn, IRteApiProps } from "./core/makeRteApi";
 export { default as createRteField } from "./field/createRteField";
+
+export { default as findTextInCurrentSelection } from "./core/utils/findTextInCurrentSelection";
+export { default as selectionIsInOneBlock } from "./core/utils/selectionIsInOneBlock";
+export { default as findEntityInCurrentSelection } from "./core/utils/findEntityInCurrentSelection";
