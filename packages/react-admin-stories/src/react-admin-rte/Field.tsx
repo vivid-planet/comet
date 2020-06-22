29456977c70bfff0eec938c75a2dcf89dadfb9e5
Simon <simon.radner@gmail.com>
2020-06-22 11:28:20 +0000

    Update rte-Story

--- packages/react-admin-stories/src/react-admin-rte/Field.tsx
+++ packages/react-admin-stories/src/react-admin-rte/Field.tsx
@@ -4,30 +4,35 @@ import { Field } from "@vivid-planet/react-admin-form";
 import { createRteField } from "@vivid-planet/react-admin-rte";
 import * as React from "react";
 import { Form } from "react-final-form";
-import { PrintAnything } from "./helper";
+import { Typography } from "@material-ui/core";
 
-const { RteField } = createRteField();
+const { RteField, RteReadOnly } = createRteField();
 
 function Story() {
-    const [submitedValue, setSubmittedValue] = React.useState<any>();
+    const [submitedValue, setSubmittedValue] = React.useState<{ rteContent: any }>({ rteContent: undefined });
 
     return (
-        <div style={{ width: "300px" }}>
-            <Form
-                onSubmit={values => {
-                    //
-                    setSubmittedValue(values);
-                }}
-                render={({ handleSubmit }) => (
-                    <form onSubmit={handleSubmit}>
-                        <Field name="rteContent" label="Rich Text" component={RteField} />
-                        <Button type="submit" component={"button"} disableTouchRipple>
-                            Submit
-                        </Button>
-                    </form>
-                )}
-            />
-            <PrintAnything label="This has been submitted">{submitedValue}</PrintAnything>
+        <div>
+            <div style={{ maxWidth: "800px" }}>
+                <Form
+                    onSubmit={values => {
+                        //
+                        setSubmittedValue(values);
+                    }}
+                    render={({ handleSubmit }) => (
+                        <form onSubmit={handleSubmit}>
+                            <Field name="rteContent" label="Rich Text" component={RteField} />
+                            <Button color="primary" variant="contained" type="submit" component={"button"} disableTouchRipple>
+                                Submit
+                            </Button>
+                        </form>
+                    )}
+                />
+            </div>
+            <Typography variant="h5" color="primary">
+                Readonly Component:
+            </Typography>
+            <RteReadOnly content={submitedValue.rteContent} />
         </div>
     );
 }
