25505c114182afb372845cfd1e30312a19484d96
Simon <simon.radner@gmail.com>
2020-07-08 12:49:19 +0000

    [i18n, stories] Add storybook-knobs, add a global intl-decorator to switch locales in storybook

--- packages/react-admin-stories/.storybook/config.tsx
+++ packages/react-admin-stories/.storybook/config.tsx
@@ -1,5 +1,8 @@
 import { addDecorator, configure } from "@storybook/react";
 import { createMuiTheme, MuiThemeProvider as ThemeProvider } from "@vivid-planet/react-admin-mui";
+import { withKnobs, select } from "@storybook/addon-knobs";
+import { IntlProvider } from "react-intl";
+
 import * as React from "react";
 
 const req = require.context("../src", true, /\.tsx$/);
@@ -7,6 +10,23 @@ const req = require.context("../src", true, /\.tsx$/);
 function loadStories() {
     req.keys().forEach(req);
 }
+
+addDecorator((story, context) => {
+    const storyWithKnobs = withKnobs(story, context); // explicitly add withKnobs
+    const messages = {
+        de: {
+            "reactAdmin.core.finalForm.save": "Speichern",
+        },
+        en: {
+            "reactAdmin.core.finalForm.save": "Save",
+        },
+    };
+    return (
+        <IntlProvider locale={select("Locale", ["de", "en"], "de")} messages={messages[select("Locale", ["de", "en"], "de")] ?? {}}>
+            {storyWithKnobs}
+        </IntlProvider>
+    );
+});
 
 addDecorator(story => {
     const theme = createMuiTheme({});
