cb8a6ace46210d5e3c30a6785f06c0c55c010af8
Franz Unger <ufx@vivid-planet.com>
2020-11-24 14:36:41 +0100

    Add initialValues in Autocomplete storybook example

--- packages/admin-stories/src/admin/form/Autocomplete.tsx
+++ packages/admin-stories/src/admin/form/Autocomplete.tsx
@@ -9,17 +9,33 @@ interface IOption {
     label: string;
 }
 
-function Story() {
-    const options: IOption[] = [
-        { id: "chocolate", label: "Chocolate" },
-        { id: "strawberry", label: "Strawberry" },
-        { id: "vanilla", label: "Vanilla" },
-    ];
+const options: IOption[] = [
+    { id: "chocolate", label: "Chocolate" },
+    { id: "strawberry", label: "Strawberry" },
+    { id: "vanilla", label: "Vanilla" },
+];
 
+const AutocompleteField = () => {
+    return <Field name="flavor" label="Flavor" optionValue="id" optionLabel="label" component={Autocomplete} options={options} />;
+};
+
+const AutocompleteAsyncField = () => {
     const autocompleteAsyncProps = useAutocompleteAsyncProps<IOption>(async () => {
         return new Promise((resolve) => setTimeout(() => resolve(options), 500));
     });
+    return (
+        <Field
+            name="flavorAsync"
+            label="Flavor (async)"
+            getOptionSelected={(option: IOption, value: IOption) => option.id === value.id}
+            getOptionLabel={(option: IOption) => option.label}
+            component={Autocomplete}
+            {...autocompleteAsyncProps}
+        />
+    );
+};
 
+function Story() {
     return (
         <div style={{ width: "300px" }}>
             <Form
@@ -27,17 +43,14 @@ function Story() {
                 onSubmit={(values) => {
                     alert(JSON.stringify(values));
                 }}
+                initialValues={{
+                    flavor: options[0],
+                    flavorAsync: options[1],
+                }}
                 render={({ handleSubmit }) => (
                     <form onSubmit={handleSubmit}>
-                        <Field name="flavor" label="Flavor" optionValue="id" optionLabel="label" component={Autocomplete} options={options} />
-                        <Field
-                            name="flavorAsync"
-                            label="Flavor (async)"
-                            getOptionSelected={(option: IOption, value: IOption) => option.id === value.id}
-                            getOptionLabel={(option: IOption) => option.label}
-                            component={Autocomplete}
-                            {...autocompleteAsyncProps}
-                        />
+                        <AutocompleteField />
+                        <AutocompleteAsyncField />
                         <Button color="primary" variant="contained" type="submit" component="button" disableTouchRipple>
                             <Typography variant="button">Submit</Typography>
                         </Button>
