d283b2fa1e1066859808f898672b3e985db0c20e
Manuel Blum <mb@vivid-planet.com>
2021-04-30 17:29:34 +0200

    refactor: toolbar themeable

---
+++ packages/admin-theme/src/cometAdminProps/toolbar.tsx
@@ -0,0 +1,7 @@
+import { ToolbarThemeProps } from "@comet/admin";
+import { ArrowLeft } from "@comet/admin-icons";
+import * as React from "react";
+
+export const cometAdminToolbarProps = (): ToolbarThemeProps => ({
+    backIcon: <ArrowLeft />,
+});
