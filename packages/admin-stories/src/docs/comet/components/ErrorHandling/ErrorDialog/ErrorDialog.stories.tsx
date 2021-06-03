06700495b31d22c7058c51b3d1c25ad1a96ea3e3
Manuel Blum <mb@vivid-planet.com>
2021-06-03 23:14:01 +0200

    refactor: move error dialog

---
+++ packages/admin-stories/src/docs/comet/components/ErrorHandling/ErrorDialog/ErrorDialog.stories.tsx
@@ -0,0 +1,82 @@
+import { useQuery } from "@apollo/client";
+import { ErrorDialogContext, ErrorDialogContextProps } from "@comet/admin";
+import { Button, Typography } from "@material-ui/core";
+import { storiesOf } from "@storybook/react";
+import gql from "graphql-tag";
+import * as React from "react";
+
+import { apolloSwapiStoryDecorator } from "../../../../../apollo-swapi-story.decorator";
+import { errorDialogStoryProviderDecorator } from "../../../../../error-dialog-provider.decorator";
+
+storiesOf("stories/components/errorhandling/errordialog", module)
+    .addDecorator(apolloSwapiStoryDecorator())
+    .addDecorator(errorDialogStoryProviderDecorator())
+    .add("Manual Error Dialog", () => {
+        return (
+            <ErrorDialogContext.Consumer>
+                {(errorDialog: ErrorDialogContextProps | undefined) => {
+                    return (
+                        <Button
+                            variant={"contained"}
+                            color={"primary"}
+                            onClick={() => {
+                                errorDialog?.showError({
+                                    title: "Error",
+                                    userMessage: "You can close the error dialog by pressing ok",
+                                    error: "This is an error detail information e.g. stack trace, which is only shown in development mode",
+                                });
+                            }}
+                        >
+                            Show Error Dialog
+                        </Button>
+                    );
+                }}
+            </ErrorDialogContext.Consumer>
+        );
+    })
+    .add("Automatic Error on Graphql Error", () => {
+        const Story = () => {
+            const [brokenQuery, setBrokenQuery] = React.useState(false);
+            const query = brokenQuery ? "{ allFilms { films { somenotavailablefield } } }" : "{ allFilms { films { title } } }";
+            const { data, error } = useQuery(
+                gql`
+                    ${query}
+                `,
+            );
+            return (
+                <div>
+                    <div style={{ backgroundColor: brokenQuery ? "red" : "green" }}>
+                        <Typography variant={"h4"}>Current Query: {brokenQuery ? "broken" : "working"}</Typography>
+                    </div>
+                    <Typography>Query: {query}</Typography>
+                    <Button
+                        variant={"contained"}
+                        color={"primary"}
+                        onClick={() => {
+                            setBrokenQuery(!brokenQuery);
+                        }}
+                    >
+                        <Typography>Change Query</Typography>
+                    </Button>
+                    {error ? (
+                        <div>
+                            <Typography>
+                                Error:
+                                <br />
+                                {JSON.stringify(error)}
+                            </Typography>
+                        </div>
+                    ) : (
+                        <div>
+                            <Typography>
+                                Response:
+                                <br />
+                                {JSON.stringify(data)}
+                            </Typography>
+                        </div>
+                    )}
+                </div>
+            );
+        };
+        return <Story />;
+    });
