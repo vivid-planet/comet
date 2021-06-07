996fd87750e59246302032c65618ee1df56112cd
Thomas Dax <thomas.dax@vivid-planet.com>
2021-06-07 13:25:27 +0200

    Forward props to FormattedNumber

--- packages/admin/src/helpers/PrettyBytes.tsx
+++ packages/admin/src/helpers/PrettyBytes.tsx
@@ -1,33 +1,32 @@
+import { NumberFormatOptions } from "@formatjs/ecma402-abstract";
 import * as React from "react";
-import { FormattedNumber } from "react-intl";
+import { CustomFormatConfig, FormattedNumber } from "react-intl";
 
 type AvailableUnits = "byte" | "kilobyte" | "megabyte" | "gigabyte" | "terabyte" | "petabyte";
 const availableUnits: AvailableUnits[] = ["byte", "kilobyte", "megabyte", "gigabyte", "terabyte", "petabyte"];
 
-interface PrettyBytesProps {
+interface PrettyBytesProps extends Omit<NumberFormatOptions & CustomFormatConfig, "style"> {
     value: number;
-    maximumFractionDigits?: number;
     unit?: AvailableUnits;
 }
 
 export const PrettyBytes = ({
     value: bytes,
-    maximumFractionDigits: customMaximumFractionDigits,
     unit: customUnit,
+    maximumFractionDigits: customMaximumFractionDigits,
+    ...props
 }: PrettyBytesProps): React.ReactElement => {
     let exponent;
     if (customUnit) {
         exponent = availableUnits.findIndex((unit) => unit === customUnit);
     } else {
         // log to base 1024
-        // => receive exponent of equation 1024 ** exponent = bytes
+        // => receive exponent of equation `1024 ** exponent = bytes`
         // => exponent determines unit
         exponent = Math.min(Math.floor(Math.log(Math.abs(bytes)) / Math.log(1024)), availableUnits.length - 1);
     }
     const unit = availableUnits[exponent];
     const size = bytes / 1024 ** exponent;
 
-    const maximumFractionDigits = customMaximumFractionDigits ?? 2;
-
-    return <FormattedNumber value={size} style="unit" unit={unit} maximumFractionDigits={maximumFractionDigits} />;
+    return <FormattedNumber value={size} style="unit" unit={unit} maximumFractionDigits={customMaximumFractionDigits ?? 2} {...props} />;
 };
