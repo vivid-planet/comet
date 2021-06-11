2aef7c797f4c87f53bb6723aa27d4c3d0fce67a1
Manuel Blum <mb@vivid-planet.com>
2021-06-11 15:10:39 +0200

    feat: add icons
    - disabled
    - online
    - paste
    - store
    - target-groups
    - move-page

--- packages/admin-icons/src/index.ts
+++ packages/admin-icons/src/index.ts
@@ -7,3 +7,4 @@ export { default as CheckboxUnchecked } from "./CheckboxUnchecked";
 export { default as RadioChecked } from "./RadioChecked";
 export { default as RadioUnchecked } from "./RadioUnchecked";
 export { default as Html } from "./Html";
+export { default as MovePage } from "./MovePage";
