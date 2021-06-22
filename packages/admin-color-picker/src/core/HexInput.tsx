c32d7b7b93f4a0fee5fbc4a259d083d48434ad3d
Ricky Smith <rs@vivid-planet.com>
2021-06-22 15:55:46 +0200

    Update react-color types to use onchange correctly
    Previously the first parameter of the onChange from EditableInput
    should have been an object, according to the types which is why it
    was used incorrectly. The value was a string, this has been corrected
    in the new version of the types.

--- packages/admin-color-picker/src/core/HexInput.tsx
+++ packages/admin-color-picker/src/core/HexInput.tsx
@@ -31,13 +31,7 @@ const HexInput: React.FC<IComponentProps & ColorPickerProps> = ({ value, classes
     <div className={classes.inputInner}>
         <div className={classes.inputInnerLeftContent}>
             {!palette || (palette && picker) ? (
-                <EditableInput
-                    style={resetInputStyles}
-                    value={value}
-                    onChange={(colorState) => {
-                        onChange(colorState.hex);
-                    }}
-                />
+                <EditableInput style={resetInputStyles} value={value} onChange={onChange} />
             ) : (
                 <div className={classes.readOnlyInput}>{value.toUpperCase()}</div>
             )}
