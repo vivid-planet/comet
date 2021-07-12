5c1276ea5f70b16a9267adbf612eebe5f693625e
Thomas Dax <thomas.dax@vivid-planet.com>
2021-07-12 16:04:52 +0200

    Add onOpenChange to AppHeaderDropdown story

--- packages/admin-stories/src/docs/components/AppHeader/stories/AppHeaderDropdown.stories.tsx
+++ packages/admin-stories/src/docs/components/AppHeader/stories/AppHeaderDropdown.stories.tsx
@@ -5,34 +5,55 @@ import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
 storiesOf("stories/components/AppHeader/Dropdown", module).add("AppHeader Dropdown", () => {
+    const [openDropdown1, setOpenDropdown1] = React.useState<boolean>(false);
+    const [openDropdown2, setOpenDropdown2] = React.useState<boolean>(false);
+
     return (
-        <AppHeader position="relative" headerHeight={60}>
-            <Typography style={{ padding: 20 }}>Header dropdown</Typography>
-            <AppHeaderFillSpace />
+        <>
+            <div style={{ marginBottom: "30px" }}>
+                Dropdown 1: {openDropdown1 ? "open" : "closed"}
+                <br />
+                Dropdown 2: {openDropdown2 ? "open" : "closed"}
+            </div>
+            <AppHeader position="relative" headerHeight={60}>
+                <Typography style={{ padding: 20 }}>Header dropdown</Typography>
+                <AppHeaderFillSpace />
 
-            <AppHeaderDropdown buttonChildren={<Snips />} dropdownArrow={null}>
-                <Box padding={4} width={200}>
-                    <Typography>
-                        <strong>Dropdown Text</strong>
-                        <br />
-                        <br />
-                        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat
-                        porttitor ligula, eget lacinia odio sem nec elit.
-                    </Typography>
-                </Box>
-            </AppHeaderDropdown>
+                <AppHeaderDropdown
+                    buttonChildren={<Snips />}
+                    dropdownArrow={null}
+                    onOpenChange={(isOpen) => {
+                        setOpenDropdown1(isOpen);
+                    }}
+                >
+                    <Box padding={4} width={200}>
+                        <Typography>
+                            <strong>Dropdown Text</strong>
+                            <br />
+                            <br />
+                            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat
+                            porttitor ligula, eget lacinia odio sem nec elit.
+                        </Typography>
+                    </Box>
+                </AppHeaderDropdown>
 
-            <AppHeaderDropdown buttonChildren="Dropdown Menu">
-                {() => {
-                    return (
-                        <MenuList>
-                            <MenuItem button>Item 1</MenuItem>
-                            <MenuItem button>Item 2</MenuItem>
-                            <MenuItem button>Item 3</MenuItem>
-                        </MenuList>
-                    );
-                }}
-            </AppHeaderDropdown>
-        </AppHeader>
+                <AppHeaderDropdown
+                    buttonChildren="Dropdown Menu"
+                    onOpenChange={(isOpen) => {
+                        setOpenDropdown2(isOpen);
+                    }}
+                >
+                    {() => {
+                        return (
+                            <MenuList>
+                                <MenuItem button>Item 1</MenuItem>
+                                <MenuItem button>Item 2</MenuItem>
+                                <MenuItem button>Item 3</MenuItem>
+                            </MenuList>
+                        );
+                    }}
+                </AppHeaderDropdown>
+            </AppHeader>
+        </>
     );
 });
