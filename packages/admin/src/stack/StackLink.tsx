39d5e57f1991b8f7f366172bf218d771b73030f9
Thomas Dax <thomas.dax@vivid-planet.com>
2021-08-16 15:53:09 +0200

    Make StackLink styling overridable in theme and allow passing child props

--- packages/admin/src/stack/StackLink.tsx
+++ packages/admin/src/stack/StackLink.tsx
@@ -1,30 +1,61 @@
-import { Link } from "@material-ui/core";
+import { Link, LinkBaseProps as MuiLinkBaseProps } from "@material-ui/core";
+import { TypographyProps } from "@material-ui/core/Typography";
+import { createStyles, WithStyles, withStyles } from "@material-ui/styles";
 import React, { PropsWithChildren } from "react";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, LinkProps as RouterLinkProps } from "react-router-dom";
 
 import { IStackSwitchApi, useStackSwitchApi } from "./Switch";
 
-interface StackLinkProps {
+type LinkProps = Omit<RouterLinkProps, "to"> &
+    Omit<MuiLinkBaseProps, "component"> & {
+        TypographyClasses?: TypographyProps["classes"];
+        underline?: "none" | "hover" | "always";
+    };
+
+interface StackLinkProps extends LinkProps {
     pageName: string;
     payload: string;
     subUrl?: string;
     switchApi?: IStackSwitchApi;
 }
 
-export const StackLink = ({
+type CometAdminStackLinkClassKeys = "root";
+
+const styles = () =>
+    createStyles<CometAdminStackLinkClassKeys, any>({
+        root: {},
+    });
+
+const StackLinkWithoutStyles = ({
+    classes,
     pageName,
     payload,
     subUrl,
     switchApi: externalSwitchApi,
     children,
-}: PropsWithChildren<StackLinkProps>): React.ReactElement => {
+    ...props
+}: WithStyles<typeof styles, false> & PropsWithChildren<StackLinkProps>): React.ReactElement => {
     const internalSwitchApi = useStackSwitchApi();
     // external switchApi allows the creation of StackLinks outside of the stack with the useStackSwitch() hook
     const _switchApi = externalSwitchApi !== undefined ? externalSwitchApi : internalSwitchApi;
 
     return (
-        <Link to={() => _switchApi.getTargetUrl(pageName, payload, subUrl)} component={RouterLink}>
+        <Link classes={{ root: classes.root }} to={() => _switchApi.getTargetUrl(pageName, payload, subUrl)} component={RouterLink} {...props}>
             {children}
         </Link>
     );
 };
+
+export const StackLink = withStyles(styles, { name: "CometAdminStackLink" })(StackLinkWithoutStyles);
+
+declare module "@material-ui/core/styles/overrides" {
+    interface ComponentNameToClassKey {
+        CometAdminStackLink: CometAdminStackLinkClassKeys;
+    }
+}
+
+declare module "@material-ui/core/styles/props" {
+    interface ComponentsPropsList {
+        CometAdminStackLink: PropsWithChildren<StackLinkProps>;
+    }
+}
