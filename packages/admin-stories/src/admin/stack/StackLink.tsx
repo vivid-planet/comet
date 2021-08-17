86ebc88841f20aac227ea64f1c0764e9c76bc7a9
Thomas Dax <thomas.dax@vivid-planet.com>
2021-08-17 15:02:33 +0200

    Base StackLink on react-router Link instead of Mui Link

--- packages/admin-stories/src/admin/stack/StackLink.tsx
+++ packages/admin-stories/src/admin/stack/StackLink.tsx
@@ -1,41 +1,28 @@
-import { MuiThemeProvider, Stack, StackLink, StackPage, StackSwitch } from "@comet/admin";
-import { createCometTheme } from "@comet/admin-theme";
+import { Stack, StackLink, StackPage, StackSwitch } from "@comet/admin";
+import { Button, Link } from "@material-ui/core";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
 import { storyRouterDecorator } from "../../story-router.decorator";
 
-const theme = createCometTheme({
-    overrides: {
-        CometAdminStackLink: {
-            root: {
-                padding: "10px",
-                border: "solid black 1px",
-            },
-        },
-    },
-});
-
 function Story() {
     return (
-        <MuiThemeProvider theme={theme}>
-            <Stack topLevelTitle="Example Stack with StackLinks">
-                <StackSwitch>
-                    <StackPage name="page1">
-                        <h3>Page 1</h3>
-                        <StackLink color="secondary" pageName="page2" payload="test">
-                            link with secondary color
-                        </StackLink>
-                    </StackPage>
-                    <StackPage name="page2">
-                        <h3>Page 2</h3>
-                        <StackLink underline="none" pageName="page1" payload="test">
-                            link to page1
-                        </StackLink>
-                    </StackPage>
-                </StackSwitch>
-            </Stack>
-        </MuiThemeProvider>
+        <Stack topLevelTitle="Example Stack with StackLinks">
+            <StackSwitch>
+                <StackPage name="page1">
+                    <h3>Page 1</h3>
+                    <Link component={StackLink} pageName="page2" payload="test">
+                        link based on StackLink
+                    </Link>
+                </StackPage>
+                <StackPage name="page2">
+                    <h3>Page 2</h3>
+                    <Button component={StackLink} pageName="page1" payload="test">
+                        button based on StackLink
+                    </Button>
+                </StackPage>
+            </StackSwitch>
+        </Stack>
     );
 }
 
--- packages/admin/src/stack/StackLink.tsx
+++ packages/admin/src/stack/StackLink.tsx
@@ -1,61 +1,30 @@
-import { Link, LinkBaseProps as MuiLinkBaseProps } from "@material-ui/core";
-import { TypographyProps } from "@material-ui/core/Typography";
-import { createStyles, WithStyles, withStyles } from "@material-ui/styles";
 import React, { PropsWithChildren } from "react";
 import { Link as RouterLink, LinkProps as RouterLinkProps } from "react-router-dom";
 
 import { IStackSwitchApi, useStackSwitchApi } from "./Switch";
 
-type LinkProps = Omit<RouterLinkProps, "to"> &
-    Omit<MuiLinkBaseProps, "component"> & {
-        TypographyClasses?: TypographyProps["classes"];
-        underline?: "none" | "hover" | "always";
-    };
-
-interface StackLinkProps extends LinkProps {
+interface StackLinkProps extends Omit<RouterLinkProps, "to"> {
     pageName: string;
     payload: string;
     subUrl?: string;
     switchApi?: IStackSwitchApi;
 }
 
-type CometAdminStackLinkClassKeys = "root";
-
-const styles = () =>
-    createStyles<CometAdminStackLinkClassKeys, any>({
-        root: {},
-    });
-
-const StackLinkWithoutStyles = ({
-    classes,
+export const StackLink = ({
     pageName,
     payload,
     subUrl,
     switchApi: externalSwitchApi,
     children,
     ...props
-}: WithStyles<typeof styles, false> & PropsWithChildren<StackLinkProps>): React.ReactElement => {
+}: PropsWithChildren<StackLinkProps>): React.ReactElement => {
     const internalSwitchApi = useStackSwitchApi();
     // external switchApi allows the creation of StackLinks outside of the stack with the useStackSwitch() hook
     const _switchApi = externalSwitchApi !== undefined ? externalSwitchApi : internalSwitchApi;
 
     return (
-        <Link classes={{ root: classes.root }} to={() => _switchApi.getTargetUrl(pageName, payload, subUrl)} component={RouterLink} {...props}>
+        <RouterLink to={() => _switchApi.getTargetUrl(pageName, payload, subUrl)} {...props}>
             {children}
-        </Link>
+        </RouterLink>
     );
 };
-
-export const StackLink = withStyles(styles, { name: "CometAdminStackLink" })(StackLinkWithoutStyles);
-
-declare module "@material-ui/core/styles/overrides" {
-    interface ComponentNameToClassKey {
-        CometAdminStackLink: CometAdminStackLinkClassKeys;
-    }
-}
-
-declare module "@material-ui/core/styles/props" {
-    interface ComponentsPropsList {
-        CometAdminStackLink: PropsWithChildren<StackLinkProps>;
-    }
-}
