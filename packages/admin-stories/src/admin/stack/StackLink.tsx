372442a353f180d2b0f9f8b37b85e24d1e4465cd
Thomas Dax <thomas.dax@vivid-planet.com>
2021-08-18 08:58:01 +0200

    Show current pathname

--- packages/admin-stories/src/admin/stack/StackLink.tsx
+++ packages/admin-stories/src/admin/stack/StackLink.tsx
@@ -2,27 +2,33 @@ import { Stack, StackLink, StackPage, StackSwitch } from "@comet/admin";
 import { Button, Link } from "@material-ui/core";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import { useLocation } from "react-router";
 
 import { storyRouterDecorator } from "../../story-router.decorator";
 
 function Story() {
+    const location = useLocation();
+
     return (
-        <Stack topLevelTitle="Example Stack with StackLinks">
-            <StackSwitch>
-                <StackPage name="page1">
-                    <h3>Page 1</h3>
-                    <Link component={StackLink} pageName="page2" payload="test">
-                        link based on StackLink
-                    </Link>
-                </StackPage>
-                <StackPage name="page2">
-                    <h3>Page 2</h3>
-                    <Button component={StackLink} pageName="page1" payload="test">
-                        button based on StackLink
-                    </Button>
-                </StackPage>
-            </StackSwitch>
-        </Stack>
+        <>
+            <p>Pathname: {location.pathname}</p>
+            <Stack topLevelTitle="Example Stack with StackLinks">
+                <StackSwitch>
+                    <StackPage name="page1">
+                        <h3>Page 1</h3>
+                        <Link component={StackLink} pageName="page2" payload="test">
+                            link based on StackLink
+                        </Link>
+                    </StackPage>
+                    <StackPage name="page2">
+                        <h3>Page 2</h3>
+                        <Button component={StackLink} pageName="page1" payload="test">
+                            button based on StackLink
+                        </Button>
+                    </StackPage>
+                </StackSwitch>
+            </Stack>
+        </>
     );
 }
 
