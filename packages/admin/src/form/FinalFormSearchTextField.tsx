b7d3fe6b686d544a950d6b13bd2d491c19f66f72
Thomas Dax <thomas.dax@vivid-planet.com>
2021-09-21 09:53:12 +0200

    Add ClearInputButton to FinalFormSearchTextField per default

--- packages/admin/src/form/FinalFormSearchTextField.tsx
+++ packages/admin/src/form/FinalFormSearchTextField.tsx
@@ -2,9 +2,19 @@ import { Search } from "@comet/admin-icons";
 import { InputAdornment } from "@material-ui/core";
 import * as React from "react";
 import { useIntl } from "react-intl";
+import styled from "styled-components";
 
+import { ClearInputButton } from "..";
 import { useComponentThemeProps } from "../mui/useComponentThemeProps";
 import { FinalFormInput, FinalFormInputProps } from "./FinalFormInput";
+
+interface ClearInputWrapperProps {
+    $hidden: boolean;
+}
+
+const ClearInputWrapper = styled.div<ClearInputWrapperProps>`
+    visibility: ${({ $hidden }) => ($hidden ? "hidden" : "initial")};
+`;
 
 export interface CometAdminFinalFormSearchTextFieldThemeProps {
     icon?: React.ReactNode;
@@ -16,9 +26,18 @@ export function FinalFormSearchTextField({ icon, placeholder, ...restProps }: Fi
 
     return (
         <FinalFormInput
-            {...restProps}
             placeholder={placeholder ?? intl.formatMessage({ id: "comet.finalformsearchtextfield.default.placeholder", defaultMessage: "Search" })}
             startAdornment={<InputAdornment position="start">{icon ?? themeProps.icon}</InputAdornment>}
+            endAdornment={
+                <ClearInputWrapper $hidden={restProps.input.value.length === 0}>
+                    <ClearInputButton
+                        onClick={() => {
+                            restProps.input.onChange("");
+                        }}
+                    />
+                </ClearInputWrapper>
+            }
+            {...restProps}
         />
     );
 }
