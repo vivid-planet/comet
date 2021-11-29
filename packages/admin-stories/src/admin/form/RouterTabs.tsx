0294dd366606165e78dc5fa9d186f3ab692ec854
Thomas Dax <thomas.dax@vivid-planet.com>
2021-11-29 09:50:18 +0100

    Add save button to RouterTabs with Form demo

--- packages/admin-stories/src/admin/form/RouterTabs.tsx
+++ packages/admin-stories/src/admin/form/RouterTabs.tsx
@@ -1,4 +1,17 @@
-import { Field, FinalForm, FinalFormInput, RouterTab, RouterTabs, Stack } from "@comet/admin";
+import {
+    Field,
+    FinalForm,
+    FinalFormInput,
+    RouterTab,
+    RouterTabs,
+    SaveButton,
+    SplitButton,
+    Stack,
+    Toolbar,
+    ToolbarActions,
+    ToolbarBackButton,
+    ToolbarFillSpace,
+} from "@comet/admin";
 import { Card, CardContent } from "@material-ui/core";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
@@ -20,22 +33,64 @@ function Story() {
                             bar: "bar",
                         }}
                     >
-                        <RouterTabs>
-                            <RouterTab label="Form 1" path="">
-                                <Card variant="outlined">
-                                    <CardContent>
-                                        <Field label="Foo" name="foo" component={FinalFormInput} />
-                                    </CardContent>
-                                </Card>
-                            </RouterTab>
-                            <RouterTab label="Form 2" path="/form2">
-                                <Card variant="outlined">
-                                    <CardContent>
-                                        <Field label="Bar" name="bar" component={FinalFormInput} />
-                                    </CardContent>
-                                </Card>
-                            </RouterTab>
-                        </RouterTabs>
+                        {({ pristine, hasValidationErrors, submitting, hasSubmitErrors, handleSubmit, ...formVars }) => {
+                            console.log({ pristine, hasValidationErrors, submitting, hasSubmitErrors, ...formVars });
+
+                            return (
+                                <>
+                                    <Toolbar>
+                                        <ToolbarBackButton />
+                                        <ToolbarFillSpace />
+                                        <ToolbarActions>
+                                            <SplitButton
+                                                disabled={pristine || hasValidationErrors || submitting}
+                                                localStorageKey={"routertabs-with-forms-save"}
+                                            >
+                                                <SaveButton
+                                                    color={"primary"}
+                                                    variant={"contained"}
+                                                    saving={submitting}
+                                                    hasErrors={hasSubmitErrors}
+                                                    type="button"
+                                                    onClick={async () => {
+                                                        handleSubmit();
+                                                    }}
+                                                >
+                                                    Save
+                                                </SaveButton>
+                                                <SaveButton
+                                                    color={"primary"}
+                                                    variant={"contained"}
+                                                    saving={submitting}
+                                                    hasErrors={hasSubmitErrors}
+                                                    onClick={async () => {
+                                                        handleSubmit();
+                                                    }}
+                                                >
+                                                    Save and go back
+                                                </SaveButton>
+                                            </SplitButton>
+                                        </ToolbarActions>
+                                    </Toolbar>
+                                    <RouterTabs>
+                                        <RouterTab label="Form 1" path="">
+                                            <Card variant="outlined">
+                                                <CardContent>
+                                                    <Field label="Foo" name="foo" component={FinalFormInput} />
+                                                </CardContent>
+                                            </Card>
+                                        </RouterTab>
+                                        <RouterTab label="Form 2" path="/form2">
+                                            <Card variant="outlined">
+                                                <CardContent>
+                                                    <Field label="Bar" name="bar" component={FinalFormInput} />
+                                                </CardContent>
+                                            </Card>
+                                        </RouterTab>
+                                    </RouterTabs>
+                                </>
+                            );
+                        }}
                     </FinalForm>
                 </RouterTab>
                 <RouterTab label="Top 2" path="/top2">
