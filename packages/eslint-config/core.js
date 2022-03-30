23cffe76cf2d6eec03842f45a55a4be5645e034f
Alexander Kaufmann <ak@vivid-planet.com>
2022-03-30 09:49:38 +0200

    feat: add own eslint package

---
+++ packages/eslint-config/core.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "simple-import-sort", "unused-imports", "import"],
+    rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
+        "prefer-template": "error",
+        "simple-import-sort/exports": "error",
+        "simple-import-sort/imports": "error",
+        "unused-imports/no-unused-imports": "error",
+        "import/no-extraneous-dependencies": "error",
+        "no-duplicate-imports": "error",
+        "no-console": "error",
+    },
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"],
+            rules: {
+                "@typescript-eslint/no-unused-vars": ["error", { args: "none", ignoreRestSiblings: true }],
+            },
+        },
+    ],
+};
