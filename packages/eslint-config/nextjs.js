6b0129da3230d64b25f9fe26132d0397d950c44d
Alexander Kaufmann <ak@vivid-planet.com>
2022-04-06 10:46:16 +0200

    fix: remove import plugin from core because of next use it internally

--- packages/eslint-config/nextjs.js
+++ packages/eslint-config/nextjs.js
@@ -1,7 +1,8 @@
 module.exports = {
-    extends: [require.resolve("./core.js"), "next/core-web-vitals"],
+    extends: [require.resolve("./core-without-import.js"), "next/core-web-vitals"],
     rules: {
         "react/display-name": "off",
         "react/prop-types": "off",
+        "import/no-extraneous-dependencies": "error",
     },
 };
