8543e358900a4ed53e3f72ab5b6410ead979ee8a
Ricky Smith <jamesricky@me.com>
2022-04-27 17:50:13 +0200

    Fix the theme color of MuiIconButton
    The default color, defined in `root`, overrides the color for all other
    color variants, preventing the ability to set a custom color.
    Unfortunately, there is no `colorDefault` class that can be accessed,
    so all colors need to be explicitly re-defined.

--- packages/admin/admin-theme/src/componentsTheme/MuiIconButton.ts
+++ packages/admin/admin-theme/src/componentsTheme/MuiIconButton.ts
@@ -6,5 +6,29 @@ export const getMuiIconButton = (palette: Palette): Components["MuiIconButton"] 
         root: {
             color: palette.grey[900],
         },
+        colorInherit: {
+            color: "inherit",
+        },
+        colorPrimary: {
+            color: palette.primary.main,
+        },
+        colorSecondary: {
+            color: palette.secondary.main,
+        },
+        // The following classes are missing from the type `IconButtonClasses`, but they do exist.
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        colorWarning: {
+            color: palette.warning.main,
+        },
+        colorError: {
+            color: palette.error.main,
+        },
+        colorSuccess: {
+            color: palette.success.main,
+        },
+        colorInfo: {
+            color: palette.info.main,
+        },
     },
 });
