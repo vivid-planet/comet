e4818884feec7f634968f287673184f16b48c509
Johannes Obermair <johannes.obermair@vivid-planet.com>
2022-06-15 11:06:37 +0200

    Add custom inline styles to RTE Adds the option `customInlineStyles`to the RTE options which can be used to define custom styles for the editor. Furthermore, features in the `FeatureButtonGroup` can now be limited using the `maxVisible` props (additional features will be shown in a popup menu).

--- packages/admin-rte/src/core/Controls/FeaturesButtonGroup.tsx
+++ packages/admin-rte/src/core/Controls/FeaturesButtonGroup.tsx
@@ -1,6 +1,8 @@
-import { WithStyles } from "@material-ui/core";
+import { ListItemIcon, Menu, MenuItem, WithStyles } from "@material-ui/core";
 import Tooltip from "@material-ui/core/Tooltip";
+import { MoreHoriz } from "@material-ui/icons";
 import { createStyles, withStyles } from "@material-ui/styles";
+import { Editor } from "draft-js";
 import * as React from "react";
 
 import { IFeatureConfig } from "../types";
@@ -9,11 +11,87 @@ import ControlButton from "./ControlButton";
 interface IProps {
     features: IFeatureConfig[];
     disabled?: boolean;
+    editorRef: React.RefObject<Editor>;
+    maxVisible?: number;
 }
 
-function FeaturesButtonGroup({ features, disabled: globallyDisabled, classes }: IProps & WithStyles<typeof styles>) {
+function FeaturesButtonGroup({ features, disabled: globallyDisabled, classes, editorRef, maxVisible }: IProps & WithStyles<typeof styles>) {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleMoreOptionsClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+
+        setTimeout(() => {
+            if (editorRef.current) {
+                editorRef.current.focus();
+            }
+        }, 0);
+    };
+
     if (!features.length) {
         return null;
+    }
+
+    if (maxVisible !== undefined) {
+        const visibleFeatures = features.slice(0, maxVisible);
+        const additionalFeatures = features.slice(maxVisible);
+
+        return (
+            <div className={classes.root}>
+                {visibleFeatures.map(({ name, onButtonClick, tooltipText, disabled, ...rest }) => (
+                    <div className={classes.buttonWrapper} key={name}>
+                        {tooltipText ? (
+                            <Tooltip title={tooltipText} placement="top">
+                                <span>
+                                    <ControlButton onButtonClick={onButtonClick} disabled={globallyDisabled || disabled} {...rest} />
+                                </span>
+                            </Tooltip>
+                        ) : (
+                            <ControlButton onButtonClick={onButtonClick} disabled={globallyDisabled || disabled} {...rest} />
+                        )}
+                    </div>
+                ))}
+                {additionalFeatures.length > 0 && (
+                    <div className={classes.buttonWrapper}>
+                        <Tooltip title="More options" placement="top">
+                            <span>
+                                <ControlButton onButtonClick={handleMoreOptionsClick} disabled={globallyDisabled} icon={MoreHoriz} />
+                            </span>
+                        </Tooltip>
+                    </div>
+                )}
+                <Menu open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={handleMenuClose}>
+                    {additionalFeatures.map(({ name, onButtonClick, disabled, selected, label, icon: Icon }) => (
+                        <MenuItem
+                            key={name}
+                            onMouseDown={(event) => {
+                                handleMenuClose();
+
+                                // See https://reactjs.org/docs/legacy-event-pooling.html for more information.
+                                event.persist();
+                                setTimeout(() => {
+                                    onButtonClick?.(event);
+                                }, 0);
+                            }}
+                            disabled={globallyDisabled || disabled}
+                            selected={selected}
+                            className={classes.listItem}
+                        >
+                            {label}
+                            {Icon && (
+                                <ListItemIcon className={classes.listItemIcon}>
+                                    <Icon />
+                                </ListItemIcon>
+                            )}
+                        </MenuItem>
+                    ))}
+                </Menu>
+            </div>
+        );
     }
 
     return (
@@ -35,7 +113,7 @@ function FeaturesButtonGroup({ features, disabled: globallyDisabled, classes }: 
     );
 }
 
-export type RteFeaturesButtonGroupClassKey = "root" | "buttonWrapper";
+export type RteFeaturesButtonGroupClassKey = "root" | "buttonWrapper" | "listItem" | "listItemIcon";
 
 const styles = () => {
     return createStyles<RteFeaturesButtonGroupClassKey, IProps>({
@@ -48,6 +126,12 @@ const styles = () => {
             "&:last-child": {
                 marginRight: 0,
             },
+        },
+        listItem: {
+            justifyContent: "space-between",
+        },
+        listItemIcon: {
+            justifyContent: "flex-end",
         },
     });
 };
---
+++ packages/admin-stories/src/admin-rte/RteCustomInlineStyles.tsx
@@ -0,0 +1,46 @@
+import { IRteOptions, IRteRef, makeRteApi, Rte } from "@comet/admin-rte";
+import { Box, Card, CardContent } from "@material-ui/core";
+import { Highlight } from "@material-ui/icons";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { PrintEditorState, useAutoFocus } from "./helper";
+
+const rteOptions: IRteOptions = {
+    supports: ["header-one", "header-two", "bold", "italic", "strikethrough", "sub", "sup"],
+    customInlineStyles: {
+        HIGHLIGHT: {
+            label: "Highlight!",
+            icon: Highlight,
+            style: {
+                color: "blue",
+                textDecoration: "underline",
+            },
+        },
+    },
+};
+
+const [useRteApi] = makeRteApi();
+
+function Story() {
+    const { editorState, setEditorState } = useRteApi();
+
+    // focus the editor to see the cursor immediately
+    const editorRef = React.useRef<IRteRef>();
+    useAutoFocus(editorRef);
+
+    return (
+        <>
+            <Box marginBottom={4}>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Rte value={editorState} onChange={setEditorState} ref={editorRef} options={rteOptions} />
+                    </CardContent>
+                </Card>
+            </Box>
+            <PrintEditorState editorState={editorState} />
+        </>
+    );
+}
+
+storiesOf("@comet/admin-rte", module).add("Custom inline styles", () => <Story />);
