740047a30724d857b2c38bceb2341986b8d511a8
Ricky Smith <jamesricky@me.com>
2022-06-15 15:08:58 +0200

    Adjust spacing & popup-height to show exactly 7 items

--- packages/admin/admin-date-time/src/TimePicker.styles.ts
+++ packages/admin/admin-date-time/src/TimePicker.styles.ts
@@ -1,18 +1,23 @@
 import { InputWithPopperClassKey } from "@comet/admin";
+import { menuItemClasses, Theme } from "@mui/material";
 import { createStyles } from "@mui/styles";
 
 import { TimePickerProps } from "./TimePicker";
 
 export type TimePickerClassKey = InputWithPopperClassKey;
 
-export const styles = () =>
+export const styles = ({ spacing }: Theme) =>
     createStyles<TimePickerClassKey, TimePickerProps>({
         root: {},
         inputBase: {},
         popper: {},
         paper: {
             minWidth: 110,
-            height: 324,
+            height: 280,
             overflowY: "auto",
+
+            [`& .${menuItemClasses.root}`]: {
+                padding: `${spacing(2)} ${spacing(3)}`,
+            },
         },
     });
