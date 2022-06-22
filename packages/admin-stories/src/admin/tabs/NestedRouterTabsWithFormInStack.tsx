e77f6a5879c689e575e95c995498bc1235a8c535
Thomas Dax <thomas.dax@vivid-planet.com>
2022-06-22 13:56:45 +0200

    docs(RouterTabs): change nested RouterTabs example to be less complicated

--- packages/admin-stories/src/admin/tabs/NestedRouterTabsWithFormInStack.tsx
+++ packages/admin-stories/src/admin/tabs/NestedRouterTabsWithFormInStack.tsx
@@ -1,26 +1,11 @@
-import { Field, FinalForm, FinalFormInput, RouterTab, RouterTabs, Stack, StackBreadcrumbs, StackPage, StackSwitch } from "@comet/admin";
+import { Field, FinalForm, FinalFormInput, RouterTab, RouterTabs, Stack } from "@comet/admin";
 import { Card, CardContent } from "@material-ui/core";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
-import { useForm } from "react-final-form";
 import { useLocation } from "react-router";
 
 import { apolloStoryDecorator } from "../../apollo-story.decorator";
 import { storyRouterDecorator } from "../../story-router.decorator";
-
-const ResetTab = () => {
-    const formApi = useForm();
-
-    React.useEffect(() => {
-        formApi.reset();
-    }, []);
-
-    return (
-        <Card variant="outlined">
-            <CardContent>You received a Discard unsaved changes prompt before navigating here</CardContent>
-        </Card>
-    );
-};
 
 const Story = () => {
     const location = useLocation();
@@ -29,86 +14,42 @@ const Story = () => {
         <div>
             <p>Location: {location.pathname}</p>
             <Stack topLevelTitle="Root Stack">
-                <StackBreadcrumbs />
-                <FinalForm
-                    mode="edit"
-                    onSubmit={(values: any) => {
-                        alert(JSON.stringify(values));
-                    }}
-                >
-                    <RouterTabs>
-                        <RouterTab label="Page 1" path="" forceRender>
-                            <Card variant="outlined">
-                                <CardContent>
-                                    <Field label="Firstname" name="firstname" component={FinalFormInput} />
-                                </CardContent>
-                            </Card>
-                            <StackSwitch>
-                                <StackPage name="table">
-                                    <RouterTabs>
-                                        <RouterTab label="Page 3" path="" forceRender>
-                                            <Card variant="outlined">
-                                                <CardContent>
-                                                    <Field label="Lastname" name="lastname" component={FinalFormInput} />
-                                                </CardContent>
-                                            </Card>
-                                        </RouterTab>
-                                        <RouterTab label="Page 4" path="/page4" forceRender>
-                                            Now the first level of RouterTabs (Page 1 and 2) disappeared
-                                            <StackSwitch>
-                                                <StackPage name="table">
-                                                    <RouterTabs>
-                                                        <RouterTab label="Page 5" path="" forceRender>
-                                                            <Card variant="outlined">
-                                                                <CardContent>
-                                                                    <Field label="Address" name="address" component={FinalFormInput} />
-                                                                </CardContent>
-                                                            </Card>
-                                                        </RouterTab>
-                                                        <RouterTab label="Page 6" path="/page6" forceRender>
-                                                            Now the second level of RouterTabs (Page 3 and 4) also disappeared
-                                                            <StackSwitch>
-                                                                <StackPage name="table">
-                                                                    <RouterTabs>
-                                                                        <RouterTab label="Page 7" path="" forceRender>
-                                                                            <Card variant="outlined">
-                                                                                <CardContent>
-                                                                                    <Field
-                                                                                        label="Country"
-                                                                                        name="country"
-                                                                                        component={FinalFormInput}
-                                                                                    />
-                                                                                </CardContent>
-                                                                            </Card>
-                                                                        </RouterTab>
-                                                                        <RouterTab label="Prompt on Navigation" path="/prompt2" promptOnNavigation>
-                                                                            <ResetTab />
-                                                                        </RouterTab>
-                                                                    </RouterTabs>
-                                                                </StackPage>
-                                                                <StackPage name="stackpage-4">StackPage 4</StackPage>
-                                                            </StackSwitch>
-                                                        </RouterTab>
-                                                    </RouterTabs>
-                                                </StackPage>
-                                                <StackPage name="stackpage-3">StackPage 3</StackPage>
-                                            </StackSwitch>
-                                        </RouterTab>
-                                    </RouterTabs>
-                                </StackPage>
-                                <StackPage name="stackpage-2">StackPage 2</StackPage>
-                            </StackSwitch>
-                        </RouterTab>
-                        <RouterTab label="Not Form" path="/not-form">
-                            <Card variant="outlined">
-                                <CardContent>Here are no form fields</CardContent>
-                            </Card>
-                        </RouterTab>
-                        <RouterTab label="Prompt on Navigation" path="/prompt" promptOnNavigation>
-                            <ResetTab />
-                        </RouterTab>
-                    </RouterTabs>
-                </FinalForm>
+                <RouterTabs>
+                    <RouterTab label="Top 1" path="">
+                        <FinalForm
+                            mode="edit"
+                            onSubmit={(values: any) => {
+                                alert(JSON.stringify(values));
+                            }}
+                            initialValues={{
+                                foo: "foo",
+                                bar: "bar",
+                            }}
+                        >
+                            <RouterTabs>
+                                <RouterTab label="Form 1" path="" forceRender>
+                                    <Card variant="outlined">
+                                        <CardContent>
+                                            <Field label="Foo" name="foo" component={FinalFormInput} />
+                                        </CardContent>
+                                    </Card>
+                                </RouterTab>
+                                <RouterTab label="Form 2" path="/form2" forceRender>
+                                    <Card variant="outlined">
+                                        <CardContent>
+                                            <Field label="Bar" name="bar" component={FinalFormInput} />
+                                        </CardContent>
+                                    </Card>
+                                </RouterTab>
+                            </RouterTabs>
+                        </FinalForm>
+                    </RouterTab>
+                    <RouterTab label="Lose Form State" path="/lose-form-state" promptOnNavigation>
+                        <Card variant="outlined">
+                            <CardContent>Form State is lost</CardContent>
+                        </Card>
+                    </RouterTab>
+                </RouterTabs>
             </Stack>
         </div>
     );
