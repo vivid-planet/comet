4962d1e53279953c646b9380e9e74520ca3f39bf
Johannes Obermair <johannes.obermair@vivid-planet.com>
2022-07-06 11:17:46 +0200

    Add block to link to news

--- demo/admin/src/common/blocks/LinkBlock.tsx
+++ demo/admin/src/common/blocks/LinkBlock.tsx
@@ -1,10 +1,11 @@
 import { BlockInterface, createOneOfBlock } from "@comet/blocks-admin";
 import { ExternalLinkBlock, InternalLinkBlock } from "@comet/cms-admin";
+import { NewsLinkBlock } from "@src/news/blocks/NewsLinkBlock";
 import * as React from "react";
 import { FormattedMessage } from "react-intl";
 
 export const LinkBlock: BlockInterface = createOneOfBlock({
-    supportedBlocks: { internal: InternalLinkBlock, external: ExternalLinkBlock },
+    supportedBlocks: { internal: InternalLinkBlock, external: ExternalLinkBlock, news: NewsLinkBlock },
     name: "Link",
     displayName: <FormattedMessage id="comet.blocks.link" defaultMessage="Link" />,
     allowEmpty: false,
