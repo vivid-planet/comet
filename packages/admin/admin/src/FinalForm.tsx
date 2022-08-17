a26b2379b0d7dd1d20a12c84f42206d40448b631
Thomas Dax <thomas.dax@vivid-planet.com>
2022-08-17 14:36:07 +0200

    feat(FinalForm): add prop to allow submission of pristine form

--- packages/admin/admin/src/FinalForm.tsx
+++ packages/admin/admin/src/FinalForm.tsx
@@ -32,6 +32,7 @@ interface IProps<FormValues = AnyObject> extends FormProps<FormValues> {
     validateWarning?: (values: FormValues) => ValidationErrors | Promise<ValidationErrors> | undefined;
     formContext?: Partial<FinalFormContext>;
     apiRef?: React.MutableRefObject<FormApi<FormValues> | undefined>;
+    allowPristineSubmission?: boolean;
 }
 
 export function FinalForm<FormValues = AnyObject>(props: IProps<FormValues>) {
@@ -48,6 +49,7 @@ export function FinalForm<FormValues = AnyObject>(props: IProps<FormValues>) {
             editDialog?.closeDialog({ delay: true });
         },
         validateWarning,
+        allowPristineSubmission = false,
     } = props;
 
     const ref = React.useRef();
@@ -69,7 +71,7 @@ export function FinalForm<FormValues = AnyObject>(props: IProps<FormValues>) {
         const submit = React.useCallback(
             (event: any) => {
                 event.preventDefault(); //  Prevents from reloading the page with GET-params on submit
-                if (!formRenderProps.dirty) return;
+                if (!formRenderProps.dirty && !allowPristineSubmission) return;
                 return new Promise<SubmissionErrors | void>((resolve) => {
                     Promise.resolve(formRenderProps.handleSubmit(event)).then(
                         () => {
