20dc10b640c58712dceb412ba82258228c7cd77c
Niko Sams <ns@vivid-planet.com>
2022-08-25 10:21:19 +0200

    FinalForm based edit of Products with blocks
    This adds a demo for a form that edits PIM data with blocks.
    
    Changed in library:
    - new useFormSaveConflict helper for easier implementation of conflicts for FinalForms
    - extend FinalFormSaveSplitButton to support navigating to edit page after add
    
    Features of product from demo:
    - conflicts (like we do for page content)
    - image block inside FinalForm (not the other way around - where we used useSaveState and auto-submitting BlocksFinalForm once there was a block needed in a form)
    - no more initial validation on add

--- demo/admin/src/products/ProductForm.gql.ts
+++ demo/admin/src/products/ProductForm.gql.ts
@@ -7,21 +7,34 @@ export const productFormFragment = gql`
         description
         price
         inStock
+        image
     }
 `;
 
 export const productQuery = gql`
     query Product($id: ID!) {
         product(id: $id) {
+            id
+            updatedAt
             ...ProductForm
         }
     }
     ${productFormFragment}
 `;
 
+export const productCheckForChangesQuery = gql`
+    query CheckForChangesProduct($id: ID!) {
+        product(id: $id) {
+            updatedAt
+        }
+    }
+`;
+
 export const createProductMutation = gql`
     mutation ProductFormCreateProduct($input: ProductInput!) {
         createProduct(input: $input) {
+            id
+            updatedAt
             ...ProductForm
         }
     }
@@ -29,8 +42,10 @@ export const createProductMutation = gql`
 `;
 
 export const updateProductMutation = gql`
-    mutation ProductFormUpdateProduct($id: ID!, $input: ProductInput!) {
-        updateProduct(id: $id, input: $input) {
+    mutation ProductFormUpdateProduct($id: ID!, $input: ProductInput!, $lastUpdatedAt: DateTime) {
+        updateProduct(id: $id, input: $input, lastUpdatedAt: $lastUpdatedAt) {
+            id
+            updatedAt
             ...ProductForm
         }
     }
