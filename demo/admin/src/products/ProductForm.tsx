7349445c41b3c48636b32400859f1b529ba21ddd
Niko Sams <ns@vivid-planet.com>
2022-08-25 10:21:19 +0200

    FinalForm based edit of Products with blocks
    This adds a demo for a form that edits PIM data with blocks.
    
    Changed in library:
    - new useFormSaveConflict helper for easier implementation of conflicts for FinalForms
    - extend FinalFormSaveSplitButton to support navigating to edit page after add
    
    Features of product from demo:
    - conflicts (like we do for page content)
    - image block inside FinalForm (not the other way around - where we used useSaveState and auto-submitting BlocksFinalForm once there was a block needed in a form)
    - no more initial validation on add

--- demo/admin/src/products/ProductForm.tsx
+++ demo/admin/src/products/ProductForm.tsx
@@ -4,21 +4,24 @@ import {
     FinalForm,
     FinalFormCheckbox,
     FinalFormInput,
+    FinalFormSaveSplitButton,
     MainContent,
-    messages,
-    SaveButton,
-    SplitButton,
     Toolbar,
     ToolbarActions,
     ToolbarFillSpace,
     ToolbarItem,
     ToolbarTitleItem,
+    useFormApiRef,
     useStackApi,
+    useStackSwitchApi,
 } from "@comet/admin";
 import { ArrowLeft } from "@comet/admin-icons";
-import { EditPageLayout } from "@comet/cms-admin";
+import { BlockState, createFinalFormBlock } from "@comet/blocks-admin";
+import { DamImageBlock, EditPageLayout, resolveHasSaveConflict, useFormSaveConflict } from "@comet/cms-admin";
 import { CircularProgress, FormControlLabel, IconButton } from "@mui/material";
 import {
+    GQLCheckForChangesProductQuery,
+    GQLCheckForChangesProductQueryVariables,
     GQLProductFormCreateProductMutation,
     GQLProductFormCreateProductMutationVariables,
     GQLProductFormFragment,
@@ -27,49 +30,89 @@ import {
     GQLProductQuery,
     GQLProductQueryVariables,
 } from "@src/graphql.generated";
-import { FORM_ERROR } from "final-form";
 import { filter } from "graphql-anywhere";
+import isEqual from "lodash.isequal";
 import React from "react";
-import { FormattedMessage, useIntl } from "react-intl";
+import { FormattedMessage } from "react-intl";
 
-import { createProductMutation, productFormFragment, productQuery, updateProductMutation } from "./ProductForm.gql";
+import { createProductMutation, productCheckForChangesQuery, productFormFragment, productQuery, updateProductMutation } from "./ProductForm.gql";
 
 interface FormProps {
     id?: string;
 }
 
+const rootBlocks = {
+    image: DamImageBlock,
+};
+
 type FormState = Omit<GQLProductFormFragment, "price"> & {
     price: string;
+    image: BlockState<typeof rootBlocks.image>;
 };
 
 function ProductForm({ id }: FormProps): React.ReactElement {
-    const intl = useIntl();
     const stackApi = useStackApi();
     const client = useApolloClient();
     const mode = id ? "edit" : "add";
+    const formApiRef = useFormApiRef<FormState>();
+    const stackSwitchApi = useStackSwitchApi();
+    const createMutationResponseRef = React.useRef<GQLProductFormCreateProductMutation>();
+
+    const { data, error, loading, refetch } = useQuery<GQLProductQuery, GQLProductQueryVariables>(
+        productQuery,
+        id ? { variables: { id } } : { skip: true },
+    );
+
+    const initialValues: Partial<FormState> = data?.product
+        ? {
+              ...filter<GQLProductFormFragment>(productFormFragment, data.product),
+              price: String(data.product.price),
+              image: rootBlocks.image.input2State(data.product.image),
+          }
+        : {
+              image: rootBlocks.image.defaultValues(),
+          };
+
+    const saveConflict = useFormSaveConflict({
+        checkConflict: async () => {
+            if (!id) return false;
+            const { data: hasConflictData } = await client.query<GQLCheckForChangesProductQuery, GQLCheckForChangesProductQueryVariables>({
+                query: productCheckForChangesQuery,
+                variables: { id },
+                fetchPolicy: "no-cache",
+            });
+            return resolveHasSaveConflict(data?.product.updatedAt, hasConflictData.product.updatedAt);
+        },
+        formApiRef,
+        loadLatestVersion: async () => {
+            await refetch();
+        },
+    });
 
     const handleSubmit = async (formState: FormState) => {
-        const input = {
+        if (await saveConflict.checkForConflicts()) throw new Error("Conflicts detected");
+        createMutationResponseRef.current = undefined;
+        const output = {
             ...formState,
             price: parseFloat(formState.price),
+            image: rootBlocks.image.state2Output(formState.image),
         };
         if (mode === "edit") {
             if (!id) throw new Error();
             await client.mutate<GQLProductFormUpdateProductMutation, GQLProductFormUpdateProductMutationVariables>({
                 mutation: updateProductMutation,
-                variables: { id, input },
+                variables: { id, input: output, lastUpdatedAt: data?.product.updatedAt },
             });
         } else {
-            await client.mutate<GQLProductFormCreateProductMutation, GQLProductFormCreateProductMutationVariables>({
+            const { data: mutationReponse } = await client.mutate<GQLProductFormCreateProductMutation, GQLProductFormCreateProductMutationVariables>({
                 mutation: createProductMutation,
-                variables: { input },
+                variables: { input: output },
             });
+            if (mutationReponse) {
+                createMutationResponseRef.current = mutationReponse;
+            }
         }
     };
-
-    const { data, error, loading } = useQuery<GQLProductQuery, GQLProductQueryVariables>(productQuery, id ? { variables: { id } } : { skip: true });
-
-    const initialValues = data?.product ? filter(productFormFragment, data.product) : {};
 
     if (error) {
         return <FormattedMessage id="demo.common.error" defaultMessage="Ein Fehler ist aufgetreten. Bitte versuchen Sie es später noch einmal." />;
@@ -81,16 +124,18 @@ function ProductForm({ id }: FormProps): React.ReactElement {
 
     return (
         <FinalForm<FormState>
+            apiRef={formApiRef}
             onSubmit={handleSubmit}
             mode={mode}
             initialValues={initialValues}
-            renderButtons={() => null}
+            initialValuesEqual={isEqual} //required to compare block data correctly
             onAfterSubmit={(values, form) => {
                 //don't go back automatically
             }}
         >
-            {({ values, pristine, hasValidationErrors, submitting, handleSubmit, hasSubmitErrors }) => (
+            {({ values }) => (
                 <EditPageLayout>
+                    {saveConflict.dialogs}
                     <Toolbar>
                         <ToolbarItem>
                             <IconButton onClick={stackApi?.goBack}>
@@ -102,26 +147,14 @@ function ProductForm({ id }: FormProps): React.ReactElement {
                         </ToolbarTitleItem>
                         <ToolbarFillSpace />
                         <ToolbarActions>
-                            <SplitButton disabled={pristine || hasValidationErrors || submitting} localStorageKey="editInspirationSave">
-                                <SaveButton color="primary" variant="contained" hasErrors={hasSubmitErrors} type="submit">
-                                    <FormattedMessage {...messages.save} />
-                                </SaveButton>
-                                <SaveButton
-                                    color="primary"
-                                    variant="contained"
-                                    saving={submitting}
-                                    hasErrors={hasSubmitErrors}
-                                    onClick={async () => {
-                                        const submitResult = await handleSubmit();
-                                        const error = submitResult?.[FORM_ERROR];
-                                        if (!error) {
-                                            stackApi?.goBack();
-                                        }
-                                    }}
-                                >
-                                    <FormattedMessage {...messages.saveAndGoBack} />
-                                </SaveButton>
-                            </SplitButton>
+                            <FinalFormSaveSplitButton
+                                onNavigateToEditPage={() => {
+                                    const id = createMutationResponseRef.current?.createProduct.id;
+                                    if (mode == "add" && id) {
+                                        stackSwitchApi.activatePage(`edit`, id);
+                                    }
+                                }}
+                            />
                         </ToolbarActions>
                     </Toolbar>
                     <MainContent>
@@ -130,14 +163,14 @@ function ProductForm({ id }: FormProps): React.ReactElement {
                             fullWidth
                             name="title"
                             component={FinalFormInput}
-                            label={intl.formatMessage({ id: "demo.product.title", defaultMessage: "Titel" })}
+                            label={<FormattedMessage id="demo.product.title" defaultMessage="Titel" />}
                         />
                         <Field
                             required
                             fullWidth
                             name="slug"
                             component={FinalFormInput}
-                            label={intl.formatMessage({ id: "demo.product.slug", defaultMessage: "Slug" })}
+                            label={<FormattedMessage id="demo.product.slug" defaultMessage="Slug" />}
                         />
                         <Field
                             required
@@ -146,22 +179,25 @@ function ProductForm({ id }: FormProps): React.ReactElement {
                             rows={5}
                             name="description"
                             component={FinalFormInput}
-                            label={intl.formatMessage({ id: "demo.product.description", defaultMessage: "Beschreibung" })}
+                            label={<FormattedMessage id="demo.product.description" defaultMessage="Beschreibung" />}
                         />
                         <Field
                             fullWidth
                             name="price"
                             component={FinalFormInput}
                             inputProps={{ type: "number" }}
-                            label={intl.formatMessage({ id: "demo.product.description", defaultMessage: "Preis" })}
+                            label={<FormattedMessage id="demo.product.price" defaultMessage="Preis" />}
                         />
                         <Field name="inStock" label="" type="checkbox" fullWidth>
                             {(props) => (
                                 <FormControlLabel
-                                    label={intl.formatMessage({ id: "demo.product.inStock", defaultMessage: "Auf Lager" })}
+                                    label={<FormattedMessage id="demo.product.inStock" defaultMessage="Auf Lager" />}
                                     control={<FinalFormCheckbox {...props} />}
                                 />
                             )}
+                        </Field>
+                        <Field name="image" isEqual={isEqual}>
+                            {createFinalFormBlock(rootBlocks.image)}
                         </Field>
                     </MainContent>
                 </EditPageLayout>
