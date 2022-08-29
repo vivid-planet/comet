dbb807f1ae6c05cfb8f3148b1cd090c448dbca16
Thomas Dax <thomas.dax@vivid-planet.com>
2022-08-29 11:24:49 +0200

    fix(FileUploadContextProvider): clear timeouts on unmount

--- packages/admin/cms-admin/src/dam/Table/fileUpload/FileUploadContext.tsx
+++ packages/admin/cms-admin/src/dam/Table/fileUpload/FileUploadContext.tsx
@@ -15,15 +15,28 @@ const FileUploadContext = React.createContext<FileUploadContextApi>({
 export const useFileUploadContext = () => React.useContext(FileUploadContext);
 
 export const FileUploadContextProvider: React.FunctionComponent = ({ children }) => {
+    const timeouts = React.useRef<NodeJS.Timeout[]>([]);
     const [newlyUploadedFileIds, setNewlyUploadedFileIds] = React.useState<string[]>([]);
+
+    React.useEffect(() => {
+        return () => {
+            for (const timeout of timeouts.current) {
+                clearTimeout(timeout);
+            }
+        };
+    }, []);
 
     const addNewlyUploadedFileIds = (fileIds: string[]) => {
         setNewlyUploadedFileIds((newlyUploadedFileIds) => [...newlyUploadedFileIds, ...fileIds]);
 
-        setTimeout(() => {
+        const timeout = setTimeout(() => {
             // remove uploaded files automatically after 5 seconds
             setNewlyUploadedFileIds((newlyUploadedFileIds) => newlyUploadedFileIds.filter((fileId) => !fileIds.includes(fileId)));
+
+            timeouts.current = timeouts.current.filter((t) => t !== timeout);
         }, 5000);
+
+        timeouts.current.push(timeout);
     };
 
     return <FileUploadContext.Provider value={{ newlyUploadedFileIds, addNewlyUploadedFileIds }}>{children}</FileUploadContext.Provider>;
