c6eed548754b8ea056b29fd8e80367deccbc40e9
Johannes Obermair <johannes.obermair@vivid-planet.com>
2022-09-29 10:58:41 +0200

    Re-add news comments field resolver

--- demo/api/src/news/news.module.ts
+++ demo/api/src/news/news.module.ts
@@ -6,10 +6,11 @@ import { NewsComment } from "./entities/news-comment.entity";
 import { NewsCrudResolver } from "./generated/news.crud.resolver";
 import { NewsService } from "./generated/news.service";
 import { NewsCommentResolver } from "./news-comment.resolver";
+import { NewsFieldResolver } from "./news-field.resolver";
 
 @Module({
     imports: [MikroOrmModule.forFeature([News, NewsComment, NewsContentScope])],
-    providers: [NewsCrudResolver, NewsCommentResolver, NewsService],
+    providers: [NewsCrudResolver, NewsCommentResolver, NewsService, NewsFieldResolver],
     exports: [],
 })
 export class NewsModule {}
