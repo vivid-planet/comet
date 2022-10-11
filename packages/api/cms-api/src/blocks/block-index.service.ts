aaface86409613c1da9da9e4ba4ffdf63837d22a
Thomas Dax <thomas.dax@vivid-planet.com>
2022-10-11 13:22:22 +0200

    feat(BlockIndexService): adjust createViews() to new block index system

--- packages/api/cms-api/src/blocks/block-index.service.ts
+++ packages/api/cms-api/src/blocks/block-index.service.ts
@@ -12,42 +12,59 @@ export class BlockIndexService {
     }
 
     async createViews(): Promise<void> {
-        this.discoverEntitiesService.discoverTargetEntities();
+        const indexSelects: string[] = [];
+        const targetEntities = this.discoverEntitiesService.discoverTargetEntities();
+        const targetEntitiesNameData = targetEntities.reduce((obj, entity) => {
+            return {
+                ...obj,
+                [entity.indexName]: { entityName: entity.entityName, tableName: entity.metadata.tableName, primary: entity.metadata.primaryKeys[0] },
+            };
+        }, {});
 
-        const damFilesIndexSelects: string[] = [];
-        for (const discoveredEntity of this.discoverEntitiesService.discoverRootBlocks()) {
-            const { metadata, column } = discoveredEntity;
+        console.log("targetEntitiesNameData ", targetEntitiesNameData);
+        console.log("json targetEntitiesNameData ", JSON.stringify(targetEntitiesNameData));
+
+        for (const rootBlockEntity of this.discoverEntitiesService.discoverRootBlocks()) {
+            const { metadata, column } = rootBlockEntity;
             const primary = metadata.primaryKeys[0];
 
-            const select = `SELECT "${metadata.tableName}"."${primary}" id,
+            const select = `SELECT 
+                            "${metadata.tableName}"."${primary}" id,
+                            '${metadata.name}' "entityName",
                             '${metadata.tableName}' "tableName",
                             '${column}' "columnName",
                             '${primary}' "primaryKey",
-                            index->>'blockname' "blockname",
-                            index->>'jsonPath' "jsonPath",
-                            (index->>'visible')::boolean "visible",
-                            files "damFileId"
+                            indexObj->>'blockname' "blockname",
+                            indexObj->>'jsonPath' "jsonPath",
+                            (indexObj->>'visible')::boolean "visible",
+                            targetTableData->>'entityName' "targetEntityName",
+                            targetTableData->>'tableName' "targetTableName",
+                            targetTableData->>'primary' "targetPrimaryKey",
+                            targetObj->>'id' "targetId"
                         FROM "${metadata.tableName}",
-                            json_array_elements("${metadata.tableName}"."${column}"->'index') index,
-                            json_array_elements_text("index"->'damFileIds') files`;
-            damFilesIndexSelects.push(select);
+                            json_array_elements("${metadata.tableName}"."${column}"->'index') indexObj,
+                            json_array_elements(indexObj->'target') targetObj,
+                            json_extract_path('${JSON.stringify(targetEntitiesNameData)}', targetObj->>'indexName') targetTableData`;
+
+            console.log(select);
+            indexSelects.push(select);
         }
 
-        const viewSql = damFilesIndexSelects.join("\n UNION ALL \n");
+        const viewSql = indexSelects.join("\n UNION ALL \n");
 
-        console.time("creating block damFiles materialized view");
-        await this.connection.execute(`DROP MATERIALIZED VIEW IF EXISTS block_index_damfiles`);
-        await this.connection.execute(`CREATE MATERIALIZED VIEW block_index_damfiles AS ${viewSql}`);
-        console.timeEnd("creating block damFiles materialized view");
+        console.time("creating block dependency materialized view");
+        await this.connection.execute(`DROP MATERIALIZED VIEW IF EXISTS block_index_dependencies`);
+        await this.connection.execute(`CREATE MATERIALIZED VIEW block_index_dependencies AS ${viewSql}`);
+        console.timeEnd("creating block dependency materialized view");
 
-        console.time("creating block damFiles materialized view index");
-        await this.connection.execute(`CREATE INDEX block_index_damfiles_damfileid ON block_index_damfiles ("damFileId")`);
-        console.timeEnd("creating block damFiles materialized view index");
+        console.time("creating block dependency materialized view index");
+        await this.connection.execute(`CREATE INDEX block_index_dependencies_targetId ON block_index_dependencies ("targetId")`);
+        console.timeEnd("creating block dependency materialized view index");
     }
 
     async refreshViews(): Promise<void> {
-        console.time("refresh materialized block damFiles");
-        await this.connection.execute("REFRESH MATERIALIZED VIEW block_index_damfiles");
-        console.timeEnd("refresh materialized block damFiles");
+        console.time("refresh materialized block dependency");
+        await this.connection.execute("REFRESH MATERIALIZED VIEW block_index_dependencies");
+        console.timeEnd("refresh materialized block dependency");
     }
 }
