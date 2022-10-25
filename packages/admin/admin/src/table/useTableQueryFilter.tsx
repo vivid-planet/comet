67e50193fe58dc8f0191c50c9c848bdf58115a88
Thomas Dax <thomas.dax@vivid-planet.com>
2022-10-25 11:08:14 +0200

    revert(useTableQueryFilter): remove stored state from useTableQueryFilter

--- packages/admin/admin/src/table/useTableQueryFilter.tsx
+++ packages/admin/admin/src/table/useTableQueryFilter.tsx
@@ -3,7 +3,6 @@ import debounce from "lodash.debounce";
 import isEqual from "lodash.isequal";
 import * as React from "react";
 
-import { useStoredState } from "../hooks/useStoredState";
 import { usePersistedState } from "./usePersistedState";
 import { IPagingApi } from "./useTableQueryPaging";
 
@@ -11,30 +10,19 @@ export interface IFilterApi<FilterValues extends AnyObject> {
     current: FilterValues;
     formApi: FormApi<FilterValues>;
 }
-export function useTableQueryFilter<FilterValues extends AnyObject>(
+export function useTableQueryFilter<FilterValues>(
     defaultValues: FilterValues,
     options: {
         pagingApi?: IPagingApi<any>;
         persistedStateId?: string;
-        browserStorage?: Storage;
     } = {},
 ): IFilterApi<FilterValues> {
-    const filterId = `${options.persistedStateId}_filter`;
-
-    if (options.browserStorage && !options.persistedStateId) {
-        throw new Error("When using browserStorage you must provide a persistedStateId");
-    }
-
-    const persistedState = usePersistedState<FilterValues>(defaultValues, {
-        persistedStateId: options.persistedStateId ? filterId : undefined,
+    const [filters, setFilters] = usePersistedState<FilterValues>(defaultValues, {
+        persistedStateId: options.persistedStateId ? `${options.persistedStateId}_filter` : undefined,
     });
-    const storedState = useStoredState<FilterValues>(filterId, defaultValues, options.browserStorage);
-
-    const [filters, setFilters] = options.browserStorage ? storedState : persistedState;
-
     const ref = React.useRef<FormApi<FilterValues>>();
     if (!ref.current) {
-        ref.current = createForm<FilterValues>({
+        ref.current = createForm({
             initialValues: filters,
             onSubmit: (values) => {
                 // Nothing todo
