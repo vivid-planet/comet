e9dedcae029bf355ae0dcc190fdda31abc6adb01
Thomas Dax <thomas.dax@vivid-planet.com>
2022-10-31 12:24:05 +0100

    feat(BlockIndex): add News to BlockIndex

--- demo/api/src/news/blocks/news-link.block.ts
+++ demo/api/src/news/blocks/news-link.block.ts
@@ -1,9 +1,19 @@
-import { BlockData, BlockField, BlockInput, createBlock, inputToData } from "@comet/blocks-api";
+import { BlockData, BlockField, BlockIndexDataArray, BlockInput, createBlock, inputToData } from "@comet/blocks-api";
+import { NEWS_BLOCK_INDEX_IDENTIFIER } from "@src/news/entities/news.entity";
 import { IsOptional, IsUUID } from "class-validator";
 
 class NewsLinkBlockData extends BlockData {
     @BlockField({ nullable: true })
     id?: string;
+
+    indexData(): BlockIndexDataArray {
+        return [
+            {
+                targetIdentifier: NEWS_BLOCK_INDEX_IDENTIFIER,
+                id: this.id,
+            },
+        ];
+    }
 }
 
 class NewsLinkBlockInput extends BlockInput {
