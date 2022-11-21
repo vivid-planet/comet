c71d0d265ec4689fe766be740f5daa88448e1968
Niko Sams <ns@vivid-planet.com>
2022-11-21 08:49:46 +0100

    Don't silently catch errors in onReceiveMessage callback

--- packages/admin/cms-admin/src/preview/site/iframebridge/useSitePreviewIFrameBridge.ts
+++ packages/admin/cms-admin/src/preview/site/iframebridge/useSitePreviewIFrameBridge.ts
@@ -1,19 +1,23 @@
 import * as React from "react";
 
-import { SitePreviewIFrameMessage } from "./SitePreviewIFrameMessage";
+import { SitePreviewIFrameMessage, SitePreviewIFrameMessageType } from "./SitePreviewIFrameMessage";
 
 export function useSitePreviewIFrameBridge(onReceiveMessage: (message: SitePreviewIFrameMessage) => void) {
     React.useEffect(() => {
         const handleMessage = (event: MessageEvent) => {
+            let message;
             try {
-                const message = JSON.parse(event.data);
-                // Check if message is an iframe message from us -> there are more messaging from e.g webpack,etc.
-                // eslint-disable-next-line no-prototype-builtins
-                if (message.hasOwnProperty("cometType")) {
-                    onReceiveMessage(message as SitePreviewIFrameMessage);
-                }
+                message = JSON.parse(event.data);
             } catch (e) {
                 // empty
+            }
+            // Check if message is an iframe message from us -> there are more messaging from e.g webpack,etc.
+            if (
+                message &&
+                Object.prototype.hasOwnProperty.call(message, "cometType") &&
+                (message.cometType == SitePreviewIFrameMessageType.OpenLink || message.cometType == SitePreviewIFrameMessageType.SitePreviewLocation)
+            ) {
+                onReceiveMessage(message as SitePreviewIFrameMessage);
             }
         };
 
