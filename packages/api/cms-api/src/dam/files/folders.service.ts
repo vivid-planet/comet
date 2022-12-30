31bc20f54e104e83cd50d7fae63472c8c2832d9d
Thomas Dax <thomas.dax@vivid-planet.com>
2022-12-30 13:00:31 +0100

    fix(FoldersService): check if a new folder parent is not a child of the folder before update

--- packages/api/cms-api/src/dam/files/folders.service.ts
+++ packages/api/cms-api/src/dam/files/folders.service.ts
@@ -142,6 +142,10 @@ export class FoldersService {
     }
 
     async updateByEntity(entity: Folder, { parentId, ...input }: UpdateFolderInput): Promise<Folder> {
+        if (!(await this.isValidParentForFolder(entity.id, parentId ?? null))) {
+            throw new Error("Cannot make a folder its own child.");
+        }
+
         const parentIsDirty = parentId !== undefined && entity.parent?.id !== parentId;
         const parent = parentId ? await this.findOneById(parentId) : null;
 
@@ -168,6 +172,17 @@ export class FoldersService {
     }
 
     async moveBatch(folderIds: string[], targetFolderId?: string): Promise<Folder[]> {
+        let isValidParentId = true;
+        for (const folderId of folderIds) {
+            if (!(await this.isValidParentForFolder(folderId, targetFolderId ?? null))) {
+                isValidParentId = false;
+            }
+        }
+
+        if (!isValidParentId) {
+            throw new Error("Cannot make a folder its own child.");
+        }
+
         const folders = [];
 
         for (const id of folderIds) {
@@ -191,6 +206,18 @@ export class FoldersService {
 
         const result = await this.foldersRepository.nativeDelete(id);
         return result === 1;
+    }
+
+    async isValidParentForFolder(folderId: string, parentId: string | null): Promise<boolean> {
+        console.log("checkIfParentIsValidForFolder");
+        const ancestors = await this.findAncestorsByParentId(parentId);
+        console.log("ancestors ", ancestors);
+        const ancestorIds = ancestors.map((ancestor) => ancestor.id);
+        console.log("ancestorIds ", ancestorIds);
+
+        console.log("!ancestorIds.includes(folderId) ", !ancestorIds.includes(folderId));
+
+        return !ancestorIds.includes(folderId);
     }
 
     async findAncestorsByParentId(parentId: string | null): Promise<Folder[]> {
