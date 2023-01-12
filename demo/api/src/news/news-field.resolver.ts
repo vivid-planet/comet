38f01bba00994bea1217ba8a0abd5d8bee62380b
Thomas Dax <thomas.dax@vivid-planet.com>
2023-01-12 10:19:31 +0100

    feat(BlockIndex): add field resolver to all resolvers with dependencies

--- demo/api/src/news/news-field.resolver.ts
+++ demo/api/src/news/news-field.resolver.ts
@@ -1,12 +1,25 @@
+import { BlockIndexDependency, BlockIndexService } from "@comet/cms-api";
 import { Parent, ResolveField, Resolver } from "@nestjs/graphql";
 
-import { News } from "./entities/news.entity";
+import { News, NEWS_BLOCK_INDEX_IDENTIFIER } from "./entities/news.entity";
 import { NewsComment } from "./entities/news-comment.entity";
 
 @Resolver(() => News)
 export class NewsFieldResolver {
+    constructor(private readonly blockIndexService: BlockIndexService) {}
+
     @ResolveField(() => [NewsComment])
     async comments(@Parent() news: News): Promise<NewsComment[]> {
         return news.comments.loadItems();
+    }
+
+    @ResolveField(() => [BlockIndexDependency])
+    async dependents(@Parent() news: News): Promise<BlockIndexDependency[]> {
+        return this.blockIndexService.getDependentsByTargetIdentifierAndTargetId(NEWS_BLOCK_INDEX_IDENTIFIER, news.id);
+    }
+
+    @ResolveField(() => [BlockIndexDependency])
+    async dependencies(@Parent() news: News): Promise<BlockIndexDependency[]> {
+        return this.blockIndexService.getDependenciesByRootIdentifierAndRootId(NEWS_BLOCK_INDEX_IDENTIFIER, news.id);
     }
 }
