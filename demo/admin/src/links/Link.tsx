b75828e0de36fd75502a1b1c0df6d6ec08cd81e3
Thomas Dax <thomas.dax@vivid-planet.com>
2023-01-24 15:40:55 +0100

    feat(Link): add resolveDependencyRoute to Link

--- demo/admin/src/links/Link.tsx
+++ demo/admin/src/links/Link.tsx
@@ -52,4 +52,7 @@ export const Link: DocumentInterface<Pick<GQLLink, "content">, GQLLinkInput> = {
     },
     menuIcon: LinkIcon,
     anchors: () => [],
+    resolveDependencyRoute: (input, { rootColumn, jsonPath }) => {
+        return LinkBlock.resolveDependencyRoute(LinkBlock.input2State(input["content"]), jsonPath.substring("root.".length));
+    },
 };
