82c40082f0238df63b4b9cdeca1e2ef522a1a0c0
Johannes Obermair <johannes.obermair@vivid-planet.com>
2023-02-14 12:43:31 +0100

    Fix root block scalars for multiple instances of the same block The `RootBlockDataScalar` and `RootBlockInputScalar` created a new GraphQL scalar type upon each call. This caused multiple scalars with the same name to appear in the GraphQL schema, which caused an error when generating the schema. To fix this, we add a cache to the scalar generation to only create a scalar for a block once.

--- packages/api/cms-api/src/blocks/rootBlocks/root-block-input.scalar.ts
+++ packages/api/cms-api/src/blocks/rootBlocks/root-block-input.scalar.ts
@@ -2,11 +2,23 @@ import { Block } from "@comet/blocks-api";
 import { GraphQLScalarType } from "graphql";
 import { GraphQLJSONObject } from "graphql-type-json";
 
+const rootBlockInputScalars = new Map<string, GraphQLScalarType>();
+
 export function RootBlockInputScalar(block: Block): GraphQLScalarType {
-    return new GraphQLScalarType({
+    let scalar = rootBlockInputScalars.get(block.name);
+
+    if (scalar !== undefined) {
+        return scalar;
+    }
+
+    scalar = new GraphQLScalarType({
         ...GraphQLJSONObject,
         specifiedByUrl: undefined,
         name: `${block.name}BlockInput`,
         description: `${block.name} root block input`,
     });
+
+    rootBlockInputScalars.set(block.name, scalar);
+
+    return scalar;
 }
