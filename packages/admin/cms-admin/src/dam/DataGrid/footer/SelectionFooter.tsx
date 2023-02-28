c373fd09385d2a590e47b7f678d7a91f45f58a22
Thomas Dax <thomas.dax@vivid-planet.com>
2023-02-28 12:53:00 +0100

    feat(SelectionFooter): change styling of the footer and readd archive and restore actions

--- packages/admin/cms-admin/src/dam/DataGrid/footer/SelectionFooter.tsx
+++ packages/admin/cms-admin/src/dam/DataGrid/footer/SelectionFooter.tsx
@@ -1,17 +1,20 @@
 import { gql, useApolloClient } from "@apollo/client";
-import { SaveButton } from "@comet/admin";
-import { Delete, Move } from "@comet/admin-icons";
-import { Button } from "@mui/material";
+import { Archive, Delete, Error, Move, Restore, ThreeDotSaving } from "@comet/admin-icons";
+import { IconButton, Tooltip, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { GraphQLError } from "graphql";
 import * as React from "react";
 import { FormattedMessage } from "react-intl";
 
 import {
+    GQLArchiveFilesMutation,
+    GQLArchiveFilesMutationVariables,
     GQLDeleteDamFileMutation,
     GQLDeleteDamFileMutationVariables,
     GQLDeleteDamFolderMutation,
     GQLDeleteDamFolderMutationVariables,
+    GQLRestoreFilesMutation,
+    GQLRestoreFilesMutationVariables,
     namedOperations,
 } from "../../../graphql.generated";
 import { ConfirmDeleteDialog } from "../../FileActions/ConfirmDeleteDialog";
@@ -37,6 +40,12 @@ export const DamSelectionFooter: React.VoidFunctionComponent<DamSelectionFooterP
     const [deleting, setDeleting] = React.useState(false);
     const [hasDeletionErrors, setHasDeletionErrors] = React.useState(false);
 
+    const [archiving, setArchiving] = React.useState<boolean>(false);
+    const [hasArchivingErrors, setHasArchivingErrors] = React.useState(false);
+
+    const [restoring, setRestoring] = React.useState<boolean>(false);
+    const [hasRestoringErrors, setHasRestoringErrors] = React.useState(false);
+
     const deleteSelected = async () => {
         if (selectedItemsMap === undefined) {
             return;
@@ -58,6 +67,7 @@ export const DamSelectionFooter: React.VoidFunctionComponent<DamSelectionFooterP
                         }
                     `,
                     variables: { id: selectedItem.id },
+                    errorPolicy: "all",
                 });
 
                 errors = result.errors;
@@ -69,6 +79,7 @@ export const DamSelectionFooter: React.VoidFunctionComponent<DamSelectionFooterP
                         }
                     `,
                     variables: { id: selectedItem.id },
+                    errorPolicy: "all",
                 });
 
                 errors = result.errors;
@@ -79,12 +90,95 @@ export const DamSelectionFooter: React.VoidFunctionComponent<DamSelectionFooterP
             setHasDeletionErrors(true);
             setTimeout(() => {
                 setHasDeletionErrors(false);
-            }, 2000);
+            }, 3000);
+        } else {
+            await apolloClient.refetchQueries({ include: [namedOperations.Query.DamItemsList] });
+            clearDamItemCache(apolloClient.cache);
         }
 
-        await apolloClient.refetchQueries({ include: [namedOperations.Query.DamItemsList] });
-        clearDamItemCache(apolloClient.cache);
         setDeleting(false);
+    };
+
+    const archiveSelected = async () => {
+        if (selectedItemsMap === undefined) {
+            return;
+        }
+
+        setArchiving(true);
+
+        const fileIds = Array.from(selectedItemsMap.entries())
+            .filter(([, type]) => type === "file")
+            .map(([id]) => {
+                return id;
+            });
+
+        const { errors } = await apolloClient.mutate<GQLArchiveFilesMutation, GQLArchiveFilesMutationVariables>({
+            mutation: gql`
+                mutation ArchiveFiles($ids: [ID!]!) {
+                    archiveDamFiles(ids: $ids) {
+                        id
+                        archived
+                    }
+                }
+            `,
+            variables: {
+                ids: fileIds,
+            },
+            errorPolicy: "all",
+        });
+
+        if (errors) {
+            setHasArchivingErrors(true);
+            setTimeout(() => {
+                setHasArchivingErrors(false);
+            }, 3000);
+        } else {
+            await apolloClient.refetchQueries({ include: [namedOperations.Query.DamItemsList] });
+            clearDamItemCache(apolloClient.cache);
+        }
+
+        setArchiving(false);
+    };
+
+    const restoreSelected = async () => {
+        if (selectedItemsMap === undefined) {
+            return;
+        }
+
+        setRestoring(true);
+
+        const fileIds = Array.from(selectedItemsMap.entries())
+            .filter(([, type]) => type === "file")
+            .map(([id]) => {
+                return id;
+            });
+
+        const { errors } = await apolloClient.mutate<GQLRestoreFilesMutation, GQLRestoreFilesMutationVariables>({
+            mutation: gql`
+                mutation RestoreFiles($ids: [ID!]!) {
+                    restoreDamFiles(ids: $ids) {
+                        id
+                        archived
+                    }
+                }
+            `,
+            variables: {
+                ids: fileIds,
+            },
+            errorPolicy: "all",
+        });
+
+        if (errors) {
+            setHasRestoringErrors(true);
+            setTimeout(() => {
+                setHasRestoringErrors(false);
+            }, 3000);
+        } else {
+            await apolloClient.refetchQueries({ include: [namedOperations.Query.DamItemsList] });
+            clearDamItemCache(apolloClient.cache);
+        }
+
+        setRestoring(false);
     };
 
     if (!open) {
@@ -94,43 +188,76 @@ export const DamSelectionFooter: React.VoidFunctionComponent<DamSelectionFooterP
     return (
         <>
             <DamFooter open={open}>
+                <Typography>
+                    <FormattedMessage
+                        id="comet.dam.footer.selected"
+                        defaultMessage="{count, plural, one {# item} other {# items}} selected"
+                        values={{
+                            count: selectedItemsMap?.size,
+                        }}
+                    />
+                </Typography>
                 <ButtonGroup>
-                    <SaveButton
-                        saving={deleting}
-                        hasErrors={hasDeletionErrors}
-                        color="secondary"
-                        onClick={() => {
-                            setDeleteDialogOpen(true);
-                        }}
-                        saveIcon={<Delete />}
-                        savingItem={<FormattedMessage id="comet.dam.footer.deleting" defaultMessage="Deleting" />}
-                        successItem={<FormattedMessage id="comet.dam.footer.successfullyDeleted" defaultMessage="Successfully deleted" />}
-                        errorItem={<FormattedMessage id="comet.dam.footer.errorWhileDeleting" defaultMessage="Error while deleting" />}
-                    >
-                        <FormattedMessage
-                            id="comet.dam.footer.delete"
-                            defaultMessage="Delete {num, plural, one {one item} other {# items}}"
-                            values={{
-                                num: selectedItemsMap?.size,
+                    <Tooltip title={<FormattedMessage id="comet.dam.footer.move" defaultMessage="Move" />}>
+                        <IconButton
+                            onClick={() => {
+                                onOpenMoveDialog();
                             }}
-                        />
-                    </SaveButton>
-                    <Button
-                        color="secondary"
-                        variant="contained"
-                        startIcon={<Move />}
-                        onClick={() => {
-                            onOpenMoveDialog();
-                        }}
-                    >
-                        <FormattedMessage
-                            id="comet.dam.footer.move"
-                            defaultMessage="Move {num, plural, one {one item} other {# items}}"
-                            values={{
-                                num: selectedItemsMap?.size,
+                            size="large"
+                            sx={{
+                                color: (theme) => theme.palette.grey.A100,
+                                paddingLeft: "4px",
+                                paddingRight: "4px",
                             }}
-                        />
-                    </Button>
+                        >
+                            <Move />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={<FormattedMessage id="comet.dam.footer.archive" defaultMessage="Archive" />}>
+                        <IconButton
+                            onClick={() => {
+                                archiveSelected();
+                            }}
+                            size="large"
+                            sx={{
+                                color: (theme) => theme.palette.grey.A100,
+                                paddingLeft: "4px",
+                                paddingRight: "4px",
+                            }}
+                        >
+                            {archiving || hasArchivingErrors ? archiving ? <ThreeDotSaving /> : <ErrorIcon /> : <Archive />}
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={<FormattedMessage id="comet.dam.footer.restore" defaultMessage="Restore" />}>
+                        <IconButton
+                            onClick={() => {
+                                restoreSelected();
+                            }}
+                            size="large"
+                            sx={{
+                                color: (theme) => theme.palette.grey.A100,
+                                paddingLeft: "4px",
+                                paddingRight: "4px",
+                            }}
+                        >
+                            {restoring || hasRestoringErrors ? restoring ? <ThreeDotSaving /> : <ErrorIcon /> : <Restore />}
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={<FormattedMessage id="comet.dam.footer.delete" defaultMessage="Delete" />}>
+                        <IconButton
+                            onClick={() => {
+                                setDeleteDialogOpen(true);
+                            }}
+                            size="large"
+                            sx={{
+                                color: (theme) => theme.palette.grey.A100,
+                                paddingLeft: "4px",
+                                paddingRight: "4px",
+                            }}
+                        >
+                            {deleting || hasDeletionErrors ? deleting ? <ThreeDotSaving /> : <ErrorIcon /> : <Delete />}
+                        </IconButton>
+                    </Tooltip>
                 </ButtonGroup>
             </DamFooter>
             <ConfirmDeleteDialog
@@ -146,3 +273,7 @@ export const DamSelectionFooter: React.VoidFunctionComponent<DamSelectionFooterP
         </>
     );
 };
+
+const ErrorIcon = styled(Error)`
+    color: ${({ theme }) => theme.palette.error.main};
+`;
