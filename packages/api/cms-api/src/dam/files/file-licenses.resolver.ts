dd2bebef808166e54f6eac77a1a8d594adbbd681
Thomas Dax <thomas.dax@vivid-planet.com>
2023-03-13 15:25:29 +0100

    refactor(FileLicensesResolver): simplify field resolvers

--- packages/api/cms-api/src/dam/files/file-licenses.resolver.ts
+++ packages/api/cms-api/src/dam/files/file-licenses.resolver.ts
@@ -1,16 +1,14 @@
 import { Parent, ResolveField, Resolver } from "@nestjs/graphql";
-import { add, compareAsc, getUnixTime } from "date-fns";
+import { add, differenceInCalendarDays, isAfter, isBefore } from "date-fns";
 
 import { License } from "./entities/license.embeddable";
-
-const thirtyDaysInSeconds = 60 * 60 * 24 * 30;
 
 @Resolver(() => License)
 export class FileLicensesResolver {
     // if durationTo = '2023-02-27T00:00:00.000Z' then the license is still valid on 27.03.2023
     // and expires at '2023-02-28T00:00:00.000Z'
     @ResolveField(() => Date, { nullable: true, description: "The expirationDate is the durationTo + 1 day" })
-    async expirationDate(@Parent() license: License): Promise<Date | undefined> {
+    expirationDate(@Parent() license: License): Date | undefined {
         if (license.durationTo) {
             return add(license.durationTo, {
                 days: 1,
@@ -20,50 +18,33 @@ export class FileLicensesResolver {
     }
 
     @ResolveField(() => Boolean)
-    async isNotValidYet(@Parent() license: License): Promise<boolean> {
+    isNotValidYet(@Parent() license: License): boolean {
         const currentDate = new Date();
 
-        if (license.durationFrom && compareAsc(currentDate, license.durationFrom) === -1) {
-            return true;
-        }
-        return false;
+        return license.durationFrom !== undefined && isBefore(currentDate, license.durationFrom);
     }
 
     @ResolveField(() => Boolean)
-    async expiresWithinThirtyDays(@Parent() license: License): Promise<boolean> {
+    expiresWithinThirtyDays(@Parent() license: License): boolean {
         const currentDate = new Date();
-        const expirationDate = await this.expirationDate(license);
-        const differenceBetweenExpirationDateAndNow = expirationDate ? getUnixTime(expirationDate) - getUnixTime(currentDate) : null;
+        const expirationDate = this.expirationDate(license);
 
-        if (
-            differenceBetweenExpirationDateAndNow !== null &&
-            differenceBetweenExpirationDateAndNow > 0 &&
-            differenceBetweenExpirationDateAndNow <= thirtyDaysInSeconds
-        ) {
-            return true;
-        }
-        return false;
+        return expirationDate !== undefined && isBefore(currentDate, expirationDate) && differenceInCalendarDays(expirationDate, currentDate) <= 30;
     }
 
     @ResolveField(() => Boolean)
-    async hasExpired(@Parent() license: License): Promise<boolean> {
+    hasExpired(@Parent() license: License): boolean {
         const currentDate = new Date();
-        const expirationDate = await this.expirationDate(license);
+        const expirationDate = this.expirationDate(license);
 
-        if (expirationDate && compareAsc(currentDate, expirationDate) === 1) {
-            return true;
-        }
-        return false;
+        return expirationDate !== undefined && isAfter(currentDate, expirationDate);
     }
 
     @ResolveField(() => Boolean)
-    async isValid(@Parent() license: License): Promise<boolean> {
-        const isNotValidYet = await this.isNotValidYet(license);
-        const hasExpired = await this.hasExpired(license);
+    isValid(@Parent() license: License): boolean {
+        const isNotValidYet = this.isNotValidYet(license);
+        const hasExpired = this.hasExpired(license);
 
-        if (isNotValidYet || hasExpired) {
-            return false;
-        }
-        return true;
+        return !(isNotValidYet || hasExpired);
     }
 }
