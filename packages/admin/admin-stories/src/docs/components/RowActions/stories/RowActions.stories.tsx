341d21cb7965160299b1068eb5322dbfbf32558e
Ricky Smith <jamesricky@me.com>
2023-03-17 09:33:28 +0100

    Remove complex & deeply nested example of RowActions
    Until we find a good solution for the behavior of nested sub-menus: https://github.com/vivid-planet/comet/pull/1018\#discussion_r1135314172

--- packages/admin/admin-stories/src/docs/components/RowActions/stories/RowActions.stories.tsx
+++ packages/admin/admin-stories/src/docs/components/RowActions/stories/RowActions.stories.tsx
@@ -1,21 +1,5 @@
 import { RowActionsItem, RowActionsMenu } from "@comet/admin";
-import {
-    Accept,
-    Add,
-    ChangeImage,
-    Copy,
-    Dashboard,
-    Delete,
-    Edit,
-    Favorite,
-    FileData,
-    Info,
-    LinkExternal,
-    Online,
-    Paste,
-    Settings,
-    Snips,
-} from "@comet/admin-icons";
+import { Copy, Delete, Edit, Online, Paste, Settings } from "@comet/admin-icons";
 import { Divider, Paper } from "@mui/material";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
@@ -53,45 +37,6 @@ storiesOf("stories/components/RowActions/RowActions", module)
                     </RowActionsMenu>
                     <Divider />
                     <RowActionsItem icon={<Delete />}>Delete</RowActionsItem>
-                </RowActionsMenu>
-            </RowActionsMenu>
-        );
-    })
-    .add("More Complex & Deeply Nested Example", () => {
-        return (
-            <RowActionsMenu>
-                <RowActionsItem icon={<Add />}>Level 0, item 1</RowActionsItem>
-                <RowActionsItem icon={<Accept />}>Level 0, item 2</RowActionsItem>
-                <RowActionsItem icon={<Info />}>Level 0, item 3</RowActionsItem>
-                <RowActionsMenu>
-                    <RowActionsItem icon={<FileData />}>Level 1, item 1</RowActionsItem>
-                    <RowActionsItem icon={<FileData />}>Level 1, item 2</RowActionsItem>
-                    <RowActionsItem textSecondary="Additional Text" icon={<FileData />} endIcon={<LinkExternal />}>
-                        Level 1, item 3
-                    </RowActionsItem>
-                    <Divider />
-                    <RowActionsMenu text="Submenu 1/1" textSecondary="Additional Text" icon={<Favorite />}>
-                        <RowActionsItem icon={<Favorite />}>Level 2, item 1</RowActionsItem>
-                        <RowActionsItem icon={<Favorite />}>Level 2, item 2</RowActionsItem>
-                        <RowActionsItem icon={<Favorite />}>Level 2, item 3</RowActionsItem>
-                        <Divider />
-                        <RowActionsMenu text="Submenu 2" icon={<Favorite />}>
-                            <RowActionsItem icon={<Favorite />}>Level 3, item 1</RowActionsItem>
-                            <RowActionsItem icon={<Favorite />}>Level 3, item 2</RowActionsItem>
-                            <RowActionsItem icon={<Favorite />}>Level 3, item 3</RowActionsItem>
-                        </RowActionsMenu>
-                    </RowActionsMenu>
-                    <RowActionsMenu text="Submenu 1/2" icon={<Dashboard />}>
-                        <RowActionsItem icon={<ChangeImage />}>Level 2, item 1</RowActionsItem>
-                        <RowActionsItem icon={<ChangeImage />}>Level 2, item 2</RowActionsItem>
-                        <RowActionsItem icon={<ChangeImage />}>Level 2, item 3</RowActionsItem>
-                        <Divider />
-                        <RowActionsMenu text="Submenu 2" icon={<Snips />}>
-                            <RowActionsItem icon={<ChangeImage />}>Level 3, item 1</RowActionsItem>
-                            <RowActionsItem icon={<ChangeImage />}>Level 3, item 2</RowActionsItem>
-                            <RowActionsItem icon={<ChangeImage />}>Level 3, item 3</RowActionsItem>
-                        </RowActionsMenu>
-                    </RowActionsMenu>
                 </RowActionsMenu>
             </RowActionsMenu>
         );
