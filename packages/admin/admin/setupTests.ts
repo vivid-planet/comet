3ea08a78d820b7a7200abb5e12d81113cd03ca8e
Niko Sams <ns@vivid-planet.com>
2023-04-01 10:28:34 +0200

    Setup tests for comet/admin

---
+++ packages/admin/admin/setupTests.ts
@@ -0,0 +1,6 @@
+import "@testing-library/jest-dom";
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+}));
