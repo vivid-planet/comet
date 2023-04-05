31485acc44cff2801c895cd518a9cab0674b4ba7
Niko Sams <ns@vivid-planet.com>
2023-04-05 17:38:34 +0200

    Add tests and stories showing the isses we still have

--- packages/admin/admin-stories/src/admin/form/RouterTabs.tsx
+++ packages/admin/admin-stories/src/admin/form/RouterTabs.tsx
@@ -23,77 +23,77 @@ function Story() {
         <Stack topLevelTitle="Root Stack">
             <RouterTabs>
                 <RouterTab label="Top 1" path="">
-                    <FinalForm
-                        mode="edit"
-                        onSubmit={(values: any) => {
-                            alert(JSON.stringify(values));
-                        }}
-                        initialValues={{
-                            foo: "foo",
-                            bar: "bar",
-                        }}
-                    >
-                        {({ pristine, dirty, hasValidationErrors, submitting, hasSubmitErrors, handleSubmit, ...formVars }) => {
-                            console.log({ pristine, hasValidationErrors, submitting, hasSubmitErrors, ...formVars });
+                    <RouterTabs>
+                        <RouterTab label="Form 1" path="" forceRender={true}>
+                            <Card variant="outlined">
+                                <CardContent>
+                                    <FinalForm
+                                        mode="edit"
+                                        onSubmit={(values: any) => {
+                                            alert(JSON.stringify(values));
+                                        }}
+                                        initialValues={{
+                                            foo: "foo",
+                                            bar: "bar",
+                                        }}
+                                    >
+                                        {({ pristine, dirty, hasValidationErrors, submitting, hasSubmitErrors, handleSubmit, ...formVars }) => {
+                                            console.log({ pristine, hasValidationErrors, submitting, hasSubmitErrors, ...formVars });
 
-                            return (
-                                <>
-                                    Pristine: {String(pristine)} <br />
-                                    Dirty: {String(dirty)}
-                                    <Toolbar>
-                                        <ToolbarBackButton />
-                                        <ToolbarFillSpace />
-                                        <ToolbarActions>
-                                            <SplitButton
-                                                disabled={pristine || hasValidationErrors || submitting}
-                                                localStorageKey="routertabs-with-forms-save"
-                                            >
-                                                <SaveButton
-                                                    color="primary"
-                                                    variant="contained"
-                                                    saving={submitting}
-                                                    hasErrors={hasSubmitErrors}
-                                                    type="button"
-                                                    onClick={async () => {
-                                                        handleSubmit();
-                                                    }}
-                                                >
-                                                    Save
-                                                </SaveButton>
-                                                <SaveButton
-                                                    color="primary"
-                                                    variant="contained"
-                                                    saving={submitting}
-                                                    hasErrors={hasSubmitErrors}
-                                                    onClick={async () => {
-                                                        handleSubmit();
-                                                    }}
-                                                >
-                                                    Save and go back
-                                                </SaveButton>
-                                            </SplitButton>
-                                        </ToolbarActions>
-                                    </Toolbar>
-                                    <RouterTabs>
-                                        <RouterTab label="Form 1" path="" forceRender={true}>
-                                            <Card variant="outlined">
-                                                <CardContent>
-                                                    <Field label="Foo" name="foo" component={FinalFormInput} />
-                                                </CardContent>
-                                            </Card>
-                                        </RouterTab>
-                                        <RouterTab label="Form 2" path="/form2" forceRender={true}>
-                                            <Card variant="outlined">
-                                                <CardContent>
-                                                    <Field label="Bar" name="bar" component={FinalFormInput} />
-                                                </CardContent>
-                                            </Card>
-                                        </RouterTab>
-                                    </RouterTabs>
-                                </>
-                            );
-                        }}
-                    </FinalForm>
+                                            return (
+                                                <>
+                                                    Pristine: {String(pristine)} <br />
+                                                    Dirty: {String(dirty)}
+                                                    <Toolbar>
+                                                        <ToolbarBackButton />
+                                                        <ToolbarFillSpace />
+                                                        <ToolbarActions>
+                                                            <SplitButton
+                                                                disabled={pristine || hasValidationErrors || submitting}
+                                                                localStorageKey="routertabs-with-forms-save"
+                                                            >
+                                                                <SaveButton
+                                                                    color="primary"
+                                                                    variant="contained"
+                                                                    saving={submitting}
+                                                                    hasErrors={hasSubmitErrors}
+                                                                    type="button"
+                                                                    onClick={async () => {
+                                                                        handleSubmit();
+                                                                    }}
+                                                                >
+                                                                    Save
+                                                                </SaveButton>
+                                                                <SaveButton
+                                                                    color="primary"
+                                                                    variant="contained"
+                                                                    saving={submitting}
+                                                                    hasErrors={hasSubmitErrors}
+                                                                    onClick={async () => {
+                                                                        handleSubmit();
+                                                                    }}
+                                                                >
+                                                                    Save and go back
+                                                                </SaveButton>
+                                                            </SplitButton>
+                                                        </ToolbarActions>
+                                                    </Toolbar>
+                                                    <Card variant="outlined">
+                                                        <Field label="Foo" name="foo" component={FinalFormInput} />
+                                                    </Card>
+                                                </>
+                                            );
+                                        }}
+                                    </FinalForm>
+                                </CardContent>
+                            </Card>
+                        </RouterTab>
+                        <RouterTab label="Outside Form" path="/outside-form" forceRender={true}>
+                            <Card variant="outlined">
+                                <CardContent>Outside Form</CardContent>
+                            </Card>
+                        </RouterTab>
+                    </RouterTabs>
                 </RouterTab>
                 <RouterTab label="Top 2" path="/top2">
                     <Card variant="outlined">
