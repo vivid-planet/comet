3d4a9827fb2a7ded2cd40701de08ce9ec6d1d0aa
Natalia <natalia.vizintini@vivid-planet.com>
2023-04-28 11:13:27 +0200

    added styling props using Theme, style overrides and default values for MuiAccordion

---
+++ packages/admin/admin-theme/src/componentsTheme/MuiAccordion.ts
@@ -0,0 +1,29 @@
+import { mergeOverrideStyles } from "../utils/mergeOverrideStyles";
+import { GetMuiComponentTheme } from "./getComponentsTheme";
+
+export const getMuiAccordion: GetMuiComponentTheme<"MuiAccordion"> = (component, { palette }) => ({
+    ...component,
+    defaultProps: {
+        square: true,
+        disableGutters: true,
+        elevation: 0,
+        ...component?.defaultProps,
+    },
+    styleOverrides: mergeOverrideStyles<"MuiAccordion">(component?.styleOverrides, {
+        root: {
+            border: `solid ${palette.divider}`,
+            "&:not(:last-child)": {
+                marginBottom: "20px",
+            },
+            "&:before": {
+                display: "none",
+            },
+            "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+                transform: "rotate(90deg)",
+            },
+            "& .MuiAccordionSummary-content": {
+                marginLeft: 10,
+            },
+        },
+    }),
+});
--- packages/admin/admin/src/common/Accordion.tsx
+++ packages/admin/admin/src/common/Accordion.tsx
@@ -1,15 +1,17 @@
 import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
-import MuiAccordion from "@mui/material/Accordion";
+import { Theme } from "@mui/material";
+import MuiAccordion, { AccordionProps as Props } from "@mui/material/Accordion";
 import MuiAccordionDetails, { AccordionDetailsProps } from "@mui/material/AccordionDetails";
 import MuiAccordionSummary, { AccordionSummaryProps } from "@mui/material/AccordionSummary";
-import { styled } from "@mui/material/styles";
+import { createStyles, WithStyles, withStyles } from "@mui/styles";
+import clsx from "clsx";
 import * as React from "react";
 
 interface AccordionComponentsProps {
     accordionSummary?: Partial<AccordionSummaryProps>;
     accordionDetails?: Partial<AccordionDetailsProps>;
 }
-interface AccordionProps {
+export interface AccordionProps extends Omit<Partial<Props>, "title"> {
     title: React.ReactNode;
     supportText?: React.ReactNode;
     endAdornment?: React.ReactNode;
@@ -17,7 +19,59 @@ interface AccordionProps {
     componentsProps?: AccordionComponentsProps;
 }
 
-export const Accordion: React.FC<AccordionProps> = ({ title, supportText, endAdornment, children, initialExpanded = false, componentsProps }) => {
+export type AccordionClassKey = "header" | "headerColumn" | "title" | "supportText" | "endAdornment" | "placeholder" | "children" | "isExpanded";
+
+const styles = (theme: Theme) =>
+    createStyles<AccordionClassKey, AccordionProps>({
+        header: {
+            display: "flex",
+            flexDirection: "row-reverse",
+            padding: "0 20px",
+            gap: "20px",
+            height: "86px",
+        },
+        headerColumn: {
+            flexDirection: "column",
+            padding: "20px",
+        },
+        title: {
+            fontWeight: theme.typography.fontWeightMedium,
+            fontSize: "14pt",
+            textTransform: "uppercase",
+            color: theme.palette.text.primary,
+            "&$isExpanded": {
+                color: theme.palette.primary.main,
+            },
+        },
+        supportText: {
+            fontSize: "10pt",
+            lineHeight: "18px",
+            color: theme.palette.text.secondary,
+        },
+        endAdornment: { alignItems: "center" },
+        placeholder: {
+            flexGrow: 1,
+            boxSizing: "inherit",
+            userSelect: "none",
+        },
+        children: {
+            display: "flex",
+            flexDirection: "column",
+            borderTop: `solid ${theme.palette.divider}`,
+            padding: "40px",
+        },
+        isExpanded: {},
+    });
+
+function Accordion({
+    title,
+    supportText,
+    endAdornment,
+    children,
+    initialExpanded = false,
+    componentsProps,
+    classes,
+}: AccordionProps & WithStyles<typeof styles>): React.ReactElement {
     const [expanded, setExpanded] = React.useState(initialExpanded);
 
     const handleChange = (event: React.SyntheticEvent, isExpanded: boolean) => {
@@ -25,74 +79,39 @@ export const Accordion: React.FC<AccordionProps> = ({ title, supportText, endAdo
     };
 
     return (
-        <AccordionComponent expanded={expanded} onChange={handleChange} disableGutters elevation={0} square>
-            <AccordionSummary {...componentsProps?.accordionSummary}>
-                <AccordionColumn>
-                    <AccordionTitleText expanded={expanded}>{title}</AccordionTitleText>
-                    <AccordionSupportText>{supportText}</AccordionSupportText>
-                </AccordionColumn>
-                <FillSpace />
-                <AccordionEndAdornment>{endAdornment}</AccordionEndAdornment>
-            </AccordionSummary>
-            <AccordionDetails {...componentsProps?.accordionDetails}>{children}</AccordionDetails>
-        </AccordionComponent>
+        <MuiAccordion expanded={expanded} onChange={handleChange}>
+            <MuiAccordionSummary classes={{ root: classes.header }} expandIcon={<ArrowForwardIosSharpIcon />} {...componentsProps?.accordionSummary}>
+                <div className={clsx(classes.headerColumn)}>
+                    <div className={clsx(classes.title, expanded && classes.isExpanded)}>{title}</div>
+                    <div className={clsx(classes.supportText)}>{supportText}</div>
+                </div>
+                <div className={clsx(classes.placeholder)} />
+                <div className={clsx(classes.endAdornment)}>{endAdornment}</div>
+            </MuiAccordionSummary>
+            <MuiAccordionDetails classes={{ root: classes.children }} {...componentsProps?.accordionDetails}>
+                {children}
+            </MuiAccordionDetails>
+        </MuiAccordion>
     );
-};
-
-const AccordionComponent = styled(MuiAccordion)(({ theme }) => ({
-    border: `1px solid ${theme.palette.divider}`,
-    "&:not(:last-child)": {
-        marginBottom: "20px",
-    },
-    "&:before": {
-        display: "none",
-    },
-}));
-
-const AccordionSummary = styled((props: AccordionSummaryProps) => <MuiAccordionSummary expandIcon={<ArrowForwardIosSharpIcon />} {...props} />)`
-    display: flex;
-    flex-direction: row-reverse;
-    padding: 0 20px;
-    gap: 20px;
-    height: 86px;
-`;
-
-const AccordionEndAdornment = styled(MuiAccordionSummary)`
-    align-items: center;
-`;
-
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-    display: "flex",
-    flexDirection: "column",
-    padding: "40px",
-    borderTop: `1px solid ${theme.palette.divider}`,
-}));
-
-const AccordionColumn = styled(MuiAccordionDetails)`
-    flex-direction: column;
-    padding: 10px;
-`;
-
-const FillSpace = styled("div")`
-    flex-grow: 1;
-    box-sizing: inherit;
-    user-select: none;
-`;
-
-interface AccordionTitleTextProps {
-    expanded: boolean;
 }
 
-const AccordionTitleText = styled("div")<AccordionTitleTextProps>`
-    font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
-    font-size: 16px;
-    line-height: 16px;
-    text-transform: uppercase;
-    color: ${({ expanded, theme }) => (expanded ? theme.palette.primary.main : theme.palette.text.primary)};
-`;
+const AccordionWithStyles = withStyles(styles, { name: "CometAdminAccordion" })(Accordion);
 
-const AccordionSupportText = styled("div")`
-    font-size: 12px;
-    line-height: 18px;
-    color: ${({ theme }) => theme.palette.text.secondary};
-`;
+export { AccordionWithStyles as Accordion };
+
+declare module "@mui/material/styles" {
+    interface ComponentsPropsList {
+        CometAdminAccordion: AccordionProps;
+    }
+
+    interface ComponentNameToClassKey {
+        CometAdminAccordion: AccordionClassKey;
+    }
+
+    interface Components {
+        CometAdminAccordion?: {
+            defaultProps?: ComponentsPropsList["CometAdminAccordion"];
+            styleOverrides?: ComponentNameToClassKey["CometAdminAccordion"];
+        };
+    }
+}
