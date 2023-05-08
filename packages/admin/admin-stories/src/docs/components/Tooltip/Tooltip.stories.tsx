85267dd22c6f19d50081fc75248930b111e256d8
Natalia <natalia.vizintini@vivid-planet.com>
2023-05-08 08:39:28 +0200

    Add another story for Tooltip variant

--- packages/admin/admin-stories/src/docs/components/Tooltip/Tooltip.stories.tsx
+++ packages/admin/admin-stories/src/docs/components/Tooltip/Tooltip.stories.tsx
@@ -6,53 +6,29 @@ import * as React from "react";
 
 storiesOf("stories/components/Tooltip", module).add("BasicTooltip", () => {
     return (
-        <>
-            <Grid container justifyContent="center" spacing={4}>
-                <Grid item>
-                    <Tooltip title="This is a basic tooltip">
-                        <div>Hover over me</div>
-                    </Tooltip>
-                </Grid>
-                <Grid item>
-                    <Tooltip title="This is a focus or touch tooltip" trigger="focus">
-                        <div>Focus or touch</div>
-                    </Tooltip>
-                </Grid>
-                <Grid item>
-                    <Tooltip trigger="click" title="This is a clickable tooltip">
-                        <div>Click here to show tooltip</div>
-                    </Tooltip>
-                </Grid>
-                <Grid item>
-                    <Tooltip trigger="click" title="This is a clickable tooltip with an info icon">
-                        <IconButton>
-                            <Info />
-                        </IconButton>
-                    </Tooltip>
-                </Grid>
+        <Grid container justifyContent="center" spacing={4}>
+            <Grid item>
+                <Tooltip title="This is a basic tooltip">
+                    <div>Hover over me</div>
+                </Tooltip>
             </Grid>
-            <Grid container justifyContent="center" spacing={4}>
-                <Grid item>
-                    <Tooltip title="This is a light tooltip" variant="light">
-                        <div>Hover over me - light</div>
-                    </Tooltip>
-                </Grid>
-                <Grid item>
-                    <Tooltip title="This is a dark tooltip" variant="dark">
-                        <div>Hover over me - dark</div>
-                    </Tooltip>
-                </Grid>
-                <Grid item>
-                    <Tooltip title="This is a neutral tooltip" variant="neutral">
-                        <div>Hover over me - neutral</div>
-                    </Tooltip>
-                </Grid>
-                <Grid item>
-                    <Tooltip title="This is a primary tooltip" variant="primary">
-                        <div>Hover over me - primary</div>
-                    </Tooltip>
-                </Grid>
+            <Grid item>
+                <Tooltip title="This is a focus or touch tooltip" trigger="focus">
+                    <div>Focus or touch</div>
+                </Tooltip>
             </Grid>
-        </>
+            <Grid item>
+                <Tooltip trigger="click" title="This is a clickable tooltip">
+                    <div>Click here to show tooltip</div>
+                </Tooltip>
+            </Grid>
+            <Grid item>
+                <Tooltip trigger="click" title="This is a clickable tooltip with an info icon">
+                    <IconButton>
+                        <Info />
+                    </IconButton>
+                </Tooltip>
+            </Grid>
+        </Grid>
     );
 });
