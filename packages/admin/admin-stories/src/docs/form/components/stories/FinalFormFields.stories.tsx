fff7ad414419d3a0bfb5ead16a05d76dbe09638d
Natalia <natalia.vizintini@vivid-planet.com>
2023-05-16 12:55:52 +0200

    Use isOptionEqualToValue

--- packages/admin/admin-stories/src/docs/form/components/stories/FinalFormFields.stories.tsx
+++ packages/admin/admin-stories/src/docs/form/components/stories/FinalFormFields.stories.tsx
@@ -77,9 +77,8 @@ storiesOf("stories/form/FinalForm Fields", module)
             () => ({
                 autocomplete: { value: "strawberry", label: "Strawberry" },
                 autocompleteAsync: { value: "strawberry", label: "Strawberry" },
-                autocompleteMultiple: [options[options.findIndex((option) => option.value == "strawberry")]],
+                autocompleteMultiple: [{ value: "strawberry", label: "Strawberry" }],
             }),
-            // eslint-disable-next-line react-hooks/exhaustive-deps
             [],
         );
 
@@ -98,9 +97,7 @@ storiesOf("stories/form/FinalForm Fields", module)
                 <Field
                     component={FinalFormAutocomplete}
                     getOptionLabel={(option: Option) => option.label}
-                    getOptionSelected={(option: Option, value: Option) => {
-                        return option.value === value.value;
-                    }}
+                    isOptionEqualToValue={(option: Option, value: Option) => option.value === value.value}
                     options={options}
                     name="autocomplete"
                     label="Autocomplete"
@@ -110,9 +107,7 @@ storiesOf("stories/form/FinalForm Fields", module)
                     component={FinalFormAutocomplete}
                     {...acAsyncProps}
                     getOptionLabel={(option: Option) => option.label}
-                    getOptionSelected={(option: Option, value: Option) => {
-                        return option.value === value.value;
-                    }}
+                    isOptionEqualToValue={(option: Option, value: Option) => option.value === value.value}
                     name="autocompleteAsync"
                     label="AutocompleteAsync"
                     fullWidth
@@ -121,9 +116,7 @@ storiesOf("stories/form/FinalForm Fields", module)
                     component={FinalFormAutocomplete}
                     multiple
                     getOptionLabel={(option: Option) => option.label}
-                    getOptionSelected={(option: Option, value: Option) => {
-                        return option.value === value.value;
-                    }}
+                    isOptionEqualToValue={(option: Option, value: Option) => option.value === value.value}
                     options={options}
                     name="autocompleteMultiple"
                     label="Autocomplete multiple select"
