491dc2c85343a8b3ab3ebdfbcfb4576d0ea0db6d
Magdalena Maislinger <magdalena.maislinger@vivid-planet.com>
2023-06-07 15:51:18 +0200

    Remove extractTextContents and replaceContents from FullWidthImage

--- demo/admin/src/pages/blocks/FullWidthImageBlock.tsx
+++ demo/admin/src/pages/blocks/FullWidthImageBlock.tsx
@@ -9,31 +9,19 @@ const FullWidthImageContentBlock = createOptionalBlock(RichTextBlock, {
     title: <FormattedMessage {...messages.content} />,
 });
 
-export const FullWidthImageBlock = createCompositeBlock(
-    {
-        name: "FullWidthImage",
-        displayName: <FormattedMessage id="blocks.fullWidthImage" defaultMessage="Full Width Image" />,
-        category: BlockCategory.Media,
-        blocks: {
-            image: {
-                block: DamImageBlock,
-                title: <FormattedMessage {...messages.image} />,
-                paper: true,
-            },
-            content: {
-                block: FullWidthImageContentBlock,
-                title: <FormattedMessage {...messages.content} />,
-            },
+export const FullWidthImageBlock = createCompositeBlock({
+    name: "FullWidthImage",
+    displayName: <FormattedMessage id="blocks.fullWidthImage" defaultMessage="Full Width Image" />,
+    category: BlockCategory.Media,
+    blocks: {
+        image: {
+            block: DamImageBlock,
+            title: <FormattedMessage {...messages.image} />,
+            paper: true,
+        },
+        content: {
+            block: FullWidthImageContentBlock,
+            title: <FormattedMessage {...messages.content} />,
         },
     },
-    (block) => ({
-        ...block,
-        extractTextContents: (block) => {
-            return FullWidthImageContentBlock.extractTextContents?.(block.content) ?? [];
-        },
-        replaceTextContents: (state, contents) => ({
-            ...state,
-            content: FullWidthImageContentBlock.replaceTextContents?.(state.content, contents) ?? state.content,
-        }),
-    }),
-);
+});
