8237183544d95e47a38b3376cac31381c7bcf149
Thomas Dax <thomas.dax@vivid-planet.com>
2023-06-13 10:35:22 +0200

    remove(ConditionalWrapper): remove the ConditionalWrapper again

--- packages/admin/cms-admin/src/common/ConditionalWrapper.tsx
+++
@@ -1,9 +0,0 @@
-type ConditionalWrapperProps = {
-    condition: boolean;
-    wrapper: (children: JSX.Element) => JSX.Element;
-    children: JSX.Element;
-};
-
-export const ConditionalWrapper = ({ condition, wrapper, children }: ConditionalWrapperProps) => {
-    return condition ? wrapper(children) : children;
-};
