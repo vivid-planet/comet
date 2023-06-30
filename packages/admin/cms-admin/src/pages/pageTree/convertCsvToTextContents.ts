eccdb6e8c6955d9cdf810d5be1ea05bbd6297b90
Magdalena Maislinger <magdalena.maislinger@vivid-planet.com>
2023-06-30 09:54:04 +0200

    Add encoding for xml when importing csv

--- packages/admin/cms-admin/src/pages/pageTree/convertCsvToTextContents.ts
+++ packages/admin/cms-admin/src/pages/pageTree/convertCsvToTextContents.ts
@@ -5,9 +5,16 @@ const convertCsvToTextContents = (csv: string) => {
     const parsedData = parse(csv).data as string[][];
     const records = parsedData.slice(1, parsedData.length);
 
-    records.forEach((record) => (content[record[0]] = record[1]));
+    records.forEach((record) => (content[encodeContent(record[0])] = encodeContent(record[1])));
 
     return { textContents: content };
 };
 
 export { convertCsvToTextContents };
+
+/* 
+    parses HTML/XML entities in text content into special characters
+*/
+function encodeContent(text: string): string {
+    return text.split("&amp;").join("&").split("&lt;").join("<").split("&gt;").join(">").split("&nbsp;").join(" ").split("<br>\n").join(`\n`);
+}
