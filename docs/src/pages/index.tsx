6077db42d21f8cae3a80a4ef93e148e60f95af3f
Thomas Dax <thomas.dax@vivid-planet.com>
2023-07-11 11:12:50 +0200

    chore(docs): add eslint

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Redirect } from "@docusaurus/router";
+import React from "react";
 
 export default function Home(): JSX.Element {
     return <Redirect to="/docs/overview/" />;
