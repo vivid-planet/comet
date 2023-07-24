59ef0c5c6e276b3530d5423b5e40dd64caf51950
Johannes <johannes.munker@vivid-planet.com>
2023-07-24 10:50:45 +0200

    feat(DamThumbnail): add preview for images on thumbnail hover

--- packages/admin/cms-admin/src/dam/DataGrid/thumbnail/DamThumbnail.tsx
+++ packages/admin/cms-admin/src/dam/DataGrid/thumbnail/DamThumbnail.tsx
@@ -1,4 +1,5 @@
 import { File, Folder, Pdf } from "@comet/admin-icons";
+import { Fade, Popper } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import * as React from "react";
 
@@ -23,6 +24,15 @@ const ImageThumbnail = styled("img")`
     border-radius: inherit;
 `;
 
+const ImagePreview = styled("img")`
+    max-width: 300px;
+    max-height: 300px;
+    padding: 0.4rem;
+    margin-left: 0.25rem;
+    background-color: ${({ theme }) => theme.palette.background.paper};
+    box-shadow: ${({ theme }) => theme.shadows[1]};
+`;
+
 const ColoredFile = styled(File)`
     color: ${({ theme }) => theme.palette.primary.main};
 `;
@@ -35,10 +45,40 @@ interface DamThumbnailProps {
 }
 
 export const DamThumbnail = ({ asset }: DamThumbnailProps): React.ReactElement => {
+    const [open, setOpen] = React.useState(false);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleMouseOver = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+        setOpen(true);
+    };
+    const handleMouseLeave = () => {
+        setAnchorEl(null);
+        setOpen(false);
+    };
+
     let thumbnail;
     if (asset.__typename === "DamFile") {
         if (asset.mimetype.startsWith("image/") && asset.image && asset.image.thumbnailUrl) {
-            thumbnail = <ImageThumbnail src={asset.image.thumbnailUrl} />;
+            const canBeOpen = open && Boolean(anchorEl);
+            const id = canBeOpen ? asset.fileUrl : undefined;
+            thumbnail = (
+                <>
+                    <ImageThumbnail
+                        aria-describedby={id}
+                        onMouseOver={handleMouseOver}
+                        onMouseLeave={handleMouseLeave}
+                        src={asset.image.thumbnailUrl}
+                    />
+                    <Popper id={id} open={open} anchorEl={anchorEl} placement="auto-end" onResize={undefined} onResizeCapture={undefined} transition>
+                        {({ TransitionProps }) => (
+                            <Fade {...TransitionProps} timeout={350}>
+                                <ImagePreview src={asset.fileUrl || undefined} id="preview" />
+                            </Fade>
+                        )}
+                    </Popper>
+                </>
+            );
         } else if (asset.mimetype.startsWith("audio/")) {
             thumbnail = <AudioThumbnail />;
         } else if (asset.mimetype.startsWith("video/")) {
