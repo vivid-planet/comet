b96621baec3679ad2197a731256503a7723d990f
Thomas Dax <thomas.dax@vivid-planet.com>
2023-07-27 17:20:39 +0200

    move(Story): move MuiThemeProvider from individual story in the Story component

--- docs/src/components/Story.tsx
+++ docs/src/components/Story.tsx
@@ -1,21 +1,27 @@
+import { MuiThemeProvider } from "@comet/admin";
+import { createCometTheme } from "@comet/admin-theme";
 import CodeBlock from "@theme/CodeBlock";
 import type { Props as PlaygroundProps } from "@theme/Playground";
 import React, { PropsWithChildren } from "react";
 import { transform } from "sucrase";
 
 export const Story = ({ children, ...props }: PropsWithChildren<PlaygroundProps>) => {
+    const theme = createCometTheme();
+
     return (
-        <CodeBlock
-            language="tsx"
-            live
-            transformCode={(code) => {
-                const compiledCode = transform(code, { transforms: ["typescript", "jsx"], jsxRuntime: "preserve" }).code;
-                const codeWithoutImports = compiledCode.replace(/import.*\n/g, "");
-                return codeWithoutImports;
-            }}
-            {...props}
-        >
-            {children}
-        </CodeBlock>
+        <MuiThemeProvider theme={theme}>
+            <CodeBlock
+                language="tsx"
+                live
+                transformCode={(code) => {
+                    const compiledCode = transform(code, { transforms: ["typescript", "jsx"], jsxRuntime: "preserve" }).code;
+                    const codeWithoutImports = compiledCode.replace(/import.*\n/g, "");
+                    return codeWithoutImports;
+                }}
+                {...props}
+            >
+                {children}
+            </CodeBlock>
+        </MuiThemeProvider>
     );
 };
