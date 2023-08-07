a299536862fa6ab96e7f0242e8344aa406438421
Thomas Dax <thomas.dax@vivid-planet.com>
2023-08-07 14:04:07 +0200

    docs(ReactLiveScope): add method to check for duplicates in imported packages

--- docs/src/theme/ReactLiveScope/index.js
+++ docs/src/theme/ReactLiveScope/index.js
@@ -7,24 +7,86 @@ let ReactLiveScope = {
     ...React,
 };
 
+// const findDuplicateKeys = (objs, names) => {
+//     const map = new Map();
+//
+//     for (let i = 0; i < objs.length; i++) {
+//         const objName = names[i];
+//         const obj = objs[i];
+//
+//         for (const key of Object.keys(obj)) {
+//             if (map.has(key)) {
+//                 map.set(key, [...map.get(key), objName]);
+//             } else {
+//                 map.set(key, [objName]);
+//             }
+//         }
+//     }
+//
+//     for (let [key, value] of map) {
+//         if (value.length > 1) {
+//             console.log(key);
+//             console.log(value);
+//             console.log("---------------\n");
+//         }
+//     }
+// };
+
 if (ExecutionEnvironment.canUseDOM) {
-    const { Field, FieldContainer, FinalFormCheckbox, MuiThemeProvider } = require("@comet/admin");
+    const cometAdminImports = require("@comet/admin");
+    const cometAdminIconsImports = require("@comet/admin-icons");
+    const cometAdminDateTimeImports = require("@comet/admin-date-time");
+    const cometAdminRteImports = require("@comet/admin-rte");
+    const cometAdminColorPickerImports = require("@comet/admin-color-picker");
+    const cometAdminReactSelectImports = require("@comet/admin-react-select");
     const { createCometTheme } = require("@comet/admin-theme");
     const { Card, CardContent, FormControlLabel, Grid } = require("@mui/material");
     const { Form } = require("react-final-form");
 
+    // const diverse = {
+    //     Card,
+    //     CardContent,
+    //     FormControlLabel,
+    //     Grid,
+    //     Form,
+    //     createCometTheme,
+    // };
+    //
+    // findDuplicateKeys(
+    //     [
+    //         ReactLiveScope,
+    //         cometAdminIconsImports,
+    //         cometAdminImports,
+    //         cometAdminDateTimeImports,
+    //         cometAdminRteImports,
+    //         cometAdminColorPickerImports,
+    //         cometAdminReactSelectImports,
+    //         diverse,
+    //     ],
+    //     [
+    //         "ReactLiveScope",
+    //         "cometAdminIconsImports",
+    //         "cometAdminImports",
+    //         "cometAdminDateTimeImports",
+    //         "cometAdminRteImports",
+    //         "cometAdminColorPickerImports",
+    //         "cometAdminReactSelectImports",
+    //         "mui",
+    //     ],
+    // );
+
     ReactLiveScope = {
         ...ReactLiveScope,
-        Field,
-        FieldContainer,
-        FinalFormCheckbox,
         Card,
         CardContent,
         FormControlLabel,
         Grid,
         Form,
         createCometTheme,
-        MuiThemeProvider,
+        ...cometAdminImports,
+        ...cometAdminDateTimeImports,
+        ...cometAdminColorPickerImports,
+        ...cometAdminReactSelectImports,
     };
 }
 
