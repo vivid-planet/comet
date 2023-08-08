6b3f6faf657595a6fc5df09a6b8a522c5d3112a0
Thomas Dax <thomas.dax@vivid-planet.com>
2023-08-08 12:35:54 +0200

    fix(Story): fix MuiThemeProvider to fix build of docs

--- docs/src/components/Story.tsx
+++ docs/src/components/Story.tsx
@@ -1,5 +1,5 @@
-import { MuiThemeProvider } from "@comet/admin";
 import { createCometTheme } from "@comet/admin-theme";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import CodeBlock from "@theme/CodeBlock";
 import type { Props as PlaygroundProps } from "@theme/Playground";
 import React, { PropsWithChildren } from "react";
@@ -9,19 +9,32 @@ export const Story = ({ children, ...props }: PropsWithChildren<PlaygroundProps>
     const theme = createCometTheme();
 
     return (
-        <MuiThemeProvider theme={theme}>
-            <CodeBlock
-                language="tsx"
-                live
-                transformCode={(code) => {
-                    const compiledCode = transform(code, { transforms: ["typescript", "jsx"], jsxRuntime: "preserve" }).code;
-                    const codeWithoutImports = compiledCode.replace(/import.*\n/g, "");
-                    return codeWithoutImports;
-                }}
-                {...props}
-            >
-                {children}
-            </CodeBlock>
-        </MuiThemeProvider>
+        <BrowserOnly>
+            {() => {
+                // https://docusaurus.io/docs/docusaurus-core/#browseronly
+                // eslint-disable-next-line @typescript-eslint/no-var-requires
+                const { MuiThemeProvider } = require("@comet/admin");
+
+                return (
+                    <MuiThemeProvider theme={theme}>
+                        <CodeBlock
+                            language="tsx"
+                            live
+                            transformCode={(code) => {
+                                const compiledCode = transform(code, {
+                                    transforms: ["typescript", "jsx"],
+                                    jsxRuntime: "preserve",
+                                }).code;
+                                const codeWithoutImports = compiledCode.replace(/import.*\n/g, "");
+                                return codeWithoutImports;
+                            }}
+                            {...props}
+                        >
+                            {children}
+                        </CodeBlock>
+                    </MuiThemeProvider>
+                );
+            }}
+        </BrowserOnly>
     );
 };
