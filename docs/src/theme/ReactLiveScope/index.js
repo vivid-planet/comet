0823f4d1f96e445448d68f3955ded1e43ed3cb0c
Thomas Dax <thomas.dax@vivid-planet.com>
2023-08-09 17:40:39 +0200

    feat(ReactLiveScope): resolve duplicates and build automatic duplicate check into scope generation

--- docs/src/theme/ReactLiveScope/index.js
+++ docs/src/theme/ReactLiveScope/index.js
@@ -7,87 +7,92 @@ let ReactLiveScope = {
     ...React,
 };
 
-// const findDuplicateKeys = (objs, names) => {
-//     const map = new Map();
-//
-//     for (let i = 0; i < objs.length; i++) {
-//         const objName = names[i];
-//         const obj = objs[i];
-//
-//         for (const key of Object.keys(obj)) {
-//             if (map.has(key)) {
-//                 map.set(key, [...map.get(key), objName]);
-//             } else {
-//                 map.set(key, [objName]);
-//             }
-//         }
-//     }
-//
-//     for (let [key, value] of map) {
-//         if (value.length > 1) {
-//             console.log(key);
-//             console.log(value);
-//             console.log("---------------\n");
-//         }
-//     }
-// };
+const generateScope = (imports) => {
+    findDuplicateImports(imports);
+
+    let scope = {};
+    for (const imp of imports) {
+        scope = { ...scope, ...imp.imports };
+    }
+    return scope;
+};
+
+const findDuplicateImports = (imports) => {
+    const map = new Map();
+
+    for (const imp of imports) {
+        const name = imp.name;
+        const imps = imp.imports;
+
+        for (const key of Object.keys(imps)) {
+            if (map.has(key)) {
+                map.set(key, [...map.get(key), name]);
+            } else {
+                map.set(key, [name]);
+            }
+        }
+    }
+
+    for (let [key, value] of map) {
+        if (value.length > 1) {
+            console.warn(`Found duplicate: ${key} \n in following packages: ${value.join(", ")}`);
+        }
+    }
+};
 
 if (ExecutionEnvironment.canUseDOM) {
     const cometAdminImports = require("@comet/admin");
-    const cometAdminIconsImports = require("@comet/admin-icons");
+    const { Visible: VisibleIcon, Grid: GridIcon, ...cometAdminIconsImports } = require("@comet/admin-icons");
     const cometAdminDateTimeImports = require("@comet/admin-date-time");
-    const cometAdminRteImports = require("@comet/admin-rte");
+    const { Toolbar, ...cometAdminRteImports } = require("@comet/admin-rte");
     const cometAdminColorPickerImports = require("@comet/admin-color-picker");
     const cometAdminReactSelectImports = require("@comet/admin-react-select");
-    const { createCometTheme } = require("@comet/admin-theme");
+    const cometAdminThemeImports = require("@comet/admin-theme");
     const { Card, CardContent, FormControlLabel, Grid } = require("@mui/material");
     const { Form } = require("react-final-form");
 
-    // const diverse = {
-    //     Card,
-    //     CardContent,
-    //     FormControlLabel,
-    //     Grid,
-    //     Form,
-    //     createCometTheme,
-    // };
-    //
-    // findDuplicateKeys(
-    //     [
-    //         ReactLiveScope,
-    //         cometAdminIconsImports,
-    //         cometAdminImports,
-    //         cometAdminDateTimeImports,
-    //         cometAdminRteImports,
-    //         cometAdminColorPickerImports,
-    //         cometAdminReactSelectImports,
-    //         diverse,
-    //     ],
-    //     [
-    //         "ReactLiveScope",
-    //         "cometAdminIconsImports",
-    //         "cometAdminImports",
-    //         "cometAdminDateTimeImports",
-    //         "cometAdminRteImports",
-    //         "cometAdminColorPickerImports",
-    //         "cometAdminReactSelectImports",
-    //         "mui",
-    //     ],
-    // );
-
-    ReactLiveScope = {
-        ...ReactLiveScope,
-        Card,
-        CardContent,
-        FormControlLabel,
-        Grid,
-        Form,
-        createCometTheme,
-        ...cometAdminImports,
-        ...cometAdminDateTimeImports,
-        ...cometAdminColorPickerImports,
-        ...cometAdminReactSelectImports,
-    };
+    ReactLiveScope = generateScope([
+        {
+            name: "ReactLiveScope",
+            imports: ReactLiveScope,
+        },
+        {
+            name: "@comet/admin",
+            imports: cometAdminImports,
+        },
+        {
+            name: "@comet/admin-icons",
+            imports: { VisibleIcon, GridIcon, ...cometAdminIconsImports },
+        },
+        {
+            name: "@comet/admin-date-time",
+            imports: cometAdminDateTimeImports,
+        },
+        {
+            name: "@comet/admin-rte",
+            imports: cometAdminRteImports,
+        },
+        {
+            name: "@comet/admin-color-picker",
+            imports: cometAdminColorPickerImports,
+        },
+        {
+            name: "@comet/admin-react-select",
+            imports: cometAdminReactSelectImports,
+        },
+        {
+            name: "@comet/admin-theme",
+            imports: cometAdminThemeImports,
+        },
+        {
+            name: "@mui/material",
+            imports: { Card, CardContent, FormControlLabel, Grid },
+        },
+        {
+            name: "react-final-form",
+            imports: { Form },
+        },
+    ]);
 }
 
 export default ReactLiveScope;
