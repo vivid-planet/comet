9f11ac1d4544fe22a4491b47393318b1c5963215
Niko Sams <ns@vivid-planet.com>
2023-09-05 17:37:20 +0200

    api generator: support number fields (additional to DecimalType) in filter and sort generation

--- packages/api/cms-api/src/generator/generate-crud.spec.ts
+++ packages/api/cms-api/src/generator/generate-crud.spec.ts
@@ -14,6 +14,15 @@ export class TestEntityWithString extends BaseEntity<TestEntityWithString, "id">
     title: string;
 }
 
+@Entity()
+export class TestEntityWithNumber extends BaseEntity<TestEntityWithNumber, "id"> {
+    @PrimaryKey({ type: "uuid" })
+    id: string = uuid();
+
+    @Property()
+    foo: number;
+}
+
 describe("GenerateCrud", () => {
     describe("resolver class", () => {
         it("should be a valid generated ts file", async () => {
@@ -41,6 +50,40 @@ describe("GenerateCrud", () => {
 
             expect(structure.properties?.length).toBe(0);
             expect(structure.methods?.length).toBe(5);
+
+            orm.close();
+        });
+    });
+
+    describe("number filter", () => {
+        it("should be a valid generated ts file", async () => {
+            LazyMetadataStorage.load();
+            const orm = await MikroORM.init({
+                type: "postgresql",
+                dbName: "test-db",
+                entities: [TestEntityWithNumber],
+            });
+
+            const out = await generateCrud({ targetDirectory: __dirname }, orm.em.getMetadata().get("TestEntityWithNumber"));
+            const lintedOut = await lintGeneratedFiles(out);
+
+            const file = lintedOut.find((file) => file.name === "dto/test-entity-with-number.filter.ts");
+            if (!file) throw new Error("File not found");
+
+            const source = parseSource(file.content);
+
+            const classes = source.getClasses();
+            expect(classes.length).toBe(1);
+
+            const cls = classes[0];
+            expect(cls.getName()).toBe("TestEntityWithNumberFilter");
+            const structure = cls.getStructure();
+
+            expect(structure.properties?.length).toBe(3);
+            if (!structure.properties || !structure.properties[0]) throw new Error("property not found");
+            const filterProp = structure.properties[0];
+            expect(filterProp.name).toBe("foo");
+            expect(filterProp.type).toBe("NumberFilter");
 
             orm.close();
         });
