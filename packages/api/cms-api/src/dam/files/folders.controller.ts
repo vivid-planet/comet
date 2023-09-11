38f3aed79970c4e508a214ec80c7d500e96f0b15
Johannes <johannes.munker@vivid-planet.com>
2023-09-11 19:56:59 +0200

    refactor(cms-api): throw not found error instead of using fallback folder name

--- packages/api/cms-api/src/dam/files/folders.controller.ts
+++ packages/api/cms-api/src/dam/files/folders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Res } from "@nestjs/common";
+import { Controller, Get, NotFoundException, Param, Res } from "@nestjs/common";
 import { Response } from "express";
 
 import { FoldersService } from "./folders.service";
@@ -10,10 +10,12 @@ export class FoldersController {
     @Get("/:folderId/zip")
     async createZip(@Param("folderId") folderId: string, @Res() res: Response): Promise<void> {
         const folder = await this.foldersService.findOneById(folderId);
-        const folderName = folder?.name ? folder.name : "folder";
+        if (!folder) {
+            throw new NotFoundException("Folder not found");
+        }
         const zipStream = await this.foldersService.createZipStreamFromFolder(folderId);
 
-        res.setHeader("Content-Disposition", `attachment; filename="${folderName || "folder"}.zip"`);
+        res.setHeader("Content-Disposition", `attachment; filename="${folder.name}.zip"`);
         res.setHeader("Content-Type", "application/zip");
         zipStream.pipe(res);
     }
