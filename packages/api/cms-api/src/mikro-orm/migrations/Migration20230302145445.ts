7d6aa4426c2e224faec992d7a5e42725128c4157
Tobias Kuhn <tobias.kuhn@vivid-planet.com>
2023-09-12 10:58:10 +0200

    Migration fix: alter table PageTreeNode only if exists

--- packages/api/cms-api/src/mikro-orm/migrations/Migration20230302145445.ts
+++ packages/api/cms-api/src/mikro-orm/migrations/Migration20230302145445.ts
@@ -2,12 +2,20 @@ import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20230302145445 extends Migration {
     async up(): Promise<void> {
-        this.addSql('alter table "PageTreeNode" add column "updatedAt" timestamp with time zone;');
-        this.addSql('update "PageTreeNode" SET "updatedAt"=CURRENT_DATE');
-        this.addSql('alter table "PageTreeNode" alter column "updatedAt" set not null');
+        this.addSql('alter table if exists "PageTreeNode" add column if not exists "updatedAt" timestamp with time zone;');
+        this.addSql(
+            `do $$
+            begin
+                if exists (select 1 from pg_class where relname='PageTreeNode')
+                then update "PageTreeNode" SET "updatedAt"=CURRENT_DATE;
+                end if;
+            end
+            $$;`,
+        );
+        this.addSql('alter table if exists "PageTreeNode" alter column "updatedAt" set not null');
     }
 
     async down(): Promise<void> {
-        this.addSql('alter table "PageTreeNode" drop column "updatedAt";');
+        this.addSql('alter table if exists "PageTreeNode" drop column "updatedAt";');
     }
 }
