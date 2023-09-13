641d8a260bdf3d8d1dfb0ab916f080a31f6fea0e
Franz Unger <franz.unger@vivid-planet.com>
2023-09-13 18:16:07 +0200

    Use fullHeight feature of MainContent

--- packages/admin/cms-admin/src/userPermissions/UserPermissionsPage.tsx
+++ packages/admin/cms-admin/src/userPermissions/UserPermissionsPage.tsx
@@ -1,5 +1,4 @@
 import { MainContent, Stack, StackPage, StackSwitch } from "@comet/admin";
-import { styled } from "@mui/material/styles";
 import * as React from "react";
 import { FormattedMessage } from "react-intl";
 
@@ -10,18 +9,11 @@ export const UserPermissionsPage = (): React.ReactElement => (
     <Stack topLevelTitle={<FormattedMessage id="comet.userPermissions.title" defaultMessage="User Management" />}>
         <StackSwitch>
             <StackPage name="table">
-                <MainContent>
-                    <DataGridContainer>
-                        <UserGrid />
-                    </DataGridContainer>
+                <MainContent fullHeight>
+                    <UserGrid />
                 </MainContent>
             </StackPage>
             <StackPage name="edit">{(userId) => <UserPage userId={userId} />}</StackPage>
         </StackSwitch>
     </Stack>
 );
-
-const DataGridContainer = styled("div")`
-    width: 100%;
-    height: calc(100vh - var(--comet-admin-master-layout-content-top-spacing) - 50px);
-`;
