30b9922f81bb227ad2f9488e362c81df84726d71
Thomas Dax <thomas.dax@vivid-planet.com>
2023-09-24 17:54:28 +0200

    Admin: configure admin generator

--- demo/admin/crud-generator-config.ts
+++ demo/admin/crud-generator-config.ts
@@ -8,4 +8,18 @@ export default [
         target: "src/news/generated",
         entityName: "News",
     },
+    {
+        target: "src/books/generated",
+        entityName: "Book",
+        // if your entity expects block JSON for some props, you might need to explicitly define rootBlocks
+        // that's not necessary if the used block is defined
+        //   - in src/common/blocks and conforms to our naming conventions
+        //   - in a blocks folder right next to the generated folder and conforms to our naming conventions
+        //   - in @comet/cms-admin or @comet/blocks-admin
+        //
+        // the definition looks like this:
+        // rootBlocks: {
+        //     prop: { name: "ExampleBlock", import: "path/or/npm/package" },
+        // },
+    },
 ] satisfies CrudGeneratorConfig[];
