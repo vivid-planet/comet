b73eb4c34c70c694fc8b267ce330af5959c50e7b
Johannes Obermair <johannes.obermair@vivid-planet.com>
2023-10-18 09:30:32 +0200

    Resolve conflicts

--- packages/api/cms-api/src/page-tree/page-tree.service.ts
+++ packages/api/cms-api/src/page-tree/page-tree.service.ts
@@ -90,15 +90,11 @@ export class PageTreeService {
         const existingNode = await readApi.getNodeOrFail(id);
         if (!existingNode) throw new Error("Can't find page-tree-node with id");
 
-<<<<<<< HEAD
-        if (input.createAutomaticRedirectsOnSlugChange && existingNode.slug != input.slug) {
-=======
         if (existingNode.slug === "home" && input.slug !== "home") {
             throw new Error(`Slug of page "home" cannot be changed`);
         }
 
-        if (existingNode.slug != input.slug) {
->>>>>>> main
+        if (input.createAutomaticRedirectsOnSlugChange && existingNode.slug != input.slug) {
             await this.redirectsService.createAutomaticRedirects(existingNode);
         }
 
