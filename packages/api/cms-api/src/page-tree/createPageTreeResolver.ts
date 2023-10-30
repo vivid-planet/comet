9003c74eebc4abc30f0f66a533a972568004d690
Thomas Dax <thomas.dax@vivid-planet.com>
2023-10-30 14:34:11 +0100

    Showcase bug

--- packages/api/cms-api/src/page-tree/createPageTreeResolver.ts
+++ packages/api/cms-api/src/page-tree/createPageTreeResolver.ts
@@ -28,6 +28,8 @@ import {
     ScopeInterface,
 } from "./types";
 
+const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay));
+
 export function createPageTreeResolver({
     PageTreeNode,
     PageTreeNodeCreateInput = DefaultPageTreeNodeCreateInput,
@@ -94,6 +96,8 @@ export function createPageTreeResolver({
             @Args("parentId", { type: () => ID, nullable: true }) parentId: string,
             @Args("slug") slug: string,
         ): Promise<SlugAvailability> {
+            await sleep(3000);
+
             const requestedPath = await this.pageTreeService.pathForParentAndSlug(parentId || null, slug);
 
             if (this.config.reservedPaths.includes(requestedPath)) {
