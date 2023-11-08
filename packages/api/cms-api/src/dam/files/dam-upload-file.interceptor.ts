40245dc29406f9e1c7f1216624494541a1d12e8e
Thomas Dax <thomas.dax@vivid-planet.com>
2023-11-08 10:16:55 +0100

    refactor(FileValidationService): move svgContainsJavaScript check back to FileValidationService

--- packages/api/cms-api/src/dam/files/dam-upload-file.interceptor.ts
+++ packages/api/cms-api/src/dam/files/dam-upload-file.interceptor.ts
@@ -1,10 +1,10 @@
-import { Inject, Injectable, mixin, NestInterceptor, Type } from "@nestjs/common";
+import { CallHandler, ExecutionContext, HttpException, Inject, Injectable, mixin, NestInterceptor, Type } from "@nestjs/common";
 import { FileInterceptor as NestFileInterceptor } from "@nestjs/platform-express";
 import { MulterOptions } from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
 import fs from "fs";
 import * as multer from "multer";
 import os from "os";
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
 import { v4 as uuid } from "uuid";
 
 import { CometValidationException } from "../../common/errors/validation.exception";
@@ -39,8 +39,8 @@ export function DamUploadFileInterceptor(fieldName: string): Type<NestIntercepto
                     fileSize: this.fileValidationService.config.maxFileSize * 1024 * 1024,
                 },
                 fileFilter: (req, file, cb) => {
-                    this.fileValidationService.validateFile(file).then((result) => {
-                        if (result === true) {
+                    this.fileValidationService.validateFileMetadata(file).then((result) => {
+                        if (result === undefined) {
                             return cb(null, true);
                         } else {
                             return cb(new CometValidationException(result), false);
@@ -52,8 +52,19 @@ export function DamUploadFileInterceptor(fieldName: string): Type<NestIntercepto
             this.fileInterceptor = new (NestFileInterceptor(fieldName, multerOptions))();
         }
 
-        intercept(...args: Parameters<NestInterceptor["intercept"]>): Observable<unknown> | Promise<Observable<unknown>> {
-            return this.fileInterceptor.intercept(...args);
+        async intercept(context: ExecutionContext, next: CallHandler<unknown>): Promise<Observable<unknown>> {
+            const fileInterceptor = await this.fileInterceptor.intercept(context, next);
+
+            const ctx = context.switchToHttp();
+            const file = ctx.getRequest().file;
+
+            const error = await this.fileValidationService.validateFileContents(file);
+
+            if (error) {
+                return throwError(() => new HttpException("Rejected File Upload: SVG must not contain JavaScript", 422));
+            }
+
+            return fileInterceptor;
         }
     }
     return mixin(Interceptor);
--- packages/api/cms-api/src/dam/files/file-validation.service.ts
+++ packages/api/cms-api/src/dam/files/file-validation.service.ts
@@ -1,11 +1,23 @@
+import { readFile, unlink } from "fs/promises";
 import * as mimedb from "mime-db";
 
 import { FileUploadInterface } from "./dto/file-upload.interface";
+import { svgContainsJavaScript } from "./files.utils";
 
 export class FileValidationService {
     constructor(public config: { maxFileSize: number; acceptedMimeTypes: string[] }) {}
 
-    async validateFile(file: FileUploadInterface): Promise<true | string> {
+    async validateFile(file: FileUploadInterface): Promise<undefined | string> {
+        let error = await this.validateFileMetadata(file);
+
+        if (error === undefined) {
+            error = await this.validateFileContents(file);
+        }
+
+        return error;
+    }
+
+    async validateFileMetadata(file: FileUploadInterface): Promise<undefined | string> {
         //maximum file size
         if (file.size > this.config.maxFileSize * 1024 * 1024) {
             return "File is too large";
@@ -35,8 +47,31 @@ export class FileValidationService {
             return `File type and extension mismatch: .${extension} and ${file.mimetype} are incompatible`;
         }
 
-        // svgContainsJavaScript check happens in the dedicated FileValidationInterceptor for technical reasons
+        return undefined;
+    }
 
-        return true;
+    async validateFileContents(file: FileUploadInterface): Promise<undefined | string> {
+        if (file.mimetype === "image/svg+xml") {
+            const fileContent = await readFile(file.path, { encoding: "utf-8" });
+
+            if (svgContainsJavaScript(fileContent)) {
+                await this.removeFile(file);
+
+                return "SVG must not contain JavaScript";
+            }
+        }
+
+        return undefined;
+    }
+
+    // https://github.com/expressjs/multer/blob/master/storage/disk.js#L54-L62
+    private async removeFile(file: FileUploadInterface) {
+        const path = file.path;
+
+        delete (file as Partial<FileUploadInterface>).destination;
+        delete (file as Partial<FileUploadInterface>).filename;
+        delete (file as Partial<FileUploadInterface>).path;
+
+        await unlink(path);
     }
 }
