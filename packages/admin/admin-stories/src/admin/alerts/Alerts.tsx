caea5a8b9e05c79f6cfcfeaf5ec72df51930befd
jennyvivid <148231586+jennyvivid@users.noreply.github.com>
2023-11-08 12:46:23 +0100

    Update packages/admin/admin-stories/src/admin/alerts/Alerts.tsx
    Co-authored-by: Ricky James Smith <jamesricky@me.com>

--- packages/admin/admin-stories/src/admin/alerts/Alerts.tsx
+++ packages/admin/admin-stories/src/admin/alerts/Alerts.tsx
@@ -4,17 +4,13 @@ import React from "react";
 
 function Story() {
     return (
-        <>
+        <Stack spacing={4}>
             <AdminComponentAlerts severity="info" title="Title" text="Notification Text" />
-            <br />
             <AdminComponentAlerts severity="warning" title="Title" text="Notification Text" />
-            <br />
             <AdminComponentAlerts severity="error" title="Title" text="Notification Text" />
-            <br />
             <AdminComponentAlerts severity="success" title="Title" text="Notification Text" />
-            <br />
             <AdminComponentAlerts severity="success" text="Notification Text" />
-        </>
+        </Stack>
     );
 }
 
