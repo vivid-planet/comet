13115144c8961445551ce6ee13e7068b76ebe51b
Johannes <johannes.munker@vivid-planet.com>
2023-11-13 08:54:05 +0100

    feat(CustomTab): add example usage to RouterTabs and Tabs story

--- packages/admin/admin-stories/src/docs/components/Tabs/stories/RouterTabs.stories.tsx
+++ packages/admin/admin-stories/src/docs/components/Tabs/stories/RouterTabs.stories.tsx
@@ -1,4 +1,6 @@
-import { RouterTab, RouterTabs } from "@comet/admin";
+import { Divider, RouterTab, RouterTabs } from "@comet/admin";
+import { Account, Domain } from "@comet/admin-icons";
+import { Favorite, HelpOutline } from "@mui/icons-material";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import { useLocation } from "react-router";
@@ -17,11 +19,27 @@ storiesOf("stories/components/Tabs/RouterTabs", module)
                     <RouterTab path="" label="Label One">
                         Content One
                     </RouterTab>
-                    <RouterTab path="/tab2" label="Label Two">
+                    <RouterTab path="/tab2" label="Label Two" status="success" disabled>
                         Content Two
                     </RouterTab>
-                    <RouterTab path="/tab3" label="Label Three">
+                    <RouterTab path="/tab3" label="Label Three" status="error" showStatusIcon showTooltip tooltipMessage="Tooltip message">
                         Content Three
+                    </RouterTab>
+                    <Divider />
+                    <RouterTab path="/tab4" label="Label Four" status="error" tabIcon={<Account />} statusIcon={<Favorite />} showStatusIcon>
+                        Content Four
+                    </RouterTab>
+                    <RouterTab
+                        path="/tab5"
+                        label="Label Five"
+                        tabIcon={<Domain />}
+                        status="warning"
+                        showStatusIcon
+                        showTooltip
+                        tooltipMessage="Tooltip message"
+                        tooltipIcon={<HelpOutline fontSize="large" />}
+                    >
+                        Content Five
                     </RouterTab>
                 </RouterTabs>
             </div>
--- packages/admin/admin-stories/src/docs/components/Tabs/stories/Tabs.stories.tsx
+++ packages/admin/admin-stories/src/docs/components/Tabs/stories/Tabs.stories.tsx
@@ -1,4 +1,6 @@
-import { Tab, Tabs } from "@comet/admin";
+import { Divider, Tab, Tabs } from "@comet/admin";
+import { Account, Domain } from "@comet/admin-icons";
+import { Favorite, HelpOutline } from "@mui/icons-material";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
@@ -10,8 +12,27 @@ storiesOf("stories/components/Tabs/Tabs", module)
         return (
             <Tabs>
                 <Tab label="Label One">Content One</Tab>
-                <Tab label="Label Two">Content Two</Tab>
-                <Tab label="Label Three">Content Three</Tab>
+                <Tab label="Label Two" status="success" disabled>
+                    Content Two
+                </Tab>
+                <Tab label="Label Three" status="error" showStatusIcon showTooltip tooltipMessage="Tooltip message">
+                    Content Three
+                </Tab>
+                <Divider />
+                <Tab label="Label Four" status="error" tabIcon={<Account />} statusIcon={<Favorite />} showStatusIcon>
+                    Content Four
+                </Tab>
+                <Tab
+                    label="Label Five"
+                    tabIcon={<Domain />}
+                    status="warning"
+                    showStatusIcon
+                    showTooltip
+                    tooltipMessage="Tooltip message"
+                    tooltipIcon={<HelpOutline fontSize="large" />}
+                >
+                    Content Five
+                </Tab>
             </Tabs>
         );
     });
