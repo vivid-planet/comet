442bd7d5886c82e3e87587874a4b97a8f8305d47
David Schwarz <david.schwarz@vivid-planet.com>
2023-12-05 10:10:22 +0100

    Add translate button that uses translate function from translation context to FinalFormInput.tsx

--- packages/admin/admin/src/form/FinalFormInput.tsx
+++ packages/admin/admin/src/form/FinalFormInput.tsx
@@ -1,28 +1,42 @@
-import { InputBase, InputBaseProps } from "@mui/material";
+import { Button, InputBase, InputBaseProps } from "@mui/material";
 import * as React from "react";
 import { FieldRenderProps } from "react-final-form";
+import { FormattedMessage } from "react-intl";
 
 import { ClearInputAdornment } from "../common/ClearInputAdornment";
+import { useTranslationConfig } from "../translator/config/useTranslationConfig";
 
 export type FinalFormInputProps = InputBaseProps &
     FieldRenderProps<string, HTMLInputElement | HTMLTextAreaElement> & {
         clearable?: boolean;
+        hideTranslate?: boolean;
     };
 
-export function FinalFormInput({ meta, input, innerRef, endAdornment, clearable, ...props }: FinalFormInputProps): React.ReactElement {
+export function FinalFormInput({ meta, input, innerRef, endAdornment, clearable, hideTranslate, ...props }: FinalFormInputProps): React.ReactElement {
+    const { enableTranslation, translate } = useTranslationConfig();
+
     return (
         <InputBase
             {...input}
             {...props}
             endAdornment={
-                clearable ? (
-                    <>
+                <>
+                    {clearable && (
                         <ClearInputAdornment position="end" hasClearableContent={Boolean(input.value)} onClick={() => input.onChange("")} />
-                        {endAdornment}
-                    </>
-                ) : (
-                    endAdornment
-                )
+                    )}
+                    {enableTranslation && !hideTranslate && (
+                        <Button
+                            onClick={() => {
+                                if (translate) {
+                                    input.onChange(translate(input.value));
+                                }
+                            }}
+                        >
+                            <FormattedMessage id="comet.translate" defaultMessage="Translate" />
+                        </Button>
+                    )}
+                    {endAdornment}
+                </>
             }
         />
     );
