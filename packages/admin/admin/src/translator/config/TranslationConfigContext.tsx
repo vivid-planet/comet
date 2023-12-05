77330483cafcc4e9abef95cfb3667e72438edf54
David Schwarz <david.schwarz@vivid-planet.com>
2023-12-05 14:21:21 +0100

    Adjust translate function to be async

--- packages/admin/admin/src/translator/config/TranslationConfigContext.tsx
+++ packages/admin/admin/src/translator/config/TranslationConfigContext.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export interface TranslationConfig {
     enableTranslation?: boolean;
-    translate?: (input: string, language?: string) => string;
+    translate?: (input: string, language?: string) => Promise<string>;
 }
 
 export const TranslationConfigContext = React.createContext<TranslationConfig | undefined>(undefined);
