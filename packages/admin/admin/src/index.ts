f6b096b79343cba100d0c895692c132cb59b7490
David Schwarz <david.schwarz@vivid-planet.com>
2023-12-05 10:09:14 +0100

    Add translation context provider

--- packages/admin/admin/src/index.ts
+++ packages/admin/admin/src/index.ts
@@ -198,3 +198,5 @@ export { RouterTabsClassKey } from "./tabs/RouterTabs.styles";
 export { Tab, Tabs, TabsProps } from "./tabs/Tabs";
 export { TabsClassKey } from "./tabs/Tabs.styles";
 export { TabScrollButton, TabScrollButtonClassKey, TabScrollButtonProps } from "./tabs/TabScrollButton";
+export { TranslationConfigProvider } from "./translator/config/TranslationProvider";
+export { useTranslationConfig } from "./translator/config/useTranslationConfig";
