b3f57631cd4ec67c139b6116fcd093c566431b28
David Schwarz <david.schwarz@vivid-planet.com>
2023-12-06 12:46:21 +0100

    Add translation module

---
+++ packages/api/cms-api/src/translation/translation.config.ts
@@ -0,0 +1,7 @@
+export interface TranslationServiceInterface {
+    translate(value: string, language?: string): Promise<string>;
+}
+
+export interface TranslationConfig {
+    service: TranslationServiceInterface;
+}
