41723bc5df9b578e092f959816508631bfb99a34
David Schwarz <david.schwarz@vivid-planet.com>
2023-12-11 13:14:50 +0100

    Remove optional language parma from TranslationConfig translate function

--- packages/admin/admin/src/translator/config/TranslationConfigContext.tsx
+++ packages/admin/admin/src/translator/config/TranslationConfigContext.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export interface TranslationConfig {
     enableTranslation?: boolean;
-    translate?: (input: string, language?: string) => Promise<string>;
+    translate?: (input: string) => Promise<string>;
 }
 
 export const TranslationConfigContext = React.createContext<TranslationConfig | undefined>(undefined);
