18ebe45a1daf071b921bee8e5e848d17741fef95
Franz Unger <franz.unger@vivid-planet.com>
2023-12-12 16:33:51 +0100

    Use failsafe tablename

--- packages/api/cms-api/src/mikro-orm/migrations/Migration20231206123505.ts
+++ packages/api/cms-api/src/mikro-orm/migrations/Migration20231206123505.ts
@@ -2,7 +2,9 @@ import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20231206123505 extends Migration {
     async up(): Promise<void> {
-        const result = await this.execute(`select count(*) from "Migrations" where name = 'Migration20230831110518';`);
+        const result = await this.execute(
+            `select count(*) from "${this.config.get("migrations").tableName}" where name = 'Migration20230831110518';`,
+        );
         if (result[0].count == 1) {
             this.addSql('drop table if exists "UserContentScopes";');
             this.addSql('drop table if exists "UserPermission";');
