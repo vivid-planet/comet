87fda6ac5b6880e44a5b336377744b6217c4fa7a
David Schwarz <david.schwarz@vivid-planet.com>
2023-12-19 09:24:14 +0100

    Adjust typing + remove unnecessary code

--- packages/admin/admin/src/translator/config/ContentTranslationServiceContext.tsx
+++ packages/admin/admin/src/translator/config/ContentTranslationServiceContext.tsx
@@ -2,12 +2,10 @@ import * as React from "react";
 
 export interface TranslationConfig {
     enabled: boolean;
-    translate: (input: string) => Promise<string | void>;
+    translate: (input: string) => Promise<string>;
 }
 
 export const ContentTranslationServiceContext = React.createContext<TranslationConfig>({
     enabled: false,
-    translate: async () => {
-        /* Noop */
-    },
+    translate: async (input) => input,
 });
