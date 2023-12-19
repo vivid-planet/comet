4c3df99970d4692a36d5183abedbcf5cae86ea89
Franz Unger <franz.unger@vivid-planet.com>
2023-12-19 08:25:50 +0100

    Remove not needed code

--- demo/api/src/db/ormconfig.cli.ts
+++ demo/api/src/db/ormconfig.cli.ts
@@ -1,5 +1,3 @@
-import { UserContentScopes } from "@comet/cms-api/lib/user-permissions/entities/user-content-scopes.entity";
-import { UserPermission } from "@comet/cms-api/lib/user-permissions/entities/user-permission.entity";
 import { Options } from "@mikro-orm/core";
 import { PageTreeNodeScope } from "@src/page-tree/dto/page-tree-node-scope";
 
@@ -7,8 +5,8 @@ import ormConfig from "./ormconfig";
 
 const config: Options = {
     ...ormConfig,
-    entities: ["./dist/**/*.entity.js", PageTreeNodeScope, UserPermission, UserContentScopes],
-    entitiesTs: ["./src/**/*.entity.ts", PageTreeNodeScope, UserPermission, UserContentScopes],
+    entities: ["./dist/**/*.entity.js", PageTreeNodeScope],
+    entitiesTs: ["./src/**/*.entity.ts", PageTreeNodeScope],
 };
 
 export = config;
