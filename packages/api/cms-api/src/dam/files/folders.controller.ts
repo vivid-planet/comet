770de6cf85601079a6a675b8d5079adfca97bb23
Johannes <johannes.munker@vivid-planet.com>
2024-01-03 11:55:16 +0100

    feat(folders.controller): add user scope check to createZip function

--- packages/api/cms-api/src/dam/files/folders.controller.ts
+++ packages/api/cms-api/src/dam/files/folders.controller.ts
@@ -1,18 +1,26 @@
-import { Controller, Get, NotFoundException, Param, Res } from "@nestjs/common";
+import { Controller, ForbiddenException, Get, NotFoundException, Param, Res } from "@nestjs/common";
 import { Response } from "express";
 
+import { CurrentUserInterface } from "../../auth/current-user/current-user";
+import { GetCurrentUser } from "../../auth/decorators/get-current-user.decorator";
+import { ContentScopeService } from "../../content-scope/content-scope.service";
 import { FoldersService } from "./folders.service";
 
 @Controller("dam/folders")
 export class FoldersController {
-    constructor(private readonly foldersService: FoldersService) {}
+    constructor(private readonly foldersService: FoldersService, private readonly contentScopeService: ContentScopeService) {}
 
     @Get("/:folderId/zip")
-    async createZip(@Param("folderId") folderId: string, @Res() res: Response): Promise<void> {
+    async createZip(@Param("folderId") folderId: string, @Res() res: Response, @GetCurrentUser() user: CurrentUserInterface): Promise<void> {
         const folder = await this.foldersService.findOneById(folderId);
         if (!folder) {
             throw new NotFoundException("Folder not found");
         }
+
+        if (folder.scope !== undefined && !this.contentScopeService.canAccessScope(folder.scope, user)) {
+            throw new ForbiddenException("The current user is not allowed to access this scope and download this folder.");
+        }
+
         const zipStream = await this.foldersService.createZipStreamFromFolder(folderId);
 
         res.setHeader("Content-Disposition", `attachment; filename="${folder.name}.zip"`);
