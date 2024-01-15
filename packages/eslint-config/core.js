62bf8c6b3536c939f2310ebd5c8ffcc0fc92c930
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2024-01-15 08:55:39 +0100

    Merge `core.js` and `core-without-import.js` ESLint configs (#1566)
    The `core-without-import.js` file was originally added to fix a problem
    with the `next/core-web-vitals` config that caused duplicate `import`
    plugins. This problem doesn't exist anymore in our current setup, so we
    may remove the extra file. This also prevents forgetting about adding
    new rules to both files.

--- packages/eslint-config/core.js
+++ packages/eslint-config/core.js
@@ -1,11 +1,30 @@
-const core = require("./core-without-import");
-
 module.exports = {
-    extends: require.resolve("./core-without-import.js"),
-    plugins: [...core.plugins, "import"],
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    plugins: ["simple-import-sort", "unused-imports", "json-files", "@comet", "import"],
     rules: {
+        "no-unused-vars": "off",
+        "prefer-template": "error",
+        "simple-import-sort/exports": "error",
+        "simple-import-sort/imports": "error",
+        "unused-imports/no-unused-imports": "error",
+        "no-console": ["error", { allow: ["warn", "error"] }],
+        "no-return-await": "error",
+        "json-files/sort-package-json": "error",
+        "@comet/no-other-module-relative-import": ["warn"],
         "import/no-extraneous-dependencies": "error",
         "import/no-duplicates": "error",
-        "import/newline-after-import": "error"
+        "import/newline-after-import": "error",
     },
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"],
+            parser: "@typescript-eslint/parser",
+            extends: ["plugin:@typescript-eslint/recommended"],
+            plugins: ["@typescript-eslint"],
+            rules: {
+                "@typescript-eslint/no-unused-vars": ["error", { args: "none", ignoreRestSiblings: true }],
+                "@typescript-eslint/no-inferrable-types": ["error", { ignoreProperties: true }],
+            },
+        },
+    ],
 };
