a5680b92a34b79ba71d28e38975257050932bd3b
Stefanie Kaltenhauser <stefanie.kaltenhauser@vivid-planet.com>
2024-01-17 14:36:06 +0100

    Change variable names to camelCase

--- packages/api/blocks-api/src/blocks/createRichTextBlock.ts
+++ packages/api/blocks-api/src/blocks/createRichTextBlock.ts
@@ -71,10 +71,10 @@ export function createRichTextBlock<LinkBlock extends Block>(
     { link: LinkBlock, indexSearchText = true }: CreateRichTextBlockOptions,
     nameOrOptions: NameOrOptions = "RichText",
 ): Block<RichTextBlockDataInterface, RichTextBlockInputInterface<ExtractBlockInput<LinkBlock>>> {
-    const BLOCK_NAME = typeof nameOrOptions === "string" ? nameOrOptions : nameOrOptions.name;
-    const MIGRATE = typeof nameOrOptions !== "string" && nameOrOptions.migrate ? nameOrOptions.migrate : { migrations: [], version: 0 };
+    const blockName = typeof nameOrOptions === "string" ? nameOrOptions : nameOrOptions.name;
+    const migrate = typeof nameOrOptions !== "string" && nameOrOptions.migrate ? nameOrOptions.migrate : { migrations: [], version: 0 };
 
-    @BlockDataMigrationVersion(MIGRATE.version)
+    @BlockDataMigrationVersion(migrate.version)
     class RichTextBlockData extends BlockData {
         @BlockField({ type: "json" })
         draftContent: RawDraftContentState;
@@ -171,8 +171,8 @@ export function createRichTextBlock<LinkBlock extends Block>(
 
     // Decorate BlockDataFactory
     let decorateBlockDataFactory = blockDataFactory;
-    if (MIGRATE.migrations) {
-        const blockDataFactoryDecorator1 = createAppliedMigrationsBlockDataFactoryDecorator(MIGRATE.migrations, BLOCK_NAME);
+    if (migrate.migrations) {
+        const blockDataFactoryDecorator1 = createAppliedMigrationsBlockDataFactoryDecorator(migrate.migrations, blockName);
         decorateBlockDataFactory = blockDataFactoryDecorator1(decorateBlockDataFactory);
     }
     decorateBlockDataFactory = strictBlockDataFactoryDecorator(decorateBlockDataFactory);
@@ -181,7 +181,7 @@ export function createRichTextBlock<LinkBlock extends Block>(
     const decorateBlockInputFactory = strictBlockInputFactoryDecorator(blockInputFactory);
 
     const RichTextBlock: Block<RichTextBlockData, RichTextBlockInputInterface<ExtractBlockInput<LinkBlock>>> = {
-        name: BLOCK_NAME,
+        name: blockName,
         blockDataFactory: decorateBlockDataFactory,
         blockInputFactory: decorateBlockInputFactory,
         blockMeta: new AnnotationBlockMeta(RichTextBlockData),
