e77c3f2ef93c0770ef1face61eeb7d482dead09d
Andrea Rutrecht <andrea.rutrecht@vivid-planet.com>
2024-01-22 09:30:04 +0100

    Refactor theming of 'Toolbar' in admin-rte

---
+++ packages/admin/admin-rte/src/core/Controls/Toolbar/Toolbar.styles.tsx
@@ -0,0 +1,73 @@
+import { css, styled } from "@mui/material/styles";
+
+export type RteToolbarClassKey = "root" | "slot";
+
+export const Root = styled("div", {
+    name: "CometAdminRteToolbar",
+    slot: "root",
+    overridesResolver(_, styles) {
+        return [styles.root];
+    },
+})(
+    ({ theme }) => css`
+        position: sticky;
+        top: 0;
+        z-index: 2;
+        display: flex;
+        flex-wrap: wrap;
+        border-top: 1px solid ${theme.components?.CometAdminRte?.defaultProps?.colors?.border};
+        background-color: ${theme.components?.CometAdminRte?.defaultProps?.colors?.toolbarBackground};
+        padding-left: 6px; //px right?
+        padding-right: 6px;
+        overflow: hidden;
+    `,
+);
+
+export const Slot = styled("div", {
+    name: "CometAdminRteToolbar",
+    slot: "slot",
+    overridesResolver(_, styles) {
+        return [styles.slot];
+    },
+})(
+    ({ theme }) => css`
+        position: relative;
+        flex-shrink: 0;
+        flex-grow: 0;
+        height: 34px; //right unit?
+        box-sizing: border-box;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        padding-right: 6px;
+        margin-right: 5px;
+
+        &::before,
+        &::after {
+            content: "";
+            position: absolute;
+            background-color: ${theme.components?.CometAdminRte?.defaultProps?.colors?.border};
+        }
+
+        &::before {
+            bottom: 0;
+            height: 1px;
+            left: -100vw;
+            right: -100vw;
+        }
+
+        &::after {
+            top: 8px;
+            right: 0;
+            bottom: 8px;
+            width: 1px;
+        }
+
+        &:last-child {
+            margin-right: 0;
+        }
+
+        &:last-child::after {
+            display: none;
+        }
+    `,
+);
---
+++ packages/admin/admin-rte/src/core/Controls/Toolbar/Toolbar.tsx
@@ -0,0 +1,59 @@
+import { ThemedComponentBaseProps } from "@comet/admin";
+import { ComponentsOverrides, Theme } from "@mui/material";
+import { useThemeProps } from "@mui/material/styles";
+import React from "react";
+
+import { IControlProps } from "../../types";
+import { Root, RteToolbarClassKey, Slot } from "./Toolbar.styles";
+
+export interface RteToolbarProps
+    extends IControlProps,
+        ThemedComponentBaseProps<{
+            root: "div";
+            slot: "div";
+        }> {
+    children: Array<(p: IControlProps) => JSX.Element | null>;
+}
+
+export function Toolbar(inProps: RteToolbarProps) {
+    const { children, slotProps, ...restProps } = useThemeProps({ props: inProps, name: "CometAdminRteToolbar" });
+
+    const childrenElements = children
+        .filter((c) => {
+            const Comp = c;
+            return Comp(restProps) !== null; // filter out unused control components
+        })
+        .map((c) => {
+            const Comp = c;
+            return React.createElement(Comp, restProps);
+        });
+
+    return (
+        <Root {...restProps} {...slotProps?.root}>
+            {childrenElements.map((c, idx) => {
+                return (
+                    <Slot key={idx} {...slotProps?.slot}>
+                        {c}
+                    </Slot>
+                );
+            })}
+        </Root>
+    );
+}
+
+declare module "@mui/material/styles" {
+    interface ComponentsPropsList {
+        CometAdminRteToolbar: RteToolbarProps;
+    }
+
+    interface ComponentNameToClassKey {
+        CometAdminRteToolbar: RteToolbarClassKey;
+    }
+
+    interface Components {
+        CometAdminRteToolbar?: {
+            defaultProps?: Partial<ComponentsPropsList["CometAdminRteToolbar"]>;
+            styleOverrides?: ComponentsOverrides<Theme>["CometAdminRteToolbar"];
+        };
+    }
+}
