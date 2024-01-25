8e86516493b038eb557e99eaf1e6620ad462a082
Franz Unger <franz.unger@vivid-planet.com>
2024-01-25 08:49:16 +0100

    Revert Next.js Preview Mode (#1617)

---
+++ packages/site/cms-site/src/preview/utils.ts
@@ -0,0 +1,71 @@
+import { ParsedUrlQuery } from "querystring";
+
+import { Url } from "./PreviewContext";
+
+export const defaultPreviewPath = "/preview";
+
+interface SitePreviewParams {
+    includeInvisibleBlocks: boolean;
+}
+
+const defaultParams: SitePreviewParams = {
+    includeInvisibleBlocks: false,
+};
+
+export function parsePreviewParams(query: ParsedUrlQuery): SitePreviewParams {
+    let previewParams: SitePreviewParams = defaultParams;
+    const param = query.__preview;
+
+    if (typeof param === "string") {
+        try {
+            previewParams = JSON.parse(param);
+        } catch {
+            // Ignore invalid preview state
+        }
+    }
+
+    return previewParams;
+}
+
+/**
+ * @deprecated Use parsePreviewParams instead
+ */
+const parsePreviewState = parsePreviewParams;
+
+export { parsePreviewState };
+
+export function createPathToPreviewPath({
+    path,
+    previewPath,
+    previewParams,
+}: {
+    path: Url;
+    previewPath: string;
+    previewParams: SitePreviewParams;
+}): Url {
+    if (typeof path === "string") {
+        const [pathname, search] = `${previewPath}${path}`.split("?");
+        const searchParams = new URLSearchParams(search);
+
+        searchParams.append("__preview", JSON.stringify(previewParams));
+
+        return `${pathname}?${searchParams.toString()}`;
+    } else {
+        let query = path.query;
+
+        if (typeof query === "string") {
+            query += `&__preview=${JSON.stringify(previewParams)}`;
+        } else if (typeof query === "object") {
+            query = {
+                ...query,
+                __preview: JSON.stringify(previewParams),
+            };
+        }
+
+        return {
+            ...path,
+            pathname: `${previewPath}${path.pathname}`,
+            query,
+        };
+    }
+}
