a1bb636a60fd99060fcdb089c52e8a250f690990
Florian Ragger <florian.ragger@vivid-planet.com>
2024-01-29 05:56:50 +0100

    Refactor theming of TableBodyRow

--- packages/admin/admin/src/table/TableBodyRow.tsx
+++ packages/admin/admin/src/table/TableBodyRow.tsx
@@ -1,40 +1,56 @@
-import { ComponentsOverrides, TableRow, Theme } from "@mui/material";
+import { ComponentsOverrides, TableRow } from "@mui/material";
+import { css, styled, Theme, useThemeProps } from "@mui/material/styles";
 import { TableRowProps } from "@mui/material/TableRow";
-import { createStyles, WithStyles, withStyles } from "@mui/styles";
-import * as React from "react";
+import { ThemedComponentBaseProps } from "helpers/ThemedComponentBaseProps";
+import React from "react";
 
 /**
  * @deprecated Use MUI X Data Grid in combination with `useDataGridRemote` instead.
  */
 export type TableBodyRowClassKey = "root" | "even" | "odd";
 
+type OwnerState = Pick<TableBodyRowProps, "index" | "hideTableHead">;
+
+const Root = styled(TableRow, {
+    name: "CometAdminTableBodyRow",
+    slot: "root",
+    overridesResolver({ ownerState }: { ownerState: OwnerState }, styles) {
+        const isOdd = ((ownerState.index || 0) + (ownerState.hideTableHead ? 1 : 0)) % 2 === 1;
+        return [styles.root, !isOdd && styles.even, isOdd && styles.odd];
+    },
+})<{ ownerState: OwnerState }>(({ ownerState: { index, hideTableHead } }) => {
+    const isOdd = ((index || 0) + (hideTableHead ? 1 : 0)) % 2 === 1;
+    if (isOdd) {
+        return css``;
+    }
+    return css``;
+});
+
 /**
  * @deprecated Use MUI X Data Grid in combination with `useDataGridRemote` instead.
  */
-export interface TableBodyRowProps extends TableRowProps {
+export interface TableBodyRowProps extends ThemedComponentBaseProps<{ root: typeof TableRow }>, TableRowProps {
     index?: number;
     hideTableHead?: boolean;
 }
 
-const styles = () => {
-    return createStyles<TableBodyRowClassKey, TableBodyRowProps>({
-        root: {},
-        even: {},
-        odd: {},
-    });
-};
-
-const Row = React.forwardRef<HTMLTableRowElement, TableBodyRowProps & WithStyles<typeof styles>>(
-    ({ index, hideTableHead, classes, ...otherProps }, ref) => {
-        const isOdd = ((index || 0) + (hideTableHead ? 1 : 0)) % 2 === 1;
-        return <TableRow ref={ref} classes={{ root: `${classes.root} ${isOdd ? classes.odd : classes.even}` }} {...otherProps} />;
-    },
-);
-
 /**
  * @deprecated Use MUI X Data Grid in combination with `useDataGridRemote` instead.
  */
-export const TableBodyRow = withStyles(styles, { name: "CometAdminTableBodyRow" })(Row);
+export function TableBodyRow(inProps: TableBodyRowProps) {
+    const { title, children, index, hideTableHead, slotProps, ...restProps } = useThemeProps({ props: inProps, name: "CometAdminTableBodyRow" });
+
+    const ownerState: OwnerState = {
+        index,
+        hideTableHead,
+    };
+
+    return (
+        <Root ownerState={ownerState} {...restProps} {...slotProps?.root}>
+            {children}
+        </Root>
+    );
+}
 
 declare module "@mui/material/styles" {
     interface ComponentNameToClassKey {
