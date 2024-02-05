3ca182909f8dfe0a8b3b3cc86aacf10ecf0e92a4
Thomas Dax <thomas.dax@vivid-planet.com>
2024-02-05 08:52:13 +0100

    Readd SplitButton stories and docs and mark as deprecated in docs

---
+++ packages/admin/admin-stories/src/docs/components/SplitButton/stories/OneChild.stories.tsx
@@ -0,0 +1,45 @@
+import { SplitButton } from "@comet/admin";
+import { Button } from "@mui/material";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+storiesOf("stories/components/Split Button/One Child", module)
+    .add("One child", () => {
+        return (
+            <SplitButton variant="contained" color="primary">
+                <Button
+                    onClick={() => {
+                        alert("primary clicked");
+                    }}
+                >
+                    Primary Action
+                </Button>
+            </SplitButton>
+        );
+    })
+    .add("One child with select", () => {
+        return (
+            <SplitButton showSelectButton={true} variant="contained" color="primary">
+                <Button
+                    onClick={() => {
+                        alert("primary clicked");
+                    }}
+                >
+                    Primary Action
+                </Button>
+            </SplitButton>
+        );
+    })
+    .add("One child disabled", () => {
+        return (
+            <SplitButton disabled={true} variant="contained" color="primary">
+                <Button
+                    onClick={() => {
+                        alert("primary clicked");
+                    }}
+                >
+                    Primary Action
+                </Button>
+            </SplitButton>
+        );
+    });
