4e2fab60bd9c2d4bb6386193e81c34df475e069a
Niko Sams <ns@vivid-planet.com>
2024-02-05 07:50:08 +0100

    use change-case package instead of own implementation
    use v4 instead of v5 to work around esm issues in nodejs

--- packages/admin/cms-admin/src/generator/future/utils/camelCaseToHumanReadable.ts
+++ packages/admin/cms-admin/src/generator/future/utils/camelCaseToHumanReadable.ts
@@ -1,4 +1,5 @@
+import { capitalCase } from "change-case";
+
 export function camelCaseToHumanReadable(s: string) {
-    const words = s.match(/[A-Za-z][a-z]*/g) || [];
-    return words.map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(" ");
+    return capitalCase(s);
 }
