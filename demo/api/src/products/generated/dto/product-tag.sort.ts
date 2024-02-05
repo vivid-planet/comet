5986b3b73d61150abb523d24f65a470fcb8fdfc7
Thomas Dax <thomas.dax@vivid-planet.com>
2024-02-05 17:12:37 +0100

    Disable rule for existing non-compliant enums

--- demo/api/src/products/generated/dto/product-tag.sort.ts
+++ demo/api/src/products/generated/dto/product-tag.sort.ts
@@ -4,11 +4,14 @@ import { SortDirection } from "@comet/cms-api";
 import { Field, InputType, registerEnumType } from "@nestjs/graphql";
 import { IsEnum } from "class-validator";
 
+/* eslint-disable @typescript-eslint/naming-convention */
+// TODO: Replace with PascalCase
 export enum ProductTagSortField {
     title = "title",
     createdAt = "createdAt",
     updatedAt = "updatedAt",
 }
+/* eslint-enable @typescript-eslint/naming-convention */
 registerEnumType(ProductTagSortField, {
     name: "ProductTagSortField",
 });
