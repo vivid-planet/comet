64b6347e4fbc71048c0b46ea82f0bf9dcac052d9
Benjamin Hohenwarter <bh@vivid-planet.com>
2024-02-12 14:54:51 +0100

    Remove unused simple deep-key-of version

--- packages/admin/cms-admin/src/generator/future/utils/deepKeyOf.ts
+++ packages/admin/cms-admin/src/generator/future/utils/deepKeyOf.ts
@@ -1,11 +1,5 @@
 // https://stackoverflow.com/a/58436959
 
-// export type DeepKeyOf<T> = T extends object
-//     ? {
-//           [K in keyof T]: `${Exclude<K, symbol>}${DeepKeyOf<T[K]> extends never ? "" : `.${DeepKeyOf<T[K]>}`}`;
-//       }[keyof T]
-//     : never;
-
 type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]];
 type Join<K, P> = K extends string | number ? (P extends string | number ? `${K}${"" extends P ? "" : "."}${P}` : never) : never;
 
