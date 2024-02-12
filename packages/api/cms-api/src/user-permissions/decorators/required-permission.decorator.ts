38b62bd819b3fec4cd35936cac03d333b4dfe587
Niko Sams <ns@vivid-planet.com>
2024-02-12 14:36:04 +0100

    Fix type of RequiredPermission to accept a non-array string for a single permission
    (Removed duplicate type definition)

--- packages/api/cms-api/src/user-permissions/decorators/required-permission.decorator.ts
+++ packages/api/cms-api/src/user-permissions/decorators/required-permission.decorator.ts
@@ -19,11 +19,9 @@ type RequiredPermissionOptions = {
     skipScopeCheck?: boolean;
 };
 
-export type RequiredPermission = {
-    requiredPermission: (keyof Permission)[] | keyof Permission;
-    options: RequiredPermissionOptions | undefined;
-};
-
-export const RequiredPermission = (requiredPermission: (keyof Permission)[], options?: RequiredPermissionOptions): CustomDecorator<string> => {
-    return SetMetadata<string, RequiredPermission>("requiredPermission", { requiredPermission, options });
+export const RequiredPermission = (
+    requiredPermission: (keyof Permission)[] | keyof Permission,
+    options?: RequiredPermissionOptions,
+): CustomDecorator<string> => {
+    return SetMetadata("requiredPermission", { requiredPermission, options });
 };
