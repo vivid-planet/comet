400ea4e012f0c3bb5c7491a0b7b3aa75e2707e27
David Schwarz <david.schwarz@vivid-planet.com>
2024-02-13 15:02:17 +0100

    Turn scrollPositions into a ref to only forget the position on dismount of the tabs component

--- packages/admin/blocks-admin/src/common/useScrollRestoration.ts
+++ packages/admin/blocks-admin/src/common/useScrollRestoration.ts
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { useLocation } from "react-router";
 
-const scrollPositions: Record<string, number | undefined> = {};
-
 interface UseScrollRestorationProps<Element> {
     ref: React.MutableRefObject<Element | null>;
     onScroll: () => void;
@@ -14,23 +12,17 @@ export function useScrollRestoration<Element extends HTMLElement>(identifier: st
 
     const ref = React.useRef<Element>(null);
 
+    const scrollPositionsRef = React.useRef<Record<string, number | undefined>>({});
+
     React.useLayoutEffect((): void => {
         if (ref.current) {
-            ref.current.scrollTo(0, scrollPositions[identifierForRoute] ?? 0);
+            ref.current.scrollTo(0, scrollPositionsRef.current[identifierForRoute] ?? 0);
         }
     }, [identifierForRoute]);
 
-    for (const key of Object.keys(scrollPositions)) {
-        const keyWithoutIdentifier = key.replace(`-${identifier}`, "");
-
-        if (keyWithoutIdentifier.startsWith(location.pathname) && keyWithoutIdentifier !== location.pathname) {
-            scrollPositions[key] = 0;
-        }
-    }
-
     const onScroll = React.useCallback(() => {
         if (ref.current) {
-            scrollPositions[identifierForRoute] = ref.current.scrollTop;
+            scrollPositionsRef.current[identifierForRoute] = ref.current.scrollTop;
         }
     }, [identifierForRoute]);
 
