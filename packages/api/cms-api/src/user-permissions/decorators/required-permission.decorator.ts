c6cf86138926aebc063bfdb9ee10cad799e252b2
Niko Sams <niko.sams@gmail.com>
2024-02-13 08:57:42 +0100

    Remove unused RequiredPermissionArgs type (#1691)

--- packages/api/cms-api/src/user-permissions/decorators/required-permission.decorator.ts
+++ packages/api/cms-api/src/user-permissions/decorators/required-permission.decorator.ts
@@ -1,19 +1,6 @@
-import { EntityClass, EntityManager } from "@mikro-orm/core";
 import { CustomDecorator, SetMetadata } from "@nestjs/common";
-import { Request } from "express";
 
-import { CurrentUser } from "../dto/current-user";
-import { ContentScope } from "../interfaces/content-scope.interface";
 import { Permission } from "../interfaces/user-permission.interface";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type RequiredPermissionArgs<ArgsType = any> = {
-    args: ArgsType;
-    getScopeFromEntity: (entityClass: EntityClass<object>, id: string) => Promise<ContentScope>;
-    user: CurrentUser;
-    entityManager: EntityManager;
-    request: Request;
-};
 
 type RequiredPermissionOptions = {
     skipScopeCheck?: boolean;
