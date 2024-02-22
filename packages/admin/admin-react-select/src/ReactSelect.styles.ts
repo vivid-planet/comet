39fdf68c821c5aca712c162ecd3e90db6f957f21
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2024-02-22 07:15:07 +0100

    refactor theming of reactSelect

--- packages/admin/admin-react-select/src/ReactSelect.styles.ts
+++ packages/admin/admin-react-select/src/ReactSelect.styles.ts
@@ -22,52 +22,19 @@ export type SelectClassKey =
 
 const styles = (theme: Theme) =>
     createStyles<SelectClassKey, Record<string, unknown>>({
-        input: {
-            display: "flex",
-            paddingRight: 0,
-        },
-        valueContainer: {
-            display: "flex",
-            flexWrap: "nowrap",
-            flex: 1,
-            alignItems: "center",
-            overflow: "hidden",
-        },
-        chip: {
-            margin: `${theme.spacing(0.5)} ${theme.spacing(0.25)}`,
-        },
+        input: {},
+        valueContainer: {},
+        chip: {},
         chipFocused: {
             backgroundColor: emphasize(theme.palette.mode === "light" ? theme.palette.grey[300] : theme.palette.grey[700], 0.08),
         },
-        noOptionsMessage: {
-            padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
-            color: theme.palette.text.secondary,
-        },
-        singleValue: {
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            whiteSpace: "nowrap",
-        },
-        placeholder: {
-            color: theme.palette.text.disabled,
-        },
+        noOptionsMessage: {},
+        singleValue: {},
+        placeholder: {},
         paper: {},
-        indicatorsContainer: {
-            display: "flex",
-        },
-        indicatorSeparator: {
-            width: 1,
-            flexGrow: 1,
-            backgroundColor: theme.palette.divider,
-        },
-        indicator: {
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            color: theme.palette.grey[500],
-            width: 32,
-            cursor: "pointer",
-        },
+        indicatorsContainer: {},
+        indicatorSeparator: {},
+        indicator: {},
         clearIndicator: {
             fontSize: 18,
         },
@@ -84,3 +51,138 @@ const styles = (theme: Theme) =>
     });
 
 export default styles;
+
+// export const Input = styled(ControlInput, {
+//     name: "CometAdminSelect",
+//     slot: "input",
+//     overridesResolver(_, styles) {
+//         return [styles.input];
+//     },
+// })(css`
+//     display: flex;
+//     padding-right: 0;
+// `);
+
+// const ValueContainer = styled("div", {
+//     name: "CometAdminSelect",
+//     slot: "valueContainer",
+//     overridesResolver(_, styles) {
+//         return [styles.valueContainer];
+//     },
+// })(css`
+//     display: flex;
+//     flex-wrap: nowrap;
+//     flex: 1;
+//     align-items: center;
+//     overflow: hidden;
+// `);
+
+// const Chip = styled(MuiChip, {
+//     name: "CometAdminSelect",
+//     slot: "chip",
+//     overridesResolver(_, styles) {
+//         return [styles.chip];
+//     },
+// })(
+//     ({ theme }) => css`
+//         margin: ${theme.spacing(0.5)} ${theme.spacing(0.25)};
+//     `,
+// );
+
+// const ChipFocused = styled(MuiChip, {
+//     name: "CometAdminSelect",
+//     slot: "chipFocused",
+//     overridesResolver(_, styles) {
+//         return [styles.chipFocused];
+//     },
+// })(
+//     ({ theme }) => css`
+//         //   TODO: Add backgroundColor
+//     `,
+// );
+
+// export const NoOptionsMessage = styled(Typography, {
+//     name: "CometAdminSelect",
+//     slot: "noOptionsMessage",
+//     overridesResolver(_, styles) {
+//         return [styles.noOptionsMessage];
+//     },
+// })(
+//     ({ theme }) => css`
+//         padding: ${theme.spacing(1)} ${theme.spacing(2)};
+//         color: ${theme.palette.text.secondary};
+//     `,
+// );
+
+// const SingleValue = styled("div", {
+//     name: "CometAdminSelect",
+//     slot: "singleValue",
+//     overridesResolver(_, styles) {
+//         return [styles.singleValue];
+//     },
+// })(css`
+//     overflow: hidden;
+//     text-overflow: "ellipsis";
+//     white-space: "nowrap";
+// `);
+
+// const Placeholder = styled("div", {
+//     name: "CometAdminSelect",
+//     slot: "placeholder",
+//     overridesResolver(_, styles) {
+//         return [styles.placeholder];
+//     },
+// })(
+//     ({ theme }) => css`
+//         color: ${theme.palette.text.disabled};
+//     `,
+// );
+
+// const Paper = styled(MuiPaper, {
+//     name: "CometAdminSelect",
+//     slot: "paper",
+//     overridesResolver(_, styles) {
+//         return [styles.paper];
+//     },
+// })();
+
+// const IndicatorsContainer = styled("div", {
+//     name: "CometAdminSelect",
+//     slot: "indicatorsContainer",
+//     overridesResolver(_, styles) {
+//         return [styles.indicatorsContainer];
+//     },
+// })(css`
+//     display: flex;
+// `);
+
+// const IndicatorSeparator = styled("span", {
+//     name: "CometAdminSelect",
+//     slot: "indicatorsSeparatpr",
+//     overridesResolver(_, styles) {
+//         return [styles.indicatorSeparator];
+//     },
+// })(
+//     ({ theme }) => css`
+//         width: 1;
+//         flex-grow: 1;
+//         background-color: ${theme.palette.divider};
+//     `,
+// );
+
+// const Indicator = styled("div", {
+//     name: "CometAdminSelect",
+//     slot: "indicator",
+//     overridesResolver(_, styles) {
+//         return [styles.indicator];
+//     },
+// })(
+//     ({ theme }) => css`
+//         display: flex;
+//         align-items: center;
+//         justify-content: center;
+//         color: ${theme.palette.grey[500]};
+//         width: 32px;
+//         cursor: pointer;
+//     `,
+// );
