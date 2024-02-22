37541d4383a7f9f9b42d327e139947f7ab6588d5
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-02-22 09:43:22 +0100

    Resolve conflicts

--- packages/api/cms-api/src/auth/guards/comet.guard.ts
+++ packages/api/cms-api/src/auth/guards/comet.guard.ts
@@ -19,11 +19,7 @@ export function createCometAuthGuard(type?: string | string[]): Type<IAuthGuard>
         }
 
         // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-<<<<<<< HEAD
-        handleRequest<CurrentUserInterface>(err: unknown, user: any, info: any): CurrentUserInterface {
-=======
-        handleRequest<CurrentUser>(err: unknown, user: any): CurrentUser {
->>>>>>> main
+        handleRequest<CurrentUser>(err: unknown, user: any, info: any): CurrentUser {
             if (err) {
                 throw err;
             }
