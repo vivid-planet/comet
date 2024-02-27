2a7bc765ca01224d1f28d246ba4e07f413e4f1ba
Ricky James Smith <jamesricky@me.com>
2024-02-27 14:03:28 +0100

    Refactor theming of remaining Admin components and remove module augmentation of `DefaultTheme` (#1758)
    ### Refactor theming of remaining admin-components
    - COM-487: `FieldSet` (Also added a dev-story)
    - COM-488: `Alert`
    - COM-489: `DatePickerNavigation`
    
    ### Remove module augmentation of `DefaultTheme` type
    As the `@mui/styles` package is no longer used for the
    styling-implementation of admin-components the `DefaultTheme` from
    `@mui/styles/defaultTheme` is no longer needed and would cause a
    typescript error when augmented.

---
+++ .changeset/strange-steaks-pull.md
@@ -0,0 +1,7 @@
+---
+"@comet/admin": major
+---
+
+Remove `message` class-key from `Alert`
+
+Use the `.MuiAlert-message` selector instead to style the message of the underlying MUI `Alert` component.
--- packages/admin/admin/src/alert/Alert.tsx
+++ packages/admin/admin/src/alert/Alert.tsx
@@ -1,11 +1,18 @@
 import { Close } from "@comet/admin-icons";
 // eslint-disable-next-line no-restricted-imports
-import { Alert as MuiAlert, AlertTitle, buttonClasses, IconButton, Theme, Typography } from "@mui/material";
-import { createStyles, WithStyles, withStyles } from "@mui/styles";
-import clsx from "clsx";
+import { Alert as MuiAlert, alertClasses, AlertTitle, buttonClasses, IconButton, Typography } from "@mui/material";
+import { css, styled, useThemeProps } from "@mui/material/styles";
+import { ThemedComponentBaseProps } from "helpers/ThemedComponentBaseProps";
 import * as React from "react";
 
-export interface AlertProps {
+export interface AlertProps
+    extends ThemedComponentBaseProps<{
+        root: typeof MuiAlert;
+        title: typeof AlertTitle;
+        text: typeof Typography;
+        action: "div";
+        closeIcon: typeof IconButton;
+    }> {
     severity?: "info" | "warning" | "error" | "success";
     title?: React.ReactNode;
     children?: React.ReactNode;
@@ -13,84 +20,140 @@ export interface AlertProps {
     action?: React.ReactNode;
 }
 
-export type AlertClassKey = "root" | "message" | "title" | "text" | "action" | "closeIcon" | "hasTitle" | "singleRow";
+export type AlertClassKey = "root" | "title" | "text" | "action" | "closeIcon" | "hasTitle" | "singleRow";
 
-const styles = (theme: Theme) =>
-    createStyles<AlertClassKey, AlertProps>({
-        root: {
-            padding: theme.spacing(4, "12px", 4, 4),
-        },
-        message: {
-            display: "flex",
-            alignItems: "center",
-            flexGrow: 1,
-        },
-        title: {},
-        text: {
-            flexGrow: 1,
-        },
-        action: {},
-        closeIcon: {},
-        hasTitle: {
-            position: "relative",
-            alignItems: "flex-start",
-            padding: theme.spacing(4, 6, "8px", 3),
+type OwnerState = {
+    hasTitle: boolean;
+    renderAsSingleRow: boolean;
+};
 
-            [`& .${buttonClasses.text}`]: {
-                marginLeft: -15,
-            },
+export const Alert = React.forwardRef<HTMLDivElement, AlertProps>((inProps, ref) => {
+    const {
+        severity = "info",
+        title,
+        children,
+        onClose,
+        action,
+        slotProps,
+        ...restProps
+    } = useThemeProps({ props: inProps, name: "CometAdminAlert" });
+    const singleRow = !title && (action || onClose);
 
-            "& $action": {
-                marginTop: theme.spacing(2),
-            },
+    const ownerState: OwnerState = {
+        hasTitle: Boolean(title),
+        renderAsSingleRow: Boolean(singleRow),
+    };
 
-            "& $closeIcon": {
-                position: "absolute",
-                right: 2,
-                top: 2,
-            },
-            "& $message": {
-                flexDirection: "column",
-                alignItems: "flex-start",
-            },
-        },
-        singleRow: {
-            display: "flex",
-            alignItems: "center",
-            padding: theme.spacing(2, "12px", 2, 4),
-        },
-    });
+    return (
+        <Root ref={ref} ownerState={ownerState} severity={severity} {...slotProps?.root} {...restProps}>
+            {Boolean(title) && <Title {...slotProps?.title}>{title}</Title>}
+            <Text variant="body2" {...slotProps?.text}>
+                {children}
+            </Text>
+            {action && (
+                <Action ownerState={ownerState} {...slotProps?.action}>
+                    {action}
+                </Action>
+            )}
+            {onClose && (
+                <CloseIcon onClick={onClose} ownerState={ownerState} {...slotProps?.closeIcon}>
+                    <Close />
+                </CloseIcon>
+            )}
+        </Root>
+    );
+});
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps & WithStyles<typeof styles>>(
-    ({ severity = "info", title, children, classes, onClose, action }, ref) => {
-        const singleRow = !title && (action || onClose);
-        return (
-            <MuiAlert
-                ref={ref}
-                classes={{
-                    root: clsx(classes.root, Boolean(title) && classes.hasTitle, singleRow && classes.singleRow),
-                    message: classes.message,
-                }}
-                severity={severity}
-            >
-                {Boolean(title) && <AlertTitle className={classes.title}>{title}</AlertTitle>}
-                <Typography className={classes.text} variant="body2">
-                    {children}
-                </Typography>
-                {action && <div className={classes.action}>{action}</div>}
-                {onClose && (
-                    <IconButton className={classes.closeIcon} onClick={onClose}>
-                        <Close />
-                    </IconButton>
-                )}
-            </MuiAlert>
-        );
+const Root = styled(MuiAlert, {
+    name: "CometAdminAlert",
+    slot: "root",
+    overridesResolver({ hasTitle, renderAsSingleRow }: OwnerState, styles) {
+        return [styles.root, hasTitle && styles.hasTitle, renderAsSingleRow && styles.singleRow];
     },
+})<{ ownerState: OwnerState }>(
+    ({ theme, ownerState }) => css`
+        padding: ${theme.spacing(4, "12px", 4, 4)};
+
+        & .${alertClasses.message} {
+            display: flex;
+            align-items: center;
+            flex-grow: 1;
+        }
+
+        ${ownerState.hasTitle &&
+        css`
+            position: relative;
+            align-items: flex-start;
+            padding: ${theme.spacing(4, 6, "8px", 3)};
+
+            & .${buttonClasses.text} {
+                margin-left: -15px;
+            }
+
+            & .${alertClasses.message} {
+                flex-direction: column;
+                align-items: flex-start;
+            }
+        `}
+
+        ${ownerState.renderAsSingleRow &&
+        css`
+            display: flex;
+            align-items: center;
+            padding: ${theme.spacing(2, "12px", 2, 4)};
+        `}
+    `,
 );
 
-const AdminComponentWithStyles = withStyles(styles, { name: "CometAdminAlert" })(Alert);
+const Title = styled(AlertTitle, {
+    name: "CometAdminAlert",
+    slot: "title",
+    overridesResolver(_, styles) {
+        return [styles.title];
+    },
+})();
 
-export { AdminComponentWithStyles as Alert };
+const Text = styled(Typography, {
+    name: "CometAdminAlert",
+    slot: "text",
+    overridesResolver(_, styles) {
+        return [styles.text];
+    },
+})(css`
+    flex-grow: 1;
+`);
+
+const Action = styled("div", {
+    name: "CometAdminAlert",
+    slot: "action",
+    overridesResolver(_, styles) {
+        return [styles.action];
+    },
+})<{ ownerState: OwnerState }>(
+    ({ theme, ownerState }) => css`
+        ${ownerState.hasTitle &&
+        css`
+            margin-top: ${theme.spacing(2)};
+        `}
+    `,
+);
+
+const CloseIcon = styled(IconButton, {
+    name: "CometAdminAlert",
+    slot: "closeIcon",
+    overridesResolver(_, styles) {
+        return [styles.closeIcon];
+    },
+})<{ ownerState: OwnerState }>(
+    ({ ownerState }) => css`
+        ${ownerState.hasTitle &&
+        css`
+            position: absolute;
+            right: 2px;
+            top: 2px;
+        `}
+    `,
+);
 
 declare module "@mui/material/styles" {
     interface ComponentsPropsList {
