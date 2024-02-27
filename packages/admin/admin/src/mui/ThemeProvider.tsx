592ac553674ab1abcffd8208fb8c1a0b7749b5a9
Ricky Smith <rs@vivid-planet.com>
2024-02-27 10:47:51 +0100

    Remove MUIs legacy class-name generation
    This happens automatically in MUI5.

--- packages/admin/admin/src/mui/ThemeProvider.tsx
+++ packages/admin/admin/src/mui/ThemeProvider.tsx
@@ -1,58 +1,16 @@
 import { Theme } from "@mui/material";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
-import { unstable_nested as nested } from "@mui/private-theming";
-import { createGenerateClassName, GenerateClassNameOptions, StylesProvider } from "@mui/styles";
-import type { GenerateId } from "jss";
 import * as React from "react";
 
-interface IProps {
+interface Props {
     theme: Theme;
     children: React.ReactNode;
 }
 
-// Docs: https://v4.mui.com/styles/api/#creategenerateclassname-options-class-name-generator
-// Code example: https://github.com/mui-org/material-ui/blob/v4.11.4/packages/material-ui-styles/src/createGenerateClassName/createGenerateClassName.js
-function createGenerateCometClassName(options: GenerateClassNameOptions = {}): GenerateId {
-    const { disableGlobal = false, seed = "" } = options;
-    const seedPrefix = seed === "" ? "" : `${seed}-`;
-    let ruleCounter = 0;
-
-    const getNextCounterId = () => {
-        ruleCounter += 1;
-        if (process.env.NODE_ENV !== "production") {
-            if (ruleCounter >= 1e10) {
-                // eslint-disable-next-line no-console
-                console.warn(["Comet Admin: You might have a memory leak.", "The ruleCounter is not supposed to grow that much."].join(""));
-            }
-        }
-        return ruleCounter;
-    };
-
-    const muiGenerateClassName = createGenerateClassName(options);
-
-    return (rule, styleSheet) => {
-        const name = (styleSheet?.options as any).name;
-
-        if (name && name.indexOf("Comet") === 0 && !styleSheet?.options.link && !disableGlobal) {
-            const prefix = `${seedPrefix}${name}-${rule.key}`;
-
-            if (!(styleSheet?.options as any).theme[nested] || seed !== "") {
-                return prefix;
-            }
-
-            return `${prefix}-${getNextCounterId()}`;
-        }
-
-        return muiGenerateClassName(rule, styleSheet);
-    };
-}
-
-export const MuiThemeProvider: React.FunctionComponent<IProps> = ({ theme, children }) => (
-    <StylesProvider generateClassName={createGenerateCometClassName()}>
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={theme}>
-                <>{children}</>
-            </ThemeProvider>
-        </StyledEngineProvider>
-    </StylesProvider>
+export const MuiThemeProvider: React.FunctionComponent<Props> = ({ theme, children }) => (
+    <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+            <>{children}</>
+        </ThemeProvider>
+    </StyledEngineProvider>
 );
