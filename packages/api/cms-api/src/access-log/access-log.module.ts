df827d63f7e18d8c3a6918785f57e3a609f94e95
Thomas Dax <thomas.dax@vivid-planet.com>
2024-02-28 10:55:24 +0100

    Replace non-existant CurrentUser with CurrentUserInterface

--- packages/api/cms-api/src/access-log/access-log.module.ts
+++ packages/api/cms-api/src/access-log/access-log.module.ts
@@ -1,12 +1,12 @@
 import { DynamicModule, Global, Module } from "@nestjs/common";
 import { APP_INTERCEPTOR } from "@nestjs/core";
 import { Request } from "express";
+import { CurrentUserInterface } from "src/auth/current-user/current-user";
 
-import { CurrentUser } from "../user-permissions/dto/current-user";
 import { SHOULD_LOG_REQUEST } from "./access-log.constants";
 import { AccessLogInterceptor } from "./access-log.interceptor";
 
-export type ShouldLogRequest = ({ user, req }: { user?: CurrentUser | true; req: Request }) => boolean;
+export type ShouldLogRequest = ({ user, req }: { user?: CurrentUserInterface | true; req: Request }) => boolean;
 
 interface AccessLogModuleOptions {
     shouldLogRequest?: ShouldLogRequest;
