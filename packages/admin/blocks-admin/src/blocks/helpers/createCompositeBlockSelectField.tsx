4e8a41f6cbb6f58a49919de3100010b042b10a66
Johannes <johannes.munker@vivid-planet.com>
2024-02-29 19:28:10 +0100

    feat(createCompositeBlockSelectField): use SelectField component

--- packages/admin/blocks-admin/src/blocks/helpers/createCompositeBlockSelectField.tsx
+++ packages/admin/blocks-admin/src/blocks/helpers/createCompositeBlockSelectField.tsx
@@ -1,4 +1,4 @@
-import { Field, FieldProps, FinalFormSelect } from "@comet/admin";
+import { SelectField, SelectFieldProps } from "@comet/admin";
 import { MenuItem } from "@mui/material";
 import * as React from "react";
 import { FormProps } from "react-final-form";
@@ -10,7 +10,7 @@ interface Options<T extends string | number> {
     defaultValue: T;
     options: Array<{ value: T; label: React.ReactNode }>;
     formProps?: Partial<FormProps<Record<"value", T>, Record<"value", T>>>;
-    fieldProps?: Partial<FieldProps<T, HTMLElement>>;
+    fieldProps?: Partial<SelectFieldProps<T>>;
 }
 
 export function createCompositeBlockSelectField<T extends string | number>({ defaultValue, formProps, fieldProps, options }: Options<T>) {
@@ -18,17 +18,13 @@ export function createCompositeBlockSelectField<T extends string | number>({ def
         defaultValue,
         AdminComponent: ({ state, updateState }) => (
             <BlocksFinalForm<{ value: typeof state }> onSubmit={({ value }) => updateState(value)} initialValues={{ value: state }} {...formProps}>
-                <Field name="value" {...fieldProps}>
-                    {(props) => (
-                        <FinalFormSelect {...props}>
-                            {options.map(({ value, label }) => (
-                                <MenuItem key={value} value={value}>
-                                    {label}
-                                </MenuItem>
-                            ))}
-                        </FinalFormSelect>
-                    )}
-                </Field>
+                <SelectField name="value" {...fieldProps}>
+                    {options.map(({ value, label }) => (
+                        <MenuItem key={value} value={value}>
+                            {label}
+                        </MenuItem>
+                    ))}
+                </SelectField>
             </BlocksFinalForm>
         ),
     });
