39d30b497f946cb74ae1db570cad3119bc0d0e92
Phillip Lechenauer <phillip.lechenauer@vivid-planet.com>
2024-03-04 10:14:04 +0100

    made loadImageAsBase64 a private method

--- packages/api/cms-api/src/content-generation/content-generation.service.ts
+++ packages/api/cms-api/src/content-generation/content-generation.service.ts
@@ -4,14 +4,6 @@ import fetch from "node-fetch";
 import { ContentGenerationConfig } from "./content-generation.config";
 import { CONTENT_GENERATION_CONFIG } from "./content-generation.constants";
 
-async function loadImageAsBase64(url: string) {
-    const response = await fetch(url);
-    const buffer = await response.buffer();
-    const base64String = buffer.toString("base64");
-    const mimeType = response.headers.get("content-type");
-    return `data:${mimeType};base64,${base64String}`;
-}
-
 @Injectable()
 export class ContentGenerationService {
     constructor(@Inject(CONTENT_GENERATION_CONFIG) private readonly config: ContentGenerationConfig) {}
@@ -22,7 +14,7 @@ export class ContentGenerationService {
             instructions:
                 "You are an alt text expert. The user will provide you with an image and your job is to provide a description of the image. This will help visually impaired users understand the content of the image. Keep yourself to a short description",
             image: {
-                base64: await loadImageAsBase64(imageUrl),
+                base64: await this.loadImageAsBase64(imageUrl),
             },
             options: {
                 maxTokens: 300,
@@ -36,8 +28,16 @@ export class ContentGenerationService {
             instructions:
                 "The user will provide you with an image. Your job is it two write a great short title for this image. Do not put the title inside quotation marks",
             image: {
-                base64: await loadImageAsBase64(imageUrl),
+                base64: await this.loadImageAsBase64(imageUrl),
             },
         });
+    }
+
+    private async loadImageAsBase64(url: string) {
+        const response = await fetch(url);
+        const buffer = await response.buffer();
+        const base64String = buffer.toString("base64");
+        const mimeType = response.headers.get("content-type");
+        return `data:${mimeType};base64,${base64String}`;
     }
 }
