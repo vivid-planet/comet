0a76c14f3eb18f6fa7b8037c7d0bc25e26b12e4a
Phillip Lechenauer <phillip.lechenauer@vivid-planet.com>
2024-03-04 09:58:16 +0100

    chnage: change permissions to permission per mutation

--- packages/api/cms-api/src/content-generation/content-generation.resolver.ts
+++ packages/api/cms-api/src/content-generation/content-generation.resolver.ts
@@ -4,15 +4,16 @@ import { RequiredPermission } from "../user-permissions/decorators/required-perm
 import { ContentGenerationService } from "./content-generation.service";
 
 @Resolver()
-@RequiredPermission(["dam"], { skipScopeCheck: true })
 export class ContentGenerationResolver {
     constructor(private readonly contentGenerationService: ContentGenerationService) {}
 
+    @RequiredPermission(["dam"], { skipScopeCheck: true })
     @Mutation(() => String)
     async generateAltText(@Args("imageUrl", { type: () => String }) imageUrl: string): Promise<string> {
         return this.contentGenerationService.generateAltText(imageUrl);
     }
 
+    @RequiredPermission(["dam"], { skipScopeCheck: true })
     @Mutation(() => String)
     async generateImageTitle(@Args("imageUrl", { type: () => String }) imageUrl: string): Promise<string> {
         return this.contentGenerationService.generateImageTitle(imageUrl);
