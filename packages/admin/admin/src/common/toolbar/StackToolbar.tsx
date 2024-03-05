03621379650e21d26eb37f07f55fdd92ed2c34fe
Niko Sams <ns@vivid-planet.com>
2024-03-05 16:28:43 +0100

    Fix naming of local variable that was copied from TabBar

--- packages/admin/admin/src/common/toolbar/StackToolbar.tsx
+++ packages/admin/admin/src/common/toolbar/StackToolbar.tsx
@@ -7,16 +7,16 @@ import { Toolbar } from "./Toolbar";
 export function StackToolbar(props: React.ComponentProps<typeof Toolbar>) {
     const stackApi = useStackApi();
     const stackSwitchApi = useStackSwitchApi();
-    let shouldShowTabBar = true;
+    let shouldShow = true;
     if (stackApi && stackSwitchApi) {
         // When inside a Stack show only the last TabBar
         const ownSwitchIndex = stackSwitchApi.id ? stackApi.switches.findIndex((i) => i.id === stackSwitchApi.id) : -1;
         const nextSwitchShowsInitialPage = stackApi.switches[ownSwitchIndex + 1] && stackApi.switches[ownSwitchIndex + 1].isInitialPageActive;
 
-        shouldShowTabBar = ownSwitchIndex === stackApi.switches.length - (nextSwitchShowsInitialPage ? 2 : 1);
+        shouldShow = ownSwitchIndex === stackApi.switches.length - (nextSwitchShowsInitialPage ? 2 : 1);
     }
 
-    if (!shouldShowTabBar) {
+    if (!shouldShow) {
         return null;
     }
 
