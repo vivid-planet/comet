c7d5328f9fe8b1ca6a69c4a36c1a434b2acfe73c
Johannes <johannes.munker@vivid-planet.com>
2024-03-06 14:19:56 +0100

    remove(createCompositeBlockTextField): formProps from options

--- packages/admin/blocks-admin/src/blocks/helpers/createCompositeBlockTextField.tsx
+++ packages/admin/blocks-admin/src/blocks/helpers/createCompositeBlockTextField.tsx
@@ -1,22 +1,19 @@
 import { TextField, TextFieldProps } from "@comet/admin";
 import * as React from "react";
-import { FormProps } from "react-final-form";
 
 import { BlocksFinalForm } from "../../form/BlocksFinalForm";
 import { createCompositeSetting } from "./composeBlocks/createCompositeSetting";
 
 interface Options {
     defaultValue: string;
-    formProps?: FormProps<Record<"value", string>, Record<"value", string>>;
     fieldProps?: Partial<TextFieldProps>;
 }
 
-export function createCompositeBlockTextField({ defaultValue, formProps, fieldProps }: Options) {
+export function createCompositeBlockTextField({ defaultValue, fieldProps }: Options) {
     return createCompositeSetting<string>({
         defaultValue,
         AdminComponent: ({ state, updateState }) => (
             <BlocksFinalForm<{ value: typeof state }>
-                {...formProps}
                 onSubmit={({ value }) => updateState(value ?? "")}
                 initialValues={{ value: state || undefined }}
             >
