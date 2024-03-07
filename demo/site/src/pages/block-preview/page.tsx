7588067e3164e37c2a0e876ad11da825ce648a00
Niko Sams <niko.sams@gmail.com>
2024-03-07 10:12:39 +0100

    Site loader: add caching mechanismn to requests made in block loader (#1682)
    This adds a simple cache for block loader in block preview. The cache is
    client side and in block preview only. No clearing, garbage collection
    or so are needed as it lives only in the preview page in the site
    iframe.
    
    Pro:
    - simple to use, no new api
    
    Con:
    - only works for graphql, adds hard dependency on graphql-request to
    `@comet/cms-site`
    
    TODO:
    
    - [ ] move re-usable parts into library (which would probably cause a
    hard dependency on graphql-request)
    
    alternative to #1681 which adds a cache a level above.

--- demo/site/src/pages/block-preview/page.tsx
+++ demo/site/src/pages/block-preview/page.tsx
@@ -1,8 +1,44 @@
-import { BlockPreviewProvider, buildGraphqlClientHeaders, createGraphQLClient, IFrameBridgeProvider, useIFrameBridge } from "@comet/cms-site";
+import {
+    BlockPreviewProvider,
+    buildGraphqlClientHeaders,
+    createGraphQLClient,
+    IFrameBridgeProvider,
+    PreviewData,
+    useIFrameBridge,
+} from "@comet/cms-site";
 import { PageContentBlockData } from "@src/blocks.generated";
 import { PageContentBlock } from "@src/blocks/PageContentBlock";
 import { recursivelyLoadBlockData } from "@src/recursivelyLoadBlockData";
+import { RequestDocument, Variables } from "graphql-request";
 import * as React from "react";
+
+function createCachingGraphQLClient(previewData?: PreviewData) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const cache: Record<string, any> = {};
+
+    const client = createGraphQLClient(previewData);
+    const originalRequest = client.request.bind(client);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async function requestWithCache<T = any, V = Variables>(
+        document: RequestDocument,
+        variables?: V,
+        requestHeaders?: RequestInit["headers"],
+    ): Promise<T> {
+        const documentString = typeof document === "string" ? document : document.loc?.source.body;
+        const cacheKey = JSON.stringify({ documentString, variables });
+
+        let result = cache[cacheKey];
+        if (result) {
+            return result;
+        } else {
+            result = await originalRequest(document, variables, requestHeaders);
+            cache[cacheKey] = result;
+        }
+        return result;
+    }
+    client.request = requestWithCache;
+    return client;
+}
 
 const PreviewPage: React.FunctionComponent = () => {
     const iFrameBridge = useIFrameBridge();
@@ -18,7 +54,7 @@ const PreviewPage: React.FunctionComponent = () => {
     }, [iFrameBridge.showOnlyVisible]);
 
     const clientRef = React.useRef(
-        createGraphQLClient({
+        createCachingGraphQLClient({
             previewDamUrls: true,
             includeInvisibleBlocks: !iFrameBridge.showOnlyVisible,
             includeInvisiblePages: true,
