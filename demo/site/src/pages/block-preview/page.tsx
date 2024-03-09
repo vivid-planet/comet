37c83912d26a5109b6cf477b4712cca68cfff265
Niko Sams <ns@vivid-planet.com>
2024-03-09 10:53:18 +0100

    Fix rebasing issues

--- demo/site/src/pages/block-preview/page.tsx
+++ demo/site/src/pages/block-preview/page.tsx
@@ -1,14 +1,8 @@
-import {
-    BlockPreviewProvider,
-    buildGraphqlClientHeaders,
-    createGraphQLClient,
-    IFrameBridgeProvider,
-    PreviewData,
-    useIFrameBridge,
-} from "@comet/cms-site";
+import { BlockPreviewProvider, buildGraphqlClientHeaders, IFrameBridgeProvider, PreviewData, useIFrameBridge } from "@comet/cms-site";
 import { PageContentBlockData } from "@src/blocks.generated";
 import { PageContentBlock } from "@src/blocks/PageContentBlock";
 import { recursivelyLoadBlockData } from "@src/recursivelyLoadBlockData";
+import createGraphQLClient from "@src/util/createGraphQLClient";
 import { RequestDocument, Variables } from "graphql-request";
 import * as React from "react";
 
@@ -44,18 +38,14 @@ function useGraphQLClient() {
     const iFrameBridge = useIFrameBridge();
     const clientRef = React.useRef(
         createCachingGraphQLClient({
-            previewDamUrls: true,
-            includeInvisibleBlocks: !iFrameBridge.showOnlyVisible,
-            includeInvisiblePages: true,
+            includeInvisible: !iFrameBridge.showOnlyVisible,
         }),
     );
     React.useEffect(() => {
         //update headers when showOnlyVisible changes
         clientRef.current.setHeaders(
             buildGraphqlClientHeaders({
-                previewDamUrls: true,
-                includeInvisibleBlocks: !iFrameBridge.showOnlyVisible,
-                includeInvisiblePages: true,
+                includeInvisible: !iFrameBridge.showOnlyVisible,
             }),
         );
     }, [iFrameBridge.showOnlyVisible]);
