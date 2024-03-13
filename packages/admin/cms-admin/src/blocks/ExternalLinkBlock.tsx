28695b5aaefed80ae774cb379aeea245a74fbb6c
Johannes <johannes.munker@vivid-planet.com>
2024-03-13 10:07:51 +0100

    refactor(isHref): rename isHref file to isLinkTarget

--- packages/admin/cms-admin/src/blocks/ExternalLinkBlock.tsx
+++ packages/admin/cms-admin/src/blocks/ExternalLinkBlock.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { FormattedMessage } from "react-intl";
 
 import { ExternalLinkBlockData, ExternalLinkBlockInput } from "../blocks.generated";
-import { isLinkTarget } from "../validation/isHref";
+import { isLinkTarget } from "../validation/isLinkTarget";
 import { validateUrl } from "../validation/validateUrl";
 
 type State = ExternalLinkBlockData;
