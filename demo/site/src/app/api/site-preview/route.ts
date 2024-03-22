20b0680eec823daba763877b3c04d953e546edab
Niko Sams <ns@vivid-planet.com>
2024-03-22 10:12:38 +0100

    Convert site-preview and status api to app router
    TODO: getValidatedSitePreviewParams

---
+++ demo/site/src/app/api/site-preview/route.ts
@@ -0,0 +1,16 @@
+import { draftMode } from "next/headers";
+import { redirect } from "next/navigation";
+import { type NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: NextRequest) {
+    const params = request.nextUrl.searchParams;
+
+    //TODO const params = await getValidatedSitePreviewParams(req, res, createGraphQLClient());
+
+    // You might want to store params.scope now
+    draftMode().enable();
+
+    redirect(params.get("path") || "/");
+}
