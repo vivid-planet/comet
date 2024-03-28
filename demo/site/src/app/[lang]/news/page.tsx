88ae6677b0a6b883c71ec105284e02c79437a67a
Niko Sams <ns@vivid-planet.com>
2024-03-28 13:20:18 +0100

    support lang for news

--- demo/site/src/app/[lang]/news/page.tsx
+++ demo/site/src/app/[lang]/news/page.tsx
@@ -1,21 +1,20 @@
-import { defaultLanguage, domain } from "@src/config";
+import { PreviewData } from "@src/app/api/site-preview/route";
+import { domain } from "@src/config";
 import { NewsList } from "@src/news/NewsList";
 import { newsListFragment } from "@src/news/NewsList.fragment";
 import createGraphQLClient from "@src/util/createGraphQLClient";
 import { gql } from "graphql-request";
 import { draftMode } from "next/headers";
 
-import { PreviewData } from "../api/site-preview/route";
 import { GQLNewsIndexPageQuery, GQLNewsIndexPageQueryVariables } from "./page.generated";
 
-export default async function NewsIndexPage() {
+export default async function NewsIndexPage({ params }: { params: { lang: string } }) {
     let previewData: PreviewData | undefined = undefined;
     if (draftMode().isEnabled) {
         previewData = { includeInvisible: false };
     }
     const client = createGraphQLClient(previewData);
-    const locale = /*context.locale ??*/ defaultLanguage;
-    const scope = { domain, language: locale };
+    const scope = { domain, language: params.lang };
 
     const { newsList } = await client.request<GQLNewsIndexPageQuery, GQLNewsIndexPageQueryVariables>(
         gql`
