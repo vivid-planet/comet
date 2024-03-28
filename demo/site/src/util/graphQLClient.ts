03fdc03bef0672bf073fc2030914e1b42a29999b
Niko Sams <niko.sams@gmail.com>
2024-03-28 17:18:58 +0100

    Remove unused export

--- demo/site/src/util/graphQLClient.ts
+++ demo/site/src/util/graphQLClient.ts
@@ -1,7 +1,7 @@
 import { createFetchWithDefaults, createFetchWithPreviewHeaders, createGraphQLFetch as createGraphQLFetchLibrary } from "@comet/cms-site";
 import { SitePreviewData } from "@src/app/api/site-preview/route";
 
-export const graphQLApiUrl = `${typeof window === "undefined" ? process.env.API_URL_INTERNAL : process.env.NEXT_PUBLIC_API_URL}/graphql`;
+const graphQLApiUrl = `${typeof window === "undefined" ? process.env.API_URL_INTERNAL : process.env.NEXT_PUBLIC_API_URL}/graphql`;
 export function createGraphQLFetch(previewData?: SitePreviewData) {
     return createGraphQLFetchLibrary(
         createFetchWithDefaults(createFetchWithPreviewHeaders(fetch, previewData), { next: { revalidate: 15 * 60 } }),
