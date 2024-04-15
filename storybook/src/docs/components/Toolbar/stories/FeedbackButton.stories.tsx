ed7101a50e29ab633adb08dfd5cc3c9fc93f6c38
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2024-04-15 16:13:50 +0200

    add story for FeedbackButton

---
+++ storybook/src/docs/components/Toolbar/stories/FeedbackButton.stories.tsx
@@ -0,0 +1,37 @@
+import { FeedbackButton, Toolbar, ToolbarActions, ToolbarFillSpace, ToolbarTitleItem } from "@comet/admin";
+import { Assets } from "@comet/admin-icons";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import { FormattedMessage } from "react-intl";
+
+import { storyRouterDecorator } from "../../../../story-router.decorator";
+import { toolbarDecorator } from "../toolbar.decorator";
+
+storiesOf("stories/components/Toolbar/Feedback Button", module)
+    .addDecorator(toolbarDecorator())
+    .addDecorator(storyRouterDecorator())
+    .add("Feedback", () => {
+        const [loading, setLoading] = React.useState(false);
+        return (
+            <Toolbar>
+                <ToolbarTitleItem>Feedback Button</ToolbarTitleItem>
+                <ToolbarFillSpace />
+                <ToolbarActions>
+                    <FeedbackButton
+                        color="primary"
+                        variant="contained"
+                        loading={loading}
+                        onClick={() => {
+                            setLoading(true);
+                            setTimeout(() => {
+                                setLoading(false);
+                            }, 1000);
+                        }}
+                        startIcon={<Assets />}
+                    >
+                        <FormattedMessage id="comet.feedback" defaultMessage="Feedback" />
+                    </FeedbackButton>
+                </ToolbarActions>
+            </Toolbar>
+        );
+    });
