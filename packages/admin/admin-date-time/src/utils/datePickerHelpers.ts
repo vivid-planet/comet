ad73068f461848bcb9c32e304a008eaa8b942e51
Ricky James Smith <jamesricky@me.com>
2024-04-15 11:42:22 +0200

    Change `DatePicker` and `DateRangePicker` values from `Date` to `string` (#1916)
    The reason for this change is that when selecting a date like
    `2024-04-10` in a timezone ahead of UTC, it would be stored in a `Date`
    object as e.g. `2024-04-09T22:00:00.000Z`. When only the date is saved
    to the database, without the time, it would be saved as `2024-04-09`,
    which differs from the selected date.

--- packages/admin/admin-date-time/src/utils/datePickerHelpers.ts
+++ packages/admin/admin-date-time/src/utils/datePickerHelpers.ts
@@ -1,5 +1,11 @@
+import { format } from "date-fns";
+
 export const defaultMinDate = new Date();
 defaultMinDate.setFullYear(defaultMinDate.getFullYear() - 120);
 
 export const defaultMaxDate = new Date();
 defaultMaxDate.setFullYear(defaultMaxDate.getFullYear() + 40);
+
+export const getIsoDateString = (date: Date) => {
+    return format(date, "yyyy-MM-dd");
+};
