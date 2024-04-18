207acd2d15c4cd50ab25c46e7b9b3af093dc00bf
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-04-18 14:01:08 +0200

    Remove unnecessary createAsyncTraverse function

--- packages/api/cms-api/src/blocks/blocks-transformer.ts
+++ packages/api/cms-api/src/blocks/blocks-transformer.ts
@@ -1,55 +1,54 @@
-import { BlockContext, BlockDataInterface, isBlockDataInterface } from "@comet/blocks-api";
+import {
+    BlockContext,
+    BlockDataInterface,
+    BlockTransformerServiceInterface,
+    isBlockDataInterface,
+    TraversableTransformResponse,
+} from "@comet/blocks-api";
+import { Type } from "@nestjs/common";
 import { INJECTABLE_WATERMARK } from "@nestjs/common/constants";
 import { ModuleRef } from "@nestjs/core";
 import opentelemetry from "@opentelemetry/api";
 
 const tracer = opentelemetry.trace.getTracer("@comet/cms-api");
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function transformToPlain(block: BlockDataInterface, ctx: BlockContext, moduleRef: ModuleRef): Promise<any> {
+export async function transformToPlain(block: BlockDataInterface, context: BlockContext, moduleRef: ModuleRef): Promise<unknown> {
     return tracer.startActiveSpan("BlockTransformer", async (span) => {
-        const traverse = createAsyncTraverse("transformToPlain", [ctx], isBlockDataInterface, moduleRef);
-        // TODO is await correct here?
-        const ret = await traverse(block);
+        async function traverse(json: unknown): Promise<unknown> {
+            if (Array.isArray(json)) {
+                return Promise.all(json.map(traverse));
+            } else if (typeof json === "object" && json !== null) {
+                let entries: [string, unknown][];
+
+                if (isBlockDataInterface(json)) {
+                    const transformResponse = await json.transformToPlain(context);
+
+                    if (isBlockTransformerService(transformResponse)) {
+                        // TODO Support transient or request-scoped services using moduleRef.resolve?
+                        const service = moduleRef.get(transformResponse, { strict: false });
+                        entries = Object.entries(await service.transformToPlain(json, context));
+                    } else {
+                        entries = Object.entries(transformResponse);
+                    }
+                } else {
+                    entries = Object.entries(json);
+                }
+
+                return Object.fromEntries(await Promise.all(entries.map(async ([key, value]: [string, unknown]) => [key, await traverse(value)])));
+            } else {
+                // Keep literal as it is
+                return json;
+            }
+        }
+
+        const result = await traverse(block);
         span.end();
-        return ret;
+        return result;
     });
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function createAsyncTraverse(methodName: string, argsArray: any[], isTargetObject: (obj: any) => boolean = () => true, moduleRef: ModuleRef) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return async function traverse(jsonObj: any): Promise<any> {
-        if (Array.isArray(jsonObj)) {
-            return Promise.all(jsonObj.map(traverse));
-        } else if (jsonObj !== null && typeof jsonObj === "object") {
-            let entries: [string, unknown][];
-
-            if (isTargetObject(jsonObj) && typeof jsonObj[methodName] === "function") {
-                const methodResponse = await jsonObj[methodName](...argsArray);
-
-                const isService = Reflect.hasMetadata(INJECTABLE_WATERMARK, methodResponse);
-
-                if (isService) {
-                    // TODO Support transient or request-scoped services using moduleRef.resolve?
-                    const service = await moduleRef.get(methodResponse, { strict: false });
-                    entries = Object.entries(await service.transformToPlain(jsonObj, argsArray[0]));
-                } else {
-                    entries = Object.entries(methodResponse);
-                }
-            } else {
-                entries = Object.entries(jsonObj);
-            }
-
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const mappedEntries = entries.map(async ([k, i]: [string, any]) => {
-                return [k, await traverse(i)];
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            }) as any;
-            return Object.fromEntries(await Promise.all(mappedEntries));
-        } else {
-            // keep literal as it is
-            return jsonObj;
-        }
-    };
+function isBlockTransformerService(
+    transformResponse: Type<BlockTransformerServiceInterface> | TraversableTransformResponse,
+): transformResponse is Type<BlockTransformerServiceInterface> {
+    return Reflect.hasMetadata(INJECTABLE_WATERMARK, transformResponse);
 }
