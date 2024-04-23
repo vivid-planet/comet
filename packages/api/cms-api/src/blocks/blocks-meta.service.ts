a3f8c85faf064a5e193944b3d9f76563394a9276
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-04-23 14:21:23 +0200

    Prevent block-meta.json write in read-only file systems This issue came up in one of our customer projects that has a read-only file system. Use `fs.access` to check if we can write before writing the file.

--- packages/api/cms-api/src/blocks/blocks-meta.service.ts
+++ packages/api/cms-api/src/blocks/blocks-meta.service.ts
@@ -4,7 +4,18 @@ import { promises as fs } from "fs";
 
 export class BlocksMetaService implements OnModuleInit {
     async onModuleInit(): Promise<void> {
-        const metaJson = getBlocksMeta();
-        await fs.writeFile("block-meta.json", JSON.stringify(metaJson, null, 4));
+        let canWrite: boolean;
+
+        try {
+            await fs.access("block-meta.json", fs.constants.W_OK);
+            canWrite = true;
+        } catch {
+            canWrite = false;
+        }
+
+        if (canWrite) {
+            const metaJson = getBlocksMeta();
+            await fs.writeFile("block-meta.json", JSON.stringify(metaJson, null, 4));
+        }
     }
 }
