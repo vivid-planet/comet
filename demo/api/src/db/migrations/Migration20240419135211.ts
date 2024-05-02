8c5593791b8857912089a1bd7a3c4acb363a8226
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2024-05-02 12:50:59 +0200

    Fix ProductToTag migration

--- demo/api/src/db/migrations/Migration20240419135211.ts
+++ demo/api/src/db/migrations/Migration20240419135211.ts
@@ -5,8 +5,8 @@ export class Migration20240419135211 extends Migration {
   async up(): Promise<void> {
     this.addSql('create table "ProductToTag" ("id" uuid not null, "product" uuid not null, "tag" uuid not null, "exampleStatus" boolean not null default true, constraint "ProductToTags_pkey" primary key ("id"));');
 
-    this.addSql('alter table "ProductToTags" add constraint "ProductToTag_product_foreign" foreign key ("product") references "Product" ("id") on update cascade on delete cascade;');
-    this.addSql('alter table "ProductToTags" add constraint "ProductToTag_tag_foreign" foreign key ("tag") references "ProductTag" ("id") on update cascade on delete cascade;');
+    this.addSql('alter table "ProductToTag" add constraint "ProductToTag_product_foreign" foreign key ("product") references "Product" ("id") on update cascade on delete cascade;');
+    this.addSql('alter table "ProductToTag" add constraint "ProductToTag_tag_foreign" foreign key ("tag") references "ProductTag" ("id") on update cascade on delete cascade;');
   }
 
   async down(): Promise<void> {
