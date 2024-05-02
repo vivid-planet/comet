ce98e4daadf240ae835e413951f44dc0cb82a502
Niko Sams <niko.sams@gmail.com>
2024-05-02 08:16:08 +0200

    Site Config: Simplify types, no need for generics (#2021)

--- site-configs.types.ts
+++ site-configs.types.ts
@@ -1,9 +1,11 @@
 import type { SiteConfig as SiteConfigLibrary, SiteConfigPrivate, SiteConfigPublic } from "@comet/cli";
 
-export type SiteConfig = SiteConfigLibrary<{
-    domain: string;
-    language: string;
-}>;
+export type SiteConfig = SiteConfigLibrary & {
+    contentScope: {
+        domain: string;
+        language: string;
+    }
+};
 
 export type PrivateSiteConfig = SiteConfigPrivate<SiteConfig>;
 export type PublicSiteConfig = SiteConfigPublic<SiteConfig>;
