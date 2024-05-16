90331751b23627f2ac3e90862dde6730dcd9f6a2
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2024-05-16 08:52:38 +0200

    remove type from CurrencyField

--- packages/admin/admin/src/form/fields/CurrencyField.tsx
+++ packages/admin/admin/src/form/fields/CurrencyField.tsx
@@ -3,6 +3,6 @@ import * as React from "react";
 import { Field, FieldProps } from "../Field";
 import { FinalFormCurrencyInput } from "../FinalFormCurrencyInput";
 
-export const CurrencyField = ({ ...restProps }: FieldProps): React.ReactElement => {
+export const CurrencyField = ({ ...restProps }: FieldProps) => {
     return <Field component={FinalFormCurrencyInput} {...restProps} />;
 };
