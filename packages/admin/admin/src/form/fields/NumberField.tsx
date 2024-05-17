eb96149d62bd3f8116fbd7d9a6fc7211e2beced8
juliawegmayr <109900447+juliawegmayr@users.noreply.github.com>
2024-05-17 13:42:21 +0200

    change FieldProps type to number instead string
    Co-authored-by: Ricky James Smith <jamesricky@me.com>

--- packages/admin/admin/src/form/fields/NumberField.tsx
+++ packages/admin/admin/src/form/fields/NumberField.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { Field, FieldProps } from "../Field";
 import { FinalFormNumberInput } from "../FinalFormNumberInput";
 
-export type NumberFieldProps = FieldProps<string, HTMLInputElement>;
+export type NumberFieldProps = FieldProps<number, HTMLInputElement>;
 
 export const NumberField = ({ ...restProps }: NumberFieldProps) => {
     return <Field component={FinalFormNumberInput} {...restProps} />;
