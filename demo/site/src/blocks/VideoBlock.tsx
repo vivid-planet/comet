a8793d074e2fa308838b4c2e60166666250c0d17
Johannes <johannes.munker@vivid-planet.com>
2024-05-21 10:23:32 +0200

    refactor(VideoBlock): to use OneOfBlock helper

--- demo/site/src/blocks/VideoBlock.tsx
+++ demo/site/src/blocks/VideoBlock.tsx
@@ -1,31 +1,17 @@
-import { PreviewSkeleton, PropsWithData, VimeoVideoBlock, withPreview, YouTubeVideoBlock } from "@comet/cms-site";
-import { DamImageBlockData, DamVideoBlockData, VimeoVideoBlockData, YouTubeVideoBlockData } from "@src/blocks.generated";
+import { OneOfBlock, PropsWithData, SupportedBlocks, VimeoVideoBlock, withPreview, YouTubeVideoBlock } from "@comet/cms-site";
+import { VideoBlockData } from "@src/blocks.generated";
 import DamVideoBlock from "@src/blocks/DamVideoBlock";
+import * as React from "react";
+
+const supportedBlocks: SupportedBlocks = {
+    youtubeVideo: (props) => <YouTubeVideoBlock data={props} />,
+    damVideo: (props) => <DamVideoBlock data={props} />,
+    vimeoVideo: (props) => <VimeoVideoBlock data={props} />,
+};
 
 const VideoBlock = withPreview(
-    ({ data: { block } }: PropsWithData<DamImageBlockData>) => {
-        if (!block) {
-            return <PreviewSkeleton type="media" hasContent={false} />;
-        }
-
-        switch (block.type) {
-            case "damVideo":
-                return <DamVideoBlock data={block.props as DamVideoBlockData} />;
-            case "youtubeVideo":
-                return <YouTubeVideoBlock data={block.props as YouTubeVideoBlockData} />;
-            case "vimeoVideo":
-                return <VimeoVideoBlock data={block.props as VimeoVideoBlockData} />;
-            default:
-                if (process.env.NODE_ENV === "development") {
-                    return (
-                        <pre>
-                            Unknown type ({block.type}): {JSON.stringify(block.props)}
-                        </pre>
-                    );
-                }
-
-                return null;
-        }
+    ({ data }: PropsWithData<VideoBlockData>) => {
+        return <OneOfBlock data={data} supportedBlocks={supportedBlocks} />;
     },
     { label: "Video" },
 );
