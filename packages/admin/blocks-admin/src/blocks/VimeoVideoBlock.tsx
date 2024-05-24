e884a958df1d17b4006b722656c83d77449eb8df
Johannes <johannes.munker@vivid-planet.com>
2024-05-24 10:19:17 +0200

    feat(VimeoVideoBlock): add fields aspectRatio, showControls and loop to admin, refactor admin, add validation

--- packages/admin/blocks-admin/src/blocks/VimeoVideoBlock.tsx
+++ packages/admin/blocks-admin/src/blocks/VimeoVideoBlock.tsx
@@ -1,52 +1,112 @@
-import { Field, FinalFormInput, FinalFormSwitch } from "@comet/admin";
+import { Field, FieldContainer, FinalFormInput, FinalFormRadio, FinalFormSwitch } from "@comet/admin";
+import { Box, FormControlLabel } from "@mui/material";
 import * as React from "react";
 import { FormattedMessage } from "react-intl";
 
-import { VimeoVideoBlockData } from "../blocks.generated";
+import { VimeoVideoBlockData, VimeoVideoBlockInput } from "../blocks.generated";
 import { BlocksFinalForm } from "../form/BlocksFinalForm";
-import { createCompositeBlock } from "./factories/createCompositeBlock";
-import { createCompositeSettings } from "./helpers/composeBlocks/createCompositeSettings";
-import { BlockCategory } from "./types";
+import { SelectPreviewComponent } from "../iframebridge/SelectPreviewComponent";
+import { useAdminComponentPaper } from "./common/AdminComponentPaper";
+import { createBlockSkeleton } from "./helpers/createBlockSkeleton";
+import { BlockCategory, BlockInterface } from "./types";
 
-export const VimeoVideoBlock = createCompositeBlock(
-    {
-        name: "VimeoVideo",
-        displayName: <FormattedMessage id="blocks.vimeoVideo" defaultMessage="Video (Vimeo)" />,
-        category: BlockCategory.Media,
-        blocks: {
-            settings: {
-                block: createCompositeSettings<Pick<VimeoVideoBlockData, "vimeoIdentifier" | "autoplay">>({
-                    defaultValues: { vimeoIdentifier: "", autoplay: false },
-                    AdminComponent: ({ state, updateState }) => {
-                        return (
-                            <BlocksFinalForm onSubmit={updateState} initialValues={state}>
-                                <Field
-                                    name="vimeoIdentifier"
-                                    label={<FormattedMessage id="blocks.vimeoVideo.vimeoIdentifier" defaultMessage="Vimeo URL or Vimeo Video ID" />}
-                                    type="text"
-                                    component={FinalFormInput}
-                                    fullWidth
-                                />
+const isValidVimeoIdentifier = (value: string) => {
+    const urlRegEx = /(https?:\/\/)?(www\.)?(player\.)?vimeo\.com\/?(showcase\/)*([0-9)([a-z]*\/)*([0-9]{6,11})[?]?.*/;
+    const idRegEx = /([0-9]{6,11})/;
 
-                                <Field
-                                    name="autoplay"
-                                    label={<FormattedMessage id="blocks.vimeoVideo.autoplay" defaultMessage="Autoplay" />}
-                                    type="checkbox"
-                                    component={FinalFormSwitch}
-                                />
-                            </BlocksFinalForm>
-                        );
-                    },
-                }),
-            },
-        },
+    const urlMatch = value.match(urlRegEx);
+    const idMatch = value.match(idRegEx);
+
+    return !!urlMatch || !!idMatch;
+};
+
+const validateIdentifier = (value?: string) => {
+    return value && isValidVimeoIdentifier(value) ? undefined : (
+        <FormattedMessage id="comet.blocks.vimeoVideo.validation" defaultMessage="Should be a valid Vimeo URL or identifier" />
+    );
+};
+
+export const VimeoVideoBlock: BlockInterface<VimeoVideoBlockData, VimeoVideoBlockData, VimeoVideoBlockInput> = {
+    ...createBlockSkeleton(),
+
+    name: "VimeoVideo",
+
+    displayName: <FormattedMessage id="blocks.vimeoVideo" defaultMessage="Video (Vimeo)" />,
+
+    defaultValues: () => ({ vimeoIdentifier: "", autoplay: false, showControls: true, loop: false, aspectRatio: "16X9" }),
+
+    category: BlockCategory.Media,
+
+    // !vimeoIdentifier to allow saving empty string
+    isValid: ({ vimeoIdentifier }) => !vimeoIdentifier || isValidVimeoIdentifier(vimeoIdentifier),
+
+    createPreviewState: (state, previewCtx) => {
+        return { ...state, autoplay: false, adminMeta: { route: previewCtx.parentUrl } };
     },
-    (block) => {
-        return {
-            ...block,
-            previewContent: (state) => {
-                return state.vimeoIdentifier ? [{ type: "text", content: state.vimeoIdentifier }] : [];
-            },
-        };
+    definesOwnPadding: true,
+    AdminComponent: ({ state, updateState }) => {
+        const isInPaper = useAdminComponentPaper();
+
+        return (
+            <Box padding={isInPaper ? 3 : 0} pb={0}>
+                <SelectPreviewComponent>
+                    <BlocksFinalForm
+                        onSubmit={(newState) => {
+                            updateState({ ...state, ...newState });
+                        }}
+                        initialValues={state}
+                    >
+                        <Field
+                            name="vimeoIdentifier"
+                            label={<FormattedMessage id="blocks.vimeoVideo.vimeoIdentifier" defaultMessage="Vimeo URL or Vimeo Video ID" />}
+                            validate={validateIdentifier}
+                            type="text"
+                            component={FinalFormInput}
+                            fullWidth
+                            disableContentTranslation
+                        />
+                        <FieldContainer label={<FormattedMessage id="comet.blocks.vimeoVideo.aspectRatio" defaultMessage="Aspect Ratio" />}>
+                            <Field name="aspectRatio" type="radio" value="16X9">
+                                {(props) => (
+                                    <FormControlLabel
+                                        label={<FormattedMessage id="comet.blocks.vimeoVideo.aspectRatio.16X9" defaultMessage="16:9" />}
+                                        control={<FinalFormRadio {...props} />}
+                                    />
+                                )}
+                            </Field>
+                            <Field name="aspectRatio" type="radio" value="4X3">
+                                {(props) => (
+                                    <FormControlLabel
+                                        label={<FormattedMessage id="comet.blocks.vimeoVideo.aspectRatio.4X3" defaultMessage="4:3" />}
+                                        control={<FinalFormRadio {...props} />}
+                                    />
+                                )}
+                            </Field>
+                        </FieldContainer>
+                        <Field
+                            label={<FormattedMessage id="blocks.vimeoVideo.autoplay" defaultMessage="Autoplay" />}
+                            name="autoplay"
+                            type="checkbox"
+                            component={FinalFormSwitch}
+                        />
+                        <Field
+                            label={<FormattedMessage id="blocks.vimeoVideo.showControls" defaultMessage="Show controls" />}
+                            name="showControls"
+                            type="checkbox"
+                            component={FinalFormSwitch}
+                        />
+                        <Field
+                            label={<FormattedMessage id="blocks.vimeoVideo.loop" defaultMessage="Loop" />}
+                            name="loop"
+                            type="checkbox"
+                            component={FinalFormSwitch}
+                        />
+                    </BlocksFinalForm>
+                </SelectPreviewComponent>
+            </Box>
+        );
     },
-);
+    previewContent: (state) => {
+        return state.vimeoIdentifier ? [{ type: "text", content: state.vimeoIdentifier }] : [];
+    },
+};
