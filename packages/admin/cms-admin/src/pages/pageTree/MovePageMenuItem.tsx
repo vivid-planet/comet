86d01fdb3439fd4f34e028cc8fe98a0397f78f8f
Thomas Dax <thomas.dax@vivid-planet.com>
2024-05-27 14:18:43 +0200

    Check if target category supports documentType before moving document to another category

--- packages/admin/cms-admin/src/pages/pageTree/MovePageMenuItem.tsx
+++ packages/admin/cms-admin/src/pages/pageTree/MovePageMenuItem.tsx
@@ -1,10 +1,11 @@
 import { gql, useMutation } from "@apollo/client";
-import { RowActionsItem, RowActionsMenu } from "@comet/admin";
+import { RowActionsItem, RowActionsMenu, useErrorDialog } from "@comet/admin";
 import { MovePage } from "@comet/admin-icons";
 import * as React from "react";
 import { FormattedMessage } from "react-intl";
 
 import { useContentScope } from "../../contentScope/Provider";
+import { DocumentInterface, DocumentType } from "../../documents/types";
 import { GQLUpdatePageTreeNodeCategoryMutation, GQLUpdatePageTreeNodeCategoryMutationVariables } from "./MovePageMenuItem.generated";
 import { PageTreePage } from "./usePageTree";
 import { usePageTreeContext } from "./usePageTreeContext";
@@ -26,13 +27,19 @@ export function MovePageMenuItem({ page }: Props): React.ReactElement | null {
         }
     `);
     const { scope } = useContentScope();
-    const { allCategories, query } = usePageTreeContext();
+    const { allCategories, query, categoryToDocumentTypesMap } = usePageTreeContext();
+    const errorDialogApi = useErrorDialog();
 
     if (allCategories.length <= 1) {
         return null;
     }
 
     const handleSubMenuItemClick = async (category: string) => {
+        if (!categorySupportsDocumentType(category, page.documentType, categoryToDocumentTypesMap)) {
+            errorDialogApi?.showError({ error: `Cannot move: Target category doesn't support documentType ${page.documentType}` });
+            return;
+        }
+
         const refetchQueries = [
             { query, variables: { contentScope: scope, category } },
             { query, variables: { contentScope: scope, category: page.category } },
@@ -46,11 +53,35 @@ export function MovePageMenuItem({ page }: Props): React.ReactElement | null {
 
     return (
         <RowActionsMenu icon={<MovePage />} text={<FormattedMessage id="comet.pages.pages.page.movePage" defaultMessage="Move page" />}>
-            {allCategories.map(({ category, label }) => (
-                <RowActionsItem key={category} disabled={category === page.category || submitting} onClick={() => handleSubMenuItemClick(category)}>
-                    {label}
-                </RowActionsItem>
-            ))}
+            {allCategories.map(({ category, label }) => {
+                const canMoveToTargetCategory = categorySupportsDocumentType(category, page.documentType, categoryToDocumentTypesMap);
+
+                return (
+                    <RowActionsItem
+                        key={category}
+                        disabled={category === page.category || !canMoveToTargetCategory || submitting}
+                        onClick={() => handleSubMenuItemClick(category)}
+                    >
+                        {label}
+                    </RowActionsItem>
+                );
+            })}
         </RowActionsMenu>
     );
 }
+
+const categorySupportsDocumentType = (
+    category: string,
+    documentType: string,
+    categoryToDocumentTypesMap?: {
+        [category: string]: Record<DocumentType, DocumentInterface>;
+    },
+) => {
+    if (categoryToDocumentTypesMap === undefined) {
+        // fallback because categoryToDocumentTypesMap can be undefined
+        return true;
+    }
+
+    const supportedDocumentTypes = Object.keys(categoryToDocumentTypesMap[category]);
+    return supportedDocumentTypes.includes(documentType);
+};
