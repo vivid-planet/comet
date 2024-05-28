1f22191576819242bcb4d53ae4ecd31422d52298
Thomas Dax <thomas.dax@vivid-planet.com>
2024-05-28 13:40:09 +0200

    Show no validation error for youtubeIdentifier if it's empty

--- packages/admin/blocks-admin/src/blocks/YouTubeVideoBlock.tsx
+++ packages/admin/blocks-admin/src/blocks/YouTubeVideoBlock.tsx
@@ -23,6 +23,8 @@ const isValidYouTubeIdentifier = (value: string) => {
 };
 
 const validateIdentifier = (value?: string) => {
+    if (!value) return false;
+
     return value && isValidYouTubeIdentifier(value) ? undefined : (
         <FormattedMessage id="comet.blocks.youTubeVideo.validation" defaultMessage="Should be a valid YouTube URL or identifier" />
     );
