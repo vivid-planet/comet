ce26574917d6fc7e494ffa6e68468fa56274bcb8
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-06-04 10:35:32 +0200

    Fix styled-components v6 type issues This is done by removing `Typography` which is based on our initial customer project. We should instead use the new typography component from Starter once the theme setup is finished.

--- demo/site/src/blocks/RichTextBlock.tsx
+++ demo/site/src/blocks/RichTextBlock.tsx
@@ -1,14 +1,11 @@
 "use client";
 import { hasRichTextBlockContent, PreviewSkeleton, PropsWithData, withPreview } from "@comet/cms-site";
 import { LinkBlockData, RichTextBlockData } from "@src/blocks.generated";
-import { useColorTheme } from "@src/blocks/ColorThemeContext";
-import { Typography } from "@src/components/common/Typography";
 import * as React from "react";
 import redraft, { Renderers } from "redraft";
 import styled from "styled-components";
 
 import { LinkBlock } from "./LinkBlock";
-import * as sc from "./RichTextBlock.sc";
 
 const GreenCustomHeader: React.FC<{ children: React.ReactNode }> = ({ children }) => <h3 style={{ color: "green" }}>{children}</h3>;
 
@@ -34,66 +31,27 @@ const defaultRenderers: Renderers = {
      */
     blocks: {
         // Paragraph
-        unstyled: (children, { keys }) =>
-            children.map((child, idx) => (
-                <Typography variant="paragraph250" component="p" disableMargin={false} key={keys[idx]}>
-                    {child}
-                </Typography>
-            )),
+        unstyled: (children, { keys }) => children.map((child, idx) => <p key={keys[idx]}>{child}</p>),
         // Headlines
-        "header-one": (children, { keys }) =>
-            children.map((child, idx) => (
-                <Typography variant="headline600" component="h1" key={keys[idx]}>
-                    {child}
-                </Typography>
-            )),
-        "header-two": (children, { keys }) =>
-            children.map((child, idx) => (
-                <Typography variant="headline550" component="h2" key={keys[idx]} disableMargin>
-                    {child}
-                </Typography>
-            )),
-        "header-three": (children, { keys }) =>
-            children.map((child, idx) => (
-                <Typography variant="headline500" component="h3" key={keys[idx]} disableMargin>
-                    {child}
-                </Typography>
-            )),
-        "header-four": (children, { keys }) =>
-            children.map((child, idx) => (
-                <Typography variant="headline450" component="h4" key={keys[idx]} disableMargin>
-                    {child}
-                </Typography>
-            )),
-        "header-five": (children, { keys }) =>
-            children.map((child, idx) => (
-                <Typography variant="headline400" component="h5" key={keys[idx]} disableMargin>
-                    {child}
-                </Typography>
-            )),
-        "header-six": (children, { keys }) =>
-            children.map((child, idx) => (
-                <Typography variant="headline350" component="h6" key={keys[idx]} disableMargin>
-                    {child}
-                </Typography>
-            )),
+        "header-one": (children, { keys }) => children.map((child, idx) => <h1 key={keys[idx]}>{child}</h1>),
+        "header-two": (children, { keys }) => children.map((child, idx) => <h2 key={keys[idx]}>{child}</h2>),
+        "header-three": (children, { keys }) => children.map((child, idx) => <h3 key={keys[idx]}>{child}</h3>),
+        "header-four": (children, { keys }) => children.map((child, idx) => <h4 key={keys[idx]}>{child}</h4>),
+        "header-five": (children, { keys }) => children.map((child, idx) => <h5 key={keys[idx]}>{child}</h5>),
+        "header-six": (children, { keys }) => children.map((child, idx) => <h6 key={keys[idx]}>{child}</h6>),
         // List
         // or depth for nested lists
         "unordered-list-item": (children, { depth, keys }) => (
-            <sc.UnorderedList key={keys[keys.length - 1]} className={`ul-level-${depth}`}>
+            <ul key={keys[keys.length - 1]} className={`ul-level-${depth}`}>
                 {children.map((child, index) => (
-                    <Typography variant="paragraph250" component="li" key={keys[index]}>
-                        {child}
-                    </Typography>
+                    <li key={keys[index]}>{child}</li>
                 ))}
-            </sc.UnorderedList>
+            </ul>
         ),
         "ordered-list-item": (children, { depth, keys }) => (
             <ol key={keys.join("|")} className={`ol-level-${depth}`}>
                 {children.map((child, index) => (
-                    <Typography variant="paragraph250" component="li" key={keys[index]}>
-                        {child}
-                    </Typography>
+                    <li key={keys[index]}>{child}</li>
                 ))}
             </ol>
         ),
@@ -120,11 +78,10 @@ interface RichTextBlockProps extends PropsWithData<RichTextBlockData> {
 
 const RichTextBlock: React.FC<RichTextBlockProps> = ({ data, renderers = defaultRenderers }) => {
     const rendered = redraft(data.draftContent, renderers);
-    const colorTheme = useColorTheme();
 
     return (
         <PreviewSkeleton title={"RichText"} type={"rows"} hasContent={hasRichTextBlockContent(data)}>
-            <sc.Wrapper $colorTheme={colorTheme}>{rendered}</sc.Wrapper>
+            {rendered}
         </PreviewSkeleton>
     );
 };
