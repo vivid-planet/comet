930a2671555718950f380f6f6a3b308b834f55be
Johannes <johannes.munker@vivid-planet.com>
2024-06-18 15:24:21 +0200

    feat(ChipSelectField): add ChipSelectField component

---
+++ packages/admin/admin/src/form/fields/ChipSelectField.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Field, FieldProps } from "../Field";
+import { FinalFormChipSelect, FinalFormChipSelectProps } from "../FinalFormChipSelect";
+
+type ChipSelectFieldPropsToExtendFrom<Value extends string | number> = FieldProps<Value, HTMLSelectElement>;
+
+// Remove `children` from the interface. Omit cannot be used here because `FieldProps` contains an index signature.
+type ChipSelectFieldPropsToExtendFromWithoutChildren<Value extends string | number> = {
+    [K in keyof ChipSelectFieldPropsToExtendFrom<Value> as K extends "children" ? never : K]: ChipSelectFieldPropsToExtendFrom<Value>[K];
+};
+
+export interface ChipSelectFieldProps<Value extends string | number> extends ChipSelectFieldPropsToExtendFromWithoutChildren<Value> {
+    children: ReturnType<Required<ChipSelectFieldPropsToExtendFrom<Value>>["children"]>;
+    componentsProps?: {
+        finalFormChipSelect?: Partial<FinalFormChipSelectProps<Value>>;
+    };
+}
+
+export function ChipSelectField<Value extends string | number>({ componentsProps = {}, children, ...restProps }: ChipSelectFieldProps<Value>) {
+    const { finalFormChipSelect: finalFormChipSelect } = componentsProps;
+    return (
+        <Field {...restProps}>
+            {(props) => (
+                <FinalFormChipSelect<Value> {...props} {...finalFormChipSelect}>
+                    {children}
+                </FinalFormChipSelect>
+            )}
+        </Field>
+    );
+}
