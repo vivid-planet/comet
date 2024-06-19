4d15768bd3e6f2cdd31a499380c6406f227afc19
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-06-19 15:15:38 +0200

    Remove leftover DTOs

--- demo/api/src/products/dto/paginated-manufacturer-countries.ts
+++
@@ -1,6 +0,0 @@
-import { PaginatedResponseFactory } from "@comet/cms-api";
-import { ObjectType } from "@nestjs/graphql";
-import { ManufacturerCountry } from "@src/products/entities/manufacturer-country.entity";
-
-@ObjectType()
-export class PaginatedManufacturerCountries extends PaginatedResponseFactory.create(ManufacturerCountry) {}
