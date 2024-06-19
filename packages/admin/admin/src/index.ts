ea8f7ab2a12bde15c32dc81281c1189088834c32
juliawegmayr <109900447+juliawegmayr@users.noreply.github.com>
2024-06-19 15:04:51 +0200

    export FinalFormNumberInputProps
    Co-authored-by: Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>

--- packages/admin/admin/src/index.ts
+++ packages/admin/admin/src/index.ts
@@ -93,7 +93,7 @@ export { FinalFormAsyncSelect, FinalFormAsyncSelectProps } from "./form/FinalFor
 export { FinalFormContext, FinalFormContextProvider, FinalFormContextProviderProps, useFinalFormContext } from "./form/FinalFormContextProvider";
 export { FinalFormFileSelect, FinalFormFileSelectClassKey, FinalFormFileSelectProps } from "./form/FinalFormFileSelect";
 export { FinalFormInput, FinalFormInputProps } from "./form/FinalFormInput";
-export { FinalFormNumberInput } from "./form/FinalFormNumberInput";
+export { FinalFormNumberInput, FinalFormNumberInputProps } from "./form/FinalFormNumberInput";
 export { FinalFormRangeInput, FinalFormRangeInputClassKey, FinalFormRangeInputProps } from "./form/FinalFormRangeInput";
 export { FinalFormSearchTextField, FinalFormSearchTextFieldProps } from "./form/FinalFormSearchTextField";
 export { FinalFormSelect, FinalFormSelectProps } from "./form/FinalFormSelect";
