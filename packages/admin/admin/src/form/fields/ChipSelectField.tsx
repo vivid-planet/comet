78fe7eb74f16dc6b2dc88434b3a7d924c684f315
Johannes <johannes.munker@vivid-planet.com>
2024-06-24 15:18:41 +0200

    feat(ChipSelectField): adjust generics

--- packages/admin/admin/src/form/fields/ChipSelectField.tsx
+++ packages/admin/admin/src/form/fields/ChipSelectField.tsx
@@ -3,22 +3,23 @@ import React from "react";
 import { Field, FieldProps } from "../Field";
 import { FinalFormChipSelect, FinalFormChipSelectProps } from "../FinalFormChipSelect";
 
-type ChipSelectFieldPropsToExtendFrom<Value extends string | number> = FieldProps<Value, HTMLSelectElement>;
+type ChipSelectFieldPropsToExtendFrom<Value extends string | number | Record<string, string | number>> = FieldProps<Value, HTMLSelectElement>;
 
 // Remove `children` from the interface. Omit cannot be used here because `FieldProps` contains an index signature.
-type ChipSelectFieldPropsToExtendFromWithoutChildren<Value extends string | number> = {
+type ChipSelectFieldPropsToExtendFromWithoutChildren<Value extends string | number | Record<string, string | number>> = {
     [K in keyof ChipSelectFieldPropsToExtendFrom<Value> as K extends "children" ? never : K]: ChipSelectFieldPropsToExtendFrom<Value>[K];
 };
 
-export interface ChipSelectFieldProps<Value extends string | number> extends ChipSelectFieldPropsToExtendFromWithoutChildren<Value> {
-    children: ReturnType<Required<ChipSelectFieldPropsToExtendFrom<Value>>["children"]>;
+export interface ChipSelectFieldProps<Value extends string | number | Record<string, string | number>>
+    extends ChipSelectFieldPropsToExtendFromWithoutChildren<Value> {
+    children?: ReturnType<Required<ChipSelectFieldPropsToExtendFrom<Value>>["children"]>;
     fullWidth?: boolean;
     componentsProps?: {
         finalFormChipSelect?: Partial<FinalFormChipSelectProps<Value>>;
     };
 }
 
-export function ChipSelectField<Value extends string | number>({
+export function ChipSelectField<Value extends string | number | Record<string, string | number>>({
     componentsProps = {},
     children,
     fullWidth,
