a662797a3fb8522199adeb5df35077aea71e968f
Franz Unger <franz.unger@vivid-planet.com>
2024-07-01 11:04:08 +0200

    Update packages/admin/cms-admin/src/userPermissions/utils/LabelsContext.ts
    Co-authored-by: Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>

--- packages/admin/cms-admin/src/userPermissions/utils/LabelsContext.ts
+++ packages/admin/cms-admin/src/userPermissions/utils/LabelsContext.ts
@@ -1,7 +1,7 @@
 import React from "react";
 
 export type LabelsContextType = {
-    permissionLabels?: Record<string, string | React.ReactNode>;
+    permissionLabels?: Record<string, React.ReactNode>;
     contentScopeLabels?: Array<{
         contentScope: Record<string, string>;
         label: string;
