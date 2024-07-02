973af905a445ce43ebe69ff32405e90bd1084c9d
Thomas Dax <thomas.dax@vivid-planet.com>
2024-07-02 15:26:41 +0200

    Add migration to add a valid file extension to each file name

---
+++ packages/api/cms-api/src/mikro-orm/migrations/Migration20240702123233.ts
@@ -0,0 +1,43 @@
+import { Migration } from "@mikro-orm/migrations";
+import * as mimedb from "mime-db";
+import { getValidExtensionsForMimetype } from "../../dam/files/files.utils";
+
+export class Migration20240702123233 extends Migration {
+    async up(): Promise<void> {
+        // This migration adds a valid file extension to every DamFile#name that doesn't have an extension yet
+
+        // Get all mimetypes in use
+        const mimetypes = (await this.execute('select distinct "DamFile".mimetype from "DamFile"')) as Array<{ mimetype: string }>;
+
+        // Create a mapping of mimetypes to their valid extensions
+        const mimetypeToExtensionsMap: { [key: string]: string[] } = mimetypes.reduce((prev, m) => {
+            const mimetype = m.mimetype;
+            const extensions = getValidExtensionsForMimetype(mimetype);
+            return { ...prev, [mimetype]: extensions };
+        }, {});
+
+        // Generate an input string for jsonb_build_object that maps mimetypes to their first valid extension
+        const sqlMimetypeToSingleExtensionMap = Object.entries(mimetypeToExtensionsMap)
+            .map(([mimetype, extensions]) => `'${mimetype}', '${extensions[0]}'`)
+            .join(", ");
+
+        // Generate SQL clauses to check if the name doesn't end with a valid extension for each mimetype
+        const sqlCaseClauses = Object.entries(mimetypeToExtensionsMap)
+            .map(([mimetype, extensions]) => {
+                const conditions = extensions.map((ext) => `name NOT LIKE '%.${ext}'`).join(" AND ");
+                return `(mimetype = '${mimetype}' AND (${conditions}))`;
+            })
+            .join(" OR ");
+
+        this.addSql(`
+          WITH mimetype_extension_map AS (
+              SELECT jsonb_build_object(${sqlMimetypeToSingleExtensionMap}) AS map
+          )
+    
+          UPDATE "DamFile"
+          SET "name" = CONCAT("name", '.', mimetype_extension_map.map ->> "DamFile".mimetype)
+          FROM mimetype_extension_map
+          WHERE ${sqlCaseClauses};
+        `);
+    }
+}
