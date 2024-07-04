c0ea943acb6a1f4611a573947cd992ea6aadc4db
Benjamin Hohenwarter <bh@vivid-planet.com>
2024-07-04 11:16:20 +0200

    Implement simple tests for crud-position support

--- packages/api/cms-api/src/generator/generate-crud-position.spec.ts
+++ packages/api/cms-api/src/generator/generate-crud-position.spec.ts
@@ -1,7 +1,12 @@
-import { BaseEntity, Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { BaseEntity, Entity, MikroORM, PrimaryKey, Property } from "@mikro-orm/core";
 import { Field, Int } from "@nestjs/graphql";
+import { LazyMetadataStorage } from "@nestjs/graphql/dist/schema-builder/storages/lazy-metadata.storage";
 import { Min } from "class-validator";
 import { v4 as uuid } from "uuid";
+
+import { generateCrud } from "./generate-crud";
+import { generateCrudInput } from "./generate-crud-input";
+import { lintSource, parseSource } from "./utils/test-helper";
 
 @Entity()
 class TestEntityWithPositionField extends BaseEntity<TestEntityWithPositionField, "id"> {
@@ -15,45 +20,74 @@ class TestEntityWithPositionField extends BaseEntity<TestEntityWithPositionField
 }
 
 describe("GenerateCrudPosition", () => {
-    it("should call update positions function", async () => {
-        // LazyMetadataStorage.load();
-        // const orm = await MikroORM.init({
-        //     type: "postgresql",
-        //     dbName: "test-db",
-        //     entities: [TestEntityArrayString],
-        // });
-        //
-        // const out = await generateCrudInput({ targetDirectory: __dirname }, orm.em.getMetadata().get("TestEntityArrayString"));
-        // const lintedOutput = await lintSource(out[0].content);
-        // const source = parseSource(lintedOutput);
-        //
-        // const classes = source.getClasses();
-        // expect(classes.length).toBe(2);
-        //
-        // {
-        //     const cls = classes[0];
-        //     const structure = cls.getStructure();
-        //
-        //     expect(structure.properties?.length).toBe(5);
-        //     expect(structure.properties?.[0].name).toBe("test1");
-        //     expect(structure.properties?.[0].type).toBe("string[]");
-        //     expect(structure.properties?.[1].name).toBe("test2");
-        //     expect(structure.properties?.[1].type).toBe("string[]");
-        //     expect(structure.properties?.[2].name).toBe("test3");
-        //     expect(structure.properties?.[2].type).toBe("string[]");
-        //     expect(structure.properties?.[3].name).toBe("test4");
-        //     expect(structure.properties?.[3].type).toBe("string[]");
-        //     expect(structure.properties?.[4].name).toBe("test5");
-        //     expect(structure.properties?.[4].type).toBe("string[]");
-        // }
-        //
-        // orm.close();
-    });
-    it("should generate create and update with optional position field", async () => {});
+    it("input should contain optional position with Int and Min(1)", async () => {
+        LazyMetadataStorage.load();
+        const orm = await MikroORM.init({
+            type: "postgresql",
+            dbName: "test-db",
+            entities: [TestEntityWithPositionField],
+        });
 
-    it("should handle create with set position by calling 'update positions function'", async () => {});
-    it("should handle create without set position by calculating", async () => {});
-    it("should update position fields for other entities on delete", async () => {});
-    // TODO add util/helper function to update positions, this could be unit-tested.
-    // TODO versuchen das ganze über nicht generierte funktionen zu lösen, die können wir unit testen.
+        const out = await generateCrudInput({ targetDirectory: __dirname }, orm.em.getMetadata().get("TestEntityWithPositionField"));
+        const lintedOutput = await lintSource(out[0].content);
+        const source = parseSource(lintedOutput);
+
+        const classes = source.getClasses();
+        expect(classes.length).toBe(2);
+
+        {
+            const cls = classes[0];
+            const structure = cls.getStructure();
+
+            expect(structure.properties?.length).toBe(1);
+            expect(structure.properties?.[0].name).toBe("position");
+            expect(structure.properties?.[0].type).toBe("number");
+            expect(structure.properties?.[0].decorators?.map((decorator) => decorator.name)).toContain("IsInt");
+            expect(structure.properties?.[0].decorators?.map((decorator) => decorator.name)).toContain("IsNullable");
+
+            const fieldDecorator = structure.properties?.[0].decorators?.find((i) => i.name === "Field");
+            expect(fieldDecorator).not.toBeUndefined();
+            expect(fieldDecorator?.arguments).toContain("() => Int");
+
+            const minDecorator = structure.properties?.[0].decorators?.find((i) => i.name === "Min");
+            expect(minDecorator).not.toBeUndefined();
+            expect(minDecorator?.arguments).toContain("1");
+        }
+
+        orm.close();
+    });
+    it("resolver should implement position-functions", async () => {
+        LazyMetadataStorage.load();
+        const orm = await MikroORM.init({
+            type: "postgresql",
+            dbName: "test-db",
+            entities: [TestEntityWithPositionField],
+        });
+
+        const out = await generateCrud({ targetDirectory: __dirname }, orm.em.getMetadata().get("TestEntityWithPositionField"));
+        const file = out.find((file) => file.name == "test-entity-with-position-field.resolver.ts");
+        if (!file) throw new Error("File not found");
+
+        const lintedOutput = await lintSource(file.content);
+        const source = parseSource(lintedOutput);
+
+        const classes = source.getClasses();
+        expect(classes.length).toBe(1);
+
+        {
+            const cls = classes[0];
+            const structure = cls.getStructure();
+
+            const incrementPositionsFunction = structure.methods?.find((method) => method.name === "incrementPositions");
+            expect(incrementPositionsFunction).not.toBeUndefined();
+
+            const decrementPositionsFunction = structure.methods?.find((method) => method.name === "decrementPositions");
+            expect(decrementPositionsFunction).not.toBeUndefined();
+
+            const getLastPositionFunction = structure.methods?.find((method) => method.name === "getLastPosition");
+            expect(getLastPositionFunction).not.toBeUndefined();
+        }
+
+        orm.close();
+    });
 });
