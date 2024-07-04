a23d2a70ad9ed768c8dbfd6bdd2568d81830e413
SebiVPS <42858722+SebiVPS@users.noreply.github.com>
2024-07-04 12:38:11 +0200

    Fix default values for `YouTubeVideoBlock` (#2256)
    The aspect ratio was removed in #2235, but forgotten to remove in the
    default values as well. This lead to an validation error when saving a
    new block with its default values.

--- packages/admin/blocks-admin/src/blocks/YouTubeVideoBlock.tsx
+++ packages/admin/blocks-admin/src/blocks/YouTubeVideoBlock.tsx
@@ -37,7 +37,7 @@ export const YouTubeVideoBlock: BlockInterface<YouTubeVideoBlockData, State, You
 
     displayName: <FormattedMessage id="comet.blocks.youTubeVideo" defaultMessage="Video (YouTube)" />,
 
-    defaultValues: () => ({ youtubeIdentifier: "", autoplay: false, showControls: false, loop: false, aspectRatio: "16X9" }),
+    defaultValues: () => ({ youtubeIdentifier: "", autoplay: false, showControls: false, loop: false }),
 
     category: BlockCategory.Media,
 
