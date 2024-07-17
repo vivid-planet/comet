ec56c5a2b917e29d5183ecba1f742555ed65a6be
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-07-17 11:17:17 +0200

    Support user-defined paths for predefined pages

--- demo/site/src/middleware/cache.ts
+++ demo/site/src/middleware/cache.ts
@@ -2,7 +2,7 @@ import { createCache, memoryStore } from "cache-manager";
 
 export const memoryCache = createCache(
     memoryStore({
-        ttl: 15 * 60 * 1000, // 15 minutes,
+        ttl: process.env.NODE_ENV === "development" ? 0 : 15 * 60 * 1000, // 15 minutes,
     }),
     {
         refreshThreshold: 5 * 60 * 1000, // refresh if less than 5 minutes TTL are remaining,
---
+++ demo/site/src/middleware/predefinedPages.ts
@@ -0,0 +1,77 @@
+import { gql } from "@comet/cms-site";
+import { languages } from "@src/config";
+import { predefinedPagePaths } from "@src/predefinedPages/predefinedPagePaths";
+import { createGraphQLFetch } from "@src/util/graphQLClient";
+
+import { memoryCache } from "./cache";
+import { GQLPredefinedPagesQuery, GQLPredefinedPagesQueryVariables } from "./predefinedPages.generated";
+
+async function getPredefinedPageRedirect(scope: { domain: string }, pathname: string): Promise<string | undefined> {
+    const pages = await fetchPredefinedPages(scope);
+
+    const matchingPredefinedPage = pages.find((page) => pathname.startsWith(page.defaultPath));
+
+    if (matchingPredefinedPage) {
+        return pathname.replace(matchingPredefinedPage.defaultPath, matchingPredefinedPage.customPath);
+    }
+
+    return undefined;
+}
+
+async function getPredefinedPageRewrite(scope: { domain: string }, pathname: string): Promise<string | undefined> {
+    const pages = await fetchPredefinedPages(scope);
+
+    const matchingPredefinedPage = pages.find((page) => pathname.startsWith(page.customPath));
+
+    if (matchingPredefinedPage) {
+        return pathname.replace(matchingPredefinedPage.customPath, matchingPredefinedPage.defaultPath);
+    }
+
+    return undefined;
+}
+
+const predefinedPagesQuery = gql`
+    query PredefinedPages($scope: PageTreeNodeScopeInput!) {
+        paginatedPageTreeNodes(scope: $scope, documentType: "PredefinedPage") {
+            nodes {
+                id
+                path
+                document {
+                    __typename
+                    ... on PredefinedPage {
+                        type
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const graphQLFetch = createGraphQLFetch();
+
+async function fetchPredefinedPages(scope: { domain: string }) {
+    const key = `predefinedPages-${JSON.stringify(scope)}`;
+
+    const pages: Array<{ defaultPath: string; customPath: string }> = [];
+
+    for (const language of languages) {
+        const { paginatedPageTreeNodes } = await graphQLFetch<GQLPredefinedPagesQuery, GQLPredefinedPagesQueryVariables>(predefinedPagesQuery, {
+            scope: { domain: scope.domain, language },
+        });
+
+        for (const node of paginatedPageTreeNodes.nodes) {
+            if (node.document?.__typename === "PredefinedPage" && node.document.type) {
+                pages.push({
+                    defaultPath: `/${language}${predefinedPagePaths[node.document.type]}`,
+                    customPath: `/${language}${node.path}`,
+                });
+            }
+        }
+    }
+
+    return memoryCache.wrap(key, async () => {
+        return pages;
+    });
+}
+
+export { getPredefinedPageRedirect, getPredefinedPageRewrite };
