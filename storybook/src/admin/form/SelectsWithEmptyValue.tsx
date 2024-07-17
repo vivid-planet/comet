0093fb1fcb83849fac930df76982a82777954b17
Ricky Smith <jamesricky@me.com>
2024-07-17 14:51:19 +0200

    Add examples of selects with empty string as custom object value

--- storybook/src/admin/form/SelectsWithEmptyValue.tsx
+++ storybook/src/admin/form/SelectsWithEmptyValue.tsx
@@ -1,5 +1,5 @@
 import { AsyncSelectField, Field, FinalFormSelect, SelectField } from "@comet/admin";
-import { Box, Button, MenuItem, Stack, Typography } from "@mui/material";
+import { Box, Button, Divider, MenuItem, Stack, Typography } from "@mui/material";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import { Form } from "react-final-form";
@@ -14,6 +14,18 @@ const optionsWithId: OptionWithId[] = [
     { id: "chocolate", title: "Chocolate" },
     { id: "strawberry", title: "Strawberry" },
     { id: "vanilla", title: "Vanilla" },
+];
+
+interface OptionWithValue {
+    value: string;
+    title: string;
+}
+
+const optionsWithValue: OptionWithValue[] = [
+    { value: "", title: "No Value" },
+    { value: "chocolate", title: "Chocolate" },
+    { value: "strawberry", title: "Strawberry" },
+    { value: "vanilla", title: "Vanilla" },
 ];
 
 storiesOf("@comet/admin/form", module).add("Selects with empty values", () => {
@@ -55,6 +67,39 @@ storiesOf("@comet/admin/form", module).add("Selects with empty values", () => {
                                             options={optionsWithId}
                                             getOptionLabel={(option: OptionWithId) => option.title}
                                             getOptionSelected={(option: OptionWithId, value: OptionWithId) => option.title === value.title}
+                                            fullWidth
+                                        />
+                                    )}
+                                </Field>
+                            </Box>
+                            <Divider />
+                            <Box>
+                                <Typography variant="h4" gutterBottom>
+                                    Selects using options object with custom value key
+                                </Typography>
+                                <AsyncSelectField
+                                    fullWidth
+                                    name="asyncSelectFieldValue"
+                                    label="AsyncSelectField (VALUE)"
+                                    loadOptions={async () => {
+                                        return optionsWithValue;
+                                    }}
+                                    getOptionLabel={(option: OptionWithValue) => option.title}
+                                />
+                                <SelectField name="selectFieldValue" label="SelectField (VALUE)" fullWidth>
+                                    {optionsWithValue.map((option: OptionWithValue) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.title}
+                                        </MenuItem>
+                                    ))}
+                                </SelectField>
+                                <Field name="finalFormSelectValue" label="FinalFormSelect (VALUE)" fullWidth>
+                                    {(props) => (
+                                        <FinalFormSelect
+                                            {...props}
+                                            options={optionsWithValue}
+                                            getOptionLabel={(option: OptionWithValue) => option.title}
+                                            getOptionSelected={(option: OptionWithValue, value: OptionWithValue) => option.title === value.title}
                                             fullWidth
                                         />
                                     )}
