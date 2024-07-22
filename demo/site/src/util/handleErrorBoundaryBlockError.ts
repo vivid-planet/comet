b7790a6c08c1228de041d61562e0d458c1298c6f
Raphael Blum <raphael.blum@vivid-planet.com>
2024-07-22 14:23:06 +0200

    update changeset and remove unnecessary comment

--- demo/site/src/util/handleErrorBoundaryBlockError.ts
+++ demo/site/src/util/handleErrorBoundaryBlockError.ts
@@ -3,8 +3,6 @@ import { ErrorInfo } from "react";
 export function handleErrorBoundaryBlockError(error: Error, errorInfo: ErrorInfo): void {
     // Add your custom error handling here (for example, reporting to an error logging tool)
 
-    // Throw error locally AND
-    // when a ListBlock or OneOfBlock is not used inside a BlocksBlock, throw the error as well when there is no onError defined.
     if (process.env.NODE_ENV === "development") {
         console.error("Error", error, errorInfo);
         throw error;
