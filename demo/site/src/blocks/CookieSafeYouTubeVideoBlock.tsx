28c1470349f3e6a151f9b8d6fb461ca966d93863
Ricky Smith <jamesricky@me.com>
2024-07-23 10:49:12 +0200

    Remove TODO as the topics were discussed in the PR

--- demo/site/src/blocks/CookieSafeYouTubeVideoBlock.tsx
+++ demo/site/src/blocks/CookieSafeYouTubeVideoBlock.tsx
@@ -4,9 +4,6 @@ import { CookieFallback } from "@src/components/common/CookieFallback";
 import { cookieIds, useCookieApi } from "@src/util/cookies";
 import styled from "styled-components";
 
-// TODO: Consider if `CookieSafe` should be used directly inside `YouTubeVideoBlock` and expose the `consented` and `fallback` props.
-// - Problem: The `fallback` may sometimes need to be rendered outside of the `YouTubeVideoBlock`.
-
 export const CookieSafeYouTubeVideoBlock = (props: PropsWithData<YouTubeVideoBlockData>) => {
     const { consentedCookies } = useCookieApi();
 
