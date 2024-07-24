490a895f51160b0437edf4e423597046d7cec09e
Ricky Smith <jamesricky@me.com>
2024-07-24 11:55:40 +0200

    Allow setting `readOnly` on `FileSelect`

--- storybook/src/docs/components/FileSelect/FileSelect.stories.tsx
+++ storybook/src/docs/components/FileSelect/FileSelect.stories.tsx
@@ -46,4 +46,32 @@ storiesOf("stories/components/FileSelect", module)
                 maxFiles={5}
             />
         );
+    })
+    .add("ReadOnly FileSelect", () => {
+        const value: FileSelectItem[] = [
+            {
+                name: "Filename.xyz",
+                size: 4.3 * 1024 * 1024, // 4.3 MB
+            },
+            {
+                name: "Another file.png",
+                size: 568 * 1024, // 568 KB
+            },
+        ];
+
+        return (
+            <FileSelect
+                onDrop={(acceptedFiles, fileRejections) => {
+                    // Handle what happens with the dropped files
+                }}
+                onRemove={(file) => {
+                    // Handle remove
+                }}
+                onDownload={(file) => {
+                    // Handle download
+                }}
+                files={value}
+                readOnly
+            />
+        );
     });
