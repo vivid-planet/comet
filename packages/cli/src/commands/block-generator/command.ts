64150736070d2dca2588334d40a2ce6412597881
Ricky Smith <jamesricky@me.com>
2024-08-02 15:02:59 +0200

    Add option to create block from existing config for easier debugging of the block creation

--- packages/cli/src/commands/block-generator/command.ts
+++ packages/cli/src/commands/block-generator/command.ts
@@ -4,7 +4,8 @@ import fs from "fs";
 
 import { addAdminBlockToPageContent } from "./addAdminBlockToPageContent";
 import { createBlockAdmin } from "./createAdminBlock";
-import { getBlockConfig } from "./getBlockConfig";
+import { exampleTeaserBlockConfig } from "./exampleBlockConfigs";
+import { BlockConfig, getBlockConfig } from "./getBlockConfig";
 
 export type FileCreationData = {
     filePath: string;
@@ -31,15 +32,28 @@ const writeFile = async (data: FileCreationData, logMessage: string) => {
     fs.writeFileSync(data.filePath, output ?? data.content);
 };
 
-export const blockGeneratorCommand = new Command("block-generator").description("generate a block (POC)").action(async () => {
-    const config = await getBlockConfig();
+type Options = {
+    exampleTeaserBlock: boolean;
+};
 
-    // eslint-disable-next-line no-console
-    console.log(`Generating block "${config.name}"`);
+export const blockGeneratorCommand = new Command("block-generator")
+    .description("generate a block (POC)")
+    .option("--example-teaser-block", "Skip the interactive block creation and use the example teaser block (used for debugging)")
+    .action(async (options: Options) => {
+        let config: BlockConfig;
 
-    const adminBlockFile = createBlockAdmin(config);
-    await writeFile(adminBlockFile, "Creating admin file");
+        if (options.exampleTeaserBlock) {
+            config = exampleTeaserBlockConfig;
+        } else {
+            config = await getBlockConfig();
+        }
 
-    const adminPageContentFile = addAdminBlockToPageContent(config);
-    await writeFile(adminPageContentFile, "Updating PageContentBlock");
-});
+        // eslint-disable-next-line no-console
+        console.log(`Generating block "${config.name}"`);
+
+        const adminBlockFile = createBlockAdmin(config);
+        await writeFile(adminBlockFile, "Creating admin file");
+
+        const adminPageContentFile = addAdminBlockToPageContent(config);
+        await writeFile(adminPageContentFile, "Updating PageContentBlock");
+    });
---
+++ packages/cli/src/commands/block-generator/exampleBlockConfigs.ts
@@ -0,0 +1,22 @@
+import { BlockConfig } from "./getBlockConfig";
+
+export const exampleTeaserBlockConfig: BlockConfig = {
+    name: "Example Teaser",
+    type: "composite",
+    children: [
+        {
+            name: "Headline",
+            type: "textfield",
+        },
+        {
+            name: "Description",
+            type: "block",
+            block: "RichTextBlock",
+        },
+        {
+            name: "Image",
+            type: "block",
+            block: "DamImage",
+        },
+    ],
+};
