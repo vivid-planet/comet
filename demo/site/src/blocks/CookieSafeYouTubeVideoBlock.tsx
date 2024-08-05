00ecdcd8dc14a0c551e01c925af0581b8bddb6e6
Ricky Smith <jamesricky@me.com>
2024-08-05 13:45:16 +0200

    Fix aspect ratio

--- demo/site/src/blocks/CookieSafeYouTubeVideoBlock.tsx
+++ demo/site/src/blocks/CookieSafeYouTubeVideoBlock.tsx
@@ -5,20 +5,21 @@ import { CookieFallback } from "@src/components/common/CookieFallback";
 import { cookieIds } from "@src/util/cookieIds";
 import styled from "styled-components";
 
+const aspectRatio = "16x9";
+
 export const CookieSafeYouTubeVideoBlock = (props: PropsWithData<YouTubeVideoBlockData>) => {
     const { consentedCookies } = useCookieApi();
 
     return (
-        // TODO: fix this (was `props.data.aspectRatio.replace("X", "/")` but `aspectRatio` prop has been removed)
-        <Root aspectRatio="16/9">
+        <Root $aspectRatio={aspectRatio.replace("x", "/")}>
             <CookieSafe consented={consentedCookies.includes(cookieIds.thirdParty)} fallback={<CookieFallback />}>
-                <YouTubeVideoBlock {...props} />
+                <YouTubeVideoBlock aspectRatio={aspectRatio} {...props} />
             </CookieSafe>
         </Root>
     );
 };
 
-const Root = styled.div<{ aspectRatio: React.CSSProperties["aspectRatio"] }>`
+const Root = styled.div<{ $aspectRatio: React.CSSProperties["aspectRatio"] }>`
     position: relative;
-    aspect-ratio: ${({ aspectRatio }) => aspectRatio};
+    aspect-ratio: ${({ $aspectRatio }) => $aspectRatio};
 `;
