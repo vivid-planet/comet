19ccfa93718a20554985b792ecc763333858f845
Markus Fichtner <markus.fichtner@vivid-planet.com>
2024-08-06 14:56:18 +0200

    Update to comet v7

--- demo/api/src/db/fixtures/generators/image-file-fixture.service.ts
+++ demo/api/src/db/fixtures/generators/image-file-fixture.service.ts
@@ -1,4 +1,4 @@
-import { FileInterface, FilesService, FileUploadService } from "@comet/cms-api";
+import { createFileUploadInputFromUrl, FileInterface, FilesService } from "@comet/cms-api";
 import { Injectable } from "@nestjs/common";
 import { DamScope } from "@src/dam/dto/dam-scope";
 import path from "path";
@@ -7,7 +7,7 @@ const IMAGE_FILE_PATHS = ["astronaut", "comet", "planet", "rocket", "sun"];
 
 @Injectable()
 export class ImageFileFixtureService {
-    constructor(private readonly filesService: FilesService, private readonly fileUploadService: FileUploadService) {}
+    constructor(private readonly filesService: FilesService) {}
 
     async uploadImagesFromSrc(scope: DamScope): Promise<Array<FileInterface>> {
         const images = [];
@@ -15,7 +15,7 @@ export class ImageFileFixtureService {
             console.log(`Downloading ${IMAGE_FILE_PATHS[index]}.`);
             const image_path = path.resolve(`./src/db/fixtures/generators/images/${IMAGE_FILE_PATHS[index]}.png`);
 
-            const downloadedImage = await this.fileUploadService.createFileUploadInputFromUrl(image_path);
+            const downloadedImage = await createFileUploadInputFromUrl(image_path);
             console.log(`Downloading ${IMAGE_FILE_PATHS[index]} done.`);
             downloadedImage.mimetype = "image/png";
             downloadedImage.originalname = `${IMAGE_FILE_PATHS[index]}.png`;
