ceaeaf60562a00d62d1a11a6b57215c90d9b9989
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-08-13 11:24:57 +0200

    Only generate download URL if downloads are configured

--- packages/api/cms-api/src/file-uploads/file-uploads-upload.controller.ts
+++ packages/api/cms-api/src/file-uploads/file-uploads-upload.controller.ts
@@ -1,22 +1,28 @@
 import { EntityManager } from "@mikro-orm/postgresql";
-import { Controller, Headers, Post, Type, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Controller, Headers, Inject, Post, Type, UploadedFile, UseInterceptors } from "@nestjs/common";
 import rimraf from "rimraf";
 
 import { DisableCometGuards } from "../auth/decorators/disable-comet-guards.decorator";
 import { FileUploadInput } from "../dam/files/dto/file-upload.input";
 import { RequiredPermission } from "../user-permissions/decorators/required-permission.decorator";
 import { FileUpload } from "./entities/file-upload.entity";
+import { FileUploadsConfig } from "./file-uploads.config";
+import { FILE_UPLOADS_CONFIG } from "./file-uploads.constants";
 import { FileUploadsService } from "./file-uploads.service";
 import { FileUploadsFileInterceptor } from "./file-uploads-file.interceptor";
 
 type FileUploadsUploadResponse = Pick<FileUpload, "id" | "name" | "size" | "mimetype" | "contentHash" | "createdAt" | "updatedAt"> & {
-    downloadUrl: string;
+    downloadUrl?: string;
 };
 
 export function createFileUploadsUploadController(options: { public: boolean }): Type<unknown> {
     @Controller("file-uploads")
     class BaseFileUploadsUploadController {
-        constructor(private readonly fileUploadsService: FileUploadsService, private readonly entityManager: EntityManager) {}
+        constructor(
+            private readonly fileUploadsService: FileUploadsService,
+            private readonly entityManager: EntityManager,
+            @Inject(FILE_UPLOADS_CONFIG) private readonly config: FileUploadsConfig,
+        ) {}
 
         @Post("upload")
         @UseInterceptors(FileUploadsFileInterceptor("file"))
@@ -34,7 +40,11 @@ export function createFileUploadsUploadController(options: { public: boolean }):
                 }
             });
 
-            const downloadUrl = this.fileUploadsService.createDownloadUrl(fileUpload, { relativeUrls: Boolean(relativeUrls) });
+            let downloadUrl: string | undefined;
+
+            if (this.config.download) {
+                downloadUrl = this.fileUploadsService.createDownloadUrl(fileUpload, { relativeUrls: Boolean(relativeUrls) });
+            }
 
             return {
                 ...fileUpload,
