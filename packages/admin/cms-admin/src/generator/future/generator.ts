05e154a01d52edaf76ad10d67b468a5bbaf7be3f
Ricky James Smith <jamesricky@me.com>
2024-08-21 09:52:02 +0200

    Admin Generator (Future): Add support for file upload fields in forms (#2363)

--- packages/admin/cms-admin/src/generator/future/generator.ts
+++ packages/admin/cms-admin/src/generator/future/generator.ts
@@ -5,6 +5,7 @@ import { glob } from "glob";
 import { introspectionFromSchema } from "graphql";
 import { basename, dirname } from "path";
 
+import { FinalFormFileUploadProps } from "../../form/file/FinalFormFileUpload";
 import { generateForm } from "./generateForm";
 import { generateGrid } from "./generateGrid";
 import { UsableFields } from "./generateGrid/usableFields";
@@ -15,6 +16,16 @@ type ImportReference = {
     import: string;
 };
 
+type SingleFileFormFieldConfig = { type: "fileUpload"; multiple?: false; maxFiles?: 1 } & Pick<
+    Partial<FinalFormFileUploadProps<false>>,
+    "maxFileSize" | "readOnly" | "layout" | "accept"
+>;
+
+type MultiFileFormFieldConfig = { type: "fileUpload"; multiple: true; maxFiles?: number } & Pick<
+    Partial<FinalFormFileUploadProps<true>>,
+    "maxFileSize" | "readOnly" | "layout" | "accept"
+>;
+
 export type FormFieldConfig<T> = (
     | { type: "text"; multiline?: boolean }
     | { type: "number" }
@@ -24,6 +35,8 @@ export type FormFieldConfig<T> = (
     | { type: "staticSelect"; values?: Array<{ value: string; label: string } | string> }
     | { type: "asyncSelect"; rootQuery: string; labelField?: string }
     | { type: "block"; block: ImportReference }
+    | SingleFileFormFieldConfig
+    | MultiFileFormFieldConfig
 ) & { name: keyof T; label?: string; required?: boolean; virtual?: boolean; validate?: ImportReference; helperText?: string; readOnly?: boolean };
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export function isFormFieldConfig<T>(arg: any): arg is FormFieldConfig<T> {
@@ -127,8 +140,9 @@ export async function runFutureGenerate(filePattern = "src/**/*.cometGen.ts") {
         if (gqlDocumentsOutputCode != "") {
             const gqlDocumentsOuputFilename = `${targetDirectory}/${basename(file.replace(/\.cometGen\.ts$/, ""))}.gql.tsx`;
             gqlDocumentsOutputCode = `import { gql } from "@apollo/client";
+                import { finalFormFileUploadFragment } from "@comet/cms-admin";
 
-            ${gqlDocumentsOutputCode}
+                ${gqlDocumentsOutputCode}
             `;
             await writeGenerated(gqlDocumentsOuputFilename, gqlDocumentsOutputCode);
         }
