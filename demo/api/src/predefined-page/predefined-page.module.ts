1c9a95433168f6d4a22e62c309d7442604a20d07
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-09-24 10:50:31 +0200

    Remove PagesModule import from PredefinedPageModule

--- demo/api/src/predefined-page/predefined-page.module.ts
+++ demo/api/src/predefined-page/predefined-page.module.ts
@@ -1,12 +1,11 @@
 import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Module } from "@nestjs/common";
-import { PagesModule } from "@src/pages/pages.module";
 
 import { PredefinedPage } from "./entities/predefined-page.entity";
 import { PredefinedPageResolver } from "./predefined-page.resolver";
 
 @Module({
-    imports: [PagesModule, MikroOrmModule.forFeature([PredefinedPage])],
+    imports: [MikroOrmModule.forFeature([PredefinedPage])],
     providers: [PredefinedPageResolver],
 })
 export class PredefinedPageModule {}
