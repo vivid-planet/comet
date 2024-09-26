58a190e9ceabbf08224f77208b52f0328b8e9924
Ben-Ho <bh@vivid-planet.com>
2024-09-26 11:05:13 +0200

    Admin Generator (Future): Support filtering async selects by other field values (#2372)

--- packages/admin/cms-admin/src/generator/future/generateGrid.ts
+++ packages/admin/cms-admin/src/generator/future/generateGrid.ts
@@ -19,6 +19,7 @@ import { getPropsForFilterProp } from "./generateGrid/getPropsForFilterProp";
 import { ActionsGridColumnConfig, GeneratorReturn, GridColumnConfig, GridConfig, StaticSelectLabelCellContent } from "./generator";
 import { camelCaseToHumanReadable } from "./utils/camelCaseToHumanReadable";
 import { findMutationType } from "./utils/findMutationType";
+import { findQueryTypeOrThrow } from "./utils/findQueryType";
 import { findRootBlocks } from "./utils/findRootBlocks";
 import { generateImportsCode, Imports } from "./utils/generateImportsCode";
 
@@ -29,20 +30,6 @@ function tsCodeRecordToString(object: TsCodeRecordToStringObject) {
         .filter(([key, value]) => value !== undefined)
         .map(([key, value]) => `${key}: ${value},`)
         .join("\n")}}`;
-}
-
-function findQueryType(queryName: string, schema: IntrospectionQuery) {
-    const queryType = schema.__schema.types.find((type) => type.name === schema.__schema.queryType.name) as IntrospectionObjectType | undefined;
-    if (!queryType) throw new Error("Can't find Query type in gql schema");
-    const ret = queryType.fields.find((field) => field.name === queryName);
-    if (!ret) throw new Error(`Can't find query ${queryName} in gql schema`);
-    return ret;
-}
-
-function findQueryTypeOrThrow(queryName: string, schema: IntrospectionQuery) {
-    const ret = findQueryType(queryName, schema);
-    if (!ret) throw new Error(`Can't find query ${queryName} in gql schema`);
-    return ret;
 }
 
 export type Prop = { type: string; optional: boolean; name: string };
