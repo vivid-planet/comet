c70bfce7336891d9745385b3d76e775d443d96e7
Markus Fichtner <markus.fichtner@vivid-planet.com>
2024-09-30 21:24:05 +0200

    Refactor slug validation regex to allow special characters and percent encoding

--- packages/api/cms-api/src/common/validators/is-slug.spec.ts
+++ packages/api/cms-api/src/common/validators/is-slug.spec.ts
@@ -6,6 +6,26 @@ describe("IsSlug", () => {
             const validator = new IsSlugConstraint();
             expect(await validator.validate("comet")).toBe(true);
         });
+        it("should accept a valid slug with numbers", async () => {
+            const validator = new IsSlugConstraint();
+            expect(await validator.validate("comet--2")).toBe(true);
+        });
+        it("should accept a valid slug with special characters", async () => {
+            const validator = new IsSlugConstraint();
+            expect(await validator.validate("comet_2~")).toBe(true);
+        });
+        it("should accept a valid slug with special char at the beginning", async () => {
+            const validator = new IsSlugConstraint();
+            expect(await validator.validate("-abc")).toBe(true);
+        });
+        it("should accept a valid slug with special char at the beginning", async () => {
+            const validator = new IsSlugConstraint();
+            expect(await validator.validate("abc-")).toBe(true);
+        });
+        it("should accept a valid slug with uppercase characters", async () => {
+            const validator = new IsSlugConstraint();
+            expect(await validator.validate("Comet")).toBe(true);
+        });
         it("should not accept space", async () => {
             const validator = new IsSlugConstraint();
             expect(await validator.validate("com et")).toBe(false);
@@ -17,6 +37,18 @@ describe("IsSlug", () => {
         it("should not allow url encoded characters", async () => {
             const validator = new IsSlugConstraint();
             expect(await validator.validate("slug%2F2")).toBeFalsy();
+        });
+        it("should not allow percent encoded special characters ../", async () => {
+            const validator = new IsSlugConstraint();
+            expect(await validator.validate("%2E%2E%2Ftest")).toBeFalsy();
+        });
+        it("should not allow  special characters ../", async () => {
+            const validator = new IsSlugConstraint();
+            expect(await validator.validate("../test")).toBeFalsy();
+        });
+        it("should not allow special character / ", async () => {
+            const validator = new IsSlugConstraint();
+            expect(await validator.validate("slug/")).toBeFalsy();
         });
     });
 });
