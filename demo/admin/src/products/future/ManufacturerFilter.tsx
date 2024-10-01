51417436e942fe76b3e289914e64289f689fc8cb
Benjamin Hohenwarter <bh@vivid-planet.com>
2024-10-01 08:07:27 +0200

    Update manufacturer filter to match demo

--- demo/admin/src/products/future/ManufacturerFilter.tsx
+++ demo/admin/src/products/future/ManufacturerFilter.tsx
@@ -22,11 +22,13 @@ const manufacturersQuery = gql`
 // Source: https://mui.com/x/react-data-grid/filtering/customization/#multiple-values-operator
 function ManufacturerFilter({ item, applyValue }: GridFilterInputValueProps) {
     const intl = useIntl();
+    const [search, setSearch] = React.useState<string | undefined>(undefined);
 
     const { data } = useQuery<GQLManufacturersFilterQuery, GQLManufacturersFilterQueryVariables>(manufacturersQuery, {
         variables: {
             offset: 0,
             limit: 10,
+            search: search,
         },
     });
 
@@ -36,7 +38,8 @@ function ManufacturerFilter({ item, applyValue }: GridFilterInputValueProps) {
             size="small"
             options={data?.manufacturers.nodes ?? []}
             autoHighlight
-            value={item.value}
+            value={item.value ? item.value : null}
+            filterOptions={(x) => x} // disable local filtering
             isOptionEqualToValue={(option, value) => {
                 // does only highlight the selected value in options-list but does not trigger getOptionLabel-Call
                 return option.id == value;
@@ -53,6 +56,10 @@ function ManufacturerFilter({ item, applyValue }: GridFilterInputValueProps) {
                 <TextField
                     {...params}
                     placeholder={intl.formatMessage({ id: "manufacturer-filter.placeholder", defaultMessage: "Choose a manufacturer" })}
+                    value={search ? search : null}
+                    onChange={(event) => {
+                        setSearch(event.target.value);
+                    }}
                     inputProps={{
                         ...params.inputProps,
                         autoComplete: "new-password", // disable autocomplete and autofill
