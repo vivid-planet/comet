26e67595d60f5c5db2b1947eed371c7d6c8bd3c5
Markus Fichtner <markus.fichtner@vivid-planet.com>
2024-10-07 14:47:40 +0200

    Refactor CheckboxField and CheckboxListField components

--- packages/admin/admin/src/form/fields/CheckboxField.tsx
+++ packages/admin/admin/src/form/fields/CheckboxField.tsx
@@ -1,6 +1,6 @@
-import { Checkbox, FormControlLabel, FormControlLabelProps } from "@mui/material";
-import { FinalFormCheckboxProps } from "form/Checkbox";
+import { FormControlLabel, FormControlLabelProps } from "@mui/material";
 
+import { FinalFormCheckbox, FinalFormCheckboxProps } from "../Checkbox";
 import { Field, FieldProps } from "../Field";
 
 export interface CheckboxFieldProps extends FieldProps<string, HTMLInputElement> {
@@ -12,24 +12,11 @@ export interface CheckboxFieldProps extends FieldProps<string, HTMLInputElement>
 }
 
 export const CheckboxField = ({ fieldLabel, label, componentsProps = {}, ...restProps }: CheckboxFieldProps) => {
-    const { formControlLabel: formControlLabelProps, finalFormCheckbox: formCheckboxProps } = componentsProps;
+    const { formControlLabel: formControlLabelProps, finalFormCheckbox: finalFormCheckboxProps } = componentsProps;
     return (
         <Field type="checkbox" label={fieldLabel} {...restProps}>
-            {({ input: { value, onChange, name } }) => (
-                <FormControlLabel
-                    label={label}
-                    value={restProps.name}
-                    name={name}
-                    onChange={(_, checked) => {
-                        if (checked) {
-                            onChange(true);
-                        } else {
-                            onChange(false);
-                        }
-                    }}
-                    control={<Checkbox disabled={restProps.disabled} checked={Boolean(value)} required={restProps.required} {...formCheckboxProps} />}
-                    {...formControlLabelProps}
-                />
+            {(props) => (
+                <FormControlLabel label={label} control={<FinalFormCheckbox {...props} {...finalFormCheckboxProps} />} {...formControlLabelProps} />
             )}
         </Field>
     );
