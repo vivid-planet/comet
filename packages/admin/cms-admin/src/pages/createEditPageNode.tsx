6dc53f479971b286d0edac3089a2e986c71f7913
Markus Fichtner <markus.fichtner@vivid-planet.com>
2024-10-07 13:39:08 +0200

    Update regex validation for slugs

--- packages/admin/cms-admin/src/pages/createEditPageNode.tsx
+++ packages/admin/cms-admin/src/pages/createEditPageNode.tsx
@@ -531,7 +531,7 @@ const transformToSlug = (name: string, locale: string) => {
 };
 
 const isValidSlug = (value: string) => {
-    return /^([a-zA-Z0-9-._~]|%[0-9a-fA-F]{2})+$/.test(decodeURIComponent(value));
+    return /^([a-zA-Z0-9-._~]|%(?!2F)[0-9a-fA-F]{2})+$/.test(value);
 };
 
 interface InitialValues {
