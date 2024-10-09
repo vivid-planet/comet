f1bf0ae868b267b8f29c4eefc5e65062d6775b24
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-10-09 10:12:04 +0200

    Remove param decorator

--- packages/api/cms-api/src/user-permissions/decorators/content-scopes.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-
-import { getRequestFromExecutionContext } from "../../common/decorators/utils";
-import { ContentScope } from "../interfaces/content-scope.interface";
-
-export const ContentScopes = createParamDecorator((data: unknown, context: ExecutionContext): ContentScope[] | undefined => {
-    const request = getRequestFromExecutionContext(context);
-    return request.contentScopes;
-});
