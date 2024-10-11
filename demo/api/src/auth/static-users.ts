52cbbccf2c547bc62d1bbe8436fff66d3b09454a
Franz Unger <franz.unger@vivid-planet.com>
2024-10-11 16:11:31 +0200

    Fix concerns

--- demo/api/src/auth/static-users.ts
+++ demo/api/src/auth/static-users.ts
@@ -10,9 +10,19 @@ export const staticUsers: User[] = [
     ...Array(50)
         .fill(0)
         .map((_f, x) => x + 1)
-        .map((id) => ({ id: `SU${id}`, name: `Superuser ${id}`, email: `demo_superuser_${id}@comext-dxp.com`, isAdmin: false })),
+        .map((id) => ({
+            id: `SU${id}`,
+            name: `Superuser ${id}`,
+            email: `demo_superuser_${id}@comet-dxp.com`,
+            isAdmin: false,
+        })),
     ...Array(1000)
         .fill(0)
         .map((_f, x) => x + 1)
-        .map((id) => ({ id: `U${id}`, name: `User ${id}`, email: `demo_user_${id}@comet-dxp.com`, isAdmin: false })),
+        .map((id) => ({
+            id: `U${id}`,
+            name: `User ${id}`,
+            email: `demo_user_${id}@comet-dxp.com`,
+            isAdmin: false,
+        })),
 ];
