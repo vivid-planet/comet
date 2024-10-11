5f3cb8bd6e819b3f1a0eb048e9ded52b99a6a0d2
andrearutrecht <andrea.rutrecht@vivid-planet.com>
2024-10-11 11:24:19 +0200

    Create story to show 'EditDialog' behaviour in 'RouterTabs'

---
+++ storybook/src/admin/edit-dialog/EditDialogInRouterTabs.tsx
@@ -0,0 +1,124 @@
+import {
+    DataGridToolbar,
+    EditDialog,
+    FinalForm,
+    IEditDialogApi,
+    MainContent,
+    messages,
+    RouterTab,
+    RouterTabs,
+    Stack,
+    TextField,
+    ToolbarActions,
+    ToolbarFillSpace,
+} from "@comet/admin";
+import { Add } from "@comet/admin-icons";
+import { Button, Typography } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { storiesOf } from "@storybook/react";
+import React from "react";
+import { FormattedMessage, useIntl } from "react-intl";
+
+import { storyRouterDecorator } from "../../story-router.decorator";
+
+const products = [
+    { id: "1", name: "Product 1" },
+    { id: "2", name: "Product 2" },
+    { id: "3", name: "Product 3" },
+    { id: "4", name: "Product 4" },
+    { id: "5", name: "Product 5" },
+];
+
+type DialogProps = {
+    dialogApiRef: React.RefObject<IEditDialogApi>;
+};
+
+const AddProductDialog: React.FC<DialogProps> = ({ dialogApiRef }) => {
+    const intl = useIntl();
+
+    return (
+        <EditDialog
+            ref={dialogApiRef}
+            title={intl.formatMessage({ id: "addProductDialog.title", defaultMessage: "Add a new Product" })}
+            componentsProps={{ dialog: { scroll: "paper", fullWidth: true } }}
+        >
+            {() => {
+                return (
+                    <FinalForm
+                        mode="edit"
+                        onSubmit={() => {
+                            console.log("Submitted!");
+                        }}
+                        onAfterSubmit={() => {
+                            dialogApiRef.current?.closeDialog();
+                        }}
+                    >
+                        <TextField name="name" label="Name" fullWidth />
+                    </FinalForm>
+                );
+            }}
+        </EditDialog>
+    );
+};
+
+function Toolbar({ toolbarAction }: { toolbarAction?: React.ReactNode }) {
+    return (
+        <DataGridToolbar>
+            <ToolbarFillSpace />
+            <ToolbarActions>{toolbarAction}</ToolbarActions>
+        </DataGridToolbar>
+    );
+}
+
+function Story() {
+    const intl = useIntl();
+    const editDialogApi = React.useRef<IEditDialogApi>(null);
+    return (
+        <>
+            <RouterTabs>
+                <RouterTab path="" label="First Tab">
+                    <MainContent fullHeight disablePadding>
+                        <Typography>Hello, I am the first Tab</Typography>
+                    </MainContent>
+                </RouterTab>
+                <RouterTab path="/second" label="Second Tab">
+                    <MainContent fullHeight disablePadding>
+                        <Stack topLevelTitle={intl.formatMessage({ id: "tabs.secondTab.title", defaultMessage: "Tab 2" })}>
+                            <MainContent fullHeight disablePadding>
+                                <DataGrid
+                                    columns={[
+                                        { field: "id", headerName: "ID", width: 90 },
+                                        { field: "name", headerName: "Name", flex: 1 },
+                                    ]}
+                                    rows={products}
+                                    components={{
+                                        Toolbar: Toolbar,
+                                    }}
+                                    componentsProps={{
+                                        toolbar: {
+                                            toolbarAction: (
+                                                <Button
+                                                    startIcon={<Add />}
+                                                    onClick={() => editDialogApi.current?.openAddDialog()}
+                                                    variant="contained"
+                                                    color="primary"
+                                                >
+                                                    <FormattedMessage {...messages.add} />
+                                                </Button>
+                                            ),
+                                        },
+                                    }}
+                                />
+                            </MainContent>
+                        </Stack>
+                    </MainContent>
+                </RouterTab>
+            </RouterTabs>
+            <AddProductDialog dialogApiRef={editDialogApi} />
+        </>
+    );
+}
+
+storiesOf("@comet/admin/edit-dialog", module)
+    .addDecorator(storyRouterDecorator())
+    .add("Edit Dialog in RouterTabs", () => <Story />);
