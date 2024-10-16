5e8bd2162b8cf11a12c7048517f4ebd968c43a12
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-10-16 10:39:32 +0200

    Use type from `@mikro-orm/core` now that packages have been merged

--- packages/api/cms-api/src/blocks/decorators/root-block-entity.ts
+++ packages/api/cms-api/src/blocks/decorators/root-block-entity.ts
@@ -1,9 +1,4 @@
-// Copied from @mikro-orm/core since we don't have a dependency on it in @comet/blocks-api.
-// TODO Remove type once we merge @comet/cms-api and @comet/blocks-api.
-// https://github.com/mikro-orm/mikro-orm/blob/master/packages/core/src/typings.ts#L237
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type AnyEntity<T = any> = Partial<T>;
+import { AnyEntity } from "@mikro-orm/core";
 
 export interface RootBlockEntityOptions<Entity extends AnyEntity = AnyEntity> {
     isVisible?: (entity: Entity) => boolean;
