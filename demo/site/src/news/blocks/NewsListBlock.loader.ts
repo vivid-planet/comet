184a3a604469db67fc27016ae21cd3506cfaf7a0
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-10-17 12:51:28 +0200

    Add `newsListByIds` query

--- demo/site/src/news/blocks/NewsListBlock.loader.ts
+++ demo/site/src/news/blocks/NewsListBlock.loader.ts
@@ -6,33 +6,30 @@ import { GQLNewsListBlockNewsFragment, GQLNewsListBlockQuery, GQLNewsListBlockQu
 export type LoadedData = GQLNewsListBlockNewsFragment[];
 
 export const loader: BlockLoader<NewsListBlockData> = async ({ blockData, graphQLFetch }): Promise<LoadedData> => {
-    const newsList: LoadedData = [];
-
-    // TODO create a require that allows loading multiple news by IDs at once?
-    for (const id of blockData.ids) {
-        const data = await graphQLFetch<GQLNewsListBlockQuery, GQLNewsListBlockQueryVariables>(
-            gql`
-                query NewsListBlock($id: ID!) {
-                    news(id: $id) {
-                        ...NewsListBlockNews
-                    }
-                }
-
-                fragment NewsListBlockNews on News {
-                    id
-                    title
-                    slug
-                    scope {
-                        domain
-                        language
-                    }
-                }
-            `,
-            { id },
-        );
-
-        newsList.push(data.news);
+    if (blockData.ids.length === 0) {
+        return [];
     }
 
-    return newsList;
+    const data = await graphQLFetch<GQLNewsListBlockQuery, GQLNewsListBlockQueryVariables>(
+        gql`
+            query NewsListBlock($ids: [ID!]!) {
+                newsListByIds(ids: $ids) {
+                    ...NewsListBlockNews
+                }
+            }
+
+            fragment NewsListBlockNews on News {
+                id
+                title
+                slug
+                scope {
+                    domain
+                    language
+                }
+            }
+        `,
+        { ids: blockData.ids },
+    );
+
+    return data.newsListByIds;
 };
