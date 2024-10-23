493954e10b7cc08cd886f0ffa6a9699c7b16c555
David Schwarz <david.schwarz@vivid-planet.com>
2024-10-23 17:59:12 +0200

    update conflict dialog styling

--- packages/admin/cms-admin/src/dam/DataGrid/duplicatedFilenames/ManuallyHandleDuplicatedFilenamesDialog.tsx
+++ packages/admin/cms-admin/src/dam/DataGrid/duplicatedFilenames/ManuallyHandleDuplicatedFilenamesDialog.tsx
@@ -1,6 +1,6 @@
-import { Tooltip } from "@comet/admin";
-import { Warning } from "@comet/admin-icons";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, List, ListItem, ListItemText, Typography } from "@mui/material";
+import { Alert } from "@comet/admin";
+import { Duplicate, Save } from "@comet/admin-icons";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, List, ListItem, ListItemText, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { Fragment } from "react";
 import { FormattedMessage } from "react-intl";
@@ -15,17 +15,6 @@ const StyledList = styled(List)`
 
 const StyledListItem = styled(ListItem)`
     padding: ${({ theme }) => theme.spacing(2)} ${({ theme }) => theme.spacing(4)};
-`;
-
-const InstructionWrapper = styled("div")`
-    display: flex;
-    align-items: center;
-    gap: ${({ theme }) => theme.spacing(1)};
-`;
-
-const WarningIcon = styled(Warning)`
-    color: ${({ theme }) => theme.palette.error.main};
-    font-size: 20px;
 `;
 
 interface DuplicateFilenameDialogProps {
@@ -44,7 +33,7 @@ export const ManuallyHandleDuplicatedFilenamesDialog: React.VoidFunctionComponen
     onReplace,
 }) => {
     return (
-        <Dialog open={open}>
+        <Dialog open={open} fullWidth maxWidth="lg">
             <DialogTitle>
                 <FormattedMessage
                     id="comet.dam.duplicateFilenameDialog.title"
@@ -53,15 +42,46 @@ export const ManuallyHandleDuplicatedFilenamesDialog: React.VoidFunctionComponen
                 />
             </DialogTitle>
             <DialogContent>
-                <InstructionWrapper>
-                    <WarningIcon />
-                    <Typography variant="h5" component="h3">
+                <Alert
+                    severity="warning"
+                    title={
                         <FormattedMessage
-                            id="comet.dam.duplicateFilenameDialog.introduction"
-                            defaultMessage="The following filenames already exist:"
+                            id="comet.dam.duplicateFilenameDialog.actionAlert"
+                            defaultMessage="The following {count, plural, one {filename} other {filenames}} already exist. Please choose an action."
+                            values={{ count: filenameData.length }}
+                        />
+                    }
+                >
+                    <Typography variant="body2">
+                        <Typography variant="body2" style={{ fontWeight: "bold" }} sx={{ display: "inline" }}>
+                            <FormattedMessage id="comet.dam.duplicateFilenameDialog.skip" defaultMessage="Skip" />:
+                        </Typography>{" "}
+                        <FormattedMessage
+                            id="comet.dam.duplicateFilenameDialog.skipDescription"
+                            defaultMessage="All duplicates are skipped. All other images are uploaded as usual."
                         />
                     </Typography>
-                </InstructionWrapper>
+
+                    <Typography variant="body2" sx={{ whiteSpace: "nowrap" }}>
+                        <Typography variant="body2" style={{ fontWeight: "bold" }} sx={{ display: "inline" }}>
+                            <FormattedMessage id="comet.dam.duplicateFilenameDialog.replace" defaultMessage="Replace" />:
+                        </Typography>{" "}
+                        <FormattedMessage
+                            id="comet.dam.duplicateFilenameDialog.replaceDescription"
+                            defaultMessage="Originals will be replaced fully by duplicates. This does not affect the cropping settings of the image."
+                        />
+                    </Typography>
+
+                    <Typography variant="body2" sx={{ whiteSpace: "nowrap" }}>
+                        <Typography variant="body2" style={{ fontWeight: "bold" }} sx={{ display: "inline" }}>
+                            <FormattedMessage id="comet.dam.duplicateFilenameDialog.saveAsCopy" defaultMessage="Save as copy" />:
+                        </Typography>{" "}
+                        <FormattedMessage
+                            id="comet.dam.duplicateFilenameDialog.saveAsCopyDescription"
+                            defaultMessage="The duplicates are uploaded as copies and “copy” is added to the end of the file names."
+                        />
+                    </Typography>
+                </Alert>
 
                 <StyledList>
                     {filenameData.map((data, index) => {
@@ -78,44 +98,37 @@ export const ManuallyHandleDuplicatedFilenamesDialog: React.VoidFunctionComponen
                 </StyledList>
             </DialogContent>
             <DialogActions>
-                <Button variant="outlined" onClick={onSkip}>
-                    <FormattedMessage
-                        id="comet.dam.duplicateFilenameDialog.action.skip"
-                        defaultMessage="Skip {count, plural, one {file} other {files}}"
-                        values={{
-                            count: filenameData.length,
-                        }}
-                    />
-                </Button>
-                <Button variant="contained" color="warning" onClick={onReplace}>
-                    <FormattedMessage
-                        id="comet.dam.duplicateFilenameDialog.action.replaceFiles"
-                        defaultMessage="Replace {count, plural, one {file} other {files}}"
-                        values={{
-                            count: filenameData.length,
-                        }}
-                    />
-                    <Tooltip
-                        trigger="hover"
-                        title={
+                <Box display="flex" justifyContent="space-between" width="100%">
+                    <Button variant="text" color="secondary" onClick={onSkip}>
+                        <FormattedMessage
+                            id="comet.dam.duplicateFilenameDialog.action.skip"
+                            defaultMessage="Skip {count, plural, one {file} other {files}}"
+                            values={{
+                                count: filenameData.length,
+                            }}
+                        />
+                    </Button>
+                    <Box>
+                        <Button variant="outlined" onClick={onReplace} startIcon={<Duplicate />} sx={{ marginRight: "10px" }}>
                             <FormattedMessage
-                                id="comet.dam.duplicateWarning"
-                                defaultMessage="This will not modify the cropping where the image file is currently used."
+                                id="comet.dam.duplicateFilenameDialog.action.replaceFiles"
+                                defaultMessage="Replace {count, plural, one {file} other {files}}"
+                                values={{
+                                    count: filenameData.length,
+                                }}
                             />
-                        }
-                    >
-                        <Warning sx={{ marginLeft: 1 }} />
-                    </Tooltip>
-                </Button>
-                <Button variant="contained" color="primary" onClick={onUpload}>
-                    <FormattedMessage
-                        id="comet.dam.duplicateFilenameDialog.action.uploadAsCopy"
-                        defaultMessage="Upload as {count, plural, one {copy} other {copies}}"
-                        values={{
-                            count: filenameData.length,
-                        }}
-                    />
-                </Button>
+                        </Button>
+                        <Button variant="contained" color="primary" onClick={onUpload} startIcon={<Save />}>
+                            <FormattedMessage
+                                id="comet.dam.duplicateFilenameDialog.action.uploadAsCopy"
+                                defaultMessage="Upload as {count, plural, one {copy} other {copies}}"
+                                values={{
+                                    count: filenameData.length,
+                                }}
+                            />
+                        </Button>
+                    </Box>
+                </Box>
             </DialogActions>
         </Dialog>
     );
