vivid-planet/comet

commit 73c07ea6ee4586a721758c33b8c5b606b6319402

Set content scopes in request object (#2580)
We sometimes need the affected content scope(s) in a block's transformer
service. To enable this, we set the content scopes retrieved in the
`UserPermissionsGuard` in the `request` object (similar to how the
`AuthGuard` sets the `user` ). This then allows accessing the content
scopes in the transformer service.

packages/api/cms-api/src/user-permissions/auth/user-permissions.guard.ts
@@ -2,6 +2,7 @@ import { CanActivate, ExecutionContext, Inject, Injectable } from "@nestjs/commo
 import { Reflector } from "@nestjs/core";
 import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
 
+import { getRequestFromExecutionContext } from "../../common/decorators/utils";
 import { ContentScopeService } from "../content-scope.service";
 import { DisablePermissionCheck, RequiredPermissionMetadata } from "../decorators/required-permission.decorator";
 import { CurrentUser } from "../dto/current-user";
@@ -19,6 +20,14 @@ export class UserPermissionsGuard implements CanActivate {
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const location = `${context.getClass().name}::${context.getHandler().name}()`;
+
+        const requiredContentScopes = await this.contentScopeService.getScopesForPermissionCheck(context);
+
+        // Ignore field resolvers as they have no scopes and would overwrite the scopes of the root query.
+        if (!this.isResolvingGraphQLField(context)) {
+            const request = getRequestFromExecutionContext(context);
+            request.contentScopes = this.contentScopeService.getUniqueScopes(requiredContentScopes);
+        }
 
         if (this.getDecorator(context, "disableCometGuards")) return true;
 
@@ -38,7 +47,6 @@ export class UserPermissionsGuard implements CanActivate {
             // At least one permission is required
             return requiredPermissions.some((permission) => this.accessControlService.isAllowed(user, permission));
         } else {
-            const requiredContentScopes = await this.contentScopeService.getScopesForPermissionCheck(context);
             if (requiredContentScopes.length === 0)
                 throw new Error(
                     `Could not get content scope. Either pass a scope-argument or add an @AffectedEntity()-decorator or enable skipScopeCheck in the @RequiredPermission()-decorator of ${location}`,

packages/api/cms-api/src/common/decorators/utils.ts
@@ -1,8 +1,7 @@
 import { ExecutionContext } from "@nestjs/common";
 import { GqlExecutionContext } from "@nestjs/graphql";
-import { Request } from "express";
 
-export const getRequestFromExecutionContext = (ctx: ExecutionContext): Request => {
+export const getRequestFromExecutionContext = (ctx: ExecutionContext) => {
     if (ctx.getType().toString() === "graphql") {
         return GqlExecutionContext.create(ctx).getContext().req;
     }

packages/api/cms-api/src/user-permissions/content-scope.service.ts
@@ -43,7 +43,19 @@ export class ContentScopeService {
     }
 
     async inferScopesFromExecutionContext(context: ExecutionContext): Promise<ContentScope[]> {
-        return [...new Set((await this.getScopesForPermissionCheck(context)).flat())];
+        return this.getUniqueScopes(await this.getScopesForPermissionCheck(context));
+    }
+
+    getUniqueScopes(scopes: ContentScope[][]): ContentScope[] {
+        const uniqueScopes: ContentScope[] = [];
+
+        scopes.flat().forEach((incomingScope) => {
+            if (!uniqueScopes.some((existingScope) => this.scopesAreEqual(existingScope, incomingScope))) {
+                uniqueScopes.push(incomingScope);
+            }
+        });
+
+        return uniqueScopes;
     }
 
     private async getContentScopesFromEntity(affectedEntity: AffectedEntityMeta, args: Record<string, string>): Promise<ContentScope[][]> {

commit 8384ab94f6e08a37e43546dd878fc7d120ba9e55

Consider `skipScopeCheck` when setting content scopes in context Fixes a bug introduced in https://github.com/vivid-planet/comet/pull/2580 where the guard would attempt to retrieve the content scopes even if `skipScopeCheck` was set to `true`.

packages/api/cms-api/src/user-permissions/auth/user-permissions.guard.ts
@@ -6,6 +6,7 @@ import { getRequestFromExecutionContext } from "../../common/decorators/utils";
 import { ContentScopeService } from "../content-scope.service";
 import { DisablePermissionCheck, RequiredPermissionMetadata } from "../decorators/required-permission.decorator";
 import { CurrentUser } from "../dto/current-user";
+import { ContentScope } from "../interfaces/content-scope.interface";
 import { ACCESS_CONTROL_SERVICE, USER_PERMISSIONS_OPTIONS } from "../user-permissions.constants";
 import { AccessControlServiceInterface, SystemUser, UserPermissionsOptions } from "../user-permissions.types";
 
@@ -21,10 +22,14 @@ export class UserPermissionsGuard implements CanActivate {
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const location = `${context.getClass().name}::${context.getHandler().name}()`;
 
-        const requiredContentScopes = await this.contentScopeService.getScopesForPermissionCheck(context);
+        let requiredContentScopes: ContentScope[][] | undefined;
+        const requiredPermission = this.getDecorator<RequiredPermissionMetadata>(context, "requiredPermission");
+        const skipScopeCheck = requiredPermission?.options?.skipScopeCheck ?? false;
 
         // Ignore field resolvers as they have no scopes and would overwrite the scopes of the root query.
-        if (!this.isResolvingGraphQLField(context)) {
+        if (!this.isResolvingGraphQLField(context) && !skipScopeCheck) {
+            requiredContentScopes = await this.contentScopeService.getScopesForPermissionCheck(context);
+
             const request = getRequestFromExecutionContext(context);
             request.contentScopes = this.contentScopeService.getUniqueScopes(requiredContentScopes);
         }
@@ -37,16 +42,18 @@ export class UserPermissionsGuard implements CanActivate {
         // System user authenticated via basic auth
         if (typeof user === "string" && this.options.systemUsers?.includes(user)) return true;
 
-        const requiredPermission = this.getDecorator<RequiredPermissionMetadata>(context, "requiredPermission");
         if (!requiredPermission && this.isResolvingGraphQLField(context)) return true;
         if (!requiredPermission) throw new Error(`RequiredPermission decorator is missing in ${location}`);
         const requiredPermissions = requiredPermission.requiredPermission;
         if (requiredPermissions.includes(DisablePermissionCheck)) return true;
         if (requiredPermissions.length === 0) throw new Error(`RequiredPermission decorator has empty permissions in ${location}`);
-        if (this.isResolvingGraphQLField(context) || requiredPermission.options?.skipScopeCheck) {
+        if (this.isResolvingGraphQLField(context) || skipScopeCheck) {
             // At least one permission is required
             return requiredPermissions.some((permission) => this.accessControlService.isAllowed(user, permission));
         } else {
+            if (!requiredContentScopes) {
+                requiredContentScopes = await this.contentScopeService.getScopesForPermissionCheck(context);
+            }
             if (requiredContentScopes.length === 0)
                 throw new Error(
                     `Could not get content scope. Either pass a scope-argument or add an @AffectedEntity()-decorator or enable skipScopeCheck in the @RequiredPermission()-decorator of ${location}`,
@@ -57,7 +64,7 @@ export class UserPermissionsGuard implements CanActivate {
             // The first level consists of submitted scopes and affected entities
             // The only case that there is more than one scope in the second level is when the ScopedEntity returns more scopes
             return requiredPermissions.some((permission) =>
-                requiredContentScopes.every((contentScopes) =>
+                requiredContentScopes?.every((contentScopes) =>
                     contentScopes.some((contentScope) => this.accessControlService.isAllowed(user, permission, contentScope)),
                 ),
             );