14fb120693bc8a39b0e98d5cdc527c23d941c1b3
Thomas Dax <thomas.dax@vivid-planet.com>
2024-11-06 16:49:19 +0100

    Add columns block to teaser and link list block to columns

--- demo/site/src/documents/pages/blocks/TeaserBlock.tsx
+++ demo/site/src/documents/pages/blocks/TeaserBlock.tsx
@@ -1,19 +1,21 @@
 "use client";
 import { PropsWithData, withPreview } from "@comet/cms-site";
 import { TeaserBlockData } from "@src/blocks.generated";
+import { ColumnsBlock } from "@src/blocks/ColumnsBlock";
 import { DamImageBlock } from "@src/blocks/DamImageBlock";
 import { HeadlineBlock } from "@src/blocks/HeadlineBlock";
 import { LinkListBlock } from "@src/blocks/LinkListBlock";
 import styled from "styled-components";
 
 const TeaserBlock = withPreview(
-    ({ data: { headline, image, links, buttons } }: PropsWithData<TeaserBlockData>) => {
+    ({ data: { headline, image, links, buttons, columns } }: PropsWithData<TeaserBlockData>) => {
         return (
             <Root>
                 <HeadlineBlock data={headline} />
                 <DamImageBlock data={image} aspectRatio="1x1" />
                 <LinkListBlock data={links} />
                 <LinkListBlock data={buttons} />
+                <ColumnsBlock data={columns} />
             </Root>
         );
     },
