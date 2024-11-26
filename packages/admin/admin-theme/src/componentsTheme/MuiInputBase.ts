6094e144ef8ac6122fbc45de1715a88b2bad1d6a
Kerstin Reichinger <kerstin.reichinger@vivid-planet.com>
2024-11-26 16:19:40 +0100

    Fix MuiInputBase border and background color

--- packages/admin/admin-theme/src/componentsTheme/MuiInputBase.ts
+++ packages/admin/admin-theme/src/componentsTheme/MuiInputBase.ts
@@ -7,27 +7,24 @@ export const getMuiInputBase: GetMuiComponentTheme<"MuiInputBase"> = (component,
     ...component,
     styleOverrides: mergeOverrideStyles<"MuiInputBase">(component?.styleOverrides, {
         root: {
-            border: `1px solid ${palette.grey[100]}`,
+            border: `1px solid ${palette.grey[200]}`,
             borderRadius: 2,
             backgroundColor: "#fff",
 
             [`&.${inputBaseClasses.focused}`]: {
                 borderColor: palette.primary.main,
             },
+
+            [`&.${inputBaseClasses.disabled}`]: {
+                borderColor: palette.grey[100],
+                backgroundColor: palette.grey[50],
+            },
         },
         adornedStart: {
             paddingLeft: spacing(2),
-
-            [`> span`]: {
-                fontWeight: 600,
-            },
         },
         adornedEnd: {
             paddingRight: spacing(2),
-
-            [`> span`]: {
-                fontWeight: 600,
-            },
         },
         multiline: {
             paddingTop: 0,
