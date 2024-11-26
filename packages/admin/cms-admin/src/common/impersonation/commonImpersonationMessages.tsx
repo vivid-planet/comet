4768ee4d901182bf3450227911929b9d491e9028
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2024-11-26 14:47:41 +0100

    correct commonImpersonationMessages object

--- packages/admin/cms-admin/src/common/impersonation/commonImpersonationMessages.tsx
+++ packages/admin/cms-admin/src/common/impersonation/commonImpersonationMessages.tsx
@@ -1,5 +1,7 @@
+import { FormattedMessage } from "react-intl";
+
 export const commonImpersonationMessages = {
-    impersonate: { id: "comet.generic.impersonate", defaultMessage: "Impersonate" },
-    startImpersonation: { id: "comet.generic.startImpersonation", defaultMessage: "Start Impersonation" },
-    stopImpersonation: { id: "comet.generic.stopImpersonation", defaultMessage: "Stop Impersonation" },
+    impersonate: <FormattedMessage id="comet.generic.impersonate" defaultMessage="Impersonate" />,
+    startImpersonation: <FormattedMessage id="comet.generic.startImpersonation" defaultMessage="Start Impersonation" />,
+    stopImpersonation: <FormattedMessage id="comet.generic.stopImpersonation" defaultMessage="Stop Impersonation" />,
 };
