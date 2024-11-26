6f43a57db98b4409cd23db7772b9957e4ac3f5f6
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2024-11-26 10:58:52 +0100

    Ensure backward compatibility in useBlockPreviewFetch (#2826)

--- .changeset/soft-apes-cheat.md
+++ .changeset/soft-apes-cheat.md
@@ -6,4 +6,37 @@
 
 Pass the `graphQLApiUrl` for `useBlockPreviewFetch` through the `IFrameBridge`
 
-It's not necessary anymore to set it in the site.
+It's not necessary to set it in the site anymore. To migrate, remove the argument from `useBlockPreviewFetch()`:
+
+```diff
+const PreviewPage = () => {
+    const iFrameBridge = useIFrameBridge();
+
+-   const { fetch, graphQLFetch } = useBlockPreviewFetch(graphQLApiUrl);
++   const { fetch, graphQLFetch } = useBlockPreviewFetch();
+
+    const [blockData, setBlockData] = useState<PageContentBlockData>();
+    useEffect(() => {
+        async function load() {
++           if (!graphQLFetch) {
++               return;
++           }
+            if (!iFrameBridge.block) {
+                setBlockData(undefined);
+                return;
+            }
+            const newData = await recursivelyLoadBlockData({
+                blockType: "PageContent",
+                blockData: iFrameBridge.block,
+                graphQLFetch,
+                fetch,
+                pageTreeNodeId: undefined, //we don't have a pageTreeNodeId in preview
+            });
+            setBlockData(newData);
+        }
+        load();
+    }, [iFrameBridge.block, fetch, graphQLFetch]);
+
+    return <div>{blockData && <PageContentBlock data={blockData} />}</div>;
+};
+```
--- packages/site/cms-site/src/iframebridge/useBlockPreviewFetch.tsx
+++ packages/site/cms-site/src/iframebridge/useBlockPreviewFetch.tsx
@@ -1,25 +1,35 @@
 import { useEffect, useRef } from "react";
 
 import { createFetchInMemoryCache } from "../graphQLFetch/fetchInMemoryCache";
-import { convertPreviewDataToHeaders, createFetchWithDefaults, createGraphQLFetch } from "../graphQLFetch/graphQLFetch";
+import { convertPreviewDataToHeaders, createFetchWithDefaults, createGraphQLFetch, GraphQLFetch } from "../graphQLFetch/graphQLFetch";
 import { useIFrameBridge } from "./useIFrameBridge";
 
 const cachingFetch = createFetchInMemoryCache(fetch);
 
-export function useBlockPreviewFetch() {
+type Fetch = typeof fetch;
+
+/**
+ * @deprecated Use useBlockPreviewFetch() without argument instead
+ * @param apiUrl the API URL
+ */
+export function useBlockPreviewFetch(apiUrl: string): { fetch: Fetch; graphQLFetch: GraphQLFetch };
+export function useBlockPreviewFetch(apiUrl?: string | undefined): { fetch: Fetch; graphQLFetch?: GraphQLFetch };
+export function useBlockPreviewFetch(apiUrl?: string | undefined) {
     const { showOnlyVisible, graphQLApiUrl } = useIFrameBridge();
 
-    const graphQLFetchRef = useRef(createBlockPreviewFetch(graphQLApiUrl, !showOnlyVisible));
+    const graphQLFetchRef = useRef(apiUrl ? createBlockPreviewFetch(apiUrl, !showOnlyVisible) : undefined);
     useEffect(() => {
-        graphQLFetchRef.current = createBlockPreviewFetch(graphQLApiUrl, !showOnlyVisible);
+        if (graphQLApiUrl) {
+            graphQLFetchRef.current = createBlockPreviewFetch(graphQLApiUrl, !showOnlyVisible);
+        }
     }, [showOnlyVisible, graphQLApiUrl]);
+
     return {
         graphQLFetch: graphQLFetchRef.current,
         fetch: cachingFetch,
     };
 }
 
-function createBlockPreviewFetch(graphqlApiUrl: string | undefined, includeInvisible: boolean) {
-    if (!graphqlApiUrl) return undefined;
+function createBlockPreviewFetch(graphqlApiUrl: string, includeInvisible: boolean) {
     return createGraphQLFetch(createFetchWithDefaults(cachingFetch, { headers: convertPreviewDataToHeaders({ includeInvisible }) }), graphqlApiUrl);
 }
