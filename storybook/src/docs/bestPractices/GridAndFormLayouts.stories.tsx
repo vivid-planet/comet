968e9538e28dd6fb4116815620d37fdcb3207d46
andrearutrecht <andrea.rutrecht@vivid-planet.com>
2024-12-09 11:07:26 +0100

    Replace 'Dialog' with 'EditDialog' in best practice stories

--- storybook/src/docs/bestPractices/GridAndFormLayouts.stories.tsx
+++ storybook/src/docs/bestPractices/GridAndFormLayouts.stories.tsx
@@ -1,5 +1,4 @@
 import {
-    CancelButton,
     CrudMoreActionsMenu,
     DataGridToolbar,
     FieldSet,
@@ -8,7 +7,6 @@ import {
     GridColDef,
     GridFilterButton,
     Loading,
-    OkayButton,
     RouterTab,
     RouterTabs,
     SaveBoundary,
@@ -26,9 +24,10 @@ import {
     ToolbarBackButton,
     ToolbarFillSpace,
     ToolbarItem,
+    useEditDialog,
 } from "@comet/admin";
 import { Add, Edit, Html, Select as SelectIcon } from "@comet/admin-icons";
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import { DataGrid, GridSelectionModel, GridToolbarQuickFilter } from "@mui/x-data-grid";
 import React, { ReactNode, useEffect, useRef, useState } from "react";
 
@@ -194,22 +193,23 @@ export const LargeFormOnAPage = {
     },
 };
 
+// TODO: Shows dirty handler dialog after save
 export const SimpleFormInADialog = {
     render: () => {
-        const Form = ({ id }: { id?: string }) => {
+        const Form = ({ id, mode }: { id?: string; mode?: "edit" | "add" }) => {
             const { rows, loading } = useData();
-            const editingExistingItem = Boolean(id);
+            // const editingExistingItem = Boolean(id);
 
-            if (editingExistingItem && loading) {
+            if (loading) {
                 return <Loading />;
             }
 
             return (
                 <FinalForm
-                    onSubmit={() => {
+                    onSubmit={(values: FormValues) => {
                         // Submit data
                     }}
-                    mode={editingExistingItem ? "edit" : "add"}
+                    mode={mode ?? "add"}
                     initialValues={rows.find((row) => row.id === id)}
                 >
                     <TextField name="title" required fullWidth variant="horizontal" label="Title" />
@@ -218,39 +218,32 @@ export const SimpleFormInADialog = {
             );
         };
 
-        const [showDialog, setShowDialog] = useState(true); // In a real application, this would generally be `false` by default
-        const editingId: string | undefined = undefined; // In a real application, this would be set when editing an existing item
+        const [EditDialog, { id: selectedId, mode }, editDialogApi] = useEditDialog();
 
         return (
-            <SaveBoundary onAfterSave={() => setShowDialog(false)}>
+            <>
                 <StackToolbar>
                     <ToolbarBackButton />
                     <ToolbarAutomaticTitleItem />
                     <ToolbarFillSpace />
                     <ToolbarActions>
-                        <Button color="primary" variant="contained" onClick={() => setShowDialog(true)}>
+                        <Button color="primary" variant="contained" onClick={() => editDialogApi.openAddDialog(selectedId)}>
                             Open dialog
                         </Button>
                     </ToolbarActions>
                 </StackToolbar>
                 <StackMainContent>
                     <Typography variant="h3">Open the dialog to see the form.</Typography>
-                    <Dialog open={showDialog} onClose={() => setShowDialog(false)}>
-                        <DialogTitle>Dialog title</DialogTitle>
-                        <DialogContent>
-                            <Form id={editingId} />
-                        </DialogContent>
-                        <DialogActions>
-                            <CancelButton onClick={() => setShowDialog(false)} />
-                            <SaveBoundarySaveButton />
-                        </DialogActions>
-                    </Dialog>
                 </StackMainContent>
-            </SaveBoundary>
+                <EditDialog title="Dialog title">
+                    <Form id={selectedId} mode={mode} />
+                </EditDialog>
+            </>
         );
     },
 };
 
+// TODO: Shows dirty handler dialog after save
 export const LargeFormInADialog = {
     render: () => {
         const Form = ({ id }: { id?: string }) => {
@@ -286,35 +279,27 @@ export const LargeFormInADialog = {
             );
         };
 
-        const [showDialog, setShowDialog] = useState(true); // In a real application, this would generally be `false` by default
-        const editingId: string | undefined = undefined; // In a real application, this would be set when editing an existing item
+        const [EditDialog, { id: selectedId }, editDialogApi] = useEditDialog();
 
         return (
-            <SaveBoundary onAfterSave={() => setShowDialog(false)}>
+            <>
                 <StackToolbar>
                     <ToolbarBackButton />
                     <ToolbarAutomaticTitleItem />
                     <ToolbarFillSpace />
                     <ToolbarActions>
-                        <Button color="primary" variant="contained" onClick={() => setShowDialog(true)}>
+                        <Button color="primary" variant="contained" onClick={() => editDialogApi.openAddDialog(selectedId)}>
                             Open dialog
                         </Button>
                     </ToolbarActions>
                 </StackToolbar>
                 <StackMainContent>
                     <Typography variant="h3">Open the dialog to see the form.</Typography>
-                    <Dialog open={showDialog} onClose={() => setShowDialog(false)}>
-                        <DialogTitle>Dialog title</DialogTitle>
-                        <DialogContent>
-                            <Form id={editingId} />
-                        </DialogContent>
-                        <DialogActions>
-                            <CancelButton onClick={() => setShowDialog(false)} />
-                            <SaveBoundarySaveButton />
-                        </DialogActions>
-                    </Dialog>
+                    <EditDialog title="Dialog title">
+                        <Form id={selectedId} />
+                    </EditDialog>
                 </StackMainContent>
-            </SaveBoundary>
+            </>
         );
     },
 };
@@ -398,6 +383,7 @@ export const SingleGridAutoHeight = {
     },
 };
 
+// TODO: Shows dirty handler dialog after save
 export const GridWithFormInADialog = {
     render: () => {
         const Form = ({ id }: { id?: string }) => {
@@ -422,7 +408,7 @@ export const GridWithFormInADialog = {
             );
         };
 
-        const [editingId, setEditingId] = useState<string | undefined>();
+        const [EditDialog, { id: selectedId, mode }, editDialogApi] = useEditDialog();
         const { rows, loading } = useData();
 
         const GridToolbar = () => {
@@ -436,7 +422,7 @@ export const GridWithFormInADialog = {
                     </ToolbarItem>
                     <ToolbarFillSpace />
                     <ToolbarActions>
-                        <Button color="primary" variant="contained" startIcon={<Add />} onClick={() => setEditingId("add")}>
+                        <Button color="primary" variant="contained" startIcon={<Add />} onClick={() => editDialogApi.openAddDialog()}>
                             Add new item
                         </Button>
                     </ToolbarActions>
@@ -452,7 +438,7 @@ export const GridWithFormInADialog = {
                 headerName: "",
                 width: 52,
                 renderCell: ({ row }) => (
-                    <IconButton color="primary" onClick={() => setEditingId(row.id)}>
+                    <IconButton color="primary" onClick={() => editDialogApi.openEditDialog(row.id)}>
                         <Edit />
                     </IconButton>
                 ),
@@ -468,18 +454,9 @@ export const GridWithFormInADialog = {
                 <StackMainContent fullHeight>
                     <DataGrid disableSelectionOnClick rows={rows} columns={columns} loading={loading} components={{ Toolbar: GridToolbar }} />
                 </StackMainContent>
-                <Dialog open={!!editingId} onClose={() => setEditingId(undefined)}>
-                    <SaveBoundary onAfterSave={() => setEditingId(undefined)}>
-                        <DialogTitle>{editingId === "add" ? "Add new item" : `${rows.find((row) => row.id === editingId)?.title}`}</DialogTitle>
-                        <DialogContent>
-                            <Form id={editingId === "add" ? undefined : editingId} />
-                        </DialogContent>
-                        <DialogActions>
-                            <CancelButton onClick={() => setEditingId(undefined)} />
-                            <SaveBoundarySaveButton />
-                        </DialogActions>
-                    </SaveBoundary>
-                </Dialog>
+                <EditDialog title={mode === "add" ? "Add new item" : `${rows.find((row) => row.id === selectedId)?.title}`}>
+                    <Form id={mode === "add" ? undefined : selectedId} />
+                </EditDialog>
             </>
         );
     },
@@ -596,6 +573,7 @@ export const GridWithFormOnAPage = {
     },
 };
 
+// TODO: Shows dirty handler dialog after save
 export const NestedGridsAndFormsWithTabs = {
     render: () => {
         const Form = ({ id }: { id?: string }) => {
@@ -620,7 +598,7 @@ export const NestedGridsAndFormsWithTabs = {
             );
         };
 
-        const [showAddDialog, setShowAddDialog] = useState(false);
+        const [EditDialog, , editDialogApi] = useEditDialog();
         const { rows, loading } = useData();
 
         const GridToolbar = () => {
@@ -634,7 +612,7 @@ export const NestedGridsAndFormsWithTabs = {
                     </ToolbarItem>
                     <ToolbarFillSpace />
                     <ToolbarActions>
-                        <Button color="primary" variant="contained" startIcon={<Add />} onClick={() => setShowAddDialog(true)}>
+                        <Button color="primary" variant="contained" startIcon={<Add />} onClick={() => editDialogApi.openAddDialog()}>
                             Add new item
                         </Button>
                     </ToolbarActions>
@@ -709,23 +687,15 @@ export const NestedGridsAndFormsWithTabs = {
                         }}
                     </StackPage>
                 </StackSwitch>
-                <Dialog open={showAddDialog} onClose={() => setShowAddDialog(false)}>
-                    <SaveBoundary onAfterSave={() => setShowAddDialog(false)}>
-                        <DialogTitle>Add new item</DialogTitle>
-                        <DialogContent>
-                            <Form />
-                        </DialogContent>
-                        <DialogActions>
-                            <CancelButton onClick={() => setShowAddDialog(false)} />
-                            <SaveBoundarySaveButton />
-                        </DialogActions>
-                    </SaveBoundary>
-                </Dialog>
+                <EditDialog title="Add new item">
+                    <Form />
+                </EditDialog>
             </>
         );
     },
 };
 
+// TODO: Shows dirty handler dialog after save
 export const NestedFormInGridInTabsInGrid = {
     render: () => {
         const Form = ({ id }: { id?: string }) => {
@@ -750,7 +720,7 @@ export const NestedFormInGridInTabsInGrid = {
             );
         };
 
-        const [showAddDialog, setShowAddDialog] = useState(false);
+        const [EditDialog, , editDialogApi] = useEditDialog();
         const { rows, loading } = useData();
 
         const GridToolbar = () => {
@@ -764,7 +734,7 @@ export const NestedFormInGridInTabsInGrid = {
                     </ToolbarItem>
                     <ToolbarFillSpace />
                     <ToolbarActions>
-                        <Button color="primary" variant="contained" startIcon={<Add />} onClick={() => setShowAddDialog(true)}>
+                        <Button color="primary" variant="contained" startIcon={<Add />} onClick={() => editDialogApi.openAddDialog()}>
                             Add new item
                         </Button>
                     </ToolbarActions>
@@ -854,18 +824,9 @@ export const NestedFormInGridInTabsInGrid = {
                         }}
                     </StackPage>
                 </StackSwitch>
-                <Dialog open={showAddDialog} onClose={() => setShowAddDialog(false)}>
-                    <SaveBoundary onAfterSave={() => setShowAddDialog(false)}>
-                        <DialogTitle>Add new item</DialogTitle>
-                        <DialogContent>
-                            <Form />
-                        </DialogContent>
-                        <DialogActions>
-                            <CancelButton onClick={() => setShowAddDialog(false)} />
-                            <SaveBoundarySaveButton />
-                        </DialogActions>
-                    </SaveBoundary>
-                </Dialog>
+                <EditDialog title="Add new item">
+                    <Form />
+                </EditDialog>
             </>
         );
     },
@@ -946,9 +907,10 @@ export const GridWithSelectionAndMoreActionsMenu = {
 
 export const GridWithSelectionInDialog = {
     render: () => {
-        const [showDialog, setShowDialog] = useState(true); // In a real application, this would generally be `false` by default
-        const [selectionModel, setSelectionModel] = useState<GridSelectionModel>([]); // TODO: Check why this is reset every time the dialog is opened. Is this only in storybook?
+        const [selectionModel, setSelectionModel] = useState<GridSelectionModel>([]);
         const { rows, loading } = useData();
+
+        const [EditDialog, , editDialogApi] = useEditDialog();
 
         const GridToolbar = () => {
             return (
@@ -975,7 +937,7 @@ export const GridWithSelectionInDialog = {
                     <ToolbarAutomaticTitleItem />
                     <ToolbarFillSpace />
                     <ToolbarActions>
-                        <Button color="primary" variant="contained" startIcon={<SelectIcon />} onClick={() => setShowDialog(true)}>
+                        <Button color="primary" variant="contained" startIcon={<SelectIcon />} onClick={() => editDialogApi.openAddDialog()}>
                             Select items
                         </Button>
                     </ToolbarActions>
@@ -1003,8 +965,7 @@ export const GridWithSelectionInDialog = {
                         <Typography variant="h4">No items selected :(</Typography>
                     )}
                 </StackMainContent>
-                <Dialog open={showDialog} onClose={() => setShowDialog(false)}>
-                    <DialogTitle>Selected items</DialogTitle>
+                <EditDialog onAfterSave={() => editDialogApi.closeDialog()} title="Selected items">
                     <DataGrid
                         disableSelectionOnClick
                         rows={rows}
@@ -1016,10 +977,7 @@ export const GridWithSelectionInDialog = {
                         selectionModel={selectionModel}
                         onSelectionModelChange={setSelectionModel}
                     />
-                    <DialogActions>
-                        <OkayButton onClick={() => setShowDialog(false)} />
-                    </DialogActions>
-                </Dialog>
+                </EditDialog>
             </>
         );
     },
