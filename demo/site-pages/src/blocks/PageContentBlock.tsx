8c22f2aeb51be66ca478cd9d77edeeac26b59725
Piotr Gulbis <piotr.gulbis@vivid-planet.com>
2024-12-10 16:43:43 +0100

    update site-pages to removed blocks

--- demo/site-pages/src/blocks/PageContentBlock.tsx
+++ demo/site-pages/src/blocks/PageContentBlock.tsx
@@ -5,17 +5,17 @@ import { AnchorBlock } from "./AnchorBlock";
 import { ColumnsBlock } from "./ColumnsBlock";
 import { DamImageBlock } from "./DamImageBlock";
 import { FullWidthImageBlock } from "./FullWidthImageBlock";
-import { HeadlineBlock } from "./HeadlineBlock";
+import { HeadingBlock } from "./HeadingBlock";
 import { LinkListBlock } from "./LinkListBlock";
 import { MediaBlock } from "./MediaBlock";
-import RichTextBlock from "./RichTextBlock";
+import { RichTextBlock } from "./RichTextBlock";
 import SpaceBlock from "./SpaceBlock";
 import { TextImageBlock } from "./TextImageBlock";
 
 const supportedBlocks: SupportedBlocks = {
     space: (props) => <SpaceBlock data={props} />,
     richText: (props) => <RichTextBlock data={props} />,
-    headline: (props) => <HeadlineBlock data={props} />,
+    headline: (props) => <HeadingBlock data={props} />,
     image: (props) => <DamImageBlock data={props} aspectRatio="inherit" />,
     textImage: (props) => <TextImageBlock data={props} />,
     damVideo: (props) => <DamVideoBlock data={props} />,
