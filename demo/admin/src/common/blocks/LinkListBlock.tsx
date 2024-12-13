15a1acb561a3dbf19c0a92795e98b68a46519592
Piotr Gulbis <pgulbis@gmail.com>
2024-12-13 11:49:35 +0100

    chore: remove link list block with additional fields block

--- demo/admin/src/common/blocks/LinkListBlock.tsx
+++ demo/admin/src/common/blocks/LinkListBlock.tsx
@@ -1,5 +1,8 @@
 import { createListBlock } from "@comet/blocks-admin";
 import { TextLinkBlock } from "@src/common/blocks/TextLinkBlock";
+import { userGroupAdditionalItemFields } from "@src/userGroups/userGroupAdditionalItemFields";
+import { UserGroupChip } from "@src/userGroups/UserGroupChip";
+import { UserGroupContextMenuItem } from "@src/userGroups/UserGroupContextMenuItem";
 import { FormattedMessage } from "react-intl";
 
 export const LinkListBlock = createListBlock({
@@ -8,4 +11,13 @@ export const LinkListBlock = createListBlock({
     displayName: <FormattedMessage id="blocks.linkList.displayName" defaultMessage="Link list" />,
     itemName: <FormattedMessage id="blocks.linkList.itemName" defaultMessage="link" />,
     itemsName: <FormattedMessage id="blocks.linkList.itemsName" defaultMessage="links" />,
+    additionalItemFields: {
+        ...userGroupAdditionalItemFields,
+    },
+    AdditionalItemContextMenuItems: ({ item, onChange, onMenuClose }) => {
+        return <UserGroupContextMenuItem item={item} onChange={onChange} onMenuClose={onMenuClose} />;
+    },
+    AdditionalItemContent: ({ item }) => {
+        return <UserGroupChip item={item} />;
+    },
 });
