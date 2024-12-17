8b825c3e58a7faf65feff2ef348db5e669dc7efa
Niko Sams <ns@vivid-planet.com>
2024-12-17 17:10:23 +0100

    Demo Site: Fix Redirect to InternalPage, use absolute path
    previously the destination was a relative path appended to the current url (redirect source url) resulting in broken redirects

--- demo/site/src/app/[domain]/[language]/[[...path]]/page.tsx
+++ demo/site/src/app/[domain]/[language]/[[...path]]/page.tsx
@@ -86,7 +86,7 @@ export default async function Page({ params }: Props) {
                     case "internal": {
                         const internalLink = target.block.props as InternalLinkBlockData;
                         if (internalLink.targetPage) {
-                            destination = `${(internalLink.targetPage.scope as GQLPageTreeNodeScope).language}/${internalLink.targetPage.path}`;
+                            destination = `/${(internalLink.targetPage.scope as GQLPageTreeNodeScope).language}/${internalLink.targetPage.path}`;
                         }
                         break;
                     }
