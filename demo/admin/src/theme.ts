376ff70869161cefd56c35aa00539972eededa47
Ricky Smith <jamesricky@me.com>
2024-12-23 13:47:31 +0100

    Styling and custom component for DataGrid panels

--- demo/admin/src/theme.ts
+++ demo/admin/src/theme.ts
@@ -1,4 +1,27 @@
+import { DataGridPanel } from "@comet/admin";
 import { createCometTheme } from "@comet/admin-theme";
 import type {} from "@mui/lab/themeAugmentation";
+import type {} from "@mui/x-data-grid/themeAugmentation";
+import { IntlShape } from "react-intl";
 
-export const theme = createCometTheme();
+export const getTheme = (intl: IntlShape) =>
+    createCometTheme({
+        components: {
+            MuiDataGrid: {
+                defaultProps: {
+                    localeText: {
+                        // TODO: Move this into the theme by default - once `@comet/admin-theme` has been merged with `@comet/admin`
+                        filterPanelColumns: intl.formatMessage({ id: "dataGrid.filterPanelColumns", defaultMessage: "Column" }),
+                        columnsPanelTextFieldPlaceholder: intl.formatMessage({
+                            id: "dataGrid.columnsPanelTextFieldPlaceholder",
+                            defaultMessage: "Find column...",
+                        }),
+                    },
+                    components: {
+                        // TODO: Move this into the theme by default - once `@comet/admin-theme` has been merged with `@comet/admin`
+                        Panel: DataGridPanel,
+                    },
+                },
+            },
+        },
+    });
