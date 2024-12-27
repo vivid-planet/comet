ea9a6a2087caa510f4b7b97ed85cafa412859677
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2024-12-27 11:48:50 +0100

    set square: false on MuiCard to override default MuiPaper behavior and apply border-radius

--- packages/admin/admin-theme/src/componentsTheme/MuiCard.ts
+++ packages/admin/admin-theme/src/componentsTheme/MuiCard.ts
@@ -1,11 +1,19 @@
+import { paperClasses } from "@mui/material";
+
 import { mergeOverrideStyles } from "../utils/mergeOverrideStyles";
 import { GetMuiComponentTheme } from "./getComponentsTheme";
 
 export const getMuiCard: GetMuiComponentTheme<"MuiCard"> = (component, { spacing }) => ({
     ...component,
+    defaultProps: {
+        square: false,
+        ...component?.defaultProps,
+    },
     styleOverrides: mergeOverrideStyles<"MuiCard">(component?.styleOverrides, {
         root: {
-            borderRadius: "4px",
+            [`&.${paperClasses.rounded}`]: {
+                borderRadius: "4px",
+            },
         },
     }),
 });
