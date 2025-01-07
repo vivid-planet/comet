42e358cee2398069a23ac1c68c31e45a23e5cb45
Ricky Smith <jamesricky@me.com>
2025-01-07 17:30:26 +0100

    Temp fix for another bug to allow repruducing the save-boundary bug

--- packages/admin/blocks-admin/src/blocks/common/AdminTabs.tsx
+++ packages/admin/blocks-admin/src/blocks/common/AdminTabs.tsx
@@ -1,48 +1,59 @@
-import { RouteWithErrorBoundary } from "@comet/admin";
+import { RouterTab, RouterTabs } from "@comet/admin";
 import { Tab as MuiTab, TabProps, Tabs as MuiTabs } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { Switch, useRouteMatch } from "react-router";
-import { Link, LinkProps } from "react-router-dom";
+import { LinkProps } from "react-router-dom";
 
 import { AdminComponentPart } from "../types";
-import { TabContent } from "./AdminTabsTabContent";
 
 export interface AdminTabsProps {
     children: AdminComponentPart[];
 }
 
 export function AdminTabs({ children }: AdminTabsProps): JSX.Element | null {
-    const match = useRouteMatch();
-    const tabRouteMatch = useRouteMatch<{ tab: string }>(`${match.path}/:tab`);
-    const selected = tabRouteMatch?.params.tab;
-    if (children.length < 1) {
-        return null;
-    }
-    const [firstTab, ...otherTabs] = children;
-    const selectedTab = children.find((tab) => tab.key === selected) ? selected : firstTab.key; //fall back to first, as <Switch> does
-
+    // Temporärer fix für https://vivid-planet.atlassian.net/browse/COM-1511
     return (
         <Root key="tabs">
-            <Tabs value={selectedTab} variant="scrollable" scrollButtons="auto">
-                {children.map((tab, index) => {
-                    const url = index === 0 ? match.url : `${match.url}/${tab.key}`;
-                    return <Tab key={tab.key} value={tab.key} label={tab.label} component={Link} to={url} />;
-                })}
-            </Tabs>
-            <TabContent selectedTab={selectedTab}>
-                <Switch>
-                    {otherTabs.map((tab) => (
-                        <RouteWithErrorBoundary key={tab.key} path={`${match.url}/${tab.key}`}>
-                            {tab.content}
-                        </RouteWithErrorBoundary>
-                    ))}
-
-                    {/* first tab is default route, last in <Switch> */}
-                    <RouteWithErrorBoundary path={match.url}>{firstTab.content}</RouteWithErrorBoundary>
-                </Switch>
-            </TabContent>
+            <RouterTabs>
+                {children.map((tab, index) => (
+                    <RouterTab key={tab.key} label={tab.label} path={index === 0 ? "" : `/${tab.key}`}>
+                        {tab.content}
+                    </RouterTab>
+                ))}
+            </RouterTabs>
         </Root>
     );
+
+    // const match = useRouteMatch();
+    // const tabRouteMatch = useRouteMatch<{ tab: string }>(`${match.path}/:tab`);
+    // const selected = tabRouteMatch?.params.tab;
+    // if (children.length < 1) {
+    //     return null;
+    // }
+    // const [firstTab, ...otherTabs] = children;
+    // const selectedTab = children.find((tab) => tab.key === selected) ? selected : firstTab.key; //fall back to first, as <Switch> does
+
+    // return (
+    //     <Root key="tabs">
+    //         <Tabs value={selectedTab} variant="scrollable" scrollButtons="auto">
+    //             {children.map((tab, index) => {
+    //                 const url = index === 0 ? match.url : `${match.url}/${tab.key}`;
+    //                 return <Tab key={tab.key} value={tab.key} label={tab.label} component={Link} to={url} />;
+    //             })}
+    //         </Tabs>
+    //         <TabContent selectedTab={selectedTab}>
+    //             <Switch>
+    //                 {otherTabs.map((tab) => (
+    //                     <RouteWithErrorBoundary key={tab.key} path={`${match.url}/${tab.key}`}>
+    //                         {tab.content}
+    //                     </RouteWithErrorBoundary>
+    //                 ))}
+
+    //                 {/* first tab is default route, last in <Switch> */}
+    //                 <RouteWithErrorBoundary path={match.url}>{firstTab.content}</RouteWithErrorBoundary>
+    //             </Switch>
+    //         </TabContent>
+    //     </Root>
+    // );
 }
 
 const Root = styled("div")`
