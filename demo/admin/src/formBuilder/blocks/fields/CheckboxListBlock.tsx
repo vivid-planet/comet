200dd32aef0bb11a2bafe8d5f30d5d66830280ec
Ricky Smith <jamesricky@me.com>
2025-01-10 13:17:01 +0100

    Add CheckboxListBlock

---
+++ demo/admin/src/formBuilder/blocks/fields/CheckboxListBlock.tsx
@@ -0,0 +1,49 @@
+import { BlockCategory, BlockInterface, createCompositeBlock, createCompositeBlockTextField } from "@comet/blocks-admin";
+import { FormattedMessage } from "react-intl";
+
+import { FieldInfoTextBlock } from "../common/FieldInfoTextBlock";
+import { CheckboxItemsBlock } from "./CheckboxItemsBlock";
+import { propsAndValidationGroup } from "./common";
+
+export const CheckboxListBlock: BlockInterface = createCompositeBlock(
+    {
+        name: "CheckboxList",
+        category: BlockCategory.Form,
+        displayName: <FormattedMessage id="blocks.checkboxList" defaultMessage="Checkbox List" />,
+        groups: {
+            display: {
+                title: <FormattedMessage id="blocks.checkboxList.display" defaultMessage="Display" />,
+                paper: true,
+                blocks: {
+                    label: {
+                        block: createCompositeBlockTextField({
+                            fieldProps: {
+                                label: <FormattedMessage id="blocks.checkboxList.label" defaultMessage="Label" />,
+                                fullWidth: true,
+                            },
+                        }),
+                        hiddenInSubroute: true,
+                    },
+                    infoText: {
+                        block: FieldInfoTextBlock,
+                        title: <FormattedMessage id="blocks.checkboxList.helperText" defaultMessage="Helper Text" />,
+                        hiddenInSubroute: true,
+                    },
+                },
+            },
+            propsAndValidation: propsAndValidationGroup,
+            items: {
+                title: <FormattedMessage id="blocks.checkboxList.items" defaultMessage="Checkbox Items" />,
+                blocks: {
+                    items: {
+                        block: CheckboxItemsBlock,
+                    },
+                },
+            },
+        },
+    },
+    (block) => {
+        block.previewContent = (state) => [{ type: "text", content: state.label }];
+        return block;
+    },
+);
