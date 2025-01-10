5583c9cff545bc6843a02a039a392bf90b080b3f
Thomas Dax <thomas.dax@vivid-planet.com>
2025-01-10 11:21:20 +0100

    Allow passing a function as child to `BlocksFinalForm` (#3054)

--- packages/admin/admin/src/index.ts
+++ packages/admin/admin/src/index.ts
@@ -210,6 +210,7 @@ export { MenuItemRouterLink, MenuItemRouterLinkProps } from "./mui/menu/ItemRout
 export { Menu, MenuProps } from "./mui/menu/Menu";
 export { MenuClassKey } from "./mui/menu/Menu.styles";
 export { MuiThemeProvider } from "./mui/ThemeProvider";
+export { renderFinalFormChildren } from "./renderFinalFormChildren";
 export { RouterBrowserRouter } from "./router/BrowserRouter";
 export { RouterConfirmationDialog, RouterConfirmationDialogClassKey, RouterConfirmationDialogProps } from "./router/ConfirmationDialog";
 export { RouterContext } from "./router/Context";
