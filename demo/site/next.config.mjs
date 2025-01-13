6b0a86b6f77f9542659a043f086675f67c0c0b37
Sebastian <sm@vivid-planet.com>
2025-01-13 10:34:19 +0100

    use import.meta.resolve instead of createRequire

--- demo/site/next.config.mjs
+++ demo/site/next.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 
-import { createRequire } from 'node:module';
-
 import nextBundleAnalyzer from "@next/bundle-analyzer";
 
-const require = createRequire(import.meta.url);
 import cometConfig from "./src/comet-config.json" with { type: "json" };
 
 const withBundleAnalyzer = nextBundleAnalyzer({
@@ -30,7 +27,7 @@ const nextConfig = {
     experimental: {
         optimizePackageImports: ["@comet/cms-site"],
     },
-    cacheHandler: process.env.REDIS_ENABLED === "true" ? require.resolve("./dist/cache-handler.js") : undefined,
+    cacheHandler: process.env.REDIS_ENABLED === "true" ? import.meta.resolve("./dist/cache-handler.js").replace("file://", "") : undefined,
     cacheMaxMemorySize: process.env.REDIS_ENABLED === "true" ? 0 : undefined, // disable default in-memory caching
 };
 
