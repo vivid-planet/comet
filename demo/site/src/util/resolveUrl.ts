c6f39d6e346fbdee2ca907c9df90abbb3da7abf6
Manuel Blum <mb@vivid-planet.com>
2025-01-13 14:27:16 +0100

    fix: scoping of breadcrumb links, add unified function that resolves urls for site

---
+++ demo/site/src/util/resolveUrl.ts
@@ -0,0 +1,44 @@
+type ResolveUrlOptions = {
+    /**
+     * If provided, the baseUrl will be prepended to the resulting path
+     *
+     * Sample: "http://localhost:3000"
+     * @default: "/"
+     */
+    baseUrl?: string;
+
+    /**
+     * path with leading /
+     *
+     * Sample: "/test/to/my/page"
+     */
+    path: string;
+
+    scope: {
+        language: string;
+    } | null;
+
+    /**
+     * If provided, the anchor will be appended to the resulting URL
+     *
+     * Sample: "my-anchor"
+     */
+    anchor?: string;
+};
+
+/**
+ * Resolves a path, scope and a baseUrl to a real URL which can be navigated to.
+ *
+ * @return {string} The resolved URL: /{scope.language}/test/to/my/page#my-anchor
+ */
+export const resolveUrl = ({ baseUrl = "/", path, scope, anchor }: ResolveUrlOptions) => {
+    let safeScope = "";
+    if (scope != null) {
+        safeScope = scope.language;
+    }
+    let anchorPostfix = "";
+    if (anchor) {
+        anchorPostfix = `#${anchor}`;
+    }
+    return `${baseUrl}${safeScope}${path}${anchorPostfix}`;
+};
