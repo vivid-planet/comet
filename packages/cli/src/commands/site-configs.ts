a90c5a0cf1cc3eb1087c5f1b1333a4e97cf7de55
Franz Unger <franz.unger@vivid-planet.com>
2025-01-14 09:22:02 +0100

    Allow only base64

--- packages/cli/src/commands/site-configs.ts
+++ packages/cli/src/commands/site-configs.ts
@@ -11,7 +11,7 @@ export const injectSiteConfigsCommand = new Command("inject-site-configs")
     .requiredOption("-i, --in-file <file>", "The filename of a template file to inject.")
     .requiredOption("-o, --out-file <file>", "Write the injected template to a file.")
     .option("-d --dotenv", "dotenv compatibility") // https://github.com/motdotla/dotenv/issues/521#issuecomment-999016064
-    .option("-b --base64", "use base64 encoding")
+    .option("--base64", "use base64 encoding")
     .option("-f, --site-config-file <file>", "Path to ts-file which provides a default export with (env: string) => SiteConfig[]")
     .action(async (options) => {
         const configFile = `${process.cwd()}/${options.siteConfigFile || "site-configs.ts"}`;
