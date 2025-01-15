9eb9d3f87ce19cda8178df69889cd861eb2d8318
Markus Frenslich <mfrenslich@vivid-planet.com>
2025-01-15 08:49:25 +0100

    feat: use Datagrid for display and select scopes instead of form checkboxes

---
+++ .changeset/healthy-fireants-behave.md
@@ -0,0 +1,5 @@
+---
+"@comet/cms-admin": minor
+---
+
+Exchange Content Scope FormField Checkbox List with Datagrid view and selection
--- packages/admin/cms-admin/src/userPermissions/user/permissions/ContentScopeGrid.tsx
+++ packages/admin/cms-admin/src/userPermissions/user/permissions/ContentScopeGrid.tsx
@@ -1,44 +1,19 @@
-import { gql, useApolloClient, useQuery } from "@apollo/client";
-import { CheckboxListField, FillSpace, FinalForm, Loading, SaveButton, ToolbarActions, ToolbarTitleItem } from "@comet/admin";
-import { Card, CardContent, Toolbar } from "@mui/material";
+import { gql, useQuery } from "@apollo/client";
+import { GridColDef, Loading, ToolbarActions, ToolbarFillSpace, ToolbarTitleItem } from "@comet/admin";
+import { Select } from "@comet/admin-icons";
+import { Button, Card, CardContent, Toolbar, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import isEqual from "lodash.isequal";
-import { FormattedMessage } from "react-intl";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+import { FormattedMessage, IntlShape, useIntl } from "react-intl";
 
 import { camelCaseToHumanReadable } from "../../utils/camelCaseToHumanReadable";
-import {
-    GQLContentScopesQuery,
-    GQLContentScopesQueryVariables,
-    GQLUpdateContentScopesMutation,
-    GQLUpdateContentScopesMutationVariables,
-} from "./ContentScopeGrid.generated";
-
-type FormValues = {
-    contentScopes: string[];
-};
-type ContentScope = {
-    [key: string]: string;
-};
+import { GQLContentScopesQuery, GQLContentScopesQueryVariables } from "./ContentScopeGrid.generated";
+import { SelectScopesDialog } from "./selectScopesDialog/SelectScopesDialog";
 
 export const ContentScopeGrid = ({ userId }: { userId: string }) => {
-    const client = useApolloClient();
-
-    const submit = async (data: FormValues) => {
-        await client.mutate<GQLUpdateContentScopesMutation, GQLUpdateContentScopesMutationVariables>({
-            mutation: gql`
-                mutation UpdateContentScopes($userId: String!, $input: UserContentScopesInput!) {
-                    userPermissionsUpdateContentScopes(userId: $userId, input: $input)
-                }
-            `,
-            variables: {
-                userId,
-                input: {
-                    contentScopes: data.contentScopes.map((contentScope) => JSON.parse(contentScope)),
-                },
-            },
-            refetchQueries: ["ContentScopes"],
-        });
-    };
+    const intl = useIntl();
+    const [open, setOpen] = useState(false);
 
     const { data, error } = useQuery<GQLContentScopesQuery, GQLContentScopesQueryVariables>(
         gql`
@@ -53,49 +28,42 @@ export const ContentScopeGrid = ({ userId }: { userId: string }) => {
         },
     );
 
+    const columns: GridColDef<{ [key in string]: string }>[] = generateGridColumnsFromContentScopeProperties(data?.userContentScopes || [], intl);
+
     if (error) throw new Error(error.message);
 
     if (!data) {
         return <Loading />;
     }
     return (
-        <Card>
-            <FinalForm<FormValues>
-                mode="edit"
-                onSubmit={submit}
-                onAfterSubmit={() => null}
-                initialValues={{ contentScopes: data.userContentScopes.map((cs) => JSON.stringify(cs)) }}
-            >
+        <>
+            <Card>
                 <CardToolbar>
                     <ToolbarTitleItem>
                         <FormattedMessage id="comet.userPermissions.scopes" defaultMessage="Scopes" />
                     </ToolbarTitleItem>
-                    <FillSpace />
+                    <ToolbarFillSpace />
                     <ToolbarActions>
-                        <SaveButton type="submit">
-                            <FormattedMessage id="comet.userPermissions.save" defaultMessage="Save" />
-                        </SaveButton>
+                        <Button startIcon={<Select />} onClick={() => setOpen(true)} variant="contained" color="primary">
+                            <FormattedMessage id="comet.userPermissions.selectScopes" defaultMessage="Select scopes" />
+                        </Button>
                     </ToolbarActions>
                 </CardToolbar>
                 <CardContent>
-                    <CheckboxListField
-                        fullWidth
-                        name="contentScopes"
-                        layout="column"
-                        options={data.availableContentScopes.map((contentScope: ContentScope) => ({
-                            label: Object.entries(contentScope).map(([scope, value]) => (
-                                <>
-                                    {camelCaseToHumanReadable(scope)}: {camelCaseToHumanReadable(value)}
-                                    <br />
-                                </>
-                            )),
-                            value: JSON.stringify(contentScope),
-                            disabled: data.userContentScopesSkipManual.some((cs: ContentScope) => isEqual(cs, contentScope)),
-                        }))}
+                    <DataGrid
+                        autoHeight={true}
+                        rows={data.userContentScopes ?? []}
+                        columns={columns}
+                        rowCount={data?.userContentScopes.length ?? 0}
+                        loading={false}
+                        getRowHeight={() => "auto"}
+                        getRowId={(row) => JSON.stringify(row)}
+                        sx={{ "&.MuiDataGrid-root .MuiDataGrid-cell": { py: "8px" } }}
                     />
                 </CardContent>
-            </FinalForm>
-        </Card>
+            </Card>
+            <SelectScopesDialog open={open} onClose={() => setOpen(false)} data={data} userId={userId} />
+        </>
     );
 };
 
@@ -103,3 +71,29 @@ const CardToolbar = styled(Toolbar)`
     top: 0px;
     border-bottom: 1px solid ${({ theme }) => theme.palette.grey[100]};
 `;
+
+export function generateGridColumnsFromContentScopeProperties(
+    data: GQLContentScopesQuery["userContentScopes"] | GQLContentScopesQuery["availableContentScopes"],
+    intl: IntlShape,
+): GridColDef[] {
+    const uniquePropertyNames = Array.from(new Set(data.flatMap((item) => Object.keys(item))));
+    return uniquePropertyNames.map((propertyName) => {
+        return {
+            field: propertyName,
+            flex: 1,
+            pinnable: false,
+            sortable: false,
+            filterable: false,
+            headerName: camelCaseToHumanReadable(
+                intl.formatMessage({ id: `comet.userPermissions.contentScope.${propertyName}`, defaultMessage: propertyName }),
+            ),
+            renderCell: ({ row }) => {
+                if (row[propertyName] != null) {
+                    return <Typography variant="subtitle2">{camelCaseToHumanReadable(row[propertyName])}</Typography>;
+                } else {
+                    return "-";
+                }
+            },
+        };
+    });
+}
