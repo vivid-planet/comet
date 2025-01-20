47be4ebd3168c2627d67a7963502bab0de6af5f5
juliawegmayr <109900447+juliawegmayr@users.noreply.github.com>
2025-01-20 10:49:47 +0100

    MUI Dialog mobile styling adaptions (#3144)
    Co-authored-by: Ricky James Smith <jamesricky@me.com>
    Co-authored-by: Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>

--- packages/admin/admin-theme/src/componentsTheme/MuiDialogActions.ts
+++ packages/admin/admin-theme/src/componentsTheme/MuiDialogActions.ts
@@ -1,13 +1,17 @@
 import { mergeOverrideStyles } from "../utils/mergeOverrideStyles";
 import { GetMuiComponentTheme } from "./getComponentsTheme";
 
-export const getMuiDialogActions: GetMuiComponentTheme<"MuiDialogActions"> = (component, { palette }) => ({
+export const getMuiDialogActions: GetMuiComponentTheme<"MuiDialogActions"> = (component, { palette, breakpoints, spacing }) => ({
     ...component,
     styleOverrides: mergeOverrideStyles<"MuiDialogActions">(component?.styleOverrides, {
         root: {
             borderTop: `1px solid ${palette.grey[100]}`,
-            padding: 20,
+            padding: spacing(2),
             justifyContent: "space-between",
+
+            [breakpoints.up("sm")]: {
+                padding: spacing(4),
+            },
 
             "&>:first-of-type:last-child": {
                 marginLeft: "auto",
