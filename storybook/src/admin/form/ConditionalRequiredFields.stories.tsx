639bb6f42ff4f6305997a89c127e867090c2ecc4
Markus Fichtner <markus.fichtner@vivid-planet.com>
2025-01-21 16:51:39 +0100

    Rename ConditionalRequiredFields story file and update to use default export format

--- storybook/src/admin/form/ConditionalRequiredFields.stories.tsx
+++ storybook/src/admin/form/ConditionalRequiredFields.stories.tsx
@@ -1,8 +1,11 @@
 import { CheckboxField, FinalForm, TextField } from "@comet/admin";
-import { storiesOf } from "@storybook/react";
 import React from "react";
 
-storiesOf("@comet/admin/form", module).add("Conditional required fields", function () {
+export default {
+    title: "@comet/admin/form",
+};
+
+export const ConditionalRequiredFields = () => {
     type FormValues = {
         description?: string;
         descriptionRequired?: boolean;
@@ -18,4 +21,4 @@ storiesOf("@comet/admin/form", module).add("Conditional required fields", functi
             )}
         </FinalForm>
     );
-});
+};
