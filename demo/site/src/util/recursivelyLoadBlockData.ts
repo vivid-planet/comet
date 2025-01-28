a8cd424b23953e778e8c51e9146a012d70f91576
Thomas Dax <thomas.dax@vivid-planet.com>
2025-01-28 09:56:02 +0100

    Demo: add site metadata generation (#3268)

--- demo/site/src/util/recursivelyLoadBlockData.ts
+++ demo/site/src/util/recursivelyLoadBlockData.ts
@@ -1,7 +1,6 @@
 import { BlockLoader, BlockLoaderDependencies, recursivelyLoadBlockData as cometRecursivelyLoadBlockData } from "@comet/cms-site";
-
-import { loader as newsDetailLoader } from "./news/blocks/NewsDetailBlock.loader";
-import { loader as newsListLoader } from "./news/blocks/NewsListBlock.loader";
+import { loader as newsDetailLoader } from "@src/news/blocks/NewsDetailBlock.loader";
+import { loader as newsListLoader } from "@src/news/blocks/NewsListBlock.loader";
 
 declare module "@comet/cms-site" {
     export interface BlockLoaderDependencies {
@@ -9,13 +8,13 @@ declare module "@comet/cms-site" {
     }
 }
 
-export const blockLoaders: Record<string, BlockLoader> = {
+const blockLoaders: Record<string, BlockLoader> = {
     NewsDetail: newsDetailLoader,
     NewsList: newsListLoader,
 };
 
 //small wrapper for @comet/cms-site recursivelyLoadBlockData that injects blockMeta from block-meta.json
 export async function recursivelyLoadBlockData(options: { blockType: string; blockData: unknown } & BlockLoaderDependencies) {
-    const blocksMeta = await import("../block-meta.json"); //dynamic import to avoid this json in client bundle
+    const blocksMeta = await import("../../block-meta.json"); //dynamic import to avoid this json in client bundle
     return cometRecursivelyLoadBlockData({ ...options, blocksMeta: blocksMeta.default, loaders: blockLoaders });
 }
