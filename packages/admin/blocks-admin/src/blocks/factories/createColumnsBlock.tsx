948e07bba25b59957bd819caf76ab99b4c3258e5
Philippe <philippe@bentegeac.com>
2025-01-30 12:35:06 +0100

    Add `override` parameter to all block factories in `@comet/blocks-admin` (#3255)
    Co-authored-by: Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>

--- packages/admin/blocks-admin/src/blocks/factories/createColumnsBlock.tsx
+++ packages/admin/blocks-admin/src/blocks/factories/createColumnsBlock.tsx
@@ -59,13 +59,12 @@ interface CreateColumnsBlockOptions<T extends BlockInterface> {
     layouts: ColumnsBlockLayout[];
 }
 
-export function createColumnsBlock<T extends BlockInterface>({
-    name,
-    displayName,
-    category = BlockCategory.Layout,
-    contentBlock,
-    layouts,
-}: CreateColumnsBlockOptions<T>): BlockInterface<ColumnsBlockFragment<T>, ColumnsBlockState<T>, ColumnsBlockFragment<T>> {
+export function createColumnsBlock<T extends BlockInterface>(
+    { name, displayName, category = BlockCategory.Layout, contentBlock, layouts }: CreateColumnsBlockOptions<T>,
+    override?: (
+        block: BlockInterface<ColumnsBlockFragment<T>, ColumnsBlockState<T>, ColumnsBlockFragment<T>>,
+    ) => BlockInterface<ColumnsBlockFragment<T>, ColumnsBlockState<T>, ColumnsBlockFragment<T>>,
+): BlockInterface<ColumnsBlockFragment<T>, ColumnsBlockState<T>, ColumnsBlockFragment<T>> {
     if (layouts.length === 0) {
         throw new Error(`Number of layouts must be greater than 0!`);
     }
@@ -466,6 +465,10 @@ export function createColumnsBlock<T extends BlockInterface>({
             return `${blockItem.key}/edit/${childPath}`;
         },
     };
+
+    if (override) {
+        return override(ColumnsBlock);
+    }
 
     return ColumnsBlock;
 }
