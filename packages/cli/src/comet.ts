264231ffb2ad039e15394b5ff06644904dc64fe0
Manuel Blum <mb@vivid-planet.com>
2025-01-31 08:33:12 +0100

    remove testing code

--- packages/cli/src/comet.ts
+++ packages/cli/src/comet.ts
@@ -1,16 +1,8 @@
 import { Command } from "commander";
 
-import { type BlockMetaField, generateBlockTypes } from "./commands/generate-block-types";
+import { generateBlockTypes } from "./commands/generate-block-types";
 import { injectSiteConfigsCommand } from "./commands/site-configs";
 
-const foo: BlockMetaField = {
-    name: "foo",
-    kind: "Boolean",
-    array: false,
-    nullable: false,
-};
-
-console.error("Foo", foo);
 const program = new Command();
 
 program.addCommand(generateBlockTypes);
