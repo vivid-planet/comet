3c2195ed25f966d0811b88646f468d3201edc88f
Fabian Hoffmann <fabian.hoffmann@vivid-planet.com>
2025-02-03 10:45:54 +0100

    Feat: CSV parsing

---
+++ demo/api/src/importer/pipes/parsers/csv-parser.pipe.ts
@@ -0,0 +1,56 @@
+import * as csv from "@fast-csv/parse";
+import { HeaderArray, ParserOptionsArgs } from "@fast-csv/parse";
+import { CsvColumnMetadata } from "@src/importer/decorators/csv-column.decorator";
+
+import { ImporterPipe } from "../importer-pipe.type";
+
+export type ParserOptions = ParserOptionsArgs;
+
+export class CSVParsePipe implements ImporterPipe {
+    private readonly parserOptions: ParserOptions;
+
+    constructor(parserOptions: ParserOptions, csvColumns: CsvColumnMetadata[]) {
+        this.parserOptions = this.getParserOption(parserOptions, csvColumns);
+    }
+
+    getPipe() {
+        return csv.parse(this.parserOptions);
+    }
+
+    private getParserOption(jobRunParserOptions: ParserOptions, csvColumns: CsvColumnMetadata[]) {
+        //check entity metadata for csv headers
+        const entityHasOnlyNumericCsvColumnNames = csvColumns.reduce((acc, column) => (typeof column.csvColumnName === "number" ? true : acc), false);
+        const entityHasOnlyStringCsvColumnNames = csvColumns.reduce((acc, column) => (typeof column.csvColumnName === "string" ? true : acc), false);
+        if (entityHasOnlyNumericCsvColumnNames && entityHasOnlyStringCsvColumnNames) {
+            throw new Error(`Error importing: CSV column names must be either all property names or all indices`);
+        }
+
+        if (jobRunParserOptions?.headers && entityHasOnlyNumericCsvColumnNames) {
+            throw new Error(`Error importing: Cannot use 'parserOptions.headers' when CSV column names are indices`);
+        }
+
+        return {
+            delimiter: ";",
+            quote: '"',
+            trim: true,
+            ...jobRunParserOptions,
+            headers:
+                jobRunParserOptions?.headers ?? entityHasOnlyNumericCsvColumnNames
+                    ? undefined
+                    : (headers: HeaderArray) =>
+                          headers.map((header) => {
+                              if (!header) {
+                                  return;
+                              }
+
+                              const csvColumn = csvColumns.find((column) => (column.csvColumnName as string).toLowerCase() === header.toLowerCase());
+
+                              if (!csvColumn) {
+                                  // There is no csvColumnName for this column. Will be ignored.
+                                  return header;
+                              }
+                              return csvColumn.key;
+                          }),
+        };
+    }
+}
