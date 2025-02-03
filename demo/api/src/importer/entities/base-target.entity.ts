aeb2a886950f5159d94ef54b908ef7f7988e6728
Fabian Hoffmann <fabian.hoffmann@vivid-planet.com>
2025-02-03 09:19:48 +0100

    Feat: Base target entity

---
+++ demo/api/src/importer/entities/base-target.entity.ts
@@ -0,0 +1,43 @@
+import { BaseEntity, Constructor, Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+import { v4 } from "uuid";
+
+export interface BaseImportInterface {
+    id: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date;
+}
+
+export interface ImporterEntityClass extends Constructor {
+    dateFormatString?: string;
+}
+
+@ObjectType({ isAbstract: true, description: "Base import entity" })
+@Entity({ abstract: true })
+export abstract class BaseTargetEntity extends BaseEntity<BaseTargetEntity, "id"> implements BaseImportInterface {
+    @Field(() => ID)
+    @PrimaryKey({ type: "uuid" })
+    id: string = v4();
+    @Field(() => Date, { nullable: true })
+    @Property({
+        columnType: "timestamp with time zone",
+        nullable: true,
+        onCreate: () => new Date(),
+    })
+    createdAt?: Date;
+    @Field(() => Date, { nullable: true })
+    @Property({
+        columnType: "timestamp with time zone",
+        nullable: true,
+        onUpdate: () => new Date(),
+    })
+    updatedAt?: Date;
+    @Field(() => Date, { nullable: true })
+    @Property({
+        columnType: "timestamp with time zone",
+        nullable: true,
+    })
+    deletedAt?: Date;
+    static dateFormatString?: string;
+}
