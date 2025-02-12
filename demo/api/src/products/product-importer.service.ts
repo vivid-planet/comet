6a180323c6b52882762a877c2f589c8add9e4bb2
Fabian Hoffmann <fabian.hoffmann@vivid-planet.com>
2025-02-12 14:37:50 +0100

    Refactor: Add comment

--- demo/api/src/products/product-importer.service.ts
+++ demo/api/src/products/product-importer.service.ts
@@ -9,6 +9,7 @@ export class ProductImporterService {
     constructor(private readonly em: EntityManager) {}
 
     async createImporter(filePath: string) {
+        // TODO: Depending on future development, this might be better solved with dependency injection
         const importer = new ProductImporter(this.em);
         await importer.init({ dataStream: new LocalFileDataStream(filePath) });
         return importer;
