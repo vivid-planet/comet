5fd1ebec81a924a5c199d1faaa23feea01bf3b45
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-02-12 12:36:57 +0100

    style MuiListItemIcon in MuiMenuItem

--- packages/admin/admin-theme/src/componentsTheme/getComponentsTheme.ts
+++ packages/admin/admin-theme/src/componentsTheme/getComponentsTheme.ts
@@ -35,8 +35,8 @@ import { getMuiInputBase } from "./MuiInputBase";
 import { getMuiLinearProgress } from "./MuiLinearProgress";
 import { getMuiLink } from "./MuiLink";
 import { getMuiListItem } from "./MuiListItem";
-import { getMuiListItemIcon } from "./MuiListItemIcon";
 import { getMuiMenu } from "./MuiMenu";
+import { getMuiListItemIcon } from "./MuiMenuItem";
 import { getMuiNativeSelect } from "./MuiNativeSelect";
 import { getMuiPaper } from "./MuiPaper";
 import { getMuiPopover } from "./MuiPopover";
@@ -100,7 +100,7 @@ export const getComponentsTheme = (components: Components, themeData: ThemeData)
     MuiLinearProgress: getMuiLinearProgress(components.MuiLinearProgress, themeData),
     MuiLink: getMuiLink(components.MuiLink, themeData),
     MuiListItem: getMuiListItem(components.MuiListItem, themeData),
-    MuiListItemIcon: getMuiListItemIcon(components.MuiListItemIcon, themeData),
+    MuiMenuItem: getMuiListItemIcon(components.MuiMenuItem, themeData),
     MuiMenu: getMuiMenu(components.MuiMenu, themeData),
     MuiNativeSelect: getMuiNativeSelect(components.MuiNativeSelect, themeData),
     MuiPaper: getMuiPaper(components.MuiPaper, themeData),
