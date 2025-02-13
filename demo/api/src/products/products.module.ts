4a8681d21593b7147889f011e5e66c8120466bc9
Fabian Hoffmann <fabian.hoffmann@vivid-planet.com>
2025-02-13 15:35:00 +0100

    Feat: Don't export ProductImporterService

--- demo/api/src/products/products.module.ts
+++ demo/api/src/products/products.module.ts
@@ -51,6 +51,6 @@ import { ProductImporterService } from "./product-importer.service";
         ProductImporterConsole,
         ProductImporterService,
     ],
-    exports: [ProductImporterService],
+    exports: [],
 })
 export class ProductsModule {}
