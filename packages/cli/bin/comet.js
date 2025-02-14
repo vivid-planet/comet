4fdef13695b08c38bb98b71da566c2ee40066434
Manuel Blum <mb@vivid-planet.com>
2025-02-14 11:19:32 +0100

    improve performance of cli commands by transpile only

--- packages/cli/bin/comet.js
+++ packages/cli/bin/comet.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 require("ts-node").register({
+    transpileOnly: true, // increases ramp up time of generator - see: https://github.com/TypeStrong/ts-node?tab=readme-ov-file#transpileonly
     require: ["tsconfig-paths/register"],
 });
 require("../lib/comet");
