ec50e14307c95ca55026bebaeacce5ab3300e60f
Sebastian <sm@vivid-planet.com>
2025-02-14 12:30:17 +0100

    adapt vendor.d.ts

--- packages/admin/cms-admin/src/vendors.d.ts
+++ packages/admin/cms-admin/src/vendors.d.ts
@@ -1,13 +1,11 @@
 /// <reference types="@comet/admin-theme" />
 
-// eslint-disable-next-line no-restricted-globals
-interface CustomInputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-    webkitdirectory?: string;
-    directory?: string;
-}
+import { type HTMLAttributes } from "react";
 
-declare namespace JSX {
-    interface IntrinsicElements {
-        input: CustomInputProps;
+declare module "react" {
+    interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+        // extends React's HTMLAttributes
+        directory?: string;
+        webkitdirectory?: string;
     }
 }
