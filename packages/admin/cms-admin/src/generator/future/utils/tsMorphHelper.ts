50966149cd6ca6131d4794393cb1189b961470cf
Niko Sams <ns@vivid-planet.com>
2025-02-14 10:28:35 +0100

    Add support for variable declaration with initializer
    this fixes generation of ProductsGrid with typeValues variable

--- packages/admin/cms-admin/src/generator/future/utils/tsMorphHelper.ts
+++ packages/admin/cms-admin/src/generator/future/utils/tsMorphHelper.ts
@@ -65,6 +65,7 @@ function astExpressionToJson(node: Expression) {
             for (const reference of referencedSymbol.getReferences()) {
                 const referenceNode = reference.getNode();
                 const importDeclaration = referenceNode.getFirstAncestorByKind(SyntaxKind.ImportDeclaration);
+                const variableDeclaration = referenceNode.getFirstAncestorByKind(SyntaxKind.VariableDeclaration);
                 if (importDeclaration) {
                     for (const namedImport of importDeclaration.getNamedImports()) {
                         if ((namedImport.getAliasNode() || namedImport.getNameNode())?.getText() == referenceNode.getText()) {
@@ -74,16 +75,25 @@ function astExpressionToJson(node: Expression) {
                             };
                         }
                     }
+                } else if (variableDeclaration) {
+                    if (variableDeclaration.getName() == node.getText()) {
+                        const variableInitializer = variableDeclaration.getInitializer();
+                        if (variableInitializer) {
+                            return astExpressionToJson(variableInitializer);
+                        } else {
+                            throw new Error(`Initializer is required for variableDeclaration '${variableDeclaration.getName()}'`);
+                        }
+                    }
                 }
             }
         }
-        throw new Error(`Unsupported identifier, only imports are supported`);
+        throw new Error(`Unsupported identifier '${node.getText()}', only imports and are variable declarations with initializer are supported`);
     } else {
         throw new Error(`Unsupported expression Kind ${node.getKindName()}`);
     }
 }
 
-export function exportedDeclarationToJson(node: ExportedDeclarations) {
+export function exportedDeclarationToJson(node: ExportedDeclarations): any {
     if (node.getKind() == SyntaxKind.VariableDeclaration) {
         const variableDeclaration = node as VariableDeclaration;
         const initializer = variableDeclaration.getInitializer();
