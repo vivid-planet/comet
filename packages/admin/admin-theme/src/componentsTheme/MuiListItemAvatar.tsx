3da5bdee3429ec8ec540dfd34b4119149a855b56
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-02-18 15:45:35 +0100

    remove defaultProps from MuiListItemAvatar

--- packages/admin/admin-theme/src/componentsTheme/MuiListItemAvatar.tsx
+++ packages/admin/admin-theme/src/componentsTheme/MuiListItemAvatar.tsx
@@ -3,10 +3,6 @@ import { GetMuiComponentTheme } from "./getComponentsTheme";
 
 export const getMuiListItemAvatar: GetMuiComponentTheme<"MuiListItemAvatar"> = (component, { spacing }) => ({
     ...component,
-    defaultProps: {
-        dense: true,
-        ...component?.defaultProps,
-    },
     styleOverrides: mergeOverrideStyles<"MuiListItemAvatar">(component?.styleOverrides, {
         root: {
             gap: spacing(2),
