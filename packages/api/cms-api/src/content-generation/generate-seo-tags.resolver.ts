bd9b3a416dda239cf5a73c408e66fbf1c5e63779
Thomas Dax <thomas.dax@vivid-planet.com>
2025-02-18 12:40:45 +0100

    Validate language with IsLocale to avoid prompt injection

--- packages/api/cms-api/src/content-generation/generate-seo-tags.resolver.ts
+++ packages/api/cms-api/src/content-generation/generate-seo-tags.resolver.ts
@@ -1,9 +1,21 @@
 import { Inject } from "@nestjs/common";
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, ArgsType, Field, Mutation, Resolver } from "@nestjs/graphql";
+import { IsLocale, IsString } from "class-validator";
 
 import { RequiredPermission } from "../user-permissions/decorators/required-permission.decorator";
 import { CONTENT_GENERATION_SERVICE } from "./content-generation.constants";
 import { ContentGenerationServiceInterface, SeoTags } from "./content-generation-service.interface";
+
+@ArgsType()
+export class GenerateSeoTagsArgs {
+    @IsString()
+    @Field(() => String)
+    content: string;
+
+    @IsLocale()
+    @Field(() => String)
+    language: string;
+}
 
 @Resolver()
 export class GenerateSeoTagsResolver {
@@ -11,10 +23,7 @@ export class GenerateSeoTagsResolver {
 
     @RequiredPermission(["pageTree"], { skipScopeCheck: true })
     @Mutation(() => SeoTags)
-    async generateSeoTags(
-        @Args("content", { type: () => String }) content: string,
-        @Args("language", { type: () => String }) language: string,
-    ): Promise<SeoTags> {
+    async generateSeoTags(@Args() { content, language }: GenerateSeoTagsArgs): Promise<SeoTags> {
         const seoTags = await this.contentGenerationService.generateSeoTags?.(content, { language });
         if (!seoTags) throw new Error("SEO tag generation failed or is not supported");
         return seoTags;
