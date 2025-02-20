463aacbe22259eba570b2037674fdaee5d508801
Johannes Obermair <johannes.obermair@vivid-planet.com>
2025-02-20 11:18:21 +0100

    Add docs

--- packages/admin/cms-admin/src/blocks/config/BlocksConfigContext.tsx
+++ packages/admin/cms-admin/src/blocks/config/BlocksConfigContext.tsx
@@ -3,6 +3,13 @@ import { createContext, type PropsWithChildren, useContext } from "react";
 import { type ContentScopeInterface } from "../../contentScope/Provider";
 
 export type BlocksConfig = {
+    /**
+     * Check if a block is allowed in the current scope.
+     *
+     * @param {string} name The name of the block.
+     * @param scope The current content scope.
+     * @returns {boolean} True if the block is allowed in the current scope.
+     */
     supportsBlock?: (name: string, scope: ContentScopeInterface) => boolean;
 };
 
