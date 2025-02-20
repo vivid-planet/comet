e452737ad71ca1c20499149fb55bbf02c0c12686
Raphael Blum <raphael.blum@vivid-planet.com>
2025-02-20 16:31:14 +0100

    fix react import

--- demo/admin/src/warnings/WarningsPage.tsx
+++ demo/admin/src/warnings/WarningsPage.tsx
@@ -1,10 +1,10 @@
 import { Stack } from "@comet/admin";
-import * as React from "react";
+import { type ReactElement } from "react";
 import { useIntl } from "react-intl";
 
 import { WarningsGrid } from "./WarningsGrid";
 
-export function WarningsPage(): React.ReactElement {
+export function WarningsPage(): ReactElement {
     const intl = useIntl();
     return (
         <Stack topLevelTitle={intl.formatMessage({ id: "warnings.warnings", defaultMessage: "Warnings" })}>
