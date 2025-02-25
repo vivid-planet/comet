ec1859ad97eb3716fccbd2c6c0dca70ea54579ca
Ricky Smith <jamesricky@me.com>
2025-02-25 13:45:15 +0100

    Adapt `SaveBoundarySaveButton` to use `FeedbackButton`

---
+++ .changeset/smart-chicken-joke.md
@@ -0,0 +1,7 @@
+---
+"@comet/admin": major
+---
+
+Adapt `SaveBoundarySaveButton` to look like the standard `FeedbackButton` and match the Comet DXP design
+
+The following props have been removed, as the component's state is handled internally and the icons are no longer used: `saving`, `hasErrors`, `hasConflict`, `savingItem`, `successItem`, `errorItem`, `conflictItem`, `saveIcon`, `savingIcon`, `successIcon`, `errorIcon`, `conflictIcon`.
--- packages/admin/admin/src/saveBoundary/SaveBoundarySaveButton.tsx
+++ packages/admin/admin/src/saveBoundary/SaveBoundarySaveButton.tsx
@@ -1,19 +1,29 @@
-import { SaveButton, type SaveButtonProps } from "../common/buttons/save/SaveButton";
+import { Save } from "@comet/admin-icons";
+import { FormattedMessage } from "react-intl";
+
+import { FeedbackButton, FeedbackButtonProps } from "../common/buttons/feedback/FeedbackButton";
+import { messages } from "../messages";
 import { useSavable, useSaveBoundaryApi } from "./SaveBoundary";
 
-export function SaveBoundarySaveButton(props: SaveButtonProps) {
+type Props = Omit<FeedbackButtonProps, "disabled" | "loading" | "hasErrors" | "onClick">;
+
+export function SaveBoundarySaveButton(props: Props) {
     const saveBoundaryState = useSavable();
     const saveBoundaryApi = useSaveBoundaryApi();
     if (!saveBoundaryState || !saveBoundaryApi) throw new Error("SaveBoundarySaveButton must be inside SaveBoundary");
+
     return (
-        <SaveButton
+        <FeedbackButton
+            startIcon={<Save />}
             disabled={!saveBoundaryState.hasChanges}
-            saving={saveBoundaryState.saving}
+            loading={saveBoundaryState.saving}
             hasErrors={saveBoundaryState.hasErrors}
             onClick={async () => {
-                return saveBoundaryApi.save();
+                await saveBoundaryApi.save();
             }}
             {...props}
-        />
+        >
+            <FormattedMessage {...messages.save} />
+        </FeedbackButton>
     );
 }
