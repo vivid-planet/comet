f768cb6a1f88b6c31d2a911c7031dabc28d12a17
Raphael Blum <raphael.blum@vivid-planet.com>
2025-02-25 13:25:58 +0100

    auto resolve block warnings

--- demo/api/src/warnings/warning-checker.command.ts
+++ demo/api/src/warnings/warning-checker.command.ts
@@ -33,6 +33,8 @@ export class WarningCheckerCommand extends CommandRunner {
 
     @CreateRequestContext()
     async run(): Promise<void> {
+        const startDate = new Date();
+
         // TODO: (in the next PRs) Check if data itself is valid in the database. (Maybe some data was put into database and is not correct or a migration was done wrong)
         for (const data of this.groupRootBlockDataByEntity()) {
             const { tableName, className, rootBlockData } = data;
@@ -64,7 +66,6 @@ export class WarningCheckerCommand extends CommandRunner {
                             const warnings = node.block.warnings();
 
                             if (warnings.length > 0) {
-                                // TODO: (in the next PRs) auto resolve warnings
                                 for (const warning of warnings) {
                                     const type = "Block";
                                     const staticNamespace = "4e099212-0341-4bc8-8f4a-1f31c7a639ae";
@@ -93,6 +94,10 @@ export class WarningCheckerCommand extends CommandRunner {
             } while (rootBlocks.length > 0);
         }
         await this.entityManager.flush();
+
+        // remove all Block-Warnings that are not present anymore
+        const bufferTime = startDate.setSeconds(startDate.getSeconds() - 1); // Create a buffer time by subtracting 1 second from the startDate to avoid deleting records inserted in the same second
+        await this.entityManager.nativeDelete(Warning, { type: "Block", updatedAt: { $lt: new Date(bufferTime) } });
     }
 
     // Group root block data by tableName and className to reduce database calls.
