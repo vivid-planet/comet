21a1898f9834b3ea9119e2c2dcc1afb5a22780d4
Fabian Hoffmann <fabian.hoffmann@vivid-planet.com>
2025-02-26 19:28:30 +0100

    Refactor: Remove not needed migration

--- demo/api/src/db/migrations/Migration20250220083741.ts
+++ demo/api/src/db/migrations/Migration20250220083741.ts
@@ -9,7 +9,5 @@ export class Migration20250220083741 extends Migration {
             "alter table \"Product\" add constraint \"Product_type_check\" check (\"type\" in ('Cap', 'Shirt', 'Tie', 'Pants', 'Jacket', 'Shoes', 'Socks', 'Mug', 'Pen', 'Calendar', 'Notebook', 'Bag', 'Watch', 'Sunglasses', 'Wallet'));",
         );
         this.addSql('alter table "Product" alter column "soldCount" type int using ("soldCount"::int);');
-
-        this.addSql('alter table "ProductVariant" add column "position" integer not null;');
     }
 }
