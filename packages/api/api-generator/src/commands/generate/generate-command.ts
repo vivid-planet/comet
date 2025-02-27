678bb0b236126aa01f7f08b9befff61a9a17759f
manuelblum <mb@vivid-planet.com>
2025-02-27 12:51:42 +0100

    Move API Generator into `@comet/api-generator` (#3445)
    Co-authored-by: Alexander Kaufmann <ak@vivid-planet.com>
    Co-authored-by: Johannes Obermair <johannes.obermair@vivid-planet.com>

---
+++ packages/api/api-generator/src/commands/generate/generate-command.ts
@@ -0,0 +1,39 @@
+import { type CrudGeneratorOptions, type CrudSingleGeneratorOptions } from "@comet/cms-api";
+import { CLIHelper } from "@mikro-orm/cli";
+import { LazyMetadataStorage } from "@nestjs/graphql/dist/schema-builder/storages/lazy-metadata.storage";
+import { Command } from "commander";
+
+import { generateCrud } from "./generateCrud/generate-crud";
+import { generateCrudSingle } from "./generateCrudSingle/generate-crud-single";
+import { writeGeneratedFiles } from "./utils/write-generated-files";
+
+export const generateCommand = new Command("generate").action(async (options) => {
+    const orm = await CLIHelper.getORM(undefined, undefined, { dbName: "generator" });
+
+    const entities = orm.em.getMetadata().getAll();
+    LazyMetadataStorage.load();
+
+    for (const name in entities) {
+        const entity = entities[name];
+        if (!entity.class) {
+            // Ignore e.g. relation entities that don't have a class
+            continue;
+        }
+        {
+            const generatorOptions = Reflect.getMetadata(`data:crudGeneratorOptions`, entity.class) as CrudGeneratorOptions | undefined;
+            if (generatorOptions) {
+                const files = await generateCrud(generatorOptions, entity);
+                await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
+            }
+        }
+        {
+            const generatorOptions = Reflect.getMetadata(`data:crudSingleGeneratorOptions`, entity.class) as CrudSingleGeneratorOptions | undefined;
+            if (generatorOptions) {
+                const files = await generateCrudSingle(generatorOptions, entity);
+                await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
+            }
+        }
+    }
+
+    await orm.close(true);
+});
