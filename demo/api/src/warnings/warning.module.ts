66c785946aec95423e57d92704efb7832ee21ea4
Raphael Blum <raphael.blum@vivid-planet.com>
2025-02-27 09:06:46 +0100

    Implement updating warnings on save

--- demo/api/src/warnings/warning.module.ts
+++ demo/api/src/warnings/warning.module.ts
@@ -3,10 +3,13 @@ import { Module } from "@nestjs/common";
 
 import { Warning } from "./entities/warning.entity";
 import { WarningResolver } from "./generated/warning.resolver";
+import { WarningService } from "./warning.service";
 import { WarningCheckerCommand } from "./warning-checker.command";
+import { WarningEventSubscriber } from "./warning-event-subscriber";
 
 @Module({
     imports: [MikroOrmModule.forFeature([Warning])],
-    providers: [WarningResolver, WarningCheckerCommand],
+    providers: [WarningResolver, WarningCheckerCommand, WarningService, WarningEventSubscriber],
+    exports: [WarningService],
 })
 export class WarningsModule {}
