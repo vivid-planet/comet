e7f45f55a711cc82c9680744152d0646cd65347f
Ricky Smith <jamesricky@me.com>
2025-02-27 13:47:07 +0100

    Use TeaserItemBlock to reproduce bugs with rendering PixelImageBlock

--- demo/site/src/documents/pages/blocks/TeaserBlock.tsx
+++ demo/site/src/documents/pages/blocks/TeaserBlock.tsx
@@ -1,7 +1,7 @@
 import { ListBlock, PropsWithData, withPreview } from "@comet/cms-site";
 import { TeaserBlockData } from "@src/blocks.generated";
 import { PageLayout } from "@src/layout/PageLayout";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 import { TeaserItemBlock } from "./TeaserItemBlock";
 
@@ -22,14 +22,4 @@ const PageLayoutContent = styled.div`
     grid-column: 3 / -3;
 `;
 
-const ItemWrapper = styled.div`
-    display: grid;
-    gap: ${({ theme }) => theme.spacing.D100};
-
-    ${({ theme }) =>
-        css`
-            ${theme.breakpoints.sm.mediaQuery} {
-                grid-template-columns: repeat(4, 1fr);
-            }
-        `}
-`;
+const ItemWrapper = styled.div``;
