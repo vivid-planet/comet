6c26270fab612ead46c1f28e10be5af8ad3dd5ff
Manuel Blum <mb@vivid-planet.com>
2025-02-27 11:32:23 +0100

    removed old generator artifact

--- packages/admin/admin-generator/src/index.ts
+++ packages/admin/admin-generator/src/index.ts
@@ -1,2 +1,1 @@
 export type { FormConfig, FormFieldConfig, GeneratorConfig, GridColumnConfig, GridConfig } from "./commands/generate/generate-command";
-export { CrudGeneratorConfig } from "./commands/generate/types";
