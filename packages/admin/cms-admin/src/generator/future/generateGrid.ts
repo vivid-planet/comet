444645df534c76ed22d5f5720204c9ca38a7cd36
Denise Buder <denise.buder@vivid-planet.com>
2025-02-27 08:28:40 +0100

    Generate handleRowOrderChange code in seperate function

--- packages/admin/cms-admin/src/generator/future/generateGrid.ts
+++ packages/admin/cms-admin/src/generator/future/generateGrid.ts
@@ -650,8 +650,6 @@ export function generateGrid(
         \${${instanceGqlTypePlural}Fragment}
     \`;
 
-
-
     ${
         allowRowReordering
             ? `const update${gqlType}PositionMutation = gql\`
@@ -732,19 +730,8 @@ export function generateGrid(
         ${hasScope ? `const { scope } = useContentScope();` : ""}
         ${gridNeedsTheme ? `const theme = useTheme();` : ""}
 
-        ${
-            allowRowReordering
-                ? `
-        const handleRowOrderChange = async ({ row: { id }, targetIndex }: GridRowOrderChangeParams) => {
-            await client.mutate<GQLUpdate${gqlType}PositionMutation, GQLUpdate${gqlType}PositionMutationVariables>({
-                mutation: update${gqlType}PositionMutation,
-                variables: { id, input: { position: targetIndex + 1 } },
-                awaitRefetchQueries: true,
-                refetchQueries: [${instanceGqlTypePlural}Query]
-            });
-        };`
-                : ""
-        }
+
+        ${generateHandleRowOrderChange(allowRowReordering, gqlType, gqlTypePlural)}
 
         const columns: GridColDef<GQL${fragmentName}Fragment>[] = [
             ${gridColumnFields
@@ -1051,4 +1038,18 @@ const generateGridExportApi = (excelExport: boolean | undefined, gqlTypePlural: 
             fileName: "${gqlTypePlural}",
         },
     });`;
+};
+
+const generateHandleRowOrderChange = (allowRowReordering: boolean, gqlType: string, gqlTypePlural: string) => {
+    if (!allowRowReordering) {
+        return "";
+    }
+    return `const handleRowOrderChange = async ({ row: { id }, targetIndex }: GridRowOrderChangeParams) => {
+        await client.mutate<GQLUpdate${gqlType}PositionMutation, GQLUpdate${gqlType}PositionMutationVariables>({
+            mutation: update${gqlType}PositionMutation,
+            variables: { id, input: { position: targetIndex + 1 } },
+            awaitRefetchQueries: true,
+            refetchQueries: [${gqlTypePlural}Query]
+        });
+    };`;
 };
