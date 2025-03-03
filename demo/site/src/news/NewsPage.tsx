dbead05118aa8a1b291d53cb80ef53237cb2dfa7
Franz Unger <franz.unger@vivid-planet.com>
2025-03-03 18:49:56 +0100

    Use basepath also for news link

--- demo/site/src/news/NewsPage.tsx
+++ demo/site/src/news/NewsPage.tsx
@@ -8,7 +8,7 @@ import styled from "styled-components";
 import { GQLNewsIndexPageQuery } from "./NewsPage.loader.generated";
 
 export function NewsPage({ initialData }: { initialData: GQLNewsIndexPageQuery["newsList"] }) {
-    const newsApiUrl = `${usePathname()}/api`;
+    const pathname = usePathname();
     const [newsList, setNewsList] = useState(initialData.nodes);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
@@ -18,7 +18,7 @@ export function NewsPage({ initialData }: { initialData: GQLNewsIndexPageQuery["
             <h1>News</h1>
             <CardList>
                 {newsList.map((news) => (
-                    <Card key={news.id} href={`news/${news.slug}`}>
+                    <Card key={news.id} href={`${pathname}/${news.slug}`}>
                         <DamImageBlock data={news.image} aspectRatio="4x3" />
                         <h2>{news.title}</h2>
                         {/* <p><FormattedDate value={news.createdAt} /></p> */}
@@ -34,7 +34,7 @@ export function NewsPage({ initialData }: { initialData: GQLNewsIndexPageQuery["
                             setError(null);
                             try {
                                 const response = await fetch(
-                                    `${newsApiUrl}?${new URLSearchParams({
+                                    `${pathname}/api?${new URLSearchParams({
                                         offset: `${newsList.length}`,
                                         limit: "2",
                                     })}`,
