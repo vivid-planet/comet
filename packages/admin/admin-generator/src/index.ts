e46f4fadac5e7084ccf43d124425cd75c86feec8
Manuel Blum <mb@vivid-planet.com>
2025-03-03 15:14:20 +0100

    export static select values type

--- packages/admin/admin-generator/src/index.ts
+++ packages/admin/admin-generator/src/index.ts
@@ -1,1 +1,9 @@
-export type { FormConfig, FormFieldConfig, GeneratorConfig, GridColumnConfig, GridConfig } from "./commands/generate/generate-command";
+export type {
+    FormConfig,
+    FormFieldConfig,
+    GeneratorConfig,
+    GridColumnConfig,
+    GridConfig,
+    StaticSelectValueConfig,
+    StaticSelectValuesConfig,
+} from "./commands/generate/generate-command";
