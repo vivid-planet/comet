543339cbd7b2f9f5e8ae406d5551c30413e1e167
Manuel Blum <mb@vivid-planet.com>
2025-03-05 09:28:23 +0100

    chore: add no-restricted-imports to eslint-config/nestjs and filter in project poc

--- packages/eslint-config/nestjs.js
+++ packages/eslint-config/nestjs.js
@@ -16,6 +16,25 @@ const config = [
             "no-console": "off",
             "no-duplicate-imports": "error",
             "sort-imports": "off",
+            "no-restricted-imports": [
+                "error",
+                {
+                    "paths": [
+                        {
+                            "name": "class-validator",
+                            "importNames": ["IsOptional"],
+                            "message": "Please use IsUndefinable or IsNullable from @comet/cms-api instead"
+                        },
+
+                        {
+                            "name": "@nestjs/common",
+                            "importNames": ["ValidationPipe"],
+                            "message": "This is just a random import to visualize the concept"
+                        }
+
+                    ]
+                }
+            ]
         },
     },
 ];
