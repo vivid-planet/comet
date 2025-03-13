da80608963bdb44b9371bc3ee547c69d6e17233e
Manuel Blum <mb@vivid-planet.com>
2025-03-13 08:33:22 +0100

    Revert "change implementation"
    This reverts commit fb9a7171bb6f4bbbbd855dc6aa6557c7593ba53b.

--- packages/api/api-generator/src/commands/generate/generate-command.ts
+++ packages/api/api-generator/src/commands/generate/generate-command.ts
@@ -8,39 +8,37 @@ import { generateCrudSingle } from "./generateCrudSingle/generate-crud-single";
 import { writeGeneratedFiles } from "./utils/write-generated-files";
 
 export const generateCommand = new Command("generate").action(async (options) => {
-    const orm = await CLIHelper.getORM(undefined, undefined, {
-        dbName: "generator",
-        discovery: {
-            warnWhenNoEntities: false,
-        },
-    });
+    try {
+        const orm = await CLIHelper.getORM(undefined, undefined, { dbName: "generator" });
 
-    const entities = orm.em.getMetadata().getAll();
-    LazyMetadataStorage.load();
+        const entities = orm.em.getMetadata().getAll();
+        LazyMetadataStorage.load();
 
-    if (Object.keys(entities).length === 0) {
-        console.warn(`No entities found for the generated command.`);
-    }
-    for (const name in entities) {
-        const entity = entities[name];
-        if (!entity.class) {
-            // Ignore e.g. relation entities that don't have a class
-            continue;
-        }
-        {
-            const generatorOptions = Reflect.getMetadata(`data:crudGeneratorOptions`, entity.class) as CrudGeneratorOptions | undefined;
-            if (generatorOptions) {
-                const files = await generateCrud(generatorOptions, entity);
-                await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
+        for (const name in entities) {
+            const entity = entities[name];
+            if (!entity.class) {
+                // Ignore e.g. relation entities that don't have a class
+                continue;
+            }
+            {
+                const generatorOptions = Reflect.getMetadata(`data:crudGeneratorOptions`, entity.class) as CrudGeneratorOptions | undefined;
+                if (generatorOptions) {
+                    const files = await generateCrud(generatorOptions, entity);
+                    await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
+                }
+            }
+            {
+                const generatorOptions = Reflect.getMetadata(`data:crudSingleGeneratorOptions`, entity.class) as
+                    | CrudSingleGeneratorOptions
+                    | undefined;
+                if (generatorOptions) {
+                    const files = await generateCrudSingle(generatorOptions, entity);
+                    await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
+                }
             }
         }
-        {
-            const generatorOptions = Reflect.getMetadata(`data:crudSingleGeneratorOptions`, entity.class) as CrudSingleGeneratorOptions | undefined;
-            if (generatorOptions) {
-                const files = await generateCrudSingle(generatorOptions, entity);
-                await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
-            }
-        }
+        await orm.close(true);
+    } catch (e) {
+        console.warn(e);
     }
-    await orm.close(true);
 });
