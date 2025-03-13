f183dd1bd38d5a56fdf6ffc0cbe3a55572677481
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-03-13 09:48:35 +0100

    remove unneccessary space from ImpersonisationButton

--- packages/admin/cms-admin/src/userPermissions/user/ImpersonationButtons.tsx
+++ packages/admin/cms-admin/src/userPermissions/user/ImpersonationButtons.tsx
@@ -9,12 +9,14 @@ export const StopImpersonationButton = (buttonProps: ButtonProps) => {
         Cookies.remove("comet-impersonate-user-id");
         location.href = "/";
     };
+
     return (
         <Button onClick={stopImpersonation} {...buttonProps}>
             {commonImpersonationMessages.stopImpersonation}
         </Button>
     );
 };
+
 export const StartImpersonationButton = ({ userId }: { userId: string }) => {
     const currentUser = useCurrentUser();
     const isAllowed = useUserPermissionCheck();
@@ -22,6 +24,7 @@ export const StartImpersonationButton = ({ userId }: { userId: string }) => {
         Cookies.set("comet-impersonate-user-id", userId);
         location.href = "/";
     };
+
     if (!isAllowed("impersonation")) return null;
 
     if (currentUser.id !== userId && !currentUser.impersonated) {
