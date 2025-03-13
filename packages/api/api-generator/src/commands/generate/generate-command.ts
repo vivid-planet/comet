fb9a7171bb6f4bbbbd855dc6aa6557c7593ba53b
Manuel Blum <mb@vivid-planet.com>
2025-03-13 07:08:52 +0100

    change implementation

--- packages/api/api-generator/src/commands/generate/generate-command.ts
+++ packages/api/api-generator/src/commands/generate/generate-command.ts
@@ -8,37 +8,39 @@ import { generateCrudSingle } from "./generateCrudSingle/generate-crud-single";
 import { writeGeneratedFiles } from "./utils/write-generated-files";
 
 export const generateCommand = new Command("generate").action(async (options) => {
-    try {
-        const orm = await CLIHelper.getORM(undefined, undefined, { dbName: "generator" });
+    const orm = await CLIHelper.getORM(undefined, undefined, {
+        dbName: "generator",
+        discovery: {
+            warnWhenNoEntities: false,
+        },
+    });
 
-        const entities = orm.em.getMetadata().getAll();
-        LazyMetadataStorage.load();
+    const entities = orm.em.getMetadata().getAll();
+    LazyMetadataStorage.load();
 
-        for (const name in entities) {
-            const entity = entities[name];
-            if (!entity.class) {
-                // Ignore e.g. relation entities that don't have a class
-                continue;
-            }
-            {
-                const generatorOptions = Reflect.getMetadata(`data:crudGeneratorOptions`, entity.class) as CrudGeneratorOptions | undefined;
-                if (generatorOptions) {
-                    const files = await generateCrud(generatorOptions, entity);
-                    await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
-                }
-            }
-            {
-                const generatorOptions = Reflect.getMetadata(`data:crudSingleGeneratorOptions`, entity.class) as
-                    | CrudSingleGeneratorOptions
-                    | undefined;
-                if (generatorOptions) {
-                    const files = await generateCrudSingle(generatorOptions, entity);
-                    await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
-                }
+    if (Object.keys(entities).length === 0) {
+        console.warn(`No entities found for the generated command.`);
+    }
+    for (const name in entities) {
+        const entity = entities[name];
+        if (!entity.class) {
+            // Ignore e.g. relation entities that don't have a class
+            continue;
+        }
+        {
+            const generatorOptions = Reflect.getMetadata(`data:crudGeneratorOptions`, entity.class) as CrudGeneratorOptions | undefined;
+            if (generatorOptions) {
+                const files = await generateCrud(generatorOptions, entity);
+                await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
             }
         }
-        await orm.close(true);
-    } catch (e) {
-        console.warn(e);
+        {
+            const generatorOptions = Reflect.getMetadata(`data:crudSingleGeneratorOptions`, entity.class) as CrudSingleGeneratorOptions | undefined;
+            if (generatorOptions) {
+                const files = await generateCrudSingle(generatorOptions, entity);
+                await writeGeneratedFiles(files, { targetDirectory: generatorOptions.targetDirectory });
+            }
+        }
     }
+    await orm.close(true);
 });
