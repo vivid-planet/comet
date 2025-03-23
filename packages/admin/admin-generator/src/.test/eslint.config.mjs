fcacfb575fb5602f6b33dd7bc205f75bc9c4bc97
Niko Sams <ns@vivid-planet.com>
2025-03-23 19:36:35 +0100

    Fix lint:eslint error:
    Parsing error: /home/runner/work/comet/comet/packages/admin/admin-generator/src/.test/eslint.config.mjs was not found by the project service. Consider either including it in the tsconfig.json or including it in allowDefaultProject

--- packages/admin/admin-generator/src/.test/eslint.config.mjs
+++ packages/admin/admin-generator/src/.test/eslint.config.mjs
@@ -1,8 +1,6 @@
 import eslintConfigReact from "@comet/eslint-config/react.js";
 
 /** @type {import('eslint')} */
-const config = [
-    ...eslintConfigReact,
-];
+const config = [...eslintConfigReact];
 
 export default config;
