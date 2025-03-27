cd238944379602ad37479a785af21092abb392fa
Denise Buder <denise.buder@vivid-planet.com>
2025-03-27 10:10:23 +0100

    Admin Generator: Add decimal config for number fields  (#3725)

--- demo/admin/src/products/future/ProductPriceForm.cometGen.ts
+++ demo/admin/src/products/future/ProductPriceForm.cometGen.ts
@@ -6,5 +6,5 @@ export default defineConfig<GQLProduct>({
     gqlType: "Product",
     mode: "edit",
     fragmentName: "ProductPriceFormDetails", // configurable as it must be unique across project
-    fields: [{ type: "number", name: "price", helperText: "Enter price in this format: 123,45", startAdornment: "€" }],
+    fields: [{ type: "number", name: "price", helperText: "Enter price in this format: 123.45", startAdornment: "€", decimals: 2 }],
 });
