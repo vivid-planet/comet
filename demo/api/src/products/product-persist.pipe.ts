f96753cf05929ddba257e256607f9d5ce2768088
Fabian Hoffmann <fabian.hoffmann@vivid-planet.com>
2025-04-01 14:16:23 +0200

    WIP

--- demo/api/src/products/product-persist.pipe.ts
+++ demo/api/src/products/product-persist.pipe.ts
@@ -1,9 +1,9 @@
-import { Connection, EntityManager, FilterQuery, IDatabaseDriver, Reference } from "@mikro-orm/core";
+import { DamImageBlock } from "@comet/cms-api";
+import { Connection, EntityManager, IDatabaseDriver, Reference } from "@mikro-orm/core";
 import { LoggerService } from "@nestjs/common";
 import { ImporterPipe, PipeData, PipeMetadata } from "@src/importer/pipes/importer-pipe.type";
 import { plainToInstance } from "class-transformer";
 import { Transform, TransformCallback } from "stream";
-import { v4 } from "uuid";
 
 import { Product } from "./entities/product.entity";
 import { ProductCategory } from "./entities/product-category.entity";
@@ -35,34 +35,57 @@ export class ProductPersist extends Transform {
             const categoryRef = category as Reference<ProductCategory>;
 
             const entityInstance = plainToInstance(Product, input);
-            const { colors, tagsWithStatus, variants, createdAt, ...productData } = entityInstance;
-            const updateQuery: FilterQuery<object> | undefined = { slug: data.slug };
-            const record: Product | null = updateQuery ? await this.em.findOne(Product, updateQuery, { fields: ["id"] }) : null;
+            const { colors, tagsWithStatus, variants, createdAt, image, ...productData } = entityInstance;
+            // const updateQuery: FilterQuery<object> | undefined = { slug: data.slug };
+            const productInput = {
+                ...productData,
+                image: DamImageBlock.blockInputFactory({
+                    activeType: "pixelImage",
+                    attachedBlocks: [
+                        {
+                            props: {},
+                            type: "pixelImage",
+                        },
+                    ],
+                }).transformToBlockData(),
+                category: categoryRef,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            };
+            console.log("DEBUG productInput: ", productInput);
+            // const product = await this.em.insert(Product, productInput);
+            const product = await this.em.upsert(Product, productInput, {
+                onConflictFields: ["slug"],
+                onConflictAction: "merge",
+                onConflictExcludeFields: ["id", "createdAt"],
+            });
+            console.log("DEBUG product: ", product);
+            // const record: Product | null = updateQuery ? await this.em.findOne(Product, updateQuery, { fields: ["id"] }) : null;
 
-            if (record) {
-                await this.logger.log(`Existing record for ${JSON.stringify(updateQuery)} with id ${record.id} found, updating...`);
-                // keeping the id from the input data (in the DataTransformer plainToInstance always creates a new Entity instance, which is populated with the new id)
-                data.id = record.id;
-                await this.em.nativeUpdate(
-                    Product.name,
-                    { id: record.id },
-                    { ...productData, category: categoryRef, id: data.id, updatedAt: new Date() },
-                );
-            } else {
-                // in case the id is not provided
-                data.id = data?.id || v4();
-                await this.logger.log(
-                    `${updateQuery ? `No record for query ${JSON.stringify(updateQuery)} found` : "No query provided"}, inserting with id ${
-                        data.id
-                    }...`,
-                );
-                await this.em.insert(Product, {
-                    ...productData,
-                    category: categoryRef,
-                    createdAt: new Date(),
-                });
-                await this.logger.log(`Inserted record with id ${data.id}`, false);
-            }
+            // if (record) {
+            //     await this.logger.log(`Existing record for ${JSON.stringify(updateQuery)} with id ${record.id} found, updating...`);
+            //     // keeping the id from the input data (in the DataTransformer plainToInstance always creates a new Entity instance, which is populated with the new id)
+            //     data.id = record.id;
+            //     await this.em.nativeUpdate(
+            //         Product.name,
+            //         { id: record.id },
+            //         { ...productData, category: categoryRef, id: data.id, updatedAt: new Date() },
+            //     );
+            // } else {
+            //     // in case the id is not provided
+            //     data.id = data?.id || v4();
+            //     await this.logger.log(
+            //         `${updateQuery ? `No record for query ${JSON.stringify(updateQuery)} found` : "No query provided"}, inserting with id ${
+            //             data.id
+            //         }...`,
+            //     );
+            //     await this.em.insert(Product, {
+            //         ...productData,
+            //         category: categoryRef,
+            //         createdAt: new Date(),
+            //     });
+            //     await this.logger.log(`Inserted record with id ${data.id}`, false);
+            // }
             this.persistedEntitiesAmount++;
             await this.logger.log(`Persisted entities amount: ${this.persistedEntitiesAmount}`);
             // clear entity manager to prevent memory leak
