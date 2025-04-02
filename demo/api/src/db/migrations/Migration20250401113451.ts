bfa6909ca9a7cde36dea06a729bcdcf2a741e53c
Fabian Hoffmann <fabian.hoffmann@vivid-planet.com>
2025-04-02 11:26:40 +0200

    Refactor: Combine migrations

--- demo/api/src/db/migrations/Migration20250401113451.ts
+++
@@ -1,7 +0,0 @@
-import { Migration } from "@mikro-orm/migrations";
-
-export class Migration20250401113451 extends Migration {
-    async up(): Promise<void> {
-        this.addSql('alter table "Product" add constraint "Product_slug_unique" unique ("slug");');
-    }
-}
