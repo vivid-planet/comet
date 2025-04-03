d210124794540f086a8749b230a12d8d15649fdb
Franz Unger <franz.unger@vivid-planet.com>
2025-04-03 13:40:15 +0200

    Keep changes minimal

--- packages/admin/cms-admin/src/userPermissions/UserPermissionsPage.tsx
+++ packages/admin/cms-admin/src/userPermissions/UserPermissionsPage.tsx
@@ -9,41 +9,43 @@ import { UserPermissionsUserPagePermissionsPanel } from "./user/permissions/Perm
 import { UserPermissionsUserPageToolbar } from "./user/UserPageToolbar";
 import { UserPermissionsUserGrid } from "./UserGrid";
 
-export const UserPermissionsPage = () => (
-    <Stack topLevelTitle={<FormattedMessage id="comet.userPermissions.title" defaultMessage="User Permissions" />}>
-        <StackSwitch>
-            <StackPage name="table">
-                <StackToolbar scopeIndicator={<ContentScopeIndicator global />} />
-                <MainContent fullHeight>
-                    <UserPermissionsUserGrid
-                        rowAction={(params) => (
-                            <IconButton color="primary" component={StackLink} pageName="edit" payload={params.row.id} subUrl="permissions">
-                                <Edit />
-                            </IconButton>
-                        )}
-                    />
-                </MainContent>
-            </StackPage>
-            <StackPage name="edit" title={<FormattedMessage id="comet.userPermissions.edit" defaultMessage="User" />}>
-                {(userId) => (
-                    <>
-                        <UserPermissionsUserPageToolbar userId={userId} />
-                        <MainContent>
-                            <RouterTabs>
-                                <RouterTab path="" label={<FormattedMessage id="comet.userPermissions.basicData" defaultMessage="Basic Data" />}>
-                                    <UserPermissionsUserPageBasicDataPanel userId={userId} />
-                                </RouterTab>
-                                <RouterTab
-                                    path="/permissions"
-                                    label={<FormattedMessage id="comet.userPermissions.permissions" defaultMessage="Permissions" />}
-                                >
-                                    <UserPermissionsUserPagePermissionsPanel userId={userId} />
-                                </RouterTab>
-                            </RouterTabs>
-                        </MainContent>
-                    </>
-                )}
-            </StackPage>
-        </StackSwitch>
-    </Stack>
-);
+export const UserPermissionsPage = () => {
+    return (
+        <Stack topLevelTitle={<FormattedMessage id="comet.userPermissions.title" defaultMessage="User Permissions" />}>
+            <StackSwitch>
+                <StackPage name="table">
+                    <StackToolbar scopeIndicator={<ContentScopeIndicator global />} />
+                    <MainContent fullHeight>
+                        <UserPermissionsUserGrid
+                            rowAction={(params) => (
+                                <IconButton color="primary" component={StackLink} pageName="edit" payload={params.row.id} subUrl="permissions">
+                                    <Edit />
+                                </IconButton>
+                            )}
+                        />
+                    </MainContent>
+                </StackPage>
+                <StackPage name="edit" title={<FormattedMessage id="comet.userPermissions.edit" defaultMessage="User" />}>
+                    {(userId) => (
+                        <>
+                            <UserPermissionsUserPageToolbar userId={userId} />
+                            <MainContent>
+                                <RouterTabs>
+                                    <RouterTab path="" label={<FormattedMessage id="comet.userPermissions.basicData" defaultMessage="Basic Data" />}>
+                                        <UserPermissionsUserPageBasicDataPanel userId={userId} />
+                                    </RouterTab>
+                                    <RouterTab
+                                        path="/permissions"
+                                        label={<FormattedMessage id="comet.userPermissions.permissions" defaultMessage="Permissions" />}
+                                    >
+                                        <UserPermissionsUserPagePermissionsPanel userId={userId} />
+                                    </RouterTab>
+                                </RouterTabs>
+                            </MainContent>
+                        </>
+                    )}
+                </StackPage>
+            </StackSwitch>
+        </Stack>
+    );
+};
