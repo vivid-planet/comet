943c744e70440c33fa4be35a0725616e027f9fb1
Ricky Smith <jamesricky@me.com>
2025-04-22 14:57:16 +0200

    Refactoring

--- docs/src/utils/generateComponentDocs.ts
+++ docs/src/utils/generateComponentDocs.ts
@@ -9,15 +9,15 @@ const main = async () => {
     console.log("Generating component docs...");
     const storybookPath = path.resolve(process.cwd(), "../storybook/src/future-docs");
     const storyFiles = findStoryFiles(storybookPath);
-    const titles = getTitlesFromStories(storyFiles);
-    generateComponentDocs(titles);
+    const storyTitles = getTitlesFromStories(storyFiles);
+    generateComponentDocs(storyTitles);
 };
 
-function generateComponentDocs(titles: string[]): void {
+const generateComponentDocs = (storyTitles: string[]) => {
     const docsDir = path.resolve(process.cwd(), "docs/6-future-admin-components");
 
-    titles.forEach((fullTitle) => {
-        const componentName = fullTitle.split("/")[1];
+    storyTitles.forEach((fullStoryTitle) => {
+        const componentName = fullStoryTitle.split("/")[1];
         if (!componentName) return;
 
         const fileName = `generated.${kebabCase(componentName)}.mdx`;
@@ -27,7 +27,7 @@ function generateComponentDocs(titles: string[]): void {
         writeFileSync(filePath, content);
         console.log(`- ${filePath}`);
     });
-}
+};
 
 const getDocsFileContent = (componentName: string) => {
     return `---
@@ -43,13 +43,13 @@ import { ComponentDoc } from "../../src/components/ComponentDoc";
 `;
 };
 
-function findStoryFiles(directoryPath: string): string[] {
+const findStoryFiles = (directoryPath: string): string[] => {
     return readdirSync(directoryPath)
         .filter((file) => file.endsWith(".stories.ts") || file.endsWith(".stories.tsx"))
         .map((file) => path.join(directoryPath, file));
-}
+};
 
-function getTitleFromConfigObject(configObject: ObjectLiteralExpression): string | undefined {
+const getTitleFromConfigObject = (configObject: ObjectLiteralExpression): string | undefined => {
     const titleProperty = configObject.getProperty("title");
 
     if (titleProperty && titleProperty.getKind() === SyntaxKind.PropertyAssignment) {
@@ -62,43 +62,39 @@ function getTitleFromConfigObject(configObject: ObjectLiteralExpression): string
             }
         }
     }
-}
+};
 
-function getTitlesFromStories(storyFiles: string[]): string[] {
+const getTitlesFromStories = (storyFiles: string[]): string[] => {
     const project = new Project();
     project.addSourceFilesAtPaths(storyFiles);
     const titles: Array<string | undefined> = [];
 
     for (const sourceFile of project.getSourceFiles()) {
-        try {
-            const defaultExport = sourceFile.getExportAssignments().find((exp) => !exp.isExportEquals());
+        const defaultExport = sourceFile.getExportAssignments().find((exportedVariable) => !exportedVariable.isExportEquals());
 
-            if (defaultExport) {
-                const defaultExportExpression = defaultExport.getExpression();
+        if (defaultExport) {
+            const defaultExportExpression = defaultExport.getExpression();
 
-                if (defaultExportExpression.getKind() === SyntaxKind.ObjectLiteralExpression) {
-                    const configObject = defaultExportExpression.asKindOrThrow(SyntaxKind.ObjectLiteralExpression);
-                    titles.push(getTitleFromConfigObject(configObject));
-                } else if (defaultExportExpression.getKind() === SyntaxKind.Identifier) {
-                    const configVariableName = defaultExportExpression.getText();
-                    const configDeclaration = sourceFile.getVariableDeclaration(configVariableName);
+            if (defaultExportExpression.getKind() === SyntaxKind.ObjectLiteralExpression) {
+                const configObject = defaultExportExpression.asKindOrThrow(SyntaxKind.ObjectLiteralExpression);
+                titles.push(getTitleFromConfigObject(configObject));
+            } else if (defaultExportExpression.getKind() === SyntaxKind.Identifier) {
+                const configVariableName = defaultExportExpression.getText();
+                const configDeclaration = sourceFile.getVariableDeclaration(configVariableName);
 
-                    if (configDeclaration) {
-                        const configInitializer = configDeclaration.getInitializer();
+                if (configDeclaration) {
+                    const configInitializer = configDeclaration.getInitializer();
 
-                        if (configInitializer && configInitializer.getKind() === SyntaxKind.ObjectLiteralExpression) {
-                            const configObject = configInitializer.asKindOrThrow(SyntaxKind.ObjectLiteralExpression);
-                            titles.push(getTitleFromConfigObject(configObject));
-                        }
+                    if (configInitializer && configInitializer.getKind() === SyntaxKind.ObjectLiteralExpression) {
+                        const configObject = configInitializer.asKindOrThrow(SyntaxKind.ObjectLiteralExpression);
+                        titles.push(getTitleFromConfigObject(configObject));
                     }
                 }
             }
-        } catch (error) {
-            console.error(`Error parsing file ${sourceFile.getFilePath()}: ${error}`);
         }
     }
 
     return titles.filter((title) => title !== undefined);
-}
+};
 
 main();
