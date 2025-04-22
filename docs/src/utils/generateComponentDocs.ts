6597e8be46d014004c4e9a4bb1dc864e29a39b6d
Ricky Smith <jamesricky@me.com>
2025-04-22 13:31:25 +0200

    Generate docs files from stories and show in iframe

--- docs/src/utils/generateComponentDocs.ts
+++ docs/src/utils/generateComponentDocs.ts
@@ -1,35 +1,55 @@
 /* eslint-disable no-console */
 
-import fs from "fs";
+import { kebabCase } from "change-case";
+import { readdirSync, writeFileSync } from "fs";
 import path from "path";
 import { type ObjectLiteralExpression, Project, SyntaxKind } from "ts-morph";
 
 const main = async () => {
     console.log("Generating component docs...");
-
     const storybookPath = path.resolve(process.cwd(), "../storybook/src/future-docs");
     const storyFiles = findStoryFiles(storybookPath);
-    const titles = extractTitlesFromStories(storyFiles);
+    const titles = getTitlesFromStories(storyFiles);
+    generateComponentDocs(titles);
+};
 
-    console.log("Found story titles starting with 'Future Docs/':");
-    titles.forEach((title) => console.log(`- ${title}`));
+function generateComponentDocs(titles: string[]): void {
+    const docsDir = path.resolve(process.cwd(), "docs/6-future-admin-components");
+
+    titles.forEach((fullTitle) => {
+        const componentName = fullTitle.split("/")[1];
+        if (!componentName) return;
+
+        const fileName = `generated.${kebabCase(componentName)}.mdx`;
+        const filePath = path.join(docsDir, fileName);
+
+        const content = getDocsFileContent(componentName);
+        writeFileSync(filePath, content);
+        console.log(`- ${filePath}`);
+    });
+}
+
+const getDocsFileContent = (componentName: string) => {
+    return `---
+title: ${componentName}
+slug: ${kebabCase(componentName)}
+---
+
+import { ComponentDoc } from "../../src/components/ComponentDoc";
+
+# ${componentName}
+
+<ComponentDoc title="${componentName}" />
+`;
 };
 
 function findStoryFiles(directoryPath: string): string[] {
-    try {
-        const files = fs
-            .readdirSync(directoryPath)
-            .filter((file) => file.endsWith(".stories.ts") || file.endsWith(".stories.tsx"))
-            .map((file) => path.join(directoryPath, file));
-
-        return files;
-    } catch (error) {
-        console.error(`Error reading directory: ${error}`);
-        return [];
-    }
+    return readdirSync(directoryPath)
+        .filter((file) => file.endsWith(".stories.ts") || file.endsWith(".stories.tsx"))
+        .map((file) => path.join(directoryPath, file));
 }
 
-function extractTitleFromConfigObject(configObject: ObjectLiteralExpression): string | undefined {
+function getTitleFromConfigObject(configObject: ObjectLiteralExpression): string | undefined {
     const titleProperty = configObject.getProperty("title");
 
     if (titleProperty && titleProperty.getKind() === SyntaxKind.PropertyAssignment) {
@@ -44,7 +64,7 @@ function extractTitleFromConfigObject(configObject: ObjectLiteralExpression): st
     }
 }
 
-function extractTitlesFromStories(storyFiles: string[]): string[] {
+function getTitlesFromStories(storyFiles: string[]): string[] {
     const project = new Project();
     project.addSourceFilesAtPaths(storyFiles);
     const titles: Array<string | undefined> = [];
@@ -58,7 +78,7 @@ function extractTitlesFromStories(storyFiles: string[]): string[] {
 
                 if (defaultExportExpression.getKind() === SyntaxKind.ObjectLiteralExpression) {
                     const configObject = defaultExportExpression.asKindOrThrow(SyntaxKind.ObjectLiteralExpression);
-                    titles.push(extractTitleFromConfigObject(configObject));
+                    titles.push(getTitleFromConfigObject(configObject));
                 } else if (defaultExportExpression.getKind() === SyntaxKind.Identifier) {
                     const configVariableName = defaultExportExpression.getText();
                     const configDeclaration = sourceFile.getVariableDeclaration(configVariableName);
@@ -68,7 +88,7 @@ function extractTitlesFromStories(storyFiles: string[]): string[] {
 
                         if (configInitializer && configInitializer.getKind() === SyntaxKind.ObjectLiteralExpression) {
                             const configObject = configInitializer.asKindOrThrow(SyntaxKind.ObjectLiteralExpression);
-                            titles.push(extractTitleFromConfigObject(configObject));
+                            titles.push(getTitleFromConfigObject(configObject));
                         }
                     }
                 }
