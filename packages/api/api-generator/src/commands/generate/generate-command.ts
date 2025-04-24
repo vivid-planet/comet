1b482cb043bb1dd63768a47892e94029287e0fce
Johannes Obermair <johannes.obermair@vivid-planet.com>
2025-04-24 14:54:34 +0200

    Re-add watch option

--- packages/api/api-generator/src/commands/generate/generate-command.ts
+++ packages/api/api-generator/src/commands/generate/generate-command.ts
@@ -8,12 +8,15 @@ type GenerateOptions = {
     file?: string;
 };
 
-export const generateCommand = new Command("generate").action(async (options: GenerateOptions) => {
-    if (options.watch) {
-        await generateFiles();
-        console.log("Watching for modified entities...");
-        await watchMode();
-    } else {
-        generateFiles(options.file);
-    }
-});
+export const generateCommand = new Command("generate")
+    .action(async (options: GenerateOptions) => {
+        if (options.watch) {
+            await generateFiles();
+            console.log("Watching for modified entities...");
+            await watchMode();
+        } else {
+            generateFiles(options.file);
+        }
+    })
+    .option("-f, --file <file>", "path to entity file")
+    .option("--watch", "Watch for changes");
