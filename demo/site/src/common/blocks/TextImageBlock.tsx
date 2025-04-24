ceac8d854acef0c7c8c16b7e3f5a4cfbc4fc5596
Stefanie Kaltenhauser <stefanie.kaltenhauser@vivid-planet.com>
2025-04-24 14:54:28 +0200

    Example: Prepare TextImageBlock to use createNextImageSizes helper

--- demo/site/src/common/blocks/TextImageBlock.tsx
+++ demo/site/src/common/blocks/TextImageBlock.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { PropsWithData, withPreview } from "@comet/cms-site";
 import { TextImageBlockData } from "@src/blocks.generated";
+import { PageLayout } from "@src/layout/PageLayout";
 import styled, { css } from "styled-components";
 
 import { DamImageBlock } from "./DamImageBlock";
@@ -9,14 +10,16 @@ import { RichTextBlock } from "./RichTextBlock";
 export const TextImageBlock = withPreview(
     ({ data: { text, image, imageAspectRatio, imagePosition } }: PropsWithData<TextImageBlockData>) => {
         return (
-            <Root $imagePosition={imagePosition}>
-                <ImageContainer>
-                    <DamImageBlock data={image} aspectRatio={imageAspectRatio} sizes="50vw" />
-                </ImageContainer>
-                <TextContainer>
-                    <RichTextBlock data={text} />
-                </TextContainer>
-            </Root>
+            <PageLayout>
+                <Root $imagePosition={imagePosition}>
+                    <ImageContainer $imageAspectRatio={imageAspectRatio.replace("x", "/")}>
+                        <DamImageBlock data={image} aspectRatio={imageAspectRatio} fill sizes="50vw" />
+                    </ImageContainer>
+                    <TextContainer>
+                        <RichTextBlock data={text} />
+                    </TextContainer>
+                </Root>
+            </PageLayout>
         );
     },
     { label: "Text/Image" },
@@ -24,8 +27,12 @@ export const TextImageBlock = withPreview(
 
 const Root = styled.div<{ $imagePosition: TextImageBlockData["imagePosition"] }>`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     gap: 20px;
+
+    ${({ theme }) => theme.breakpoints.md.mediaQuery} {
+        flex-direction: row;
+    }
 
     ${({ $imagePosition }) =>
         $imagePosition === "left" &&
@@ -34,8 +41,10 @@ const Root = styled.div<{ $imagePosition: TextImageBlockData["imagePosition"] }>
         `}
 `;
 
-const ImageContainer = styled.div`
+const ImageContainer = styled.div<{ $imageAspectRatio: TextImageBlockData["imageAspectRatio"] }>`
+    position: relative;
     flex: 1;
+    aspect-ratio: ${({ $imageAspectRatio }) => $imageAspectRatio};
 `;
 
 const TextContainer = styled.div`
