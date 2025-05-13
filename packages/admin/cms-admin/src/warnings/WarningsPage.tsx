28176c51c1d1b4317fd31af640717a1583399bac
Raphael Blum <raphael.blum@vivid-planet.com>
2025-05-13 13:17:58 +0200

    remove unnecessary warningMessage property

--- packages/admin/cms-admin/src/warnings/WarningsPage.tsx
+++ packages/admin/cms-admin/src/warnings/WarningsPage.tsx
@@ -1,20 +1,15 @@
 import { Stack, StackToolbar } from "@comet/admin";
-import { type ReactNode } from "react";
 import { useIntl } from "react-intl";
 
 import { ContentScopeIndicator } from "../contentScope/ContentScopeIndicator";
 import { WarningsGrid } from "./WarningsGrid";
 
-interface WarningsPageProps {
-    warningMessages?: Record<string, ReactNode>;
-}
-
-export function WarningsPage({ warningMessages }: WarningsPageProps) {
+export function WarningsPage() {
     const intl = useIntl();
     return (
         <Stack topLevelTitle={intl.formatMessage({ id: "warnings.warnings", defaultMessage: "Warnings" })}>
             <StackToolbar scopeIndicator={<ContentScopeIndicator global />} />
-            <WarningsGrid warningMessages={warningMessages} />
+            <WarningsGrid />
         </Stack>
     );
 }
