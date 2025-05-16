9f8f1bfdcb5938ecc689ac53a3a8f13c797c67e6
Thomas Dax <thomas.dax@vivid-planet.com>
2025-05-16 12:27:51 +0200

    Add possiblity to pass service to RootBlockEntity isVisible

--- packages/api/blocks-api/src/blocks/decorators/root-block-entity.ts
+++ packages/api/blocks-api/src/blocks/decorators/root-block-entity.ts
@@ -1,12 +1,22 @@
+import { Type } from "@nestjs/common";
+
 // Copied from @mikro-orm/core since we don't have a dependency on it in @comet/blocks-api.
 // TODO Remove type once we merge @comet/cms-api and @comet/blocks-api.
 // https://github.com/mikro-orm/mikro-orm/blob/master/packages/core/src/typings.ts#L237
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type AnyEntity<T = any> = Partial<T>;
 
+export type GetEntityVisibility<Entity extends AnyEntity = AnyEntity> = (item: Entity) => boolean | Promise<boolean>;
+export interface EntityVisibilityServiceInterface<Entity extends AnyEntity = AnyEntity> {
+    getEntityVisibility: GetEntityVisibility<Entity>;
+}
+
+export type EntityVisibilityGetter<Entity extends AnyEntity = AnyEntity> =
+    | GetEntityVisibility<Entity>
+    | Type<EntityVisibilityServiceInterface<Entity>>;
+
 export interface RootBlockEntityOptions<Entity extends AnyEntity = AnyEntity> {
-    isVisible?: (entity: Entity) => boolean;
+    isVisible?: EntityVisibilityGetter<Entity>;
 }
 export function RootBlockEntity<Entity extends AnyEntity = AnyEntity>(options: RootBlockEntityOptions<Entity> = {}): ClassDecorator {
     // eslint-disable-next-line @typescript-eslint/ban-types
