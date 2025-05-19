816b69ae4923d2633b698a4a7799f3d132c34ff5
Manuel Blum <mb@vivid-planet.com>
2025-05-19 12:37:41 +0200

    refactor: remove deprecation of FinalForm components in admin-color-picker package, and mention that Fields should be preffered

--- packages/admin/admin-color-picker/src/FinalFormColorPicker.tsx
+++ packages/admin/admin-color-picker/src/FinalFormColorPicker.tsx
@@ -4,6 +4,11 @@ import { ColorPicker, type ColorPickerProps } from "./ColorPicker";
 
 export type FinalFormColorPickerProps = ColorPickerProps & FieldRenderProps<string, HTMLInputElement | HTMLTextAreaElement>;
 
+/**
+ * Final Form-compatible ColorPicker component.
+ *
+ * @see {@link ColorField} – preferred for typical form use. Use this only if no Field wrapper is needed.
+ */
 export const FinalFormColorPicker = ({ meta, input, ...restProps }: FinalFormColorPickerProps) => {
     return <ColorPicker {...input} {...restProps} />;
 };
