20da28158e0240d7d9f835ce7295df533d17201e
Thomas Dax <thomas.dax@vivid-planet.com>
2025-05-20 12:33:14 +0200

    Copy site-nextjs to site-react and rename

---
+++ packages/site/site-react/CHANGELOG.md
@@ -0,0 +1,1 @@
+# @comet/site-react
---
+++ packages/site/site-react/src/index.ts
@@ -0,0 +1,53 @@
+export { recursivelyLoadBlockData } from "./blockLoader/blockLoader";
+export { DamFileDownloadLinkBlock } from "./blocks/DamFileDownloadLinkBlock";
+export { DamVideoBlock } from "./blocks/DamVideoBlock";
+export { EmailLinkBlock } from "./blocks/EmailLinkBlock";
+export { ExternalLinkBlock } from "./blocks/ExternalLinkBlock";
+export { BlocksBlock } from "./blocks/factories/BlocksBlock";
+export { ListBlock } from "./blocks/factories/ListBlock";
+export { OneOfBlock } from "./blocks/factories/OneOfBlock";
+export { OptionalBlock } from "./blocks/factories/OptionalBlock";
+export { SeoBlock } from "./blocks/factories/SeoBlock";
+export type { SupportedBlocks } from "./blocks/factories/types";
+export { hasRichTextBlockContent } from "./blocks/helpers/RichTextBlockHelper";
+export type { VideoPreviewImageProps } from "./blocks/helpers/VideoPreviewImage";
+export { VideoPreviewImage } from "./blocks/helpers/VideoPreviewImage";
+export { InternalLinkBlock } from "./blocks/InternalLinkBlock";
+export { PhoneLinkBlock } from "./blocks/PhoneLinkBlock";
+export { PixelImageBlock } from "./blocks/PixelImageBlock";
+export type { PropsWithData } from "./blocks/PropsWithData";
+export { SvgImageBlock } from "./blocks/SvgImageBlock";
+export { VimeoVideoBlock } from "./blocks/VimeoVideoBlock";
+export { YouTubeVideoBlock } from "./blocks/YouTubeVideoBlock";
+export { type CookieApi, CookieApiProvider, useCookieApi } from "./cookies/CookieApiContext";
+export { CookieSafe } from "./cookies/CookieSafe";
+export { useCookieBotCookieApi } from "./cookies/useCookieBotCookieApi";
+export { useLocalStorageCookieApi } from "./cookies/useLocalStorageCookieApi";
+export { useOneTrustCookieApi } from "./cookies/useOneTrustCookieApi";
+export { ErrorHandlerBoundary } from "./errorHandler/ErrorHandlerBoundary";
+export { ErrorHandlerProvider } from "./errorHandler/ErrorHandlerProvider";
+export {
+    type GraphQLFetch,
+    convertPreviewDataToHeaders,
+    createFetchWithDefaults,
+    createFetchWithPreviewHeaders,
+    createGraphQLFetch,
+    gql,
+} from "./graphQLFetch/graphQLFetch";
+export { IFrameBridgeProvider } from "./iframebridge/IFrameBridge";
+export { IFrameMessageType } from "./iframebridge/IFrameMessage";
+export { Preview } from "./iframebridge/Preview";
+export { useBlockPreviewFetch } from "./iframebridge/useBlockPreviewFetch";
+export { useIFrameBridge } from "./iframebridge/useIFrameBridge";
+export { type WithPreviewProps, isWithPreviewPropsData, withPreview } from "./iframebridge/withPreview";
+export type { ImageDimensions } from "./image/Image";
+export { calculateInheritAspectRatio, generateImageUrl, getMaxDimensionsFromArea, Image, parseAspectRatio } from "./image/Image";
+export { BlockPreviewProvider } from "./preview/BlockPreviewProvider";
+export { usePreview } from "./preview/usePreview";
+export { PreviewSkeleton } from "./previewskeleton/PreviewSkeleton";
+export { previewParams, sitePreviewRoute } from "./sitePreview/appRouter/sitePreviewRoute";
+export { sendSitePreviewIFrameMessage } from "./sitePreview/iframebridge/sendSitePreviewIFrameMessage";
+export { SitePreviewIFrameMessageType } from "./sitePreview/iframebridge/SitePreviewIFrameMessage";
+export { legacyPagesRouterSitePreviewApiHandler } from "./sitePreview/pagesRouter/legacyPagesRouterSitePreviewApiHandler";
+export { SitePreviewProvider } from "./sitePreview/SitePreviewProvider";
+export type { SitePreviewData, SitePreviewParams } from "./sitePreview/SitePreviewUtils";
