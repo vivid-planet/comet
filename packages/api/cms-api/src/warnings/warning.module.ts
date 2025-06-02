585b326955261ef758d5dba899ed5c50acfef420
Thomas Dax <thomas.dax@vivid-planet.com>
2025-06-02 10:16:02 +0200

    Include entity info in warning grid (#3869)

--- packages/api/cms-api/src/warnings/warning.module.ts
+++ packages/api/cms-api/src/warnings/warning.module.ts
@@ -1,6 +1,7 @@
 import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Module } from "@nestjs/common";
 
+import { EntityInfoModule } from "../common/entityInfo/entity-info.module";
 import { Warning } from "./entities/warning.entity";
 import { WarningResolver } from "./warning.resolver";
 import { WarningService } from "./warning.service";
@@ -8,7 +9,7 @@ import { WarningCheckerCommand } from "./warning-checker.command";
 import { WarningEventSubscriber } from "./warning-event-subscriber";
 
 @Module({
-    imports: [MikroOrmModule.forFeature([Warning])],
+    imports: [MikroOrmModule.forFeature([Warning]), EntityInfoModule],
     providers: [WarningResolver, WarningCheckerCommand, WarningService, WarningEventSubscriber],
     exports: [WarningService],
 })
