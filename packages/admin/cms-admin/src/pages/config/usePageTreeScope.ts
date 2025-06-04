11e8b77cf5374f7064ee9b55d1800be3dccd0172
Johannes Obermair <johannes.obermair@vivid-planet.com>
2025-06-04 16:28:49 +0200

    Add unit tests

--- packages/admin/cms-admin/src/pages/config/usePageTreeScope.ts
+++ packages/admin/cms-admin/src/pages/config/usePageTreeScope.ts
@@ -1,12 +1,13 @@
+import { CmsBlockContext } from "../../blocks/CmsBlockContextProvider";
 import { useCmsBlockContext } from "../../blocks/useCmsBlockContext";
 import { useContentScope } from "../../contentScope/Provider";
 
 export function usePageTreeScope(): Record<string, unknown> {
-    const { pageTreeScopeParts } = useCmsBlockContext();
+    const context = useCmsBlockContext() as CmsBlockContext | undefined;
     const { scope: completeScope } = useContentScope();
 
-    if (pageTreeScopeParts) {
-        const pageTreeScope = pageTreeScopeParts.reduce((pageTreeScope, dimension) => {
+    if (context?.pageTreeScopeParts) {
+        const pageTreeScope = context.pageTreeScopeParts.reduce((pageTreeScope, dimension) => {
             if (completeScope[dimension] !== undefined) {
                 pageTreeScope[dimension] = completeScope[dimension];
             }
