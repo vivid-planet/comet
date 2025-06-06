6c97fc6b6b536077c04f5b42bca9a4b5bb8090b8
Thomas Dax <thomas.dax@vivid-planet.com>
2025-06-06 10:03:06 +0200

    Fix lint errors

--- packages/api/cms-api/src/blob-storage/backends/s3/blob-storage-s3.config.ts
+++ packages/api/cms-api/src/blob-storage/backends/s3/blob-storage-s3.config.ts
@@ -1,6 +1,6 @@
-import { S3ClientConfig } from "@aws-sdk/client-s3";
+import { type S3ClientConfig } from "@aws-sdk/client-s3";
 
 export interface BlobStorageS3Config {
     driver: "s3";
-    s3: S3ClientConfig & { bucket: string; };
+    s3: S3ClientConfig & { bucket: string };
 }
