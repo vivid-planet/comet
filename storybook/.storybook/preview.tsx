103a7598ad25f9156ab9ee56e8cc828632cfedd0
Manuel Blum <mb@vivid-planet.com>
2025-06-10 14:02:19 +0200

    refactor: move layout (default/padded) into its own decorator and to global context

--- storybook/.storybook/preview.tsx
+++ storybook/.storybook/preview.tsx
@@ -1,14 +1,12 @@
 import "@fontsource-variable/roboto-flex/full.css";
 
-import { MainContent } from "@comet/admin";
-import { GlobalStyles } from "@mui/material";
 import type { Preview } from "@storybook/react-webpack5";
 import { type GlobalTypes } from "storybook/internal/csf";
 
 import { IntlDecorator, LocaleOption } from "./decorators/IntlProvider.decorator";
+import { LayoutDecorator, LayoutOptions } from "./decorators/Layout.decorator";
 import { ThemeOption, ThemeProviderDecorator } from "./decorators/ThemeProvider.decorator";
 import { worker } from "./mocks/browser";
-import { previewGlobalStyles } from "./preview.styles";
 
 export const globalTypes: GlobalTypes = {
     theme: {
@@ -38,32 +36,25 @@ export const globalTypes: GlobalTypes = {
             dynamicTitle: true,
         },
     },
+    layout: {
+        description: "Layout",
+        toolbar: {
+            title: "Layout",
+            icon: "switchalt",
+            items: [
+                { value: LayoutOptions.Padded, title: "Padded" },
+                { value: LayoutOptions.Default, title: "Default" },
+            ],
+            showName: true,
+            dynamicTitle: true,
+        },
+    },
 };
 
 const preview: Preview = {
     tags: ["autodocs"],
-    decorators: [
-        ThemeProviderDecorator,
-        IntlDecorator,
-        (Story, context) => {
-            return (
-                <>
-                    <GlobalStyles styles={previewGlobalStyles} />
-                    <>
-                        {context.parameters.layout === "padded" ? (
-                            <MainContent>
-                                <Story />
-                            </MainContent>
-                        ) : (
-                            <Story />
-                        )}
-                    </>
-                </>
-            );
-        },
-    ],
+    decorators: [ThemeProviderDecorator, IntlDecorator, LayoutDecorator],
     parameters: {
-        layout: "padded",
         options: {
             /**
              * Note: according to: https://storybook.js.org/docs/writing-stories/naming-components-and-hierarchy#sorting-stories
