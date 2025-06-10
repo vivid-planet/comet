126c3fe872c4d50ea771ebe0dc9add2c727903ca
Manuel Blum <mb@vivid-planet.com>
2025-06-10 08:44:04 +0200

    refactor: simplify SingleFileUploadProps and MultipleFileUploadProps

--- packages/admin/cms-admin/src/form/file/FileUploadField.tsx
+++ packages/admin/cms-admin/src/form/file/FileUploadField.tsx
@@ -3,27 +3,19 @@ import { Field, type FieldProps } from "@comet/admin";
 import { FinalFormFileUpload, type FinalFormFileUploadProps } from "./FinalFormFileUpload";
 import { type GQLFinalFormFileUploadDownloadableFragment, type GQLFinalFormFileUploadFragment } from "./FinalFormFileUpload.generated";
 
-type SingleFileUploadProps<Multiple extends boolean | undefined> = FieldProps<
-    GQLFinalFormFileUploadFragment | GQLFinalFormFileUploadDownloadableFragment,
-    HTMLInputElement
-> &
-    Partial<FinalFormFileUploadProps<Multiple>> & {
+type SingleFileUploadProps = FieldProps<GQLFinalFormFileUploadFragment | GQLFinalFormFileUploadDownloadableFragment, HTMLInputElement> &
+    Partial<FinalFormFileUploadProps<false>> & {
         multiple?: false;
         maxFiles?: 1;
     };
 
-type MultipleFileUploadProps<Multiple extends boolean | undefined> = FieldProps<
-    Array<GQLFinalFormFileUploadFragment | GQLFinalFormFileUploadDownloadableFragment>,
-    HTMLInputElement
-> &
-    Partial<FinalFormFileUploadProps<Multiple>> & {
+type MultipleFileUploadProps = FieldProps<Array<GQLFinalFormFileUploadFragment | GQLFinalFormFileUploadDownloadableFragment>, HTMLInputElement> &
+    Partial<FinalFormFileUploadProps<true>> & {
         multiple: true;
         maxFiles?: number;
     };
 
-export type FileUploadFieldProps<Multiple extends boolean | undefined> = Multiple extends true
-    ? MultipleFileUploadProps<Multiple>
-    : SingleFileUploadProps<Multiple>;
+export type FileUploadFieldProps<Multiple extends boolean | undefined> = Multiple extends true ? MultipleFileUploadProps : SingleFileUploadProps;
 
 export const FileUploadField = <Multiple extends boolean | undefined>({ name, ...restProps }: FileUploadFieldProps<Multiple>) => {
     return (
