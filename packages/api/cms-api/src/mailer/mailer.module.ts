9b1d54a7624d9fda4025d5e5ec1b0c940aad0600
Benjamin Hohenwarter <bh@vivid-planet.com>
2025-06-13 12:56:41 +0200

    MailerModule: add logging possibility

--- packages/api/cms-api/src/mailer/mailer.module.ts
+++ packages/api/cms-api/src/mailer/mailer.module.ts
@@ -1,6 +1,8 @@
+import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { type DynamicModule, Global, Module } from "@nestjs/common";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 
+import { MailerLog } from "./entities/mailer-log.entity";
 import { MAILER_MODULE_OPTIONS } from "./mailer.constants";
 import { MailerService } from "./mailer.service";
 
@@ -8,6 +10,7 @@ export type MailerModuleConfig = (SMTPTransport | SMTPTransport.Options) & {
     defaultSender: string;
     sendAllMailsTo?: string[];
     sendAllMailsBcc?: string[];
+    disableMailLog?: boolean;
 };
 
 @Global()
@@ -16,6 +19,7 @@ export class MailerModule {
     static register(config: MailerModuleConfig): DynamicModule {
         return {
             module: MailerModule,
+            imports: [MikroOrmModule.forFeature([MailerLog])],
             providers: [{ provide: MAILER_MODULE_OPTIONS, useValue: config }, MailerService],
             exports: [MailerService],
         };
