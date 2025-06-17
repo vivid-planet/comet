5ec136fbf961bd82a1d0adca810d675d4bd8d651
Manuel Blum <mb@vivid-planet.com>
2025-06-17 10:10:11 +0200

    refactor: remove omit error from FinalFormAsyncSelectProps

--- packages/admin/admin/src/form/FinalFormAsyncSelect.tsx
+++ packages/admin/admin/src/form/FinalFormAsyncSelect.tsx
@@ -3,7 +3,7 @@ import { type SelectProps } from "@mui/material";
 import { useAsyncOptionsProps } from "../hooks/useAsyncOptionsProps";
 import { FinalFormSelect, type FinalFormSelectProps } from "./FinalFormSelect";
 
-export interface FinalFormAsyncSelectProps<T> extends FinalFormSelectProps<T>, Omit<SelectProps, "input" | "error"> {
+export interface FinalFormAsyncSelectProps<T> extends FinalFormSelectProps<T>, Omit<SelectProps, "input"> {
     loadOptions: () => Promise<T[]>;
 }
 
