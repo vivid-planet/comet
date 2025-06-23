d6ef213555fff2c9c936999c7a855105f9630847
raphaelblum <44967610+raphaelblum@users.noreply.github.com>
2025-06-23 11:23:54 +0200

    Combine migrations and move into library (#3984)

--- demo/api/src/db/migrations/Migration20250311143752.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20250311143752 extends Migration {
-
-  override async up(): Promise<void> {
-    this.addSql(`alter table "Warning" add column "scope" jsonb null;`);
-  }
-
-}
