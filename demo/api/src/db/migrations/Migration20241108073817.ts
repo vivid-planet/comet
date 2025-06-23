15550c6cda9459457e051e2a54c644d22ae72f2a
Raphael Blum <raphael.blum@vivid-planet.com>
2025-06-23 10:56:25 +0200

    combine migrations and put into library

--- demo/api/src/db/migrations/Migration20241108073817.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20241108073817 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('create table "Warning" ("id" uuid not null, "createdAt" timestamptz(0) not null, "updatedAt" timestamptz(0) not null, "message" text not null, "severity" text check ("severity" in (\'high\', \'medium\', \'low\')) not null, "status" text check ("status" in (\'open\', \'resolved\', \'ignored\')) not null default \'open\', constraint "Warning_pkey" primary key ("id"));');
-  }
-
-}
--- demo/api/src/db/migrations/Migration20250218090446.ts
+++
@@ -1,8 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20250218090446 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('alter table "Warning" add column "sourceInfo" jsonb not null;');
-  }
-}
