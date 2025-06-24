e46e85ab0e949ca656981ee746cc4c4211e4f7b5
Thomas Dax <thomas.dax@vivid-planet.com>
2025-06-24 15:42:26 +0200

    Add UI for displaying the selected file to the FileField (#3988)

--- packages/admin/cms-admin/src/blocks/PixelImageBlock.tsx
+++ packages/admin/cms-admin/src/blocks/PixelImageBlock.tsx
@@ -1,28 +1,15 @@
-import { gql, useApolloClient } from "@apollo/client";
+import { gql } from "@apollo/client";
 import { Field } from "@comet/admin";
-import { Crop, Delete, MoreVertical, OpenNewTab } from "@comet/admin-icons";
-import {
-    AdminComponentButton,
-    AdminComponentPaper,
-    BlockCategory,
-    BlockInterface,
-    BlocksFinalForm,
-    createBlockSkeleton,
-    IPreviewContext,
-    SelectPreviewComponent,
-} from "@comet/blocks-admin";
+import { Crop } from "@comet/admin-icons";
+import { BlockCategory, BlockInterface, BlocksFinalForm, createBlockSkeleton, IPreviewContext, SelectPreviewComponent } from "@comet/blocks-admin";
 import { BlockDependency } from "@comet/blocks-admin/lib/blocks/types";
-import { ButtonBase, Divider, Grid, IconButton, ListItemIcon, Menu, MenuItem, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { deepClone } from "@mui/x-data-grid/utils/utils";
 import { useCallback, useState } from "react";
 import { FormattedMessage } from "react-intl";
 
 import { PixelImageBlockData, PixelImageBlockInput } from "../blocks.generated";
-import { useContentScope } from "../contentScope/Provider";
 import { useDamAcceptedMimeTypes } from "../dam/config/useDamAcceptedMimeTypes";
-import { useDependenciesConfig } from "../dependencies/DependenciesConfig";
-import { DamPathLazy } from "../form/file/DamPathLazy";
 import { FileField } from "../form/file/FileField";
 import { CmsBlockContext } from "./CmsBlockContextProvider";
 import { EditImageDialog } from "./image/EditImageDialog";
@@ -157,12 +144,8 @@ export const PixelImageBlock: BlockInterface<PixelImageBlockData, ImageBlockStat
 
     AdminComponent: ({ state, updateState }) => {
         const [open, setOpen] = useState(false);
-        const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
         const context = useCmsBlockContext();
         const { filteredAcceptedMimeTypes } = useDamAcceptedMimeTypes();
-        const contentScope = useContentScope();
-        const apolloClient = useApolloClient();
-        const dependencyMap = useDependenciesConfig();
 
         // useSyncImageAttributes({ state, updateState });
 
@@ -172,111 +155,55 @@ export const PixelImageBlock: BlockInterface<PixelImageBlockData, ImageBlockStat
 
         const handleCropClick = () => {
             setOpen(true);
-
-            handleMenuClose();
-        };
-
-        const handleMenuClose = () => {
-            setAnchorEl(null);
         };
 
         const previewUrl = createPreviewUrl(state, context.damConfig.apiUrl, { width: 320, height: 320 });
 
         return (
             <SelectPreviewComponent>
-                {state.damFile?.image ? (
-                    <>
-                        <AdminComponentPaper disablePadding>
-                            <ContentRoot component="div" onClick={() => setOpen(true)}>
-                                <Grid container alignItems="center" spacing={3}>
-                                    <Grid item>{previewUrl && <PreviewImage src={previewUrl} width="70" height="70" />}</Grid>
-                                    <Grid item xs>
-                                        <Typography variant="subtitle1">{state.damFile.name}</Typography>
-                                        <Typography variant="body1" color="textSecondary">
-                                            <DamPathLazy fileId={state.damFile.id} />
-                                        </Typography>
-                                    </Grid>
-                                    <Grid item>
-                                        <IconButton
-                                            onMouseDown={(event) => event.stopPropagation()}
-                                            onClick={(event) => {
-                                                event.stopPropagation();
-                                                setAnchorEl(event.currentTarget);
-                                            }}
-                                            size="large"
-                                        >
-                                            <MoreVertical />
-                                        </IconButton>
-                                    </Grid>
-                                </Grid>
-                            </ContentRoot>
-                            <Divider />
-                            <AdminComponentButton startIcon={<Delete />} onClick={() => updateState({ damFile: undefined, cropArea: undefined })}>
-                                <FormattedMessage id="comet.blocks.image.empty" defaultMessage="Empty" />
-                            </AdminComponentButton>
-                        </AdminComponentPaper>
-                        <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleMenuClose}>
-                            <MenuItem onClick={handleCropClick}>
-                                <ListItemIcon>
-                                    <Crop />
-                                </ListItemIcon>
-                                <FormattedMessage id="comet.blocks.image.cropImage" defaultMessage="Crop image" />
-                            </MenuItem>
-                            {dependencyMap["DamFile"] && state.damFile?.id && (
-                                <MenuItem
-                                    onClick={async () => {
-                                        // id is checked three lines above
-                                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                                        const path = await dependencyMap["DamFile"].resolvePath({ apolloClient, id: state.damFile!.id });
-                                        const url = contentScope.match.url + path;
-                                        window.open(url, "_blank");
-                                    }}
-                                >
-                                    <ListItemIcon>
-                                        <OpenNewTab />
-                                    </ListItemIcon>
-                                    <FormattedMessage id="comet.blocks.image.openInDam" defaultMessage="Open in DAM" />
-                                </MenuItem>
-                            )}
-                        </Menu>
-                        {open && (
-                            <EditImageDialog
-                                image={{
-                                    name: state.damFile.name,
-                                    url: state.damFile.fileUrl,
-                                    width: state.damFile.image.width,
-                                    height: state.damFile.image.height,
-                                    size: state.damFile.size,
-                                }}
-                                damFileId={state.damFile.id}
-                                initialValues={{
-                                    useInheritedDamSettings: state.cropArea === undefined,
-                                    cropArea: state.cropArea ?? state.damFile.image.cropArea,
-                                }}
-                                inheritedDamSettings={{ cropArea: state.damFile.image.cropArea }}
-                                onSubmit={(cropArea) => {
-                                    updateState((prevState) => ({ ...prevState, cropArea }));
-                                    setOpen(false);
-                                }}
-                                onClose={handleClose}
-                            />
-                        )}
-                    </>
-                ) : (
-                    <BlocksFinalForm<{ damFile?: ImageBlockState["damFile"] }>
-                        onSubmit={(newValues) => {
-                            updateState((prevState) => ({ ...prevState, damFile: newValues.damFile || undefined, cropArea: undefined })); // reset local crop area when image changes
+                <BlocksFinalForm<{ damFile?: ImageBlockState["damFile"] }>
+                    onSubmit={(newValues) => {
+                        updateState((prevState) => ({ ...prevState, damFile: newValues.damFile || undefined, cropArea: undefined })); // reset local crop area when image changes
+                    }}
+                    initialValues={{ damFile: state.damFile }}
+                >
+                    <Field
+                        name="damFile"
+                        component={FileField}
+                        fullWidth
+                        buttonText={<FormattedMessage id="comet.blocks.image.chooseImage" defaultMessage="Choose image" />}
+                        allowedMimetypes={filteredAcceptedMimeTypes.pixelImage}
+                        preview={<PreviewImage src={previewUrl} width="70" height="70" />}
+                        menuActions={[
+                            {
+                                label: <FormattedMessage id="comet.blocks.image.cropImage" defaultMessage="Crop image" />,
+                                icon: <Crop />,
+                                onClick: handleCropClick,
+                            },
+                        ]}
+                    />
+                </BlocksFinalForm>
+                {open && state.damFile?.image && (
+                    <EditImageDialog
+                        image={{
+                            name: state.damFile.name,
+                            url: state.damFile.fileUrl,
+                            width: state.damFile.image.width,
+                            height: state.damFile.image.height,
+                            size: state.damFile.size,
                         }}
-                        initialValues={{ damFile: state.damFile }}
-                    >
-                        <Field
-                            name="damFile"
-                            component={FileField}
-                            fullWidth
-                            buttonText={<FormattedMessage id="comet.blocks.image.chooseImage" defaultMessage="Choose image" />}
-                            allowedMimetypes={filteredAcceptedMimeTypes.pixelImage}
-                        />
-                    </BlocksFinalForm>
+                        damFileId={state.damFile.id}
+                        initialValues={{
+                            useInheritedDamSettings: state.cropArea === undefined,
+                            cropArea: state.cropArea ?? state.damFile.image.cropArea,
+                        }}
+                        inheritedDamSettings={{ cropArea: state.damFile.image.cropArea }}
+                        onSubmit={(cropArea) => {
+                            updateState((prevState) => ({ ...prevState, cropArea }));
+                            setOpen(false);
+                        }}
+                        onClose={handleClose}
+                    />
                 )}
             </SelectPreviewComponent>
         );
@@ -301,12 +228,6 @@ export const PixelImageBlock: BlockInterface<PixelImageBlockData, ImageBlockStat
         return contents;
     },
 };
-
-const ContentRoot = styled(ButtonBase)`
-    padding: ${({ theme }) => theme.spacing(3)};
-    width: 100%;
-    text-align: left;
-` as typeof ButtonBase;
 
 const PreviewImage = styled("img")`
     object-fit: cover;
--- packages/admin/cms-admin/src/dam/blocks/DamFileDownloadLinkBlock.tsx
+++ packages/admin/cms-admin/src/dam/blocks/DamFileDownloadLinkBlock.tsx
@@ -1,22 +1,12 @@
 import { gql } from "@apollo/client";
-import { Field, FinalFormSelect, messages } from "@comet/admin";
-import { Delete } from "@comet/admin-icons";
-import {
-    AdminComponentButton,
-    AdminComponentPaper,
-    BlockCategory,
-    BlockDependency,
-    BlockInterface,
-    BlocksFinalForm,
-    createBlockSkeleton,
-} from "@comet/blocks-admin";
-import { Box, Divider, MenuItem, Typography } from "@mui/material";
+import { Field, FinalFormSelect } from "@comet/admin";
+import { AdminComponentPaper, BlockCategory, BlockDependency, BlockInterface, BlocksFinalForm, createBlockSkeleton } from "@comet/blocks-admin";
+import { Divider, MenuItem } from "@mui/material";
 import { deepClone } from "@mui/x-data-grid/utils/utils";
 import { FormattedMessage } from "react-intl";
 
 import { DamFileDownloadLinkBlockData, DamFileDownloadLinkBlockInput } from "../../blocks.generated";
 import { CmsBlockContext } from "../../blocks/CmsBlockContextProvider";
-import { DamPathLazy } from "../../form/file/DamPathLazy";
 import { FileField } from "../../form/file/FileField";
 import { GQLDamFileDownloadLinkFileQuery, GQLDamFileDownloadLinkFileQueryVariables } from "./DamFileDownloadLinkBlock.generated";
 
@@ -123,22 +113,7 @@ export const DamFileDownloadLinkBlock: BlockInterface<DamFileDownloadLinkBlockDa
                     openFileType: state.openFileType ?? "Download",
                 }}
             >
-                {state.file === undefined ? (
-                    <Field name="file" component={FileField} fullWidth />
-                ) : (
-                    <AdminComponentPaper disablePadding>
-                        <Box padding={3}>
-                            <Typography variant="subtitle1">{state.file.name}</Typography>
-                            <Typography variant="body1" color="textSecondary">
-                                <DamPathLazy fileId={state.file.id} />
-                            </Typography>
-                        </Box>
-                        <Divider />
-                        <AdminComponentButton startIcon={<Delete />} onClick={() => updateState({ ...state, file: undefined })}>
-                            <FormattedMessage {...messages.empty} />
-                        </AdminComponentButton>
-                    </AdminComponentPaper>
-                )}
+                <Field name="file" component={FileField} fullWidth />
                 <Divider />
                 <AdminComponentPaper>
                     <Field
