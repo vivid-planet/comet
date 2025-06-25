0617d7d0c3af9be33fce4a7c976c26c71d677394
Johannes Obermair <johannes.obermair@vivid-planet.com>
2025-06-25 11:08:52 +0200

    Ignore UUID columns in `searchToMikroOrmQuery` when inferring fields from entity metadata

--- packages/api/cms-api/src/generator/utils/search-fields-from-metadata.ts
+++ packages/api/cms-api/src/generator/utils/search-fields-from-metadata.ts
@@ -8,7 +8,7 @@ export function getCrudSearchFieldsFromMetadata(metadata: EntityMetadata<any>) {
         .filter((prop) => prop.name != "status")
         .filter((prop) => hasFieldFeature(metadata.class, prop.name, "search") && !prop.name.startsWith("scope_"))
         .reduce((acc, prop) => {
-            if (prop.type === "string" || prop.type === "text") {
+            if ((prop.type === "string" || prop.type === "text") && !prop.columnTypes.includes("uuid")) {
                 acc.push(prop.name);
             } else if (prop.reference == "m:1") {
                 if (!prop.targetMeta) {
@@ -18,7 +18,7 @@ export function getCrudSearchFieldsFromMetadata(metadata: EntityMetadata<any>) {
                     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                     .filter((innerProp) => hasFieldFeature(prop.targetMeta!.class, innerProp.name, "search") && !innerProp.name.startsWith("scope_"))
                     .forEach((innerProp) => {
-                        if (innerProp.type === "string" || innerProp.type === "text") {
+                        if ((innerProp.type === "string" || innerProp.type === "text") && !innerProp.columnTypes.includes("uuid")) {
                             acc.push(`${prop.name}.${innerProp.name}`);
                         }
                     });
