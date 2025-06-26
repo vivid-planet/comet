9a932fc3f58aa3d37ba8f40b7f345d7e77ecd978
Thomas Dax <thomas.dax@vivid-planet.com>
2025-06-26 11:34:56 +0200

    Support dot separated filenames like MyBlock.loader.gql.ts in no-private-sibling-import rule

--- packages/eslint-plugin/src/rules/no-private-sibling-import.ts
+++ packages/eslint-plugin/src/rules/no-private-sibling-import.ts
@@ -18,7 +18,7 @@ export default {
         return {
             ImportDeclaration: function (node) {
                 const optionSiblingExtensions = context.options[0] ?? ["gql", "sc"];
-                const filePath = context.getPhysicalFilename ? context.getPhysicalFilename() : context.getFilename();
+                const filePath = context.getPhysicalFilename() ? context.getPhysicalFilename() : context.getFilename();
                 if (filePath == "<text>") return; // If the input is from stdin, this test can't fail
                 const isPrivateFileMatch = String(node.source.value).match(new RegExp(`^(.*)\\.(${optionSiblingExtensions.join("|")})$`));
                 if (isPrivateFileMatch) {
@@ -28,7 +28,10 @@ export default {
                             message: "Import private siblings always with relative imports",
                         });
                     } else {
-                        if (isPrivateFileMatch[1] != `./${path.basename(filePath).replace(/\.(.*)$/, "")}`) {
+                        const pattern = `\\.((${[...optionSiblingExtensions, "ts"].join("|")}).*)$`;
+                        const regex = new RegExp(pattern);
+
+                        if (isPrivateFileMatch[1] != `./${path.basename(filePath).replace(regex, "")}`) {
                             context.report({
                                 node,
                                 message: "Avoid private sibling import from other files",
