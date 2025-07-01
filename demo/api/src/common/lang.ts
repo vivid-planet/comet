ad2f8edb0f3215b2eec7caa08927480f1e613e7c
Benjamin Hohenwarter <bh@vivid-planet.com>
2025-07-01 19:17:54 +0200

    Add translation to demo-api

---
+++ demo/api/src/common/lang.ts
@@ -0,0 +1,19 @@
+import { type ResolvedIntlConfig } from "react-intl";
+
+// eslint-disable-next-line @comet/no-other-module-relative-import
+import project_messages_de from "../../lang-compiled/comet-demo-api/de.json";
+// eslint-disable-next-line @comet/no-other-module-relative-import
+import project_messages_en from "../../lang-compiled/comet-demo-api/en.json";
+
+export type Messages = ResolvedIntlConfig["messages"];
+
+const projectMessages = {
+    en: project_messages_en,
+    de: project_messages_de,
+};
+
+export const getMessages = (language: keyof typeof projectMessages): Messages => {
+    return {
+        ...projectMessages[language],
+    };
+};
