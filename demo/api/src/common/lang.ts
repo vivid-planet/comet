53d180f723f68653fb0c83bd562d3f3ba0224524
Benjamin Hohenwarter <bh@vivid-planet.com>
2025-07-03 11:20:14 +0200

    Remove unused export

--- demo/api/src/common/lang.ts
+++ demo/api/src/common/lang.ts
@@ -5,7 +5,7 @@ import project_messages_de from "../../lang-compiled/comet-demo-api/de.json";
 // eslint-disable-next-line @comet/no-other-module-relative-import
 import project_messages_en from "../../lang-compiled/comet-demo-api/en.json";
 
-export type Messages = ResolvedIntlConfig["messages"];
+type Messages = ResolvedIntlConfig["messages"];
 
 const projectMessages = {
     en: project_messages_en,
