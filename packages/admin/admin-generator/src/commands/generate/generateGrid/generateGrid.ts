c1a343665d4da2725fcd17733bcaacd6a053ffde
Manuel Blum <mb@vivid-planet.com>
2025-07-07 23:48:06 +0200

    refactor: generic naming from T to Row

--- packages/admin/admin-generator/src/commands/generate/generateGrid/generateGrid.ts
+++ packages/admin/admin-generator/src/commands/generate/generateGrid/generateGrid.ts
@@ -154,6 +154,7 @@ export function generateGrid<T extends { __typename?: string }>(
         targetDirectory,
         gqlIntrospection,
     }: { exportName: string; baseOutputFilename: string; targetDirectory: string; gqlIntrospection: IntrospectionQuery },
+
     config: GridConfig<T>,
 ): GeneratorReturn {
     const gqlType = config.gqlType;
