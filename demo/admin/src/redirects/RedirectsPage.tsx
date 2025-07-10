604a3635240526d441f4ea31068fb66727d467db
raphaelblum <44967610+raphaelblum@users.noreply.github.com>
2025-07-10 08:32:11 +0200

    Pass link block instead of custom targets for createRedirectsPage (#4055)

--- demo/admin/src/redirects/RedirectsPage.tsx
+++ demo/admin/src/redirects/RedirectsPage.tsx
@@ -1,9 +1,8 @@
 import { createRedirectsLinkBlock, createRedirectsPage } from "@comet/cms-admin";
 import { NewsLinkBlock } from "@src/news/blocks/NewsLinkBlock";
 
-const customTargets = {
+export const RedirectsLinkBlock = createRedirectsLinkBlock({
     news: NewsLinkBlock,
-};
-export const RedirectsLinkBlock = createRedirectsLinkBlock(customTargets);
-// TODO: instead of passing customTargets here, we should use the createRedirectsLinkBlock function to create a custom link block for the redirects. Needs a rework in the library before this is possible.
-export const RedirectsPage = createRedirectsPage({ customTargets, scopeParts: ["domain"] });
+});
+
+export const RedirectsPage = createRedirectsPage({ linkBlock: RedirectsLinkBlock, scopeParts: ["domain"] });
