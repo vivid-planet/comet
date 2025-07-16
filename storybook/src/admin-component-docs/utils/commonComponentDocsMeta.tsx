7b6cca5b0d77399bcb43f1c758fc1434abe59cd5
Ricky Smith <jamesricky@me.com>
2025-07-16 15:51:17 +0200

    Set more accurate title for stories

--- storybook/src/admin-component-docs/utils/commonComponentDocsMeta.tsx
+++ storybook/src/admin-component-docs/utils/commonComponentDocsMeta.tsx
@@ -43,7 +43,7 @@ export const commonComponentDocsMeta: Meta = {
                     <Description />
                     <Primary />
                     <Controls />
-                    <Stories includePrimary={false} />
+                    <Stories title="More usage examples" includePrimary={false} />
                 </>
             ),
         },
