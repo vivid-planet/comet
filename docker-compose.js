65744a5eedcf62a8d89fcb5248c53c0748d9e1b6
Thomas Dax <thomas.dax@vivid-planet.com>
2025-07-24 15:08:58 +0200

    Fix docker compose

--- docker-compose.js
+++ docker-compose.js
@@ -1,9 +1,9 @@
 const { spawn, exec } = require("child_process");
 
-spawn("docker-compose", ["up"], { stdio: "inherit" });
+spawn("docker", ["compose", "up"], { stdio: "inherit" });
 
 process.on("SIGINT", function () {
-    exec(`docker-compose down`, function (error) {
+    exec(`docker compose down`, function (error) {
         process.exit(error ? 1 : 0);
     });
 });
