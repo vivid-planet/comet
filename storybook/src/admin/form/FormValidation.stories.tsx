0b3fc63666428f8280b028b0df400d77b267e6e8
Manuel Blum <mb@vivid-planet.com>
2025-08-04 09:48:12 +0200

    add interaction test directly to FormValidation story

--- storybook/src/admin/form/FormValidation.stories.tsx
+++ storybook/src/admin/form/FormValidation.stories.tsx
@@ -1,5 +1,6 @@
 import { Alert, FinalForm, SaveBoundary, SaveBoundarySaveButton, TextField } from "@comet/admin";
 import type { Meta, StoryObj } from "@storybook/react-webpack5";
+import { expect, within } from "storybook/test";
 
 type Story = StoryObj;
 const config: Meta = {
@@ -50,8 +51,17 @@ export const FormValidation: Story = {
                                 />
                                 <TextField name="text3" label="Text" fullWidth required />
 
-                                <SaveBoundarySaveButton />
-                                <Alert title="FormState">
+                                <SaveBoundarySaveButton data-testid="saveButton" />
+                                <Alert
+                                    title="FormState"
+                                    slotProps={{
+                                        text: {
+                                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                                            // @ts-expect-error
+                                            component: "div",
+                                        },
+                                    }}
+                                >
                                     <pre>
                                         {JSON.stringify(
                                             { submitError, submitFailed, submitSucceeded, hasSubmitErrors, touched, values, errors },
@@ -66,5 +76,37 @@ export const FormValidation: Story = {
                 </FinalForm>
             </SaveBoundary>
         );
+    },
+    play: async ({ canvas, userEvent, step }) => {
+        const textFieldInputElement = canvas.getByTestId("text-input");
+        const saveButton = canvas.getByTestId("saveButton");
+
+        await step("Initial State", async () => {
+            expect(textFieldInputElement).toBeInTheDocument();
+            expect(saveButton).toBeInTheDocument();
+            expect(saveButton).toBeDisabled();
+        });
+
+        await step("Enter Data State", async () => {
+            await userEvent.type(textFieldInputElement, "Lorem ipsum");
+            expect(textFieldInputElement).toHaveValue("Lorem ipsum");
+            expect(saveButton).not.toBeDisabled();
+
+            await userEvent.click(saveButton);
+        });
+
+        await step("Required Fields should report error", async () => {
+            const textField2 = canvas.getByTestId("text2-field-container-helper-texts-wrapper");
+            expect(textField2).toBeInTheDocument();
+
+            const isRequiredTextField2 = within(textField2).getByText("Required");
+            expect(isRequiredTextField2).toBeInTheDocument();
+
+            const textField3 = canvas.getByTestId("text3-field-container-helper-texts-wrapper");
+            expect(textField3).toBeInTheDocument();
+
+            const isRequiredTextField3 = within(textField3).getByText("Required");
+            expect(isRequiredTextField3).toBeInTheDocument();
+        });
     },
 };
