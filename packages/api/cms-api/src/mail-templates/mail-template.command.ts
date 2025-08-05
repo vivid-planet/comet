ec86ba3a9741e0d406ae0dfcf68e43ec68fe33fc
Benjamin Hohenwarter <bh@vivid-planet.com>
2025-08-05 14:39:14 +0200

    MailTemplates Module: First draft

---
+++ packages/api/cms-api/src/mail-templates/mail-template.command.ts
@@ -0,0 +1,29 @@
+import { CreateRequestContext, MikroORM } from "@mikro-orm/core";
+import { Logger } from "@nestjs/common";
+import { Command, CommandRunner } from "nest-commander";
+
+import { MailTemplateInterface, MailTemplateService } from "./mail-template.service";
+
+@Command({
+    name: "mail-template:test",
+    description: "test mail template, e.g. ",
+    arguments: "[mailTemplateId] [preparedTestParamsIndex]",
+})
+export class MailTemplateCommand extends CommandRunner {
+    private readonly logger = new Logger(MailTemplateCommand.name);
+
+    constructor(
+        private readonly orm: MikroORM,
+        private readonly mailTemplateService: MailTemplateService,
+    ) {
+        super();
+    }
+
+    @CreateRequestContext()
+    async run([mailTemplateId, preparedTestParamsIndex]: Array<string>): Promise<void> {
+        const mailTemplate: MailTemplateInterface<object> = await this.mailTemplateService.getMailTemplate(mailTemplateId);
+        const preparedTestParams = (await mailTemplate.getPreparedTestParams())[parseInt(preparedTestParamsIndex)];
+        this.logger.log(`Sending test mail for ${mailTemplate.name} (prepared params: ${preparedTestParams.name})`);
+        await this.mailTemplateService.sendMail<object>(mailTemplate, preparedTestParams.params);
+    }
+}
