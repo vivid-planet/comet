3878cd725dd7bb5e1d3c4bedc17ae86a8a163fe9
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-08-06 11:12:22 +0200

    move tags from StandAloneCallToActionListBlock to LinkBlock

--- demo/admin/src/common/blocks/StandaloneCallToActionListBlock.tsx
+++ demo/admin/src/common/blocks/StandaloneCallToActionListBlock.tsx
@@ -1,7 +1,7 @@
 import { BlockCategory, createCompositeBlock, createCompositeBlockSelectField } from "@comet/cms-admin";
 import { type StandaloneCallToActionListBlockData } from "@src/blocks.generated";
 import { CallToActionListBlock } from "@src/common/blocks/CallToActionListBlock";
-import { defineMessage, FormattedMessage } from "react-intl";
+import { FormattedMessage } from "react-intl";
 
 export const StandaloneCallToActionListBlock = createCompositeBlock(
     {
@@ -24,10 +24,6 @@ export const StandaloneCallToActionListBlock = createCompositeBlock(
                 hiddenInSubroute: true,
             },
         },
-        tags: [
-            defineMessage({ id: "callToActionBlock.tag.link", defaultMessage: "Link" }),
-            defineMessage({ id: "callToActionBlock.tag.button", defaultMessage: "Button" }),
-        ],
     },
     (block) => {
         block.category = BlockCategory.Navigation;
