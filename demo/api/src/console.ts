feb462faf17a1e748ea27fb032a5ff0363812b21
Péter Bakonyi-Kárpáti <pbk@vivid-planet.com>
2025-08-07 12:20:47 +0200

    Activate dependency injection in api console

--- demo/api/src/console.ts
+++ demo/api/src/console.ts
@@ -1,3 +1,6 @@
+import { NestFactory } from "@nestjs/core";
+import type { NestExpressApplication } from "@nestjs/platform-express";
+import { useContainer } from "class-validator";
 import { CommandFactory } from "nest-commander";
 
 import { AppModule } from "./app.module";
@@ -8,6 +11,12 @@ const config = createConfig(process.env);
 async function bootstrap() {
     const appModule = AppModule.forRoot(config);
 
+    // class-validator should use Nest for dependency injection.
+    // See https://github.com/nestjs/nest/issues/528,
+    //     https://github.com/typestack/class-validator#using-service-container.
+    const app = await NestFactory.create<NestExpressApplication>(appModule);
+    useContainer(app, { fallbackOnErrors: true });
+
     await CommandFactory.run(appModule, {
         logger: ["error", "warn", "log"],
         serviceErrorHandler: async (error) => {
@@ -15,6 +24,7 @@ async function bootstrap() {
             process.exit(1);
         },
     });
+    await app.close();
 }
 
 bootstrap();
