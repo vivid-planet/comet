7d644519a95eb7bd938e7647214c1c06f95036c8
Thomas Dax <thomas.dax@vivid-planet.com>
2025-08-08 09:09:44 +0200

    Create app via CommandFactory and use appModule instance in useContainer

--- demo/api/src/console.ts
+++ demo/api/src/console.ts
@@ -1,37 +1,37 @@
-import { NestFactory } from "@nestjs/core";
-import type { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "@src/app.module";
 import { useContainer } from "class-validator";
 import { CommandFactory } from "nest-commander";
 
-import { AppModule } from "./app.module";
 import { createConfig } from "./config/config";
 
 const config = createConfig(process.env);
+const appModule = AppModule.forRoot(config);
 
 async function bootstrap() {
-    const appModule = AppModule.forRoot(config);
-
-    // class-validator should use Nest for dependency injection.
-    // See https://github.com/nestjs/nest/issues/528,
-    //     https://github.com/typestack/class-validator#using-service-container.
-    const app = await NestFactory.create<NestExpressApplication>(appModule);
-    useContainer(app.select(AppModule), { fallbackOnErrors: true });
+    const app = await CommandFactory.createWithoutRunning(appModule, {
+        logger: ["error", "warn", "log"],
+        serviceErrorHandler: async (error) => {
+            console.error(error);
+            process.exit(1);
+        },
+    });
 
     try {
-        await CommandFactory.run(appModule, {
-            logger: ["error", "warn", "log"],
-            serviceErrorHandler: async (error) => {
-                // Log the error and rethrow to be caught below
-                console.error(error);
-                throw error;
-            },
-        });
+        await app.init();
+
+        // class-validator should use Nest for dependency injection.
+        // See https://github.com/nestjs/nest/issues/528,
+        //     https://github.com/typestack/class-validator#using-service-container.
+        useContainer(app.select(appModule), { fallbackOnErrors: true });
+
+        await CommandFactory.runApplication(app);
+        process.exit(0);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
     } finally {
         await app.close();
     }
 }
 
-bootstrap().catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
+bootstrap();
