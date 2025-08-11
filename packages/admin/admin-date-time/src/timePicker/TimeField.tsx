6409b1ab6867fbc493f064e29956e39c9fee1722
Manuel Blum <mb@vivid-planet.com>
2025-08-11 09:28:24 +0200

    refactor: improve prop typings for TimeField

--- packages/admin/admin-date-time/src/timePicker/TimeField.tsx
+++ packages/admin/admin-date-time/src/timePicker/TimeField.tsx
@@ -1,8 +1,8 @@
 import { Field, type FieldProps } from "@comet/admin";
 
-import { FinalFormTimePicker } from "./FinalFormTimePicker";
+import { FinalFormTimePicker, type FinalFormTimePickerProps } from "./FinalFormTimePicker";
 
-export type TimeFieldProps = FieldProps<string, HTMLInputElement>;
+export type TimeFieldProps = FieldProps<string, HTMLInputElement> & FinalFormTimePickerProps;
 
 export const TimeField = ({ ...restProps }: TimeFieldProps) => {
     return <Field component={FinalFormTimePicker} {...restProps} />;
