5cd2abf86ad76ad60aa851c1b5af9d6b7a6fcb19
Manuel Blum <mb@vivid-planet.com>
2025-08-12 14:43:04 +0200

    add slot system to diffheader

--- packages/admin/admin/src/actionLog/diffHeader/DiffHeader.sc.ts
+++ packages/admin/admin/src/actionLog/diffHeader/DiffHeader.sc.ts
@@ -1,10 +1,37 @@
-import { Box } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { css } from "@emotion/react";
+import { Box, Typography } from "@mui/material";
 
-export const Root = styled(Box)`
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    background-color: ${({ theme }) => theme.palette.grey.A200};
-    padding: ${({ theme }) => theme.spacing(2)};
-`;
+import { createComponentSlot } from "../../helpers/createComponentSlot";
+import { type DiffHeaderClassKey } from "./DiffHeader";
+
+export const Root = createComponentSlot(Box)<DiffHeaderClassKey>({
+    componentName: "DiffHeader",
+    slotName: "root",
+})(
+    ({ theme }) => css`
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        background-color: ${theme.palette.grey.A200};
+        padding: ${theme.spacing(2)};
+    `,
+);
+
+export const VersionTypography = createComponentSlot(Typography)<DiffHeaderClassKey>({
+    componentName: "DiffHeader",
+    slotName: "versionTypography",
+})(() => css``);
+export const UserNameTypography = createComponentSlot(Typography)<DiffHeaderClassKey>({
+    componentName: "DiffHeader",
+    slotName: "userNameTypography",
+})(() => css``);
+
+export const DateTypography = createComponentSlot(Typography)<DiffHeaderClassKey>({
+    componentName: "DiffHeader",
+    slotName: "dateTypography",
+})(() => css``);
+
+export const InfoTypography = createComponentSlot(Typography)<DiffHeaderClassKey>({
+    componentName: "DiffHeader",
+    slotName: "infoTypography",
+})(() => css``);
