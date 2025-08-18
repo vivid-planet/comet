d0f62449bf331c37bf066511e75ff6252fa7e8ff
Thomas Dax <thomas.dax@vivid-planet.com>
2025-08-18 09:42:07 +0200

    Add useDataGridRemote to EditDialogInDataGrid story

--- storybook/src/admin/edit-dialog/EditDialogInDataGrid.stories.tsx
+++ storybook/src/admin/edit-dialog/EditDialogInDataGrid.stories.tsx
@@ -1,9 +1,10 @@
-import { Button, DataGridToolbar, FillSpace, FinalForm, Stack, StackMainContent, useEditDialog } from "@comet/admin";
+import { Button, DataGridToolbar, FillSpace, FinalForm, MainContent, useDataGridRemote, useEditDialog } from "@comet/admin";
 import { Add as AddIcon } from "@comet/admin-icons";
 import { DialogContent, TextField } from "@mui/material";
 import { DataGrid, type GridToolbarProps } from "@mui/x-data-grid";
 import { type ReactNode } from "react";
-import { FormattedMessage, useIntl } from "react-intl";
+import { FormattedMessage } from "react-intl";
+import { useLocation } from "react-router";
 
 import { storyRouterDecorator } from "../../story-router.decorator";
 
@@ -16,8 +17,6 @@ interface ToolbarProps extends GridToolbarProps {
     toolbarAction?: ReactNode;
 }
 
-const products = Array.from({ length: 150 }, (_, productIndex) => ({ id: productIndex.toString(), name: `Product ${productIndex + 1}` }));
-
 function Toolbar({ toolbarAction }: ToolbarProps) {
     return (
         <DataGridToolbar>
@@ -29,49 +28,59 @@ function Toolbar({ toolbarAction }: ToolbarProps) {
 
 export const EditDialogInDataGrid = {
     render: () => {
-        const intl = useIntl();
         const [EditDialog, , editDialogApi] = useEditDialog();
-        console.log(products);
+        const location = useLocation();
+
+        const dataGridProps = useDataGridRemote({
+            initialSort: [{ field: "name", sort: "asc" }],
+            queryParamsPrefix: "products",
+        });
 
         return (
-            <Stack topLevelTitle={intl.formatMessage({ id: "devStories.editDailogBug", defaultMessage: "Edit-Dialog in DataGrid" })}>
-                <StackMainContent fullHeight>
-                    <DataGrid
-                        columns={[
-                            { field: "id", headerName: "ID", width: 90 },
-                            { field: "name", headerName: "Name", flex: 1 },
-                        ]}
-                        rows={products}
-                        slots={{
-                            toolbar: Toolbar,
-                        }}
-                        slotProps={{
-                            toolbar: {
-                                toolbarAction: (
-                                    <Button responsive startIcon={<AddIcon />} onClick={() => editDialogApi?.openAddDialog()}>
-                                        <FormattedMessage id="devStories.editDailogBug.newProduct" defaultMessage="New Product" />
-                                    </Button>
-                                ),
-                            } as ToolbarProps,
-                        }}
-                    />
-                    <EditDialog>
-                        <DialogContent>
-                            <FinalForm
-                                mode="edit"
-                                onSubmit={() => {
-                                    console.log("Submitted!");
-                                }}
-                                onAfterSubmit={() => {
-                                    editDialogApi.closeDialog();
-                                }}
-                            >
-                                <TextField name="name" label="Name" fullWidth />
-                            </FinalForm>
-                        </DialogContent>
-                    </EditDialog>
-                </StackMainContent>
-            </Stack>
+            <MainContent fullHeight>
+                <p>
+                    Current Path: {location.pathname}
+                    {location.search}
+                </p>
+                <DataGrid
+                    columns={[
+                        { field: "id", headerName: "ID", width: 90 },
+                        { field: "name", headerName: "Name", flex: 1 },
+                    ]}
+                    rows={[]}
+                    rowCount={0}
+                    sortingMode="server"
+                    sortModel={dataGridProps.sortModel}
+                    onSortModelChange={dataGridProps.onSortModelChange}
+                    slots={{
+                        toolbar: Toolbar,
+                    }}
+                    slotProps={{
+                        toolbar: {
+                            toolbarAction: (
+                                <Button responsive startIcon={<AddIcon />} onClick={() => editDialogApi?.openAddDialog()}>
+                                    <FormattedMessage id="devStories.editDailogBug.newProduct" defaultMessage="New Product" />
+                                </Button>
+                            ),
+                        } as ToolbarProps,
+                    }}
+                />
+                <EditDialog>
+                    <DialogContent>
+                        <FinalForm
+                            mode="edit"
+                            onSubmit={() => {
+                                console.log("Submitted!");
+                            }}
+                            onAfterSubmit={() => {
+                                editDialogApi.closeDialog();
+                            }}
+                        >
+                            <TextField name="name" label="Name" fullWidth />
+                        </FinalForm>
+                    </DialogContent>
+                </EditDialog>
+            </MainContent>
         );
     },
     name: "EditDialog in DataGrid",
