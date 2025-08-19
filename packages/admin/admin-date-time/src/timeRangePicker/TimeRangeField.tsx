98e334f07c5b84d52bd7540be5995c430fc68f3d
manuelblum <mb@vivid-planet.com>
2025-08-19 11:14:31 +0200

    Improve prop typings for TimeRangeField (#4316)

--- packages/admin/admin-date-time/src/timeRangePicker/TimeRangeField.tsx
+++ packages/admin/admin-date-time/src/timeRangePicker/TimeRangeField.tsx
@@ -1,9 +1,9 @@
 import { Field, type FieldProps } from "@comet/admin";
 
-import { FinalFormTimeRangePicker } from "./FinalFormTimeRangePicker";
+import { FinalFormTimeRangePicker, type FinalFormTimeRangePickerProps } from "./FinalFormTimeRangePicker";
 import { type TimeRange } from "./TimeRangePicker";
 
-export type TimeRangeFieldProps = FieldProps<TimeRange, HTMLInputElement>;
+export type TimeRangeFieldProps = FieldProps<TimeRange, HTMLInputElement> & FinalFormTimeRangePickerProps;
 
 export const TimeRangeField = ({ ...restProps }: TimeRangeFieldProps) => {
     return <Field component={FinalFormTimeRangePicker} {...restProps} />;
