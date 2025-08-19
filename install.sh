d3bbacd23a9c3f5d71aff24b1537b461fe4a10ce
Manuel Blum <mb@vivid-planet.com>
2025-08-19 14:42:01 +0200

    fix lint errors

--- install.sh
+++ install.sh
@@ -48,10 +48,15 @@ ln -sf ../api/schema.gql ./demo/site-pages/schema.gql
 ln -sf ../api/block-meta.json ./demo/site-pages/block-meta.json
 ln -sf ../../api/src/comet-config.json ./demo/site-pages/src/comet-config.json
 
+# admin-generator
+ln -sf ../../api/cms-api/block-meta.json ./packages/admin/admin-generator/block-meta.json
+
 # Lang install
 sh ./demo/admin/intl-update.sh
 sh ./demo/site/intl-update.sh
 sh ./demo/site-pages/intl-update.sh
+
+
 
 # Build the packages CLI and eslint-plugin to be used for dev startup
 pnpm --filter '@comet/cli' --filter '@comet/eslint-plugin' run build
--- packages/admin/admin-generator/eslint.config.mjs
+++ packages/admin/admin-generator/eslint.config.mjs
@@ -2,6 +2,9 @@ import eslintConfigCore from "@comet/eslint-config/core.js";
 
 /** @type {import('eslint')} */
 const config = [
+    {
+        ignores: ["schema.json", "src/fragmentTypes.json", "dist/**", "src/**/*.generated.ts", "src/**/generated/**"],
+    },
     ...eslintConfigCore,
     {
         rules: {
