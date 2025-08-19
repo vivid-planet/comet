b0ee6c9b61531cd02cb88d60f10cd72ebeee8cdb
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-08-19 16:14:34 +0200

    undo changes in demo/site-pages

--- demo/site-pages/src/common/blocks/MediaGalleryBlock.tsx
+++ demo/site-pages/src/common/blocks/MediaGalleryBlock.tsx
@@ -7,10 +7,8 @@ import { MediaBlock } from "@src/common/blocks/MediaBlock";
 import { Typography } from "@src/common/components/Typography";
 import { PageLayout } from "@src/layout/PageLayout";
 import styled from "styled-components";
-import { Pagination } from "swiper/modules";
-import { SwiperSlide } from "swiper/react";
-
-import { BasicSwiper } from "../components/BasicSwiper";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 type MediaGalleryBlockProps = PropsWithData<MediaGalleryBlockData>;
 
@@ -20,12 +18,10 @@ export const MediaGalleryBlock = withPreview(
 
         return (
             <SwiperWrapper
+                modules={[Navigation]}
                 slidesPerView={1}
                 slidesPerGroup={1}
-                modules={[Pagination]}
-                pagination={{
-                    clickable: true,
-                }}
+                navigation
                 longSwipesRatio={0.1}
                 threshold={3}
                 allowTouchMove
@@ -82,7 +78,7 @@ const MediaCaption = styled(Typography)`
     }
 `;
 
-const SwiperWrapper = styled(BasicSwiper)<{ $aspectRatioHorizontal: string; $aspectRatioVertical: string }>`
+const SwiperWrapper = styled(Swiper)<{ $aspectRatioHorizontal: string; $aspectRatioVertical: string }>`
     --swiper-button-size: 16px;
 
     .swiper-button-prev,
