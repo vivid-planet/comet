85a9ff70bea4dae0ab1baf184fb32845b97018ce
manuelblum <mb@vivid-planet.com>
2025-08-19 11:08:17 +0200

    Improve prop typings for DateRangeField (#4313)

--- packages/admin/admin-date-time/src/dateRangePicker/DateRangeField.tsx
+++ packages/admin/admin-date-time/src/dateRangePicker/DateRangeField.tsx
@@ -1,9 +1,9 @@
 import { Field, type FieldProps } from "@comet/admin";
 
 import { type DateRange } from "./DateRangePicker";
-import { FinalFormDateRangePicker } from "./FinalFormDateRangePicker";
+import { FinalFormDateRangePicker, type FinalFormDateRangePickerProps } from "./FinalFormDateRangePicker";
 
-export type DateRangeFieldProps = FieldProps<DateRange, HTMLInputElement>;
+export type DateRangeFieldProps = FieldProps<DateRange, HTMLInputElement> & FinalFormDateRangePickerProps;
 
 export const DateRangeField = ({ ...restProps }: DateRangeFieldProps) => {
     return <Field component={FinalFormDateRangePicker} {...restProps} />;
