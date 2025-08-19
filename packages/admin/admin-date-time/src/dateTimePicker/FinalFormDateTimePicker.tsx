32b669dce40cfacc9563b1c4ddc3fab66c1fc3e2
manuelblum <mb@vivid-planet.com>
2025-08-19 11:13:09 +0200

    refactor: improve prop typings for DateTimeField (#4314)

--- packages/admin/admin-date-time/src/dateTimePicker/FinalFormDateTimePicker.tsx
+++ packages/admin/admin-date-time/src/dateTimePicker/FinalFormDateTimePicker.tsx
@@ -2,13 +2,14 @@ import { type FieldRenderProps } from "react-final-form";
 
 import { DateTimePicker, type DateTimePickerProps } from "./DateTimePicker";
 
-export type FinalFormDateTimePickerProps = DateTimePickerProps & FieldRenderProps<Date, HTMLInputElement | HTMLTextAreaElement>;
+export type FinalFormDateTimePickerProps = DateTimePickerProps;
+type FinalFormDateTimePickerInternalProps = FieldRenderProps<Date, HTMLInputElement | HTMLTextAreaElement>;
 
 /**
  * Final Form-compatible DateTimePicker component.
  *
  * @see {@link DateTimeField} – preferred for typical form use. Use this only if no Field wrapper is needed.
  */
-export const FinalFormDateTimePicker = ({ meta, input, ...restProps }: FinalFormDateTimePickerProps) => {
+export const FinalFormDateTimePicker = ({ meta, input, ...restProps }: FinalFormDateTimePickerProps & FinalFormDateTimePickerInternalProps) => {
     return <DateTimePicker {...input} {...restProps} />;
 };
