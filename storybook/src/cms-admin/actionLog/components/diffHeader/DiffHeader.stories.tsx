ff065c3dbd6e5369dad10ebbdbb6cc56508bf6d8
Manuel Blum <mb@vivid-planet.com>
2025-08-20 14:12:34 +0200

    refactor: move DiffHeader docs to @comet/cms-admin

--- storybook/src/cms-admin/actionLog/components/diffHeader/DiffHeader.stories.tsx
+++ storybook/src/cms-admin/actionLog/components/diffHeader/DiffHeader.stories.tsx
@@ -4,7 +4,7 @@ import type { Meta, StoryObj } from "@storybook/react-webpack5";
 type Story = StoryObj<typeof DiffHeader>;
 const meta: Meta<typeof DiffHeader> = {
     component: DiffHeader,
-    title: "@comet/admin/Action log/Components/Diff header/Diff header",
+    title: "@comet/cms-admin/Action log/Components/Diff header/Diff header",
 };
 export default meta;
 
