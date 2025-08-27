8961296130e13a1e4de96b30dff1ef726dcf94a9
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-08-27 16:03:01 +0200

    simplify useEscapeKeyPressed by using useEventListener from usehooks-ts library

--- demo/site/src/util/useEscapeKeyPressed.ts
+++ demo/site/src/util/useEscapeKeyPressed.ts
@@ -1,14 +1,13 @@
-import { useEffect } from "react";
+import { useEventListener } from "usehooks-ts";
 
 export const useEscapeKeyPressed = (keyPressed: () => void) => {
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-            if (event.key === "Escape") keyPressed();
-        };
-
-        window.addEventListener("keydown", handleKeyDown);
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-        };
-    }, [keyPressed]);
+    useEventListener(
+        "keydown",
+        (event: KeyboardEvent) => {
+            if (event.key === "Escape") {
+                keyPressed();
+            }
+        },
+        undefined,
+    );
 };
