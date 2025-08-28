58df9cfbca154682ea366b41bd196d9061bf0c1e
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-08-28 10:37:15 +0200

    add SectionHeadline admin component

---
+++ packages/admin/admin/src/section/SectionHeadline.tsx
@@ -0,0 +1,138 @@
+import { Info } from "@comet/admin-icons";
+import { type ComponentsOverrides, Divider, Typography } from "@mui/material";
+import { css, type Theme, useThemeProps } from "@mui/material/styles";
+import { type ReactNode } from "react";
+
+import { Tooltip } from "../common/Tooltip";
+import { createComponentSlot } from "../helpers/createComponentSlot";
+import { type ThemedComponentBaseProps } from "../helpers/ThemedComponentBaseProps";
+
+export type SectionHeadlineClassKey = "root" | "headlineWrapper" | "titleContainer" | "divider" | "supportText" | "infoTooltipText" | "infoIcon";
+
+const Root = createComponentSlot("div")<SectionHeadlineClassKey>({
+    componentName: "SectionHeadline",
+    slotName: "root",
+})();
+
+const HeadlineWrapper = createComponentSlot("div")<SectionHeadlineClassKey>({
+    componentName: "SectionHeadline",
+    slotName: "headlineWrapper",
+})(
+    () => css`
+        display: flex;
+        flex-direction: row;
+        align-items: flex-end;
+        justify-content: space-between;
+    `,
+);
+
+const TitleContainer = createComponentSlot("div")<SectionHeadlineClassKey>({
+    componentName: "SectionHeadline",
+    slotName: "titleContainer",
+})(
+    () => css`
+        display: flex;
+        align-items: center;
+    `,
+);
+
+const SupportText = createComponentSlot(Typography)<SectionHeadlineClassKey>({
+    componentName: "SectionHeadline",
+    slotName: "supportText",
+})(
+    ({ theme }) => css`
+        color: ${theme.palette.text.secondary};
+    `,
+);
+
+const StyledTooltip = createComponentSlot(Tooltip)<SectionHeadlineClassKey>({
+    componentName: "SectionHeadline",
+    slotName: "infoTooltipText",
+})(
+    ({ theme }) => css`
+        color: ${theme.palette.text.secondary};
+        margin-left: 10px;
+    `,
+);
+
+const StyledDivider = createComponentSlot(Divider)<SectionHeadlineClassKey>({
+    componentName: "SectionHeadline",
+    slotName: "divider",
+})(
+    () => css`
+        margin-top: 10px;
+        color: "inherit";
+    `,
+);
+
+const StyledInfoIcon = createComponentSlot(Info)<SectionHeadlineClassKey>({
+    componentName: "SectionHeadline",
+    slotName: "infoIcon",
+})(
+    () => css`
+        font-size: 12px;
+    `,
+);
+
+export interface SectionHeadlineProps
+    extends ThemedComponentBaseProps<{
+        root: "div";
+        headlineWrapper: "div";
+        titleContainer: "div";
+        infoTooltipText: typeof Tooltip;
+        infoIcon: typeof Info;
+        supportText: typeof Typography;
+        divider: typeof Divider;
+    }> {
+    children: ReactNode;
+    divider?: boolean;
+    supportText?: string;
+    infoTooltipText?: string;
+}
+
+export function SectionHeadline(inProps: SectionHeadlineProps) {
+    const { children, divider, supportText, infoTooltipText, slotProps, ...restProps } = useThemeProps({
+        props: inProps,
+        name: "CometAdminSectionHeadline",
+    });
+
+    return (
+        <Root {...slotProps?.root} {...restProps}>
+            <HeadlineWrapper {...slotProps?.headlineWrapper}>
+                <TitleContainer {...slotProps?.titleContainer}>
+                    {children}
+                    {infoTooltipText && (
+                        <StyledTooltip title={infoTooltipText} {...slotProps?.infoTooltipText}>
+                            <StyledInfoIcon {...slotProps?.infoIcon} />
+                        </StyledTooltip>
+                    )}
+                </TitleContainer>
+
+                {supportText && (
+                    <SupportText variant="caption" {...slotProps?.supportText}>
+                        {supportText}
+                    </SupportText>
+                )}
+            </HeadlineWrapper>
+
+            {divider && <StyledDivider {...slotProps?.divider} />}
+        </Root>
+    );
+}
+
+declare module "@mui/material/styles" {
+    interface ComponentNameToClassKey {
+        CometAdminSectionHeadline: SectionHeadlineClassKey;
+    }
+
+    interface ComponentsPropsList {
+        CometAdminSectionHeadline: SectionHeadlineProps;
+    }
+
+    interface Components {
+        CometAdminSectionHeadline?: {
+            defaultProps?: Partial<ComponentsPropsList["CometAdminSectionHeadline"]>;
+            styleOverrides?: ComponentsOverrides<Theme>["CometAdminSectionHeadline"];
+        };
+    }
+}
