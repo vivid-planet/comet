cbbff33c231a1f4198edbc3d994aeeb03b283993
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-08-28 10:38:15 +0200

    add dev story

---
+++ storybook/src/admin/SectionHeadline.stories.tsx
@@ -0,0 +1,20 @@
+import { SectionHeadline } from "@comet/admin";
+import { Stack, Typography } from "@mui/material";
+
+export default {
+    title: "@comet/admin/SectionHeadline",
+};
+
+export const _SectionHeadline = () => {
+    return (
+        <Stack spacing={15}>
+            <SectionHeadline supportText="Support Text" divider infoTooltipText="Tooltip Info Text">
+                <Typography variant="h4">Section Title</Typography>
+            </SectionHeadline>
+
+            <SectionHeadline>
+                <Typography variant="h4">Section Title</Typography>
+            </SectionHeadline>
+        </Stack>
+    );
+};
