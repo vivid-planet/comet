fb9461ee228d86dfbf670bf3a0012eb0210317e7
Alexander Kaufmann <ak@vivid-planet.com>
2025-09-02 13:13:47 +0200

    refactor: use decorator constants

--- packages/api/cms-api/src/action-logs/action-logs.module.ts
+++ packages/api/cms-api/src/action-logs/action-logs.module.ts
@@ -5,6 +5,7 @@ import { GqlExecutionContext } from "@nestjs/graphql";
 import type { Request } from "express";
 import { ClsModule } from "nestjs-cls";
 
+import { DISABLE_COMET_GUARDS_METADATA_KEY } from "../auth/decorators/disable-comet-guards.decorator";
 import { User } from "../user-permissions/interfaces/user";
 import { ActionLogsResolver } from "./action-logs.resolver";
 import { ActionLogsService } from "./action-logs.service";
@@ -23,7 +24,10 @@ import { ActionLog } from "./entities/action-log.entity";
                             context.getType().toString() === "graphql"
                                 ? GqlExecutionContext.create(context).getContext().req
                                 : context.switchToHttp().getRequest();
-                        const disableCometGuard = reflector.getAllAndOverride("disableCometGuards", [context.getHandler(), context.getClass()]);
+                        const disableCometGuard = reflector.getAllAndOverride(DISABLE_COMET_GUARDS_METADATA_KEY, [
+                            context.getHandler(),
+                            context.getClass(),
+                        ]);
                         if (disableCometGuard) return;
 
                         await new ActionLogsService(cls).setUserId(request.user.id);
