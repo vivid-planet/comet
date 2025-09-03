fe2034fd1e9c2c0187092a18461a74e9d42ab6fc
Manuel Blum <mb@vivid-planet.com>
2025-09-03 08:28:55 +0200

    update lint errors

--- storybook/src/cms-admin/contentScope/noContentScopeError/NoContentScopeError.stories.tsx
+++ storybook/src/cms-admin/contentScope/noContentScopeError/NoContentScopeError.stories.tsx
@@ -1,17 +1,16 @@
-import { NoContentScopeError } from "@comet/cms-admin";
+import { NoContentScopeFallback } from "@comet/cms-admin";
 import type { Meta, StoryObj } from "@storybook/react-webpack5";
 
 import { CurrentUserProviderDecorator } from "../../../../.storybook/decorators/CurrentUserProvider.decorator";
 import { apolloStoryDecorator } from "../../../apollo-story.decorator";
 
-type Story = StoryObj<typeof NoContentScopeError>;
+type Story = StoryObj<typeof NoContentScopeFallback>;
 
-const config: Meta<typeof NoContentScopeError> = {
-    component: NoContentScopeError,
-    title: "@comet/cms-admin/contentScope/noContentScopeError/NoContentScopeError",
+const config: Meta<typeof NoContentScopeFallback> = {
+    component: NoContentScopeFallback,
+    title: "@comet/cms-admin/contentScope/noContentScopeFallback/NoContentScopeFallback",
     decorators: [CurrentUserProviderDecorator, apolloStoryDecorator("/graphql")],
 };
 
 export default config;
-export const NoContentScopeErrorStory: Story = {};
-NoContentScopeErrorStory.storyName = "NoContentScopeError";
+export const Default: Story = {};
