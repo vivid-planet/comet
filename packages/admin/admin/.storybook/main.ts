25a5c3f25dd67318b7c8b5e24da77510c757adf4
Manuel Blum <mb@vivid-planet.com>
2025-09-03 11:10:37 +0200

    refactor documentation -> __stories__ directory

--- packages/admin/admin/.storybook/main.ts
+++ packages/admin/admin/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    stories: ["../src/**/*.mdx", "../src/**/__stories__/*.stories.@(js|jsx|mjs|ts|tsx)"],
     staticDirs: ["./public"],
 
     addons: ["@storybook/addon-docs", "storybook-addon-tag-badges"],
