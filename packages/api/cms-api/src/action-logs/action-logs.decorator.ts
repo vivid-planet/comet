85d4c7317136c375677d83878766ddd4eb08d7a0
Alexander Kaufmann <ak@vivid-planet.com>
2025-09-10 12:52:30 +0200

    refactor: support also single values for action logs permission

--- packages/api/cms-api/src/action-logs/action-logs.decorator.ts
+++ packages/api/cms-api/src/action-logs/action-logs.decorator.ts
@@ -1,13 +1,23 @@
 import type { Permission } from "../user-permissions/user-permissions.types";
 
+interface ActionLogOptions {
+    requiredPermission: Permission | Array<Permission>;
+}
+
 export interface ActionLogMetadata {
     requiredPermission: Array<Permission>;
 }
 
 export const ACTION_LOGS_METADATA_KEY = "actionLogs";
 
-export function ActionLogs(metadata: ActionLogMetadata): ClassDecorator {
+export function ActionLogs({ requiredPermission }: ActionLogOptions): ClassDecorator {
     return (entity) => {
-        Reflect.defineMetadata(ACTION_LOGS_METADATA_KEY, metadata, entity.prototype);
+        Reflect.defineMetadata(
+            ACTION_LOGS_METADATA_KEY,
+            {
+                requiredPermission: Array.isArray(requiredPermission) ? requiredPermission : [requiredPermission],
+            } satisfies ActionLogMetadata,
+            entity.prototype,
+        );
     };
 }
