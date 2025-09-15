88cc7307a068e10ea1ab29b71fa1e140fb5af213
Sebastian <sm@vivid-planet.com>
2025-09-15 16:15:24 +0200

    migrate CallToActionListBlock to CSS modules

--- demo/site/src/common/blocks/StandaloneCallToActionListBlock.tsx
+++ demo/site/src/common/blocks/StandaloneCallToActionListBlock.tsx
@@ -2,25 +2,19 @@
 import { type PropsWithData, withPreview } from "@comet/site-nextjs";
 import { type StandaloneCallToActionListBlockData } from "@src/blocks.generated";
 import { PageLayout } from "@src/layout/PageLayout";
-import { type CSSProperties } from "react";
-import styled from "styled-components";
+import clsx from "clsx";
 
 import { CallToActionListBlock } from "./CallToActionListBlock";
+import styles from "./StandaloneCallToActionListBlock.module.scss";
 
 type StandaloneCallToActionListBlockProps = PropsWithData<StandaloneCallToActionListBlockData>;
-
-const alignmentMap: Record<StandaloneCallToActionListBlockData["alignment"], CSSProperties["justifyContent"]> = {
-    left: "flex-start",
-    center: "center",
-    right: "flex-end",
-};
 
 export const StandaloneCallToActionListBlock = withPreview(
     ({ data: { callToActionList, alignment } }: StandaloneCallToActionListBlockProps) => {
         return (
-            <Root $alignment={alignmentMap[alignment]}>
+            <div className={clsx(styles.root, styles[`root--${alignment}`])}>
                 <CallToActionListBlock data={callToActionList} />
-            </Root>
+            </div>
         );
     },
     { label: "CallToActionList" },
@@ -28,17 +22,8 @@ export const StandaloneCallToActionListBlock = withPreview(
 
 export const PageContentStandaloneCallToActionListBlock = (props: StandaloneCallToActionListBlockProps) => (
     <PageLayout grid>
-        <PageLayoutContent>
+        <div className={styles.pageLayoutContent}>
             <StandaloneCallToActionListBlock {...props} />
-        </PageLayoutContent>
+        </div>
     </PageLayout>
 );
-
-const Root = styled.div<{ $alignment: CSSProperties["justifyContent"] }>`
-    display: flex;
-    justify-content: ${({ $alignment }) => $alignment};
-`;
-
-const PageLayoutContent = styled.div`
-    grid-column: 3 / -3;
-`;
