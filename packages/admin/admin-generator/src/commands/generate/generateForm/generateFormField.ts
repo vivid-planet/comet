94c92b9a6dd45a0144c11446867d3bfd6404ef4c
Niko Sams <ns@vivid-planet.com>
2025-09-16 11:10:56 +0200

    Admin Generator: Fix sending null for date field

--- packages/admin/admin-generator/src/commands/generate/generateForm/generateFormField.ts
+++ packages/admin/admin-generator/src/commands/generate/generateForm/generateFormField.ts
@@ -223,6 +223,9 @@ export function generateFormField({
                 }
                 ${validateCode}
             />`;
+        if (!config.virtual && !required && !config.readOnly) {
+            formValueToGqlInputCode = `${name}: formValues.${name} ?? null,`;
+        }
     } else if (config.type == "dateTime") {
         code = `<DateTimeField
                 ${required ? "required" : ""}
