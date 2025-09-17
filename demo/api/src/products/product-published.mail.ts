955509332fc58aa643aa802d25f0956b6c031f14
Benjamin Hohenwarter <bh@vivid-planet.com>
2025-09-17 11:47:46 +0200

    Add example for test-params

--- demo/api/src/products/product-published.mail.ts
+++ demo/api/src/products/product-published.mail.ts
@@ -1,24 +1,63 @@
 import { MailTemplate, MailTemplateInterface } from "@comet/cms-api";
+import { PreparedTestParams } from "@src/../../../packages/api/cms-api/src/mail-templates/mail-template.service";
+
+type MailProps = {
+    recipientName: string;
+    countProductPublished: "all" | number;
+};
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isMailProp(arg: any): arg is MailProps {
+    return arg.recipientName !== undefined && arg.countProductPublished !== undefined;
+}
 
 @MailTemplate()
-export class ProductPublishedMail implements MailTemplateInterface<unknown> {
+export class ProductPublishedMail implements MailTemplateInterface<MailProps> {
     id = "products-published";
     type = "products";
     name = "Products published";
 
-    async generateMail(): Promise<{
+    async generateMail(params: MailProps): Promise<{
         subject: string;
         text: string;
         html: string;
     }> {
+        if (!isMailProp(params)) throw new Error(`Incompatible params`);
+
         return {
-            subject: "All products have been published",
-            text: "All products have been published",
-            html: "<p>All products have been published</p>",
+            subject: "Products published",
+            text:
+                `Hello ${params.recipientName},` +
+                "\r\n" +
+                `${
+                    params.countProductPublished === "all"
+                        ? "All products have been published"
+                        : `${params.countProductPublished} products have been published`
+                }`,
+            html: `<p>Hello ${params.recipientName},</p>
+                ${
+                    params.countProductPublished === "all"
+                        ? "<p>All products have been published</p>"
+                        : `<p>${params.countProductPublished} products have been published<p>`
+                }`,
         };
     }
 
-    async getPreparedTestParams() {
-        return [];
+    async getPreparedTestParams(): Promise<PreparedTestParams<MailProps>[]> {
+        return [
+            {
+                name: `Testing "all" Mail`,
+                params: {
+                    recipientName: "John Doe",
+                    countProductPublished: "all",
+                },
+            },
+            {
+                name: `Testing "count" Mail`,
+                params: {
+                    recipientName: "John Doe",
+                    countProductPublished: 5,
+                },
+            },
+        ];
     }
 }
