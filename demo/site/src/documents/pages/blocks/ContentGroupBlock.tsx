fb25c9f0cfcdfb7e5ba9ce148f4dcdf8e3fdbde0
Sebastian <sm@vivid-planet.com>
2025-09-18 11:24:54 +0200

    remove unused import

--- demo/site/src/documents/pages/blocks/ContentGroupBlock.tsx
+++ demo/site/src/documents/pages/blocks/ContentGroupBlock.tsx
@@ -12,7 +12,6 @@ import { ColumnsBlock } from "@src/documents/pages/blocks/ColumnsBlock";
 import { KeyFactsBlock } from "@src/documents/pages/blocks/KeyFactsBlock";
 import { TeaserBlock } from "@src/documents/pages/blocks/TeaserBlock";
 import { PageLayout } from "@src/layout/PageLayout";
-import clsx from "clsx";
 
 import styles from "./ContentGroupBlock.module.scss";
 
