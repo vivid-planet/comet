f9fdd012d479f0c1137505fc997e269102de25c3
Sebastian <sm@vivid-planet.com>
2025-09-19 09:22:27 +0200

    RichText with text alignment

--- demo/admin/src/documents/pages/blocks/PageContentBlock.tsx
+++ demo/admin/src/documents/pages/blocks/PageContentBlock.tsx
@@ -2,11 +2,11 @@ import { AnchorBlock, createBlocksBlock, DamImageBlock } from "@comet/cms-admin"
 import { AccordionBlock } from "@src/common/blocks/AccordionBlock";
 import { LayoutBlock } from "@src/common/blocks/LayoutBlock";
 import { MediaGalleryBlock } from "@src/common/blocks/MediaGalleryBlock";
-import { RichTextBlock } from "@src/common/blocks/RichTextBlock";
 import { SpaceBlock } from "@src/common/blocks/SpaceBlock";
 import { StandaloneCallToActionListBlock } from "@src/common/blocks/StandaloneCallToActionListBlock";
 import { StandaloneHeadingBlock } from "@src/common/blocks/StandaloneHeadingBlock";
 import { StandaloneMediaBlock } from "@src/common/blocks/StandaloneMediaBlock";
+import { StandaloneRichTextBlock } from "@src/common/blocks/StandaloneRichTextBlock";
 import { TextImageBlock } from "@src/common/blocks/TextImageBlock";
 import { NewsDetailBlock } from "@src/news/blocks/NewsDetailBlock";
 import { NewsListBlock } from "@src/news/blocks/NewsListBlock";
@@ -30,7 +30,7 @@ export const PageContentBlock = createBlocksBlock({
         billboardTeaser: BillboardTeaserBlock,
         space: SpaceBlock,
         teaser: TeaserBlock,
-        richtext: RichTextBlock,
+        richtext: StandaloneRichTextBlock,
         heading: StandaloneHeadingBlock,
         columns: ColumnsBlock,
         callToActionList: StandaloneCallToActionListBlock,
