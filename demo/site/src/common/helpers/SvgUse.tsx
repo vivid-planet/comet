33f187dc8b79dd2a7977aa0e8fc8b8d4ddb9d5dd
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-09-22 15:20:00 +0200

    revert changes in SvgUse

--- demo/site/src/common/helpers/SvgUse.tsx
+++ demo/site/src/common/helpers/SvgUse.tsx
@@ -3,12 +3,10 @@ import { type SVGProps } from "react";
 
 interface SvgUseProps extends SVGProps<SVGSVGElement> {
     href: string;
-    title?: string;
 }
 
-export const SvgUse = ({ title, href, ...props }: SvgUseProps) => (
-    <svg aria-hidden={title ? undefined : true} {...props}>
-        {title && <title>{title}</title>}
+export const SvgUse = ({ href, ...props }: SvgUseProps) => (
+    <svg aria-hidden="true" {...props}>
         <use href={href} xlinkHref={href} />
     </svg>
 );
