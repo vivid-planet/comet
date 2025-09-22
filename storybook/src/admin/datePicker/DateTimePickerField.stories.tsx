2dab4a9d3c77f657a42d15fb41527d20745e75ed
Manuel Blum <mb@vivid-planet.com>
2025-09-22 08:49:59 +0200

    moved DateTimePicker (field) stories to DateTimePickerField stories

--- storybook/src/admin/datePicker/DateTimePickerField.stories.tsx
+++ storybook/src/admin/datePicker/DateTimePickerField.stories.tsx
@@ -1,15 +1,15 @@
 import { Alert, FinalForm, Future_DateTimePickerField } from "@comet/admin";
-import type { Meta, StoryObj } from "@storybook/react-webpack5";
+import type { Meta, StoryFn } from "@storybook/react-webpack5";
 
-type Story = StoryObj<typeof Future_DateTimePickerField>;
+type Story = StoryFn<typeof Future_DateTimePickerField>;
 const config: Meta<typeof Future_DateTimePickerField> = {
     component: Future_DateTimePickerField,
     title: "@comet/admin/datePicker/Future_DateTimePickerField",
 };
 export default config;
 
-export const Default: Story = {
-    render: () => {
+const TemplateStory: Story = (props) => {
+    const Story = () => {
         interface FormValues {
             value: string;
         }
@@ -24,7 +24,7 @@ export const Default: Story = {
                 {({ values }) => {
                     return (
                         <>
-                            <Future_DateTimePickerField name="value" label="Date Time Picker" fullWidth variant="horizontal" />
+                            <Future_DateTimePickerField label="Date Time Picker" {...props} name="value" />
 
                             <Alert title="FormState">
                                 <pre>{JSON.stringify(values, null, 2)}</pre>
@@ -34,41 +34,42 @@ export const Default: Story = {
                 }}
             </FinalForm>
         );
-    },
+    };
+
+    return <Story />;
 };
 
-export const MinMaxDate: Story = {
-    render: () => {
-        interface FormValues {
-            value: string;
-        }
-        return (
-            <FinalForm<FormValues>
-                mode="edit"
-                onSubmit={() => {
-                    // not handled
-                }}
-                subscription={{ values: true }}
-            >
-                {({ values }) => {
-                    return (
-                        <>
-                            <Future_DateTimePickerField
-                                name="value"
-                                label="Date Picker with Min/Max Date"
-                                fullWidth
-                                variant="horizontal"
-                                minDate={new Date(2025, 0, 1)}
-                                maxDate={new Date(2025, 11, 31)}
-                            />
+export const Default = TemplateStory.bind({});
+Default.args = {
+    fullWidth: true,
+    variant: "horizontal",
+};
 
-                            <Alert title="FormState">
-                                <pre>{JSON.stringify(values, null, 2)}</pre>
-                            </Alert>
-                        </>
-                    );
-                }}
-            </FinalForm>
-        );
-    },
+export const MinMaxDate = TemplateStory.bind({});
+MinMaxDate.args = {
+    fullWidth: true,
+    variant: "horizontal",
+    minDate: new Date(2025, 0, 1),
+    maxDate: new Date(2025, 11, 31),
+};
+
+export const Required = TemplateStory.bind({});
+Required.args = {
+    fullWidth: true,
+    variant: "horizontal",
+    required: true,
+};
+
+export const Disabled = TemplateStory.bind({});
+Disabled.args = {
+    fullWidth: true,
+    variant: "horizontal",
+    disabled: true,
+};
+
+export const Readonly = TemplateStory.bind({});
+Readonly.args = {
+    fullWidth: true,
+    variant: "horizontal",
+    readOnly: true,
 };
