1f328ba6276c0c62678a7087ea8aebd70508eaf5
Sebastian <sm@vivid-planet.com>
2025-09-24 10:40:06 +0200

    remove styled components

---
+++ demo/site/src/styles/breakpoints.ts
@@ -0,0 +1,6 @@
+export const breakpoints = {
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1600,
+};
--- demo/site/src/util/createImageSizes.ts
+++ demo/site/src/util/createImageSizes.ts
@@ -1,18 +1,16 @@
-import { type Theme, theme } from "@src/theme";
+import { breakpoints } from "@src/styles/breakpoints";
 
-type BreakpointWidths = { default: string | number } & Partial<Record<keyof Theme["breakpoints"], string | number>>;
+type BreakpointWidths = { default: string | number } & Partial<Record<keyof typeof breakpoints, string | number>>;
 
 export function createImageSizes(breakpointWidths: BreakpointWidths) {
     const sizes: string[] = [];
-    const breakpoints = Object.entries(theme.breakpoints).sort((a, b) => b[1].value - a[1].value); // breakpoint values have to be sorted in descending order when using min-width in the sizes property
+    const breakpointEntries = Object.entries(breakpoints).sort((a, b) => b[1] - a[1]); // breakpoint values have to be sorted in descending order when using min-width in the sizes property
     const defaultSize = breakpointWidths.default;
 
-    breakpoints.forEach(([breakpointKey, breakpointValue]) => {
+    breakpointEntries.forEach(([breakpointKey, breakpointValue]) => {
         const size = breakpointWidths[breakpointKey];
-        const minWidth = breakpointValue.value;
-
         if (size !== undefined) {
-            sizes.push(`(min-width: ${minWidth}px) ${typeof size === "string" ? size : `${size}px`}`);
+            sizes.push(`(min-width: ${breakpointValue}px) ${typeof size === "string" ? size : `${size}px`}`);
         }
     });
 
