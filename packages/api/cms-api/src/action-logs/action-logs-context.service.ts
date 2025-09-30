3b1bc5e56ea5641a140ab2ce666db4c3a84f8438
Alexander Kaufmann <ak@vivid-planet.com>
2025-09-30 14:31:35 +0200

    refactor: make nestjs-cls store keys unique

--- packages/api/cms-api/src/action-logs/action-logs-context.service.ts
+++ packages/api/cms-api/src/action-logs/action-logs-context.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { ClsService } from "nestjs-cls";
 
+const USER_ID_KEY = Symbol("userId");
+
 declare module "nestjs-cls" {
     interface ClsStore {
-        userId: string;
+        [USER_ID_KEY]: string;
     }
 }
 
@@ -11,15 +13,19 @@ declare module "nestjs-cls" {
 export class ActionLogsContextService {
     constructor(private readonly cls: ClsService) {}
 
+    hasUserId(): boolean {
+        return this.cls.has(USER_ID_KEY);
+    }
+
     async getUserId(): Promise<string> {
-        return this.cls.get("userId");
+        return this.cls.get(USER_ID_KEY);
     }
 
     async setUserId(userId: string): Promise<void> {
-        this.cls.set("userId", userId);
+        this.cls.set(USER_ID_KEY, userId);
     }
 
     async runWithUserId(userId: string, callback: () => Promise<void>): Promise<void> {
-        return this.cls.runWith({ userId }, callback);
+        return this.cls.runWith({ [USER_ID_KEY]: userId }, callback);
     }
 }
