c8bbb75f76916ad1866098b6b8a63e28ea7a4637
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-10-01 15:03:58 +0200

    add play/pause button

---
+++ packages/site/site-nextjs/src/blocks/helpers/PlayPauseButton.tsx
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+
+import styles from "./PlayPauseButton.module.scss";
+
+interface PlayPauseButtonProps {
+    isPlaying: boolean;
+    onClick: () => void;
+    ariaLabel?: string;
+    className?: string;
+}
+
+export const PlayPauseButton = ({ isPlaying, onClick, ariaLabel, className }: PlayPauseButtonProps) => {
+    return (
+        <button className={clsx(styles.button, className)} onClick={onClick} aria-label={ariaLabel ?? (isPlaying ? "Pause" : "Play")}>
+            <div className={clsx(styles.animatedPlayPause, !isPlaying && styles.animatedPlayPausePause)} />
+        </button>
+    );
+};
