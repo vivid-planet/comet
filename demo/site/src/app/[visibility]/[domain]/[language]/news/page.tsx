6f23ef5c40f35e7a95ab07a51ea5e70bcf10e8d1
Johannes Obermair <johannes.obermair@vivid-planet.com>
2025-10-02 13:47:16 +0200

    Change to asynchronous Dynamic APIs

--- demo/site/src/app/[visibility]/[domain]/[language]/news/page.tsx
+++ demo/site/src/app/[visibility]/[domain]/[language]/news/page.tsx
@@ -5,13 +5,14 @@ import { NewsPage } from "@src/news/NewsPage";
 import { fetchNewsList } from "@src/news/NewsPage.loader";
 import { setVisibilityParam } from "@src/util/ServerContext";
 
-export type PageParams = {
+type Params = Promise<{
     domain: string;
     language: string;
     visibility: VisibilityParam;
-};
+}>;
 
-export default async function NewsIndexPage({ params: { domain, language, visibility } }: { params: PageParams }) {
+export default async function NewsIndexPage({ params }: { params: Params }) {
+    const { visibility, domain, language } = await params;
     setVisibilityParam(visibility);
     return <NewsPage scope={{ domain, language }} initialData={await fetchNewsList({ scope: { domain, language }, limit: 2 })} />;
 }
