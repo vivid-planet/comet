639e4e45a1d087a39e7d3b19cdfd3b77255a57e2
Manuel Blum <mb@vivid-planet.com>
2025-10-06 12:22:12 +0200

    export HelpDialogActionProps and HelpDialogActionClassKey

--- packages/admin/admin/src/index.ts
+++ packages/admin/admin/src/index.ts
@@ -18,6 +18,7 @@ export { CopyToClipboardButton, type CopyToClipboardButtonClassKey, type CopyToC
 export { DeleteButton, type DeleteButtonClassKey, type DeleteButtonProps } from "./common/buttons/delete/DeleteButton";
 export { FeedbackButton, type FeedbackButtonClassKey, type FeedbackButtonProps } from "./common/buttons/feedback/FeedbackButton";
 export { HelpDialogButton, HelpDialogButtonClassKey, HelpDialogButtonProps } from "./common/buttons/helpDialogButton/HelpDialogButton";
+export { HelpDialogButton, HelpDialogButtonClassKey, HelpDialogButtonProps } from "./common/buttons/helpDialogButton/HelpDialogButton";
 export { OkayButton, type OkayButtonClassKey, type OkayButtonProps } from "./common/buttons/okay/OkayButton";
 export { SaveButton, type SaveButtonClassKey, type SaveButtonProps } from "./common/buttons/SaveButton";
 export { SplitButton, type SplitButtonClassKey, type SplitButtonProps } from "./common/buttons/split/SplitButton";
