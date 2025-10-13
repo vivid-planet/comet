e4bec6d71baa11f2d4dac90e8b597aa64a142cc2
Raphael Blum <raphael.blum@vivid-planet.com>
2025-10-13 08:36:19 +0200

    Add support for temporary file uploads

---
+++ packages/api/cms-api/src/file-uploads/file-uploads-delete-expired-files.command.ts
@@ -0,0 +1,26 @@
+import { CreateRequestContext } from "@mikro-orm/core";
+import { InjectRepository } from "@mikro-orm/nestjs";
+import { EntityRepository } from "@mikro-orm/postgresql";
+import { Injectable } from "@nestjs/common";
+import { Command, CommandRunner } from "nest-commander";
+
+import { FileUpload } from "./entities/file-upload.entity";
+
+@Injectable()
+@Command({
+    name: "delete-expired-file-uploads",
+    description: "Deletes all expired file uploads",
+})
+export class FileUploadsDeleteExpiredFilesCommand extends CommandRunner {
+    constructor(@InjectRepository(FileUpload) private readonly repository: EntityRepository<FileUpload>) {
+        super();
+    }
+
+    @CreateRequestContext()
+    async run(): Promise<void> {
+        const result = await this.repository.nativeDelete({
+            expiresAt: { $lt: new Date() },
+        });
+        console.log(`Deleted ${result} expired file uploads`);
+    }
+}
