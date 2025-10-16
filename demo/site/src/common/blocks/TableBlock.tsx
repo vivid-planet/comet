cb9bab36036a3f8b9336284e92fbee820e3adc55
Ricky James Smith <jamesricky@me.com>
2025-10-16 08:50:43 +0200

    Demonstrate highlighting of rows and/or columns in styling (#4657)

--- demo/site/src/common/blocks/TableBlock.tsx
+++ demo/site/src/common/blocks/TableBlock.tsx
@@ -1,32 +1,45 @@
 import { type PropsWithData } from "@comet/site-nextjs";
 import { type TableBlockData } from "@src/blocks.generated";
+import clsx from "clsx";
+
+import styles from "./TableBlock.module.scss";
 
 // TODO: Used for debugging - either remove this or implement properly before merging feature branch
 export const TableBlock = ({ data }: PropsWithData<TableBlockData>) => {
     return (
         <div>
-            <pre style={{ fontSize: 9, lineHeight: 1, backgroundColor: "lightgrey", padding: 10 }}>{JSON.stringify(data, null, 2)}</pre>
-            <table border={1} cellPadding={10} cellSpacing={2}>
+            <pre className={styles.debugData}>{JSON.stringify(data, null, 2)}</pre>
+            <table border={0} cellPadding={10} cellSpacing={1} className={styles.table}>
                 <tbody>
                     {data.rows.map(({ cellValues, ...rowData }) => (
-                        <tr key={rowData.id}>
-                            {data.columns.map((column) => (
-                                <td key={column.id}>
-                                    <p style={{ margin: 0, fontSize: 12, whiteSpace: "pre-wrap" }}>
-                                        {cellValues.find(({ columnId }) => columnId === column.id)?.value}
-                                    </p>
-                                    <pre style={{ fontSize: 9, lineHeight: 1, backgroundColor: "lightgrey", padding: 10 }}>
-                                        {JSON.stringify(
-                                            {
-                                                row: rowData,
-                                                col: data.columns.find((c) => c.id === column.id),
-                                            },
-                                            null,
-                                            2,
-                                        )}
-                                    </pre>
-                                </td>
-                            ))}
+                        <tr key={rowData.id} className={styles.tableRow}>
+                            {data.columns.map((column) => {
+                                const columnIsHighlighted = column.highlighted;
+                                const rowIsHighlighted = rowData.highlighted;
+
+                                return (
+                                    <td
+                                        key={column.id}
+                                        className={clsx([
+                                            styles.cell,
+                                            columnIsHighlighted && styles["cell--columnIsHighlighted"],
+                                            rowIsHighlighted && styles["cell--rowIsHighlighted"],
+                                        ])}
+                                    >
+                                        <p className={styles.cell__text}>{cellValues.find(({ columnId }) => columnId === column.id)?.value}</p>
+                                        <pre className={styles.cell__debugData}>
+                                            {JSON.stringify(
+                                                {
+                                                    row: rowData,
+                                                    col: data.columns.find((c) => c.id === column.id),
+                                                },
+                                                null,
+                                                2,
+                                            )}
+                                        </pre>
+                                    </td>
+                                );
+                            })}
                         </tr>
                     ))}
                 </tbody>
