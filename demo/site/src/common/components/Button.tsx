060f4d2620ced6fc59d6cea6960a8a9b4c6911b7
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-10-29 10:50:03 +0100

    Revert "support start- and endIcon in Button component"
    This reverts commit c9c025a6920a4882a9e911985a0e67268b26c55e.

--- demo/site/src/common/components/Button.tsx
+++ demo/site/src/common/components/Button.tsx
@@ -1,7 +1,6 @@
 import clsx from "clsx";
 import { type ComponentProps, type ElementType } from "react";
 
-import { SvgUse } from "../helpers/SvgUse";
 import styles from "./Button.module.scss";
 
 export type ButtonVariant = "contained" | "outlined" | "text";
@@ -9,17 +8,9 @@ export type ButtonVariant = "contained" | "outlined" | "text";
 type ButtonProps<T extends ElementType> = {
     as?: T;
     variant?: ButtonVariant;
-    startIcon?: string;
-    endIcon?: string;
 } & Omit<ComponentProps<T>, "variant">;
 
 export const Button = <T extends ElementType = "button">(props: ButtonProps<T>) => {
-    const { as: Component = "button", variant = "contained", className, startIcon, endIcon, children, ...rest } = props;
-    return (
-        <Component className={clsx(styles.button, styles[variant], className)} {...rest}>
-            {startIcon && <SvgUse href={startIcon} width={16} height={16} />}
-            {children}
-            {endIcon && <SvgUse href={endIcon} width={16} height={16} />}
-        </Component>
-    );
+    const { as: Component = "button", variant = "contained", className, ...rest } = props;
+    return <Component className={clsx(styles.button, styles[variant], className)} {...rest} />;
 };
