52fe6ffd3b3b8639c42bab79add4c5dc70ac8305
Raphael Blum <raphael.blum@vivid-planet.com>
2025-11-05 10:29:37 +0100

    Refactor FileUploadExpirationSubscriber constructor by removing unused imports and simplifying parameter definitions.

--- packages/api/cms-api/src/file-uploads/file-upload-expiration.subscriber.ts
+++ packages/api/cms-api/src/file-uploads/file-upload-expiration.subscriber.ts
@@ -1,18 +1,15 @@
 import { EntityName, EventArgs, EventSubscriber } from "@mikro-orm/core";
 import { EntityManager } from "@mikro-orm/postgresql";
-import { Inject, Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 
 import { FileUpload } from "./entities/file-upload.entity";
-import { FileUploadsConfig } from "./file-uploads.config";
-import { FILE_UPLOADS_CONFIG } from "./file-uploads.constants";
 import { FileUploadsService } from "./file-uploads.service";
 
 @Injectable()
 export class FileUploadExpirationSubscriber implements EventSubscriber {
     constructor(
-        private readonly entityManager: EntityManager,
+        entityManager: EntityManager,
         private readonly fileUploadsService: FileUploadsService,
-        @Inject(FILE_UPLOADS_CONFIG) private readonly config: FileUploadsConfig,
     ) {
         entityManager.getEventManager().registerSubscriber(this);
     }
