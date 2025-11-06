a568681bf2aa109a7911450f76d73f8c7cc56f22
Thomas Dax <thomas.dax@vivid-planet.com>
2025-11-06 23:07:36 +0100

    Remove now unused userContentScopesSkipManual from graphql query

--- packages/admin/cms-admin/src/userPermissions/user/permissions/OverrideContentScopesDialog.tsx
+++ packages/admin/cms-admin/src/userPermissions/user/permissions/OverrideContentScopesDialog.tsx
@@ -76,7 +76,6 @@ export const OverrideContentScopesDialog = ({ permissionId, userId, handleDialog
                     overrideContentScopes
                     contentScopes
                 }
-                userContentScopesSkipManual: userPermissionsContentScopes(userId: $userId, skipManual: true)
             }
         `,
         {
