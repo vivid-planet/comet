4c452a1854bd08d2cc924bda867a83d86f8d04b3
Niko Sams <ns@vivid-planet.com>
2025-11-10 10:00:35 +0100

    Add mitmproxy as binary instead of running in docker (#4687)

--- packages/cli/src/comet.ts
+++ packages/cli/src/comet.ts
@@ -1,5 +1,6 @@
 import { Command } from "commander";
 
+import { downloadMitmproxyCommand } from "./commands/download-mitmproxy";
 import { downloadOAuth2ProxyCommand } from "./commands/download-oauth2-proxy";
 import { generateBlockTypes } from "./commands/generate-block-types";
 import { injectSiteConfigsCommand } from "./commands/site-configs";
@@ -9,5 +10,6 @@ const program = new Command();
 program.addCommand(generateBlockTypes);
 program.addCommand(injectSiteConfigsCommand);
 program.addCommand(downloadOAuth2ProxyCommand);
+program.addCommand(downloadMitmproxyCommand);
 
 program.parse();
