96f98dfa9ab5a98deb76870c48609f614d1debd7
Raphael Blum <raphael.blum@vivid-planet.com>
2025-11-11 15:53:51 +0100

    Remove comments

--- packages/api/cms-api/src/warnings/warning-checker.command.ts
+++ packages/api/cms-api/src/warnings/warning-checker.command.ts
@@ -131,6 +131,11 @@ export class WarningCheckerCommand extends CommandRunner {
                 }
 
                 offset += queryBuilderLimit;
+
+                // Flush every 10000 root blocks to reduce memory usage for large entities
+                if (offset % 10000 === 0) {
+                    await this.entityManager.flush();
+                }
             } while (rootBlocks.length > 0);
         }
         await this.entityManager.flush();
@@ -181,6 +186,7 @@ export class WarningCheckerCommand extends CommandRunner {
                 }
             }
         }
+        await this.entityManager.flush();
 
         // remove all warnings that are not present anymore
         await this.entityManager.nativeDelete(Warning, { updatedAt: { $lt: startDate } });
