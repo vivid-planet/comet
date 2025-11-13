9b7a035bb047a9cacf3448f0367b40f5553547a1
Raphael Blum <raphael.blum@vivid-planet.com>
2025-11-13 14:29:15 +0100

    Make demo-saas runnable

--- install.sh
+++ install.sh
@@ -13,11 +13,15 @@ pnpm install
 
 pnpm run copy-project-files
 
-# Lang install
+# Lang install demo
 sh ./demo/admin/intl-update.sh
 sh ./demo/api/intl-update.sh
 sh ./demo/site/intl-update.sh
 sh ./demo/site-pages/intl-update.sh
+
+# Lang install demo-saas
+sh ./demo-saas/admin/intl-update.sh
+sh ./demo-saas/api/intl-update.sh
 
 # Build the packages CLI and eslint-plugin to be used for dev startup
 pnpm --filter '@comet/cli' --filter '@comet/eslint-plugin' run build
