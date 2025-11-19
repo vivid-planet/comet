5d68470e013f6a6bc602ab1695fc36fcdba2065e
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-11-19 17:27:41 +0100

    split up types

--- packages/site/site-react/src/blocks/helpers/PlayPauseButton.tsx
+++ packages/site/site-react/src/blocks/helpers/PlayPauseButton.tsx
@@ -5,11 +5,14 @@ import styles from "./PlayPauseButton.module.scss";
 export interface PlayPauseButtonProps {
     isPlaying: boolean;
     onClick: () => void;
+}
+
+export interface ExtendedPlayPauseButtonProps extends PlayPauseButtonProps {
     ariaLabelPlay?: string;
     ariaLabelPause?: string;
 }
 
-export const PlayPauseButton = ({ isPlaying, onClick, ariaLabelPlay, ariaLabelPause }: PlayPauseButtonProps) => {
+export const PlayPauseButton = ({ isPlaying, onClick, ariaLabelPlay, ariaLabelPause }: ExtendedPlayPauseButtonProps) => {
     return (
         <button className={clsx(styles.button)} onClick={onClick} aria-label={isPlaying ? (ariaLabelPause ?? "Pause") : (ariaLabelPlay ?? "Play")}>
             <div className={clsx(styles.animatedPlayPause, !isPlaying && styles.animatedPlayPausePaused)} />
