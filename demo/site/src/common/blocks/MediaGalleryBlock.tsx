83355c014487b1f9e2f565859d78e6925bbf7cfb
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-11-19 12:48:48 +0100

    style media gallery according to current design

--- demo/site/src/common/blocks/MediaGalleryBlock.tsx
+++ demo/site/src/common/blocks/MediaGalleryBlock.tsx
@@ -1,14 +1,17 @@
 import "swiper/css";
 import "swiper/css/navigation";
 
-import { parseAspectRatio, type PropsWithData, withPreview } from "@comet/site-nextjs";
+import { type PropsWithData, withPreview } from "@comet/site-nextjs";
 import { type MediaGalleryBlockData } from "@src/blocks.generated";
 import { MediaBlock } from "@src/common/blocks/MediaBlock";
 import { Typography } from "@src/common/components/Typography";
 import { PageLayout } from "@src/layout/PageLayout";
-import { type CSSProperties } from "react";
-import { Pagination } from "swiper/modules";
+import clsx from "clsx";
+import { useEffect, useRef, useState } from "react";
+import { useIntl } from "react-intl";
+import { Navigation, Pagination } from "swiper/modules";
 import { SwiperSlide } from "swiper/react";
+import { type Swiper as SwiperClass } from "swiper/types";
 
 import { BasicSwiper } from "../components/BasicSwiper";
 import styles from "./MediaGalleryBlock.module.scss";
@@ -17,31 +20,54 @@ type MediaGalleryBlockProps = PropsWithData<MediaGalleryBlockData>;
 
 export const MediaGalleryBlock = withPreview(
     ({ data }: MediaGalleryBlockProps) => {
-        // Set aspect ratio as CSS variable for use in calculation in SCSS
-        const swiperStyle: CSSProperties = { "--aspect-ratio-numeric": parseAspectRatio(data.aspectRatio) };
+        const nextButtonRef = useRef<HTMLButtonElement | null>(null);
+        const prevButtonRef = useRef<HTMLButtonElement | null>(null);
+        const [swiper, setSwiper] = useState<SwiperClass | null>(null);
+
+        const intl = useIntl();
+
+        useEffect(() => {
+            if (!swiper) {
+                return;
+            }
+        }, [swiper]);
 
         return (
-            <BasicSwiper
-                className={styles.swiperWrapper}
-                style={swiperStyle}
-                slidesPerView={1}
-                slidesPerGroup={1}
-                modules={[Pagination]}
-                pagination={{ clickable: true }}
-                longSwipesRatio={0.1}
-                threshold={3}
-                allowTouchMove
-                watchOverflow
-            >
-                {data.items.blocks.map((block) => (
-                    <SwiperSlide key={block.key}>
-                        <MediaBlock data={block.props.media} aspectRatio={data.aspectRatio} />
-                        <Typography variant="p200" className={styles.mediaCaption}>
-                            {block.props.caption}
-                        </Typography>
-                    </SwiperSlide>
-                ))}
-            </BasicSwiper>
+            <>
+                <BasicSwiper
+                    className={styles.swiperWrapper}
+                    slidesPerView={1}
+                    slidesPerGroup={1}
+                    modules={[Pagination, Navigation]}
+                    pagination={{ clickable: true }}
+                    navigation={{ prevEl: prevButtonRef.current, nextEl: nextButtonRef.current }}
+                    longSwipesRatio={0.1}
+                    threshold={3}
+                    allowTouchMove
+                    watchOverflow
+                    speed={400}
+                    onSwiper={setSwiper}
+                >
+                    {data.items.blocks.map((block) => (
+                        <SwiperSlide key={block.key}>
+                            <MediaBlock data={block.props.media} aspectRatio={data.aspectRatio} />
+                            <Typography variant="p200" className={styles.mediaCaption}>
+                                {block.props.caption}
+                            </Typography>
+                        </SwiperSlide>
+                    ))}
+                </BasicSwiper>
+                <button
+                    ref={prevButtonRef}
+                    className={clsx(styles.navigationButton, "media-gallery-prev")}
+                    aria-label={intl.formatMessage({ id: "mediaGalleryBlock.prevSlide", defaultMessage: "Previous slide" })}
+                />
+                <button
+                    ref={nextButtonRef}
+                    className={clsx(styles.navigationButton, styles["navigationButton--next"], "media-gallery-next")}
+                    aria-label={intl.formatMessage({ id: "mediaGalleryBlock.nextSlide", defaultMessage: "Next slide" })}
+                />
+            </>
         );
     },
     { label: "MediaGallery" },
