f4cbe264c6f21cdfb8ff48f06142039d2d67de59
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2025-11-20 11:18:08 +0100

    Remove Demo Site Pages (#4781)

--- demo/site-pages/src/pages/api/status.tsx
+++
@@ -1,7 +0,0 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
-
-const Status = (req: NextApiRequest, res: NextApiResponse): void => {
-    res.statusCode = 200;
-    res.send("OK");
-};
-export default Status;
