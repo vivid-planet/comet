89cb5f7c673ef0e7ee00aeba772e1a67bf389a8e
max-debug022 <maximilian.pelka@vivid-planet.com>
2025-11-24 10:32:27 +0100

    Demo: Update content security headers (#3640)
    Co-authored-by: Johannes Obermair <johannes.obermair@vivid-planet.com>

--- demo/site/next.config.mjs
+++ demo/site/next.config.mjs
@@ -24,6 +24,47 @@ const nextConfig = {
     experimental: {
         optimizePackageImports: ["@comet/site-nextjs"],
     },
+    poweredByHeader: false,
+    // https://nextjs.org/docs/advanced-features/security-headers (Content-Security-Policy and CORS are set in middleware/cspHeaders.ts)
+    headers: async () => [
+        {
+            source: "/:path*",
+            headers: [
+                {
+                    key: "Strict-Transport-Security", // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+                    value: "max-age=63072000; includeSubDomains; preload", // 2 years (recommended when subdomains are included)
+                },
+                {
+                    key: "Cross-Origin-Opener-Policy", // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Opener-Policy
+                    value: "same-origin", // Only allow the same origin to open the page in a browsing context
+                },
+                {
+                    key: "Cross-Origin-Embedder-Policy",
+                    // This value should be set to 'require-corp' as soon as iframe credentialless is supported by all browsers
+                    // https://developer.mozilla.org/en-US/docs/Web/Security/IFrame_credentialless
+                    // https://caniuse.com/mdn-html_elements_iframe_credentialless
+                    value: "unsafe-none",
+                },
+                {
+                    key: "Cross-Origin-Resource-Policy", // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Resource-Policy
+                    value: "same-site", // Do not allow cross-origin requests to access the response
+                },
+                {
+                    key: "Permissions-Policy", // https://developer.mozilla.org/en-US/docs/Web/HTTP/Permissions_Policy
+                    value: "",
+                },
+                {
+                    key: "X-Content-Type-Options", // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+                    value: "nosniff", // Prevent MIME sniffing
+                },
+                {
+                    // This should be changed when using web analytics tools. For example, use "strict-origin-when-cross-origin" for Google Analytics
+                    key: "Referrer-Policy", // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+                    value: "same-origin", // Only use referer on own domain.
+                },
+            ],
+        },
+    ],
     cacheHandler: process.env.REDIS_ENABLED === "true" ? import.meta.resolve("./dist/cache-handler.js").replace("file://", "") : undefined,
     cacheMaxMemorySize: process.env.REDIS_ENABLED === "true" ? 0 : undefined, // disable default in-memory caching
     rewrites: () => {
