94e17d947a03bc56abb449d9d045950cae7bc96d
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-11-24 08:00:21 +0100

    undo changes for PlayPauseButtonProps

--- packages/site/site-react/src/blocks/helpers/PlayPauseButton.tsx
+++ packages/site/site-react/src/blocks/helpers/PlayPauseButton.tsx
@@ -5,14 +5,11 @@ import styles from "./PlayPauseButton.module.scss";
 export interface PlayPauseButtonProps {
     isPlaying: boolean;
     onClick: () => void;
-}
-
-interface ExtendedPlayPauseButtonProps extends PlayPauseButtonProps {
     ariaLabelPlay?: string;
     ariaLabelPause?: string;
 }
 
-export const PlayPauseButton = ({ isPlaying, onClick, ariaLabelPlay, ariaLabelPause }: ExtendedPlayPauseButtonProps) => {
+export const PlayPauseButton = ({ isPlaying, onClick, ariaLabelPlay, ariaLabelPause }: PlayPauseButtonProps) => {
     return (
         <button className={clsx(styles.button)} onClick={onClick} aria-label={isPlaying ? (ariaLabelPause ?? "Pause") : (ariaLabelPlay ?? "Play")}>
             <div className={clsx(styles.animatedPlayPause, !isPlaying && styles.animatedPlayPausePaused)} />
