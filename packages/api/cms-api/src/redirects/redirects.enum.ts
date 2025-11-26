ac2ed99871cc6d79f748cfd663f6ac41ad462e25
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-11-26 10:44:58 +0100

    add domain as value for the redirects enum

--- packages/api/cms-api/src/redirects/redirects.enum.ts
+++ packages/api/cms-api/src/redirects/redirects.enum.ts
@@ -2,6 +2,7 @@ import { registerEnumType } from "@nestjs/graphql";
 
 export enum RedirectSourceTypeValues {
     "path" = "path",
+    "domain" = "domain",
 }
 registerEnumType(RedirectSourceTypeValues, { name: "RedirectSourceTypeValues" });
 
