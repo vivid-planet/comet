bcf2c00d46869c64fb9cf96a3050b38750b5f98a
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-11-27 10:53:42 +0100

    reduce to one state

--- packages/admin/cms-admin/src/common/dataGrid/DataGrid.tsx
+++ packages/admin/cms-admin/src/common/dataGrid/DataGrid.tsx
@@ -2,39 +2,29 @@ import { DataGrid as MuiDataGrid, type DataGridProps } from "@mui/x-data-grid";
 import { type FC, useEffect, useState } from "react";
 
 export const DataGrid: FC<DataGridProps> = (props) => {
-    const [ProComponent, setProComponent] = useState<null | FC<DataGridProps>>(null);
-    const [PremiumComponent, setPremiumComponent] = useState<null | FC<DataGridProps>>(null);
+    const [Component, setComponent] = useState<FC<DataGridProps>>(MuiDataGrid);
 
     useEffect(() => {
         let isMounted = true;
         import("@mui/x-data-grid-premium")
             .then((module) => {
                 if (isMounted && module.DataGridPremium) {
-                    setPremiumComponent(() => module.DataGridPremium as unknown as FC<DataGridProps>);
+                    setComponent(() => module.DataGridPremium as unknown as FC<DataGridProps>);
                 }
             })
             .catch(() => {
-                setPremiumComponent(null);
                 import("@mui/x-data-grid-pro")
                     .then((module) => {
                         if (isMounted && module.DataGridPro) {
-                            setProComponent(() => module.DataGridPro as unknown as FC<DataGridProps>);
+                            setComponent(() => module.DataGridPro as unknown as FC<DataGridProps>);
                         }
                     })
-                    .catch(() => {
-                        setProComponent(null);
-                    });
+                    .catch(() => {});
             });
         return () => {
             isMounted = false;
         };
     }, []);
 
-    if (PremiumComponent) {
-        return <PremiumComponent {...props} />;
-    }
-    if (ProComponent) {
-        return <ProComponent {...props} />;
-    }
-    return <MuiDataGrid {...props} />;
+    return <Component {...props} />;
 };
