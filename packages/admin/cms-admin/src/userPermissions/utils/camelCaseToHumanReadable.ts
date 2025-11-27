735b8935a70c6de6781b9f310698b10875f8e4af
Harald Müller <harald.mueller@vivid-planet.com>
2025-11-27 15:17:22 +0100

    Fix: Format Shortcuts correctly for camelCaseToHumanReadable

--- packages/admin/cms-admin/src/userPermissions/utils/camelCaseToHumanReadable.ts
+++ packages/admin/cms-admin/src/userPermissions/utils/camelCaseToHumanReadable.ts
@@ -1,4 +1,9 @@
 export function camelCaseToHumanReadable(s: string | number) {
-    const words = s.toString().match(/[A-Za-z0-9][a-z0-9]*/g) || [];
+    const words =
+        s
+            .toString()
+            .replace(/([A-Z0-9]+)([A-Z][a-z])/g, "$1 $2") // "XYZTest" -> "XYZ Test"
+            .replace(/([a-z])([A-Z])/g, "$1 $2") // "xyzTest" -> "xyz Test"
+            .split(/\s+/) || [];
     return words.map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(" ");
 }
