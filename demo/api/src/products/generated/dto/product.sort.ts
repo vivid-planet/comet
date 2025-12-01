90d6896156d263d49ed5e22fd0d55fc91cd2fd32
Niko Sams <ns@vivid-planet.com>
2025-12-01 13:16:43 +0100

    run api-generator

--- demo/api/src/products/generated/dto/product.sort.ts
+++ demo/api/src/products/generated/dto/product.sort.ts
@@ -40,13 +40,11 @@ export enum ProductSortField {
     manufacturer_addressAsEmbeddable_alternativeAddress_zip = "manufacturer_addressAsEmbeddable_alternativeAddress_zip",
     manufacturer_addressAsEmbeddable_alternativeAddress_country = "manufacturer_addressAsEmbeddable_alternativeAddress_country",
     manufacturer_updatedAt = "manufacturer_updatedAt",
-    priceList_id = "priceList_id",
     priceList_name = "priceList_name",
     priceList_mimetype = "priceList_mimetype",
     priceList_contentHash = "priceList_contentHash",
     priceList_createdAt = "priceList_createdAt",
-    priceList_updatedAt = "priceList_updatedAt",
-    priceList_expiresAt = "priceList_expiresAt"
+    priceList_updatedAt = "priceList_updatedAt"
 }
 registerEnumType(ProductSortField, {
     name: "ProductSortField",
