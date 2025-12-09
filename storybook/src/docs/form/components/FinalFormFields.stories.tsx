f9f20be8e09c5415c89b061f1114a41548f58542
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-12-09 15:17:24 +0100

    add read only and disabled variants to story

--- storybook/src/docs/form/components/FinalFormFields.stories.tsx
+++ storybook/src/docs/form/components/FinalFormFields.stories.tsx
@@ -36,10 +36,69 @@ export const _FinalFormInput = {
                 }}
             >
                 <Field component={FinalFormInput} name="text" label="Text" placeholder="Some Text" fullWidth required />
+                <Field
+                    component={FinalFormInput}
+                    name="textDisabled"
+                    label="Text (disabled)"
+                    placeholder="Some Text"
+                    fullWidth
+                    disabled
+                    defaultValue="disabled"
+                />
+                <Field
+                    component={FinalFormInput}
+                    name="textReadOnly"
+                    label="Text (read only)"
+                    placeholder="Some Text"
+                    defaultValue="read only"
+                    fullWidth
+                    readOnly
+                />
                 <Field component={FinalFormInput} name="textOptional" label="Text (optional)" placeholder="Some Text" fullWidth />
                 <Field component={FinalFormInput} name="number" label="Number" type="number" placeholder="12" fullWidth required />
+                <Field
+                    component={FinalFormInput}
+                    name="numberReadOnly"
+                    label="Number (read only)"
+                    type="number"
+                    placeholder="12"
+                    fullWidth
+                    readOnly
+                    defaultValue="12"
+                />
+                <Field
+                    component={FinalFormInput}
+                    name="numberDisabled"
+                    label="Number (disabled)"
+                    type="number"
+                    placeholder="12"
+                    fullWidth
+                    disabled
+                    defaultValue="12"
+                />
                 <Field component={FinalFormInput} name="numberOptional" label="Number (optional)" type="number" placeholder="12" fullWidth />
                 <Field component={FinalFormInput} name="email" label="Email" type="email" placeholder="john.doe@example.com" fullWidth required />
+                <Field
+                    component={FinalFormInput}
+                    name="emailReadOnly"
+                    label="Email (read only)"
+                    type="email"
+                    placeholder="john.doe@example.com"
+                    fullWidth
+                    readOnly
+                    defaultValue="john.doe@example.com"
+                />
+                <Field
+                    component={FinalFormInput}
+                    name="emailDisabled"
+                    label="Email (disabled)"
+                    type="email"
+                    placeholder="john.doe@example.com"
+                    fullWidth
+                    disabled
+                    defaultValue="john.doe@example.com"
+                />
+
                 <Field
                     component={FinalFormInput}
                     name="emailOptional"
@@ -49,6 +108,27 @@ export const _FinalFormInput = {
                     fullWidth
                 />
                 <Field component={FinalFormInput} name="password" label="Password" type="password" placeholder="Password" fullWidth required />
+                <Field
+                    component={FinalFormInput}
+                    name="password"
+                    label="Password (read only)"
+                    type="password"
+                    placeholder="Password"
+                    fullWidth
+                    readOnly
+                    defaultValue="Password"
+                />
+                <Field
+                    component={FinalFormInput}
+                    name="passwordDisabled"
+                    label="Password (disabled)"
+                    type="password"
+                    placeholder="Password"
+                    fullWidth
+                    disabled
+                    defaultValue="Password"
+                />
+
                 <Field
                     component={FinalFormInput}
                     name="passwordOptional"
@@ -74,7 +154,26 @@ export const _FinalFormSearchTextField = {
                     alert(JSON.stringify(values, null, 4));
                 }}
             >
-                <Field name="search" label="FinalFormSearchTextField" component={FinalFormSearchTextField} fullWidth />
+                <Field name="search" label="FinalFormSearchTextField" component={FinalFormSearchTextField} fullWidth required />
+
+                <Field
+                    name="searchReadOnly"
+                    label="FinalFormSearchTextField (read only)"
+                    defaultValue="read only value"
+                    component={FinalFormSearchTextField}
+                    fullWidth
+                    readOnly
+                />
+                <Field
+                    name="searchDisabled"
+                    label="FinalFormSearchTextField (disabled)"
+                    defaultValue="disabled value"
+                    component={FinalFormSearchTextField}
+                    fullWidth
+                    disabled
+                />
+                <Field name="searchOptional" label="FinalFormSearchTextField (optional)" component={FinalFormSearchTextField} fullWidth />
+
                 <Button type="submit">Submit</Button>
             </FinalForm>
         );
@@ -131,6 +230,28 @@ export const _FinalFormAutocomplete = {
                     getOptionLabel={(option: Option) => option.label}
                     isOptionEqualToValue={(option: Option, value: Option) => option.value === value.value}
                     options={options}
+                    name="autocompleteReadOnly"
+                    label="Autocomplete (read only)"
+                    fullWidth
+                    readOnly
+                />
+
+                <Field
+                    component={FinalFormAutocomplete}
+                    getOptionLabel={(option: Option) => option.label}
+                    isOptionEqualToValue={(option: Option, value: Option) => option.value === value.value}
+                    options={options}
+                    name="autocompleteDisabled"
+                    label="Autocomplete (disabled)"
+                    fullWidth
+                    disabled
+                />
+
+                <Field
+                    component={FinalFormAutocomplete}
+                    getOptionLabel={(option: Option) => option.label}
+                    isOptionEqualToValue={(option: Option, value: Option) => option.value === value.value}
+                    options={options}
                     name="autocompleteOptional"
                     label="Autocomplete (optional)"
                     fullWidth
@@ -168,6 +289,32 @@ export const _FinalFormAutocomplete = {
                     label="Autocomplete multiple select"
                     fullWidth
                     required
+                />
+                <Field
+                    component={FinalFormAutocomplete}
+                    multiple
+                    getOptionLabel={(option: Option) => option.label}
+                    isOptionEqualToValue={(option: Option, value: Option) => option.value === value.value}
+                    options={options}
+                    name="autocompleteMultipleReadOnly"
+                    label="Autocomplete multiple select (read only)"
+                    fullWidth
+                    required
+                    defaultValue={initialValues.autocompleteMultiple}
+                    readOnly
+                />
+                <Field
+                    component={FinalFormAutocomplete}
+                    multiple
+                    getOptionLabel={(option: Option) => option.label}
+                    isOptionEqualToValue={(option: Option, value: Option) => option.value === value.value}
+                    options={options}
+                    name="autocompleteMultipleDisabled"
+                    label="Autocomplete multiple select (disabled)"
+                    fullWidth
+                    required
+                    defaultValue={initialValues.autocompleteMultiple}
+                    disabled
                 />
                 <Field
                     component={FinalFormAutocomplete}
