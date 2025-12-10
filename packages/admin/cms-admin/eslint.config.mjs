fd0bf02b56d7aa176103cd576e462f96b8350571
Ricky Smith <jamesricky@me.com>
2025-12-10 08:07:32 +0100

    Make cms-admin test-utils consistent with admin

--- packages/admin/cms-admin/eslint.config.mjs
+++ packages/admin/cms-admin/eslint.config.mjs
@@ -1,5 +1,9 @@
 import eslintConfigReact from "@comet/eslint-config/future/react.js";
 
+const importedObjectWithNoRestrictedImports = eslintConfigReact.find((config) => config.rules?.["no-restricted-imports"]);
+const [importedNoRestrictedImportsType, importedNoRestrictedImportsPathsObject] =
+    importedObjectWithNoRestrictedImports.rules["no-restricted-imports"];
+
 /** @type {import('eslint')} */
 const config = [
     {
@@ -9,6 +13,19 @@ const config = [
     {
         rules: {
             "@comet/no-other-module-relative-import": "off",
+            "no-restricted-imports": [
+                importedNoRestrictedImportsType,
+                {
+                    ...importedNoRestrictedImportsPathsObject,
+                    paths: [
+                        ...importedNoRestrictedImportsPathsObject.paths,
+                        {
+                            name: "@testing-library/react",
+                            message: 'Please import from "test-utils" instead.',
+                        },
+                    ],
+                },
+            ],
         },
     },
 ];
--- packages/admin/cms-admin/src/testing/test-utils.tsx
+++ packages/admin/cms-admin/src/testing/test-utils.tsx
@@ -1,5 +1,7 @@
+// Inspired by https://testing-library.com/docs/react-testing-library/setup/#custom-render
 import { MuiThemeProvider, SnackbarProvider } from "@comet/admin";
 import { createTheme } from "@mui/material";
+// eslint-disable-next-line no-restricted-imports
 import { render as testingLibraryRender, type RenderOptions, type RenderResult } from "@testing-library/react";
 import { type ReactElement, type ReactNode } from "react";
 import { IntlProvider } from "react-intl";
@@ -17,6 +19,10 @@ function DefaultWrapper({ children }: { children?: ReactNode }) {
     );
 }
 
-export function render(ui: ReactElement, options?: Omit<RenderOptions, "queries">): RenderResult {
+function customRender(ui: ReactElement, options?: Omit<RenderOptions, "queries">): RenderResult {
     return testingLibraryRender(ui, { wrapper: DefaultWrapper, ...options });
 }
+
+// eslint-disable-next-line no-restricted-imports
+export * from "@testing-library/react";
+export { customRender as render };
