517a510f0b522790fe78c87b4882880410cb3da6
Raphael Blum <raphael.blum@vivid-planet.com>
2025-12-15 11:33:15 +0100

    Use id for getContentScopesForUsers instead of name

--- demo-saas/api/src/auth/access-control.service.ts
+++ demo-saas/api/src/auth/access-control.service.ts
@@ -25,11 +25,17 @@ export class AccessControlService extends AbstractAccessControlService {
             return availablePermissions.filter((permission) => !deniedPermissions.includes(permission)).map((permission) => ({ permission }));
         }
     }
-    getContentScopesForUser(user: User): ContentScopesForUser {
+    async getContentScopesForUser(user: User): Promise<ContentScopesForUser> {
         if (user.isAdmin) {
             return UserPermissions.allContentScopes;
         } else {
-            return [{ department: "main" }];
+            // TODO: instead of randomly taking one, take the departments the user is assigned to
+            // Needs to be implemented when user assignment is implemented
+            const departments = await this.entityManager.find(Department, {}, { limit: 1 });
+            if (departments.length > 0) {
+                return [{ department: departments[0].id }];
+            }
+            return [];
         }
     }
 
