0651b3b55bd1cf3263cb4c59f1a0d18177d98fd4
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-12-16 10:22:34 +0100

    add SelectField styling

--- demo/site/src/common/components/form/SelectField.tsx
+++ demo/site/src/common/components/form/SelectField.tsx
@@ -1,6 +1,9 @@
 import { SvgUse } from "@src/common/helpers/SvgUse";
 import { useState } from "react";
 
+import { Typography } from "../Typography";
+import styles from "./SelectField.module.scss";
+
 interface SelectFieldProps {
     label: React.ReactNode;
     required?: boolean;
@@ -34,27 +37,46 @@ export function SelectField({
     };
 
     return (
-        <div>
-            <div>
-                {label}
-                {!required && <span>(optional)</span>}
-            </div>
-            <div>
-                <button type="button" onClick={handleToggle}>
-                    <span>{selectedOption?.label || placeholder}</span>
-                    <SvgUse href="/assets/icons/chevron-down.svg#root" width={16} height={16} />
+        <div className={styles.select}>
+            <label className={styles.selectWrapper}>
+                <Typography as="span" variant="paragraph300" className={styles.inputLabel}>
+                    {label}
+                </Typography>
+                {!required && (
+                    <Typography as="span" variant="paragraph200" color="var(--text-secondary)">
+                        (optional)
+                    </Typography>
+                )}
+            </label>
+            <div className={styles.selectWrapper}>
+                <button type="button" onClick={handleToggle} className={styles.selectField}>
+                    <span className={selectedOption ? styles.selectedText : styles.placeholderText}>{selectedOption?.label || placeholder}</span>
+                    <SvgUse
+                        href="/assets/icons/chevron-down.svg#root"
+                        width={16}
+                        height={16}
+                        className={`${styles.chevron} ${isOpen ? styles["chevron--expanded"] : ""}`}
+                    />
                 </button>
                 {isOpen && (
-                    <div>
+                    <div className={styles.dropdown}>
                         {options.map((option) => (
-                            <div key={option.value} onClick={() => handleSelectOption(option.value, option.label)}>
+                            <div
+                                key={option.value}
+                                onClick={() => handleSelectOption(option.value, option.label)}
+                                className={`${styles.dropdownOption} ${selectedOption?.value === option.value ? styles["dropdownOption--selected"] : ""}`}
+                            >
                                 {option.label}
                             </div>
                         ))}
                     </div>
                 )}
             </div>
-            {helperText && <div>{helperText}</div>}
+            {helperText && (
+                <div className={styles.helperText}>
+                    <Typography variant="paragraph200">{helperText}</Typography>
+                </div>
+            )}
         </div>
     );
 }
