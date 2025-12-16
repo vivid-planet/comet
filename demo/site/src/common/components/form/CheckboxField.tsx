23520e8bdcb39a73485ab58075e22bad3c5750a7
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-12-16 10:22:51 +0100

    add CheckboxField styling

--- demo/site/src/common/components/form/CheckboxField.tsx
+++ demo/site/src/common/components/form/CheckboxField.tsx
@@ -1,17 +1,35 @@
+import { Typography } from "../Typography";
+import styles from "./CheckboxField.module.scss";
+
 interface CheckboxFieldProps {
     label: React.ReactNode;
     helperText?: string;
     required?: boolean;
 }
 
-export function CheckboxField({ label, helperText }: CheckboxFieldProps): React.ReactElement {
+export function CheckboxField({ label, helperText, required }: CheckboxFieldProps): React.ReactElement {
     return (
         <div>
-            <div>
-                <input type="checkbox" />
-                {label}
+            <div className={styles.checkboxWrapper}>
+                <input type="checkbox" className={styles.checkboxInput} />
+                <label>
+                    <Typography as="span" variant="paragraph300" className={styles.checkboxLabel}>
+                        {label}
+                    </Typography>
+                    {!required && (
+                        <span>
+                            <Typography as="span" variant="paragraph200" color="var(--text-secondary)">
+                                (optional)
+                            </Typography>
+                        </span>
+                    )}
+                </label>
             </div>
-            {helperText && <div>{helperText}</div>}
+            {helperText && (
+                <div className={styles.helperText}>
+                    <Typography variant="paragraph200">{helperText}</Typography>
+                </div>
+            )}
         </div>
     );
 }
