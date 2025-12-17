607c9facdbeb3b24a22fc1cdbe800bd4df038ea5
Abel Castro <abel.castro@vivid-planet.com>
2025-12-17 11:18:09 +0100

    Add input-testid to SelectField

--- packages/admin/admin/src/form/fields/SelectField.test.tsx
+++ packages/admin/admin/src/form/fields/SelectField.test.tsx
@@ -4,7 +4,7 @@ import { Form } from "react-final-form";
 import { SelectField } from "./SelectField";
 
 describe("SelectField", () => {
-    it("should render data-testid on the component when setting it in the root prop dataTestId", () => {
+    it("the prop data-testid should render a data-testid on component root", () => {
         render(
             <Form onSubmit={jest.fn()}>
                 {() => (
@@ -21,6 +21,27 @@ describe("SelectField", () => {
         );
 
         const selectField = screen.getByTestId("test-select");
+        expect(selectField).toBeInTheDocument();
+        expect(selectField.tagName).toBe("DIV");
+    });
+
+    it("the prop input-testid should render a data-testid on component hidden input", () => {
+        render(
+            <Form onSubmit={jest.fn()}>
+                {() => (
+                    <SelectField
+                        name="test"
+                        input-testid="test-select-input"
+                        options={[
+                            { label: "Option 1", value: "1" },
+                            { label: "Option 2", value: "2" },
+                        ]}
+                    />
+                )}
+            </Form>,
+        );
+
+        const selectField = screen.getByTestId("test-select-input");
         expect(selectField).toBeInTheDocument();
         expect(selectField.tagName).toBe("INPUT");
     });
