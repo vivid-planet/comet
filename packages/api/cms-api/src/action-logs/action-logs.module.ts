01a52b7eedd46117505e28edfb3a7873e99782ca
Alexander Kaufmann <ak@vivid-planet.com>
2025-12-19 06:32:26 +0100

    fix: add missing ActionLogsService to providers

--- packages/api/cms-api/src/action-logs/action-logs.module.ts
+++ packages/api/cms-api/src/action-logs/action-logs.module.ts
@@ -3,6 +3,7 @@ import { type AnyEntity } from "@mikro-orm/postgresql";
 import { type DynamicModule, type Type } from "@nestjs/common";
 
 import { ActionLogsResolverFactory } from "./action-logs.resolver.factory";
+import { ActionLogsService } from "./action-logs.service";
 import { ActionLogsSubscriber } from "./action-logs.subscriber";
 import { ActionLogsFeatureModule } from "./action-logs-feature.module";
 import { ActionLog } from "./entities/action-log.entity";
@@ -12,7 +13,7 @@ export class ActionLogsModule {
         return {
             module: ActionLogsModule,
             imports: [MikroOrmModule.forFeature([ActionLog]), ActionLogsFeatureModule],
-            providers: [ActionLogsSubscriber],
+            providers: [ActionLogsSubscriber, ActionLogsService],
         };
     }
 
