f2062ed482d87d9473d19478e93d30633733d8fa
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2025-12-22 10:52:18 +0100

    Migrate `@comet/cms-admin` tests to Vitest (#4973)

--- packages/admin/cms-admin/src/blocks/createImageLinkBlock.test.tsx
+++ packages/admin/cms-admin/src/blocks/createImageLinkBlock.test.tsx
@@ -1,9 +1,14 @@
+import { describe, expect, it } from "vitest";
+
+import { createImageLinkBlock } from "./createImageLinkBlock";
+import { InternalLinkBlock } from "./InternalLinkBlock";
+import { PixelImageBlock } from "./PixelImageBlock";
+
 describe("createImageLinkBlock", () => {
-    it.skip("should allow overriding the name", () => {
-        // TODO fix Jest setup to support ESM imports
-        // expect(createImageLinkBlock({ image: PixelImageBlock, link: InternalLinkBlock }).name).toEqual("ImageLink");
-        // expect(createImageLinkBlock({ image: PixelImageBlock, link: InternalLinkBlock, name: "MyCustomImageLink" }).name).toEqual(
-        //     "MyCustomImageLink",
-        // );
+    it("should allow overriding the name", () => {
+        expect(createImageLinkBlock({ image: PixelImageBlock, link: InternalLinkBlock }).name).toEqual("ImageLink");
+        expect(createImageLinkBlock({ image: PixelImageBlock, link: InternalLinkBlock, name: "MyCustomImageLink" }).name).toEqual(
+            "MyCustomImageLink",
+        );
     });
 });
