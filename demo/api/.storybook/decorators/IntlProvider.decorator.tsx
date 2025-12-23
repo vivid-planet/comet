88fe20b4b68641af87c6e75436a61b151a5f96f7
Ricky Smith <jamesricky@me.com>
2025-12-23 08:32:26 +0100

    Remove unnecessary messages from admin

--- demo/api/.storybook/decorators/IntlProvider.decorator.tsx
+++ demo/api/.storybook/decorators/IntlProvider.decorator.tsx
@@ -19,35 +19,8 @@ const dateFnsLocales: Record<LocaleOption, DateFnsLocale> = {
 
 // @TODO: use messages from lang-package
 const messages: Record<LocaleOption, Record<string, string>> = {
-    en: {
-        "comet.core.deleteMutation.promptDelete": "Delete data?",
-        "comet.core.deleteMutation.yes": "Yes",
-        "comet.core.deleteMutation.no": "No",
-        "comet.core.dirtyHandler.discardChanges": "Discard unsaved changes?",
-        "comet.core.editDialog.edit": "Edit",
-        "comet.core.editDialog.add": "Add",
-        "comet.core.editDialog.cancel": "Cancel",
-        "comet.core.editDialog.save": "Save",
-        "comet.core.finalForm.abort": "Cancel",
-        "comet.core.finalForm.save": "Save",
-        "comet.core.router.confirmationDialog.confirm": "OK",
-        "comet.core.router.confirmationDialog.abort": "Cancel",
-        "comet.core.stack.stack.back": "Back",
-        "comet.core.table.addButton": "Add",
-        "comet.core.table.excelExportButton": "Export",
-        "comet.core.table.deleteButton": "Delete",
-        "comet.core.table.pagination.pageInfo": "Page {current} of {total}",
-        "comet.core.table.localChangesToolbar.save": "Save",
-        "comet.core.table.localChangesToolbar.unsavedItems":
-            "{count, plural, =0 {no unsaved changes} one {# unsaved change} other {# unsaved changes}}",
-        "comet.core.table.tableFilterFinalForm.resetButton": "Reset Filter",
-        "comet.core.table.tableQuery.error": "Error :( {error}",
-    },
-    de: {
-        "comet.core.table.localChangesToolbar.unsavedItems":
-            "{count, plural, =0 {keine ungespeicherten Änderungen} one {# ungespeicherte Änderung} other {# ungespeicherte Änderungen}}",
-        "comet.core.table.tableQuery.error": "Fehler :( {error}",
-    },
+    en: {},
+    de: {},
 };
 
 export const IntlDecorator: Decorator = (fn, context) => {
