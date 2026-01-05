0f9694dad37e7d57d93f3672cf3f86c190f782b6
Thomas Dax <thomas.dax@vivid-planet.com>
2026-01-05 15:50:23 +0100

    Copy blocks from @comet/mail-rendering package in brevo repo

---
+++ packages/mail-react/src/blocks/helpers/imageBlockHelpers.ts
@@ -0,0 +1,55 @@
+import { ImageLoaderProps } from "next/image";
+
+export const validSizes: number[] = (process.env.DAM_ALLOWED_IMAGE_SIZES as string)
+    .split(",")
+    .map((value) => parseInt(value))
+    .sort((a, b) => a - b);
+
+export const getDamAllowedImageWidth = (minimumWidth: number, contentWidth: number): number => {
+    let width: number | null = null;
+    const largestPossibleWidth = validSizes[validSizes.length - 1];
+
+    validSizes.forEach((validWidth) => {
+        if (minimumWidth === contentWidth) {
+            width = contentWidth * 2;
+        } else if (!width && validWidth >= minimumWidth * 2) {
+            width = validWidth;
+        }
+    });
+
+    if (!width) {
+        return largestPossibleWidth;
+    }
+
+    return width;
+};
+
+interface ImageDimensions {
+    width: number;
+    height: number;
+}
+
+export function calculateInheritAspectRatio(
+    imageDimensions: ImageDimensions,
+    cropArea: {
+        focalPoint: "SMART" | "CENTER" | "NORTHWEST" | "NORTHEAST" | "SOUTHWEST" | "SOUTHEAST";
+        width?: number;
+        height?: number;
+        x?: number;
+        y?: number;
+    },
+): number {
+    if (cropArea.focalPoint === "SMART") {
+        return imageDimensions.width / imageDimensions.height;
+    } else {
+        if (cropArea.width === undefined || cropArea.height === undefined) {
+            throw new Error("Missing crop dimensions");
+        }
+
+        return (cropArea.width * imageDimensions.width) / 100 / ((cropArea.height * imageDimensions.height) / 100);
+    }
+}
+
+export function generateImageUrl({ src, width }: Pick<ImageLoaderProps, "src" | "width">, aspectRatio: number): string {
+    return src.replace("$resizeWidth", String(width)).replace("$resizeHeight", String(Math.ceil(width / aspectRatio)));
+}
