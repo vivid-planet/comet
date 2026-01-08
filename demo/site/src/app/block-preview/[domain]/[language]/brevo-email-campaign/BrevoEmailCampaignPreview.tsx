254cbe8c329706a9f46ca6b49d1c24c9cda3dfa4
Thomas Dax <thomas.dax@vivid-planet.com>
2026-01-08 12:11:06 +0100

    Make the preview work

---
+++ demo/site/src/app/block-preview/[domain]/[language]/brevo-email-campaign/BrevoEmailCampaignPreview.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { createFetchInMemoryCache, useIFrameBridge } from "@comet/site-nextjs";
+import { renderToMjml } from "@faire/mjml-react/utils/renderToMjml";
+import { generateMjmlMailContent } from "@src/app/brevo-email-campaign/generateMjmlMailContent";
+import { type EmailCampaignContentBlockData } from "@src/blocks.generated";
+import { RenderedMail } from "@src/brevo/components/RenderedMail";
+import { type ContentScope } from "@src/site-configs";
+import { withBlockPreview } from "@src/util/blockPreview";
+import { createGraphQLFetch } from "@src/util/graphQLClient";
+import { recursivelyLoadBlockData } from "@src/util/recursivelyLoadBlockData";
+import { useEffect, useState } from "react";
+import { type IntlConfig } from "react-intl";
+
+const cachingFetch = createFetchInMemoryCache(fetch);
+
+interface BrevoEmailCampaignPreviewProps {
+    language: string;
+    messages: IntlConfig["messages"];
+}
+
+function BrevoEmailCampaignPreviewComponent({ language, messages }: BrevoEmailCampaignPreviewProps) {
+    const iFrameBridge = useIFrameBridge();
+    const [blockData, setBlockData] = useState<EmailCampaignContentBlockData>();
+
+    useEffect(() => {
+        async function load() {
+            if (!iFrameBridge.block) {
+                setBlockData(undefined);
+                return;
+            }
+            const graphQLFetch = createGraphQLFetch({ fetch: cachingFetch });
+            const newData = await recursivelyLoadBlockData({
+                blockType: "EmailCampaignContent",
+                blockData: iFrameBridge.block,
+                graphQLFetch,
+                fetch: cachingFetch,
+                scope: iFrameBridge.contentScope as ContentScope,
+            });
+
+            setBlockData(newData.content);
+        }
+        load();
+    }, [iFrameBridge.block, iFrameBridge.contentScope]);
+
+    if (blockData === undefined) {
+        return null;
+    }
+
+    const mjmlContent = renderToMjml(generateMjmlMailContent(blockData, { locale: language, messages }));
+
+    return <RenderedMail mjmlContent={mjmlContent} />;
+}
+
+export const BrevoEmailCampaignPreview = withBlockPreview(BrevoEmailCampaignPreviewComponent);
--- demo/site/src/app/block-preview/[domain]/[language]/brevo-email-campaign/page.tsx
+++ demo/site/src/app/block-preview/[domain]/[language]/brevo-email-campaign/page.tsx
@@ -1,50 +1,9 @@
-"use client";
+import { loadMessages } from "@src/util/loadMessages";
 
-import { createFetchInMemoryCache, useIFrameBridge } from "@comet/site-nextjs";
-import { type EmailCampaignContentBlockData } from "@src/blocks.generated";
-import { type ContentScope } from "@src/site-configs";
-import { withBlockPreview } from "@src/util/blockPreview";
-import { createGraphQLFetch } from "@src/util/graphQLClient";
-import { recursivelyLoadBlockData } from "@src/util/recursivelyLoadBlockData";
-import { useEffect, useState } from "react";
-import type { IntlConfig } from "react-intl";
+import { BrevoEmailCampaignPreview } from "./BrevoEmailCampaignPreview";
 
-const cachingFetch = createFetchInMemoryCache(fetch);
+export default async function Page({ params: { language } }: { params: { language: string } }) {
+    const messages = await loadMessages(language);
 
-export default withBlockPreview((params: unknown) => {
-    const language = "en";
-    const iFrameBridge = useIFrameBridge();
-
-    const [blockData, setBlockData] = useState<EmailCampaignContentBlockData>();
-    const [messages, setMessages] = useState<IntlConfig["messages"]>();
-    useEffect(() => {
-        async function load() {
-            if (!iFrameBridge.block) {
-                setBlockData(undefined);
-                return;
-            }
-            const graphQLFetch = createGraphQLFetch({ fetch: cachingFetch });
-            const newData = await recursivelyLoadBlockData({
-                blockType: "EmailCampaignContent",
-                blockData: iFrameBridge.block,
-                graphQLFetch,
-                fetch: cachingFetch,
-                scope: iFrameBridge.contentScope as ContentScope,
-            });
-            // const messages = await loadMessages(language);
-
-            setBlockData(newData);
-            // setMessages(messages);
-        }
-        load();
-    }, [iFrameBridge.block, iFrameBridge.contentScope, messages]);
-
-    if (blockData === undefined) {
-        return null;
-    }
-
-    // const mjmlContent = generateMjmlMailContent(blockData, { locale: language, messages: {} });
-    return <div>Hallo preview</div>;
-
-    // return <RenderedMail mjmlContent={mjmlContent} />;
-});
+    return <BrevoEmailCampaignPreview language={language} messages={messages} />;
+}
