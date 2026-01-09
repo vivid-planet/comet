c836ef15c9d37d136b157a4b909e31645d4ff75e
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2026-01-09 10:24:26 +0100

    Revert "remove api folder from skipRewrite and move route handler to /[domain]/[language]"
    This reverts commit de39a3d2dc9b5acc9d0103a4a37d488960f8b1c1.

--- demo/site/src/middleware/skipRewrite.ts
+++ demo/site/src/middleware/skipRewrite.ts
@@ -4,7 +4,7 @@ import { type CustomMiddleware } from "./chain";
 
 export function withSkipRewriteMiddleware(middleware: CustomMiddleware) {
     const skipFiles = ["/favicon.ico", "/apple-icon.png", "/icon.svg", "/manifest.json"];
-    const skipPaths = ["/_next/static/", "/_next/image/", "/assets/"];
+    const skipPaths = ["/_next/static/", "/_next/image/", "/assets/", "/api/"];
 
     return async (request: NextRequest) => {
         if (skipFiles.some((file) => request.nextUrl.pathname === file)) {
