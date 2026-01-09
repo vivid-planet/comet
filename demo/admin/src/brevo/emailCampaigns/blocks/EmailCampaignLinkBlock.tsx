bb80782b80200164772cdd8e6e2d4c1217b18dd7
Thomas Dax <thomas.dax@vivid-planet.com>
2026-01-09 11:35:43 +0100

    Remove unnecessary EmailCampaignLinkBlock

--- demo/admin/src/brevo/emailCampaigns/blocks/EmailCampaignLinkBlock.tsx
+++
@@ -1,7 +0,0 @@
-import { createLinkBlock, ExternalLinkBlock } from "@comet/cms-admin";
-
-export const EmailCampaignLinkBlock = createLinkBlock({
-    supportedBlocks: {
-        external: ExternalLinkBlock,
-    },
-});
