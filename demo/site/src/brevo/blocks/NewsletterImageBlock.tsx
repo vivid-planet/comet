9dcd3751652d1c550371ae6ecc19fa4dbfc2cc27
Thomas Dax <thomas.dax@vivid-planet.com>
2026-01-20 12:41:58 +0100

    Remove ImageBlock from mail-react completely and move all necessary code to demo site

--- demo/site/src/brevo/blocks/NewsletterImageBlock.tsx
+++ demo/site/src/brevo/blocks/NewsletterImageBlock.tsx
@@ -1,5 +1,6 @@
-import { ImageBlock, type PropsWithData } from "@comet/mail-react";
-import { MjmlColumn, MjmlSection } from "@faire/mjml-react";
+import { type PropsWithData } from "@comet/mail-react";
+import { calculateInheritAspectRatio, generateImageUrl } from "@comet/site-nextjs";
+import { MjmlColumn, MjmlImage, MjmlSection } from "@faire/mjml-react";
 import { type NewsletterImageBlockData } from "@src/blocks.generated";
 
 interface NewsletterImageBlockProps extends PropsWithData<NewsletterImageBlockData> {
@@ -9,18 +10,66 @@ interface NewsletterImageBlockProps extends PropsWithData<NewsletterImageBlockDa
     baseUrl: string;
 }
 
-export const NewsletterImageBlock = ({ data, desktopRenderWidth, contentWidth, validSizes, baseUrl }: NewsletterImageBlockProps) => {
+export const NewsletterImageBlock = ({ data, desktopRenderWidth = 600, contentWidth = 600, validSizes, baseUrl }: NewsletterImageBlockProps) => {
+    const { damFile, cropArea, urlTemplate } = data.image;
+
+    if (!damFile?.image) {
+        return null;
+    }
+
+    const usedCropArea = cropArea ?? damFile.image.cropArea;
+    const usedAspectRatio = calculateInheritAspectRatio(damFile.image, usedCropArea);
+
+    const imageUrl: string = generateImageUrl(
+        {
+            width: getOptimalAllowedImageWidth(validSizes, desktopRenderWidth, contentWidth),
+            src: urlTemplate,
+        },
+        usedAspectRatio,
+    );
+
+    const desktopImageHeight = Math.round(desktopRenderWidth / usedAspectRatio);
+
     return (
         <MjmlSection>
             <MjmlColumn>
-                <ImageBlock
-                    data={data.image}
-                    desktopRenderWidth={desktopRenderWidth}
-                    contentWidth={contentWidth}
-                    validSizes={validSizes}
-                    baseUrl={baseUrl}
+                <MjmlImage
+                    src={isAbsoluteUrl(imageUrl) ? imageUrl : `${baseUrl}${imageUrl}`}
+                    // @ts-expect-error mjml-react expects a boolean but mjml2html requires "true" as string
+                    fluidOnMobile="true"
+                    cssClass="image-block"
+                    width={desktopRenderWidth}
+                    height={desktopImageHeight}
+                    alt={damFile.altText}
+                    title={damFile.title}
+                    padding={0}
                 />
             </MjmlColumn>
         </MjmlSection>
     );
+};
+
+function isAbsoluteUrl(url: string): boolean {
+    return !url.startsWith("/");
+}
+
+export const getOptimalAllowedImageWidth = (validSizes: number[], minimumWidth: number, contentWidth: number): number => {
+    const sortedValidSizes = validSizes.sort((a, b) => a - b);
+
+    let width: number | null = null;
+    const largestPossibleWidth = sortedValidSizes[sortedValidSizes.length - 1];
+
+    sortedValidSizes.forEach((validWidth) => {
+        if (minimumWidth === contentWidth) {
+            width = contentWidth * 2;
+        } else if (!width && validWidth >= minimumWidth * 2) {
+            width = validWidth;
+        }
+    });
+
+    if (!width) {
+        return largestPossibleWidth;
+    }
+
+    return width;
 };
--- packages/mail-react/src/index.ts
+++ packages/mail-react/src/index.ts
@@ -1,3 +1,2 @@
 export type { PropsWithData } from "./blocks/helpers/PropsWithData.js";
-export { ImageBlock } from "./blocks/ImageBlock.js";
 export { css } from "./utils/css.js";
