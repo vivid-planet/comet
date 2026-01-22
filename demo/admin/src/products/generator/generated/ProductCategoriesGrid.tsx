12466e4b828659d40613afde300f02f7d9166f5a
Niko Sams <ns@vivid-planet.com>
2026-01-22 10:13:42 +0100

    CrudContextMenu: add deleteType ("delete"|"remove") that changes menu item and dialog from delete to remove for non-destructive data (relations) (#4971)
    Co-authored-by: Thomas Dax <thomas.dax@vivid-planet.com>

--- demo/admin/src/products/generator/generated/ProductCategoriesGrid.tsx
+++ demo/admin/src/products/generator/generated/ProductCategoriesGrid.tsx
@@ -156,6 +156,7 @@ export function ProductCategoriesGrid() {
                                     });
                                 }}
                                 refetchQueries={[productCategoriesQuery]}
+                                deleteType="remove"
                             />
                         </>
                     );
