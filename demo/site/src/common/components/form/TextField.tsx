108bec4ab68aff6f2f6d4308e0cdbead6a14e2f7
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2026-02-03 13:11:36 +0100

    style text field using mixins

--- demo/site/src/common/components/form/TextField.tsx
+++ demo/site/src/common/components/form/TextField.tsx
@@ -1,6 +1,10 @@
+import { SvgUse } from "@src/common/helpers/SvgUse";
 import { type InputHTMLAttributes, type ReactNode } from "react";
 import { Controller, type ControllerProps, type FieldValues } from "react-hook-form";
 import { FormattedMessage } from "react-intl";
+
+import { Typography } from "../Typography";
+import styles from "./TextField.module.scss";
 
 type TextFieldProps<TFieldValues extends FieldValues> = Omit<InputHTMLAttributes<HTMLInputElement>, "name"> &
     Pick<ControllerProps<TFieldValues>, "name" | "control" | "rules"> & {
@@ -17,6 +21,7 @@ export const TextField = <TFieldValues extends FieldValues>({
     ...inputProps
 }: TextFieldProps<TFieldValues>) => {
     const required = !!rules?.required;
+
     return (
         <Controller
             name={name}
@@ -24,17 +29,26 @@ export const TextField = <TFieldValues extends FieldValues>({
             rules={rules}
             render={({ field, fieldState }) => (
                 <div>
-                    <label>
-                        {label}
+                    <div className={styles.labelWrapper}>
+                        <label className={styles.label}>{label}</label>
                         {!required && (
-                            <span>
-                                <FormattedMessage id="inputField.optional" defaultMessage="(optional)" />
+                            <span className={styles.optionalText}>
+                                <FormattedMessage id="inputField.optional" defaultMessage="optional" />
                             </span>
                         )}
-                    </label>
-                    <input required={required} {...inputProps} {...field} />
-                    {helperText && <div>{helperText}</div>}
-                    {fieldState.error?.message && <div style={{ color: "red" }}>{fieldState.error.message}</div>}
+                    </div>
+                    <Typography as="input" variant="paragraph200" {...inputProps} {...field} className={styles.inputField} />
+                    {helperText && (
+                        <Typography variant="paragraph200" className={styles.helperText}>
+                            {helperText}
+                        </Typography>
+                    )}
+                    {fieldState.error?.message && (
+                        <div className={styles.errorWrapper}>
+                            <SvgUse href="/assets/icons/error.svg#root" width={16} height={16} />
+                            <Typography variant="paragraph200">{fieldState.error.message}</Typography>
+                        </div>
+                    )}
                 </div>
             )}
         />
