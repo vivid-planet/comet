46f5b065967d9a5543e7c15e847558bf57afcfef
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2026-02-03 13:43:18 +0100

    remove unneccessary type check

--- demo/site/src/middleware/redirectToMainHost.ts
+++ demo/site/src/middleware/redirectToMainHost.ts
@@ -102,7 +102,7 @@ export function withRedirectToMainHostMiddleware(middleware: CustomMiddleware) {
 
                 if (domainRedirectTarget) {
                     let destination: string | undefined;
-                    if (typeof domainRedirectTarget === "object" && domainRedirectTarget.block !== undefined) {
+                    if (domainRedirectTarget.block !== undefined) {
                         destination = getRedirectTargetUrl(domainRedirectTarget.block, host);
                     }
                     if (destination) {
