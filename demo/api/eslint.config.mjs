777d41b673441e2e3d9505f8f43ab2f13f45d8b1
Johannes Obermair <johannes.obermair@vivid-planet.com>
2026-02-24 09:01:44 +0100

    ESLint: use existing ignore files

--- demo/api/eslint.config.mjs
+++ demo/api/eslint.config.mjs
@@ -1,20 +1,17 @@
 import eslintConfigNestJs from "@comet/eslint-config/future/nestjs.js";
 import storybook from "eslint-plugin-storybook";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+import { globalIgnores } from "eslint/config";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+const prettierignorePath = fileURLToPath(new URL(".prettierignore", import.meta.url));
 
 /** @type {import('eslint')} */
 const config = [
-    {
-        ignores: [
-            "src/db/migrations/**",
-            "dist/**",
-            "src/**/*.generated.ts",
-            "src/**/generated/**",
-            "block-meta.json",
-            "lang/**",
-            "lang-compiled/**",
-            "lang-extracted/**",
-        ],
-    },
+    includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+    includeIgnoreFile(prettierignorePath, "Imported .prettierignore patterns"),
+    globalIgnores(["src/db/migrations/", "src/**/generated/"]),
     ...eslintConfigNestJs,
     ...storybook.configs["flat/recommended"],
 ];
