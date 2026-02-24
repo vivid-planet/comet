e178107fb826c9edbab6cacde7da316a779bbf7a
Ricky James Smith <jamesricky@me.com>
2026-02-24 13:30:58 +0100

    TableBlock: Limit width of RTE (#5221)

--- packages/admin/cms-admin/src/blocks/table/EditCell.tsx
+++ packages/admin/cms-admin/src/blocks/table/EditCell.tsx
@@ -55,6 +55,7 @@ const EditCellHandle = styled("div")({
 
 const EditPopper = styled(Popper)(({ theme }) => ({
     zIndex: theme.zIndex.modal + 1,
+    maxWidth: 460,
 }));
 
 const EditorWrapper = styled(Box)(({ theme }) => ({
