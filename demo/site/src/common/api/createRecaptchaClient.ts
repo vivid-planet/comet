620dedf3cf6d841a8e71833b97c413ee1a9901fd
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2026-03-06 09:18:57 +0100

    rename RECAPTCHA_SECRET_KEY to RECAPTCHA_SERVICE_ACCOUNT

--- demo/site/src/common/api/createRecaptchaClient.ts
+++ demo/site/src/common/api/createRecaptchaClient.ts
@@ -1,10 +1,10 @@
 import { RecaptchaEnterpriseServiceClient } from "@google-cloud/recaptcha-enterprise";
 
 export function createRecaptchaClient() {
-    if (process.env.RECAPTCHA_SECRET_KEY === undefined) {
-        throw new Error("Missing RECAPTCHA_SECRET_KEY environment variable");
+    if (process.env.RECAPTCHA_SERVICE_ACCOUNT === undefined) {
+        throw new Error("Missing RECAPTCHA_SERVICE_ACCOUNT environment variable");
     }
-    const serviceAccountObject = JSON.parse(Buffer.from(process.env.RECAPTCHA_SECRET_KEY, "base64").toString("utf8"));
+    const serviceAccountObject = JSON.parse(Buffer.from(process.env.RECAPTCHA_SERVICE_ACCOUNT, "base64").toString("utf8"));
     const recaptchaClient = new RecaptchaEnterpriseServiceClient({
         credentials: serviceAccountObject,
     });
