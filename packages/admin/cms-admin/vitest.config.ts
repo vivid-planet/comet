7ce60b2e2d7d35ca047b8c7594b5fe6bdfa9b326
Ricky Smith <jamesricky@me.com>
2026-03-11 15:24:44 +0100

    Work around cssstyle crash in vitest for border shorthands with `var()`

--- packages/admin/cms-admin/vitest.config.ts
+++ packages/admin/cms-admin/vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [tsconfigPaths()],
     test: {
         environment: "jsdom",
+        setupFiles: ["./src/testing/vitest.setup.ts"],
         reporters: ["default", "junit"],
         outputFile: {
             junit: "./junit.xml",
