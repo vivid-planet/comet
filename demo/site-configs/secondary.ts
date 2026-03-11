2bad26e2b2a871f5f02fb2186fe2043ed3922158
Benjamin Hohenwarter <bh@vivid-planet.com>
2026-03-11 06:57:40 +0100

    Use env-vars in site-configs

--- demo/site-configs/secondary.ts
+++ demo/site-configs/secondary.ts
@@ -1,7 +1,10 @@
 import { Environment, GetSiteConfig } from "./site-configs";
 
+const host = process.env.SERVER_HOST ?? "localhost";
+const port = parseInt(process.env.SITE_PORT || "3000", 10);
+
 const envToDomainMap: Record<Environment, string> = {
-    local: "localhost:3001",
+    local: `${host}:${port+1}`,
 };
 
 export default ((env) => {
