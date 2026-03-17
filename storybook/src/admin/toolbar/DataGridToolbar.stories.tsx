38d087c03b25f10151a4c79d9f4c43b33bf7837f
Johannes Obermair <johannes.obermair@vivid-planet.com>
2026-03-17 10:32:04 +0100

    Fix DataGridToolbar story

--- storybook/src/admin/toolbar/DataGridToolbar.stories.tsx
+++ storybook/src/admin/toolbar/DataGridToolbar.stories.tsx
@@ -70,6 +70,7 @@ export const _DataGridToolbar = {
                 slots={{
                     toolbar: Toolbar,
                 }}
+                showToolbar
             />
         );
     },
