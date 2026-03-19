49a071617d90e4e79ee6fa26b70f42d81cc80fa5
Johannes Obermair <johannes.obermair@vivid-planet.com>
2026-03-19 14:21:22 +0100

    Remove checks for .env and .env.local

--- dev-pm.config.ts
+++ dev-pm.config.ts
@@ -210,7 +210,7 @@ export default defineConfig({
         //group demo api
         {
             name: "demo-docker",
-            script: "set -a; [ -f .env ] && . .env; [ -f .env.local ] && . .env.local; [ -f .env.secrets ] && . .env.secrets; set +a; docker compose up",
+            script: "set -a; . .env; . .env.local; [ -f .env.secrets ] && . .env.secrets; set +a; docker compose up",
             group: ["demo-api", "demo"],
         },
         {
