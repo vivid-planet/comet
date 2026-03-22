5c7edaaa54c8990e2f03c5729e69478189eb7756
Ricky Smith <jamesricky@me.com>
2026-03-22 14:05:32 +0100

    Add MjmlMailRoot component with zero-padding defaults

--- packages/mail-react/.storybook/MailRenderer.decorator.tsx
+++ packages/mail-react/.storybook/MailRenderer.decorator.tsx
@@ -1,14 +1,12 @@
 import type { Decorator } from "@storybook/react-vite";
 import mjml2html from "mjml-browser";
-import { Mjml, MjmlBody, renderToMjml } from "../src/index.ts";
+import { MjmlMailRoot, renderToMjml } from "../src/index.ts";
 
 export const MailRendererDecorator: Decorator = (Story) => {
     const mjmlString = renderToMjml(
-        <Mjml>
-            <MjmlBody>
-                <Story />
-            </MjmlBody>
-        </Mjml>,
+        <MjmlMailRoot>
+            <Story />
+        </MjmlMailRoot>,
     );
 
     const { html, errors: mjmlWarnings } = mjml2html(mjmlString);
