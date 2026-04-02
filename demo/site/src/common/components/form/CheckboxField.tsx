8a40b327c967532f76ac86621cd1e1886b9ebf42
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2026-04-02 16:35:46 +0200

    fix styling of error message

--- demo/site/src/common/components/form/CheckboxField.tsx
+++ demo/site/src/common/components/form/CheckboxField.tsx
@@ -1,9 +1,9 @@
+import { SvgUse } from "@src/common/helpers/SvgUse";
 import clsx from "clsx";
 import { type InputHTMLAttributes, type ReactNode, useId } from "react";
 import { Controller, type ControllerProps, type FieldValues } from "react-hook-form";
 
 import styles from "./CheckboxField.module.scss";
-import { FieldContainer } from "./FieldContainer";
 
 type CheckboxFieldProps<TFieldValues extends FieldValues> = Omit<InputHTMLAttributes<HTMLInputElement>, "name"> &
     Pick<ControllerProps<TFieldValues>, "name" | "control" | "rules"> & {
@@ -20,19 +20,28 @@ export const CheckboxField = <TFieldValues extends FieldValues>({
     ...inputProps
 }: CheckboxFieldProps<TFieldValues>) => {
     const id = useId();
+
     return (
         <Controller
             name={name}
             control={control}
             rules={rules}
             render={({ field: { value, ...field }, fieldState }) => (
-                <FieldContainer helperText={helperText} errorText={fieldState.error?.message}>
-                    <label htmlFor={id} className={styles.wrapper}>
-                        <input type="checkbox" id={id} {...inputProps} {...field} checked={value} className={styles.input} />
-                        <span className={clsx(styles.checkbox, fieldState.error && styles["checkbox--error"])} />
+                <label htmlFor={id} className={styles.wrapper}>
+                    <input type="checkbox" id={id} {...inputProps} {...field} checked={value} className={styles.input} />
+                    <span className={clsx(styles.checkbox, fieldState.error && styles["checkbox--error"])} />
+                    <span className={styles.labelContent}>
                         <span className={styles.labelText}>{label}</span>
-                    </label>
-                </FieldContainer>
+                        {fieldState.error?.message ? (
+                            <span className={styles.error}>
+                                <SvgUse href="/assets/icons/error.svg#root" width={16} height={16} />
+                                {fieldState.error.message}
+                            </span>
+                        ) : helperText ? (
+                            <span className={styles.helperText}>{helperText}</span>
+                        ) : null}
+                    </span>
+                </label>
             )}
         />
     );
