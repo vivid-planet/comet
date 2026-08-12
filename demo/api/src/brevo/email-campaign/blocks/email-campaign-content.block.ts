af03a3a59d3ef24e2679c6e39a5b3dd48fd2841d
Ricky Smith <jamesricky@me.com>
2026-08-12 16:17:01 +0200

    Demo: Use the shared mail blocks in the email campaign
    Both mails take their image render width from the mail theme, so the
    campaign and the welcome mail size images the same way.

--- demo/api/src/brevo/email-campaign/blocks/email-campaign-content.block.ts
+++ demo/api/src/brevo/email-campaign/blocks/email-campaign-content.block.ts
@@ -1,7 +1,9 @@
-import { NewsletterImageBlock } from "@dextinity/brevo-api";
 import { createBlocksBlock } from "@dextinity/cms-api";
+import { MailButtonBlock } from "@src/mail/blocks/mail-button.block";
 import { MailDividerBlock } from "@src/mail/blocks/mail-divider.block";
+import { MailImageBlock } from "@src/mail/blocks/mail-image.block";
 import { MailRichTextBlock } from "@src/mail/blocks/mail-rich-text.block";
+import { MailSpacerBlock } from "@src/mail/blocks/mail-spacer.block";
 
 import { EmailCampaignSalutationBlock } from "./email-campaign-salutation.block";
 
@@ -9,9 +11,11 @@ export const EmailCampaignContentBlock = createBlocksBlock(
     {
         supportedBlocks: {
             text: MailRichTextBlock,
+            image: MailImageBlock,
+            button: MailButtonBlock,
             divider: MailDividerBlock,
+            spacer: MailSpacerBlock,
             salutation: EmailCampaignSalutationBlock,
-            image: NewsletterImageBlock,
         },
     },
     {
